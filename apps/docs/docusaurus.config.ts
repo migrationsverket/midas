e130253843be2dddcf9f03f66ce8e00bfeb650e3
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2024-02-22 20:32:41 +0100

    chore(docusaurus): fix linting errors (#5)

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -73,7 +73,7 @@ const config: Config = {
             items: [
               {
                 label: 'Components',
-                to: '/docs/intro',
+                to: 'components',
               },
             ],
           },
