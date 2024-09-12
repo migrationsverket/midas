c4aed746775ba4215ae15b74031672b5bdcbd1c3
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2024-09-12 14:48:20 +0200

    feat(table): new component!

---
+++ packages/table/vite.config.ts
@@ -0,0 +1,69 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import * as path from 'path'
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
+import preserveDirectives from 'rollup-preserve-directives'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/packages/table',
+
+  plugins: [
+    react(),
+    nxViteTsPaths(),
+    dts({
+      entryRoot: 'src',
+      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
+      skipDiagnostics: true,
+      insertTypesEntry: true,
+    }),
+    cssInjectedByJsPlugin(),
+    {
+      name: 'custom-swap-directive',
+      generateBundle(_, bundle) {
+        for (const chunk of Object.values(bundle)) {
+          if (chunk.type === 'chunk') {
+            if ('code' in chunk) {
+              if (chunk.code.includes('use client')) {
+                chunk.code = chunk.code.replace(/['"]use client['"];/, '')
+                chunk.code = `'use client';\n${chunk.code}`
+              }
+              if (chunk.code.includes('use server')) {
+                chunk.code = chunk.code.replace(/['"]use server['"];/, '')
+                chunk.code = `'use server';\n${chunk.code}`
+              }
+            }
+          }
+        }
+      },
+      enforce: 'post',
+    },
+    preserveDirectives(),
+  ],
+
+  // Configuration for building your library.
+  // See: https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    outDir: '../../dist/packages/table',
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      name: 'Table',
+      fileName: 'index',
+      // Change this to the formats you want to support.
+      // Don't forget to update your package.json as well.
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      // External packages that should not be bundled into your library.
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+    },
+  },
+})
