255d67b0eb8e67248053e07b35d5b51a61e10adb
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2024-09-12 14:43:39 +0200

    chore(search-field): release version 0.0.2 [skip-ci]

--- packages/search-field/vite.config.ts
+++ packages/search-field/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import * as path from 'path'
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
-import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import preserveDirectives from 'rollup-preserve-directives'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
   root: __dirname,
@@ -19,13 +20,29 @@ export default defineConfig({
       skipDiagnostics: true,
       insertTypesEntry: true,
     }),
-    libInjectCss(),
+    cssInjectedByJsPlugin(),
+    {
+      name: 'custom-swap-directive',
+      generateBundle(_, bundle) {
+        for (const chunk of Object.values(bundle)) {
+          if (chunk.type === 'chunk') {
+            if ('code' in chunk) {
+              if (chunk.code.includes('use client')) {
+                chunk.code = chunk.code.replace(/['"]use client['"];/, '')
+                chunk.code = `'use client';\n${chunk.code}`
+              }
+              if (chunk.code.includes('use server')) {
+                chunk.code = chunk.code.replace(/['"]use server['"];/, '')
+                chunk.code = `'use server';\n${chunk.code}`
+              }
+            }
+          }
+        }
+      },
+      enforce: 'post',
+    },
+    preserveDirectives(),
   ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
 
   build: {
     outDir: '../../dist/packages/search-field',
