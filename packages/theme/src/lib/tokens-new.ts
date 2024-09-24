74aa1da0cc699b00d62abff89dcfb4fbfb434ec8
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2024-09-24 07:49:01 +0200

    refactor(theme): tokens from figma

---
+++ packages/theme/src/lib/tokens-new.ts
@@ -0,0 +1,73 @@
+export const baseColor = {
+  black: '#000000',
+  blue130: '#25607f',
+  blue150: '#143c50',
+  blue90: '#4289ad',
+  grey10: '#f2f2f2',
+  grey150: '#1f1f1f',
+  grey20: '#e6e6e6',
+  grey50: '#bfbfbf',
+  grey80: '#999999',
+  white: '#ffffff',
+}
+
+export const signalColor = {
+  signalRed100: '#e62323',
+  signalRed130: '#b31b1b',
+  signalRed150: '#801313',
+}
+
+export const font = {
+  size01: '0.75rem',
+  size02: '0.875rem',
+  lineHeight01: '1rem',
+  size03: '1rem',
+  lineHeight02: '1.125rem',
+  size04: '1.125rem',
+  lineHeight03: '1.25rem',
+  size05: '1.25rem',
+  lineHeight04: '1.375rem',
+  lineHeight05: '1.5rem',
+  size06: '1.5rem',
+  lineHeight06: '1.75rem',
+  size07: '1.75rem',
+  lineHeight07: '2rem',
+  lineHeight08: '2.25rem',
+  size09: '2.25rem',
+  lineHeight09: '2.75rem',
+}
+
+export const border = {
+  blue01: baseColor.blue150,
+  blue02: baseColor.blue130,
+  disabled: baseColor.grey50,
+}
+
+export const brand = {
+  primary: baseColor.blue150,
+  white: baseColor.white,
+}
+
+export const button = {
+  danger: signalColor.signalRed100,
+  dangerActive: signalColor.signalRed150,
+  dangerHover: signalColor.signalRed130,
+  disabled: baseColor.grey10,
+  primary: baseColor.blue150,
+  primaryActive: baseColor.blue90,
+  primaryHover: baseColor.blue130,
+  secondaryActive: baseColor.grey20,
+  secondaryHover: baseColor.grey10,
+  skeleton: baseColor.grey10,
+  tertiaryActive: baseColor.grey20,
+  tertiaryHover: baseColor.grey10,
+}
+
+export const text = {
+  brandPrimary: baseColor.blue150,
+  disabled: baseColor.grey50,
+  subtle01: baseColor.grey80,
+  textBlack: baseColor.grey150,
+  white: baseColor.white,
+}
+
--- packages/theme/vite.config.ts
+++ packages/theme/vite.config.ts
@@ -4,22 +4,9 @@ import react from '@vitejs/plugin-react'
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
 import dts from 'vite-plugin-dts'
 import * as path from 'path'
-import {
-  accessibility,
-  baseColors,
-  borderRadius,
-  borderWidths,
-  breakpoints,
-  buttonColors,
-  font,
-  fontSizes,
-  fontWeights,
-  inputColor,
-  midasColors,
-  midasTransitions,
-  signalColors,
-} from './src/lib/tokens'
 import fs from 'fs'
+
+const TOKENS_FILE = path.join(__dirname, '/src/lib/figma/data.json')
 
 export default defineConfig({
   root: __dirname,
@@ -30,43 +17,139 @@ export default defineConfig({
       name: 'create-colors-css',
       apply: 'build',
       writeBundle() {
-        const variableGroups = [
-          baseColors,
-          buttonColors,
-          inputColor,
-          signalColors,
-          borderRadius,
-          borderWidths,
-          fontWeights,
-          fontSizes,
-          midasColors,
-          midasTransitions,
-          breakpoints,
-          accessibility,
-          font,
-        ]
+        const tokensJson = fs.readFileSync(TOKENS_FILE, 'utf-8')
+        const tokens = JSON.parse(tokensJson)
+
+        const primitiveTokens = {}
+        const aliasedTokens = {}
+
+        tokens.forEach((group) => {
+          const { name, values } = group
+          const groupedTokens =
+            name === 'Primitives' ? primitiveTokens : aliasedTokens
+
+          values.forEach((value) => {
+            for (const type in value) {
+              const tokens = Array.isArray(value[type]) ? value[type] : []
+              tokens.forEach((token) => {
+                const { name, value, rootAlias } = token
+                const [groupName, tokenName] = name.split('/')
+                if (!groupedTokens[groupName]) {
+                  groupedTokens[groupName] = {}
+                }
+                groupedTokens[groupName][tokenName] = rootAlias || value
+              })
+            }
+          })
+        })
+
         let cssContents = ''
 
-        variableGroups.forEach((values, name) => {
-          cssContents += `/* ${name} */\n`
+        for (const groupName in primitiveTokens) {
+          cssContents += `/* ${groupName} */\n`
 
-          for (const key in values) {
-            cssContents += `@value ${key}: ${values[key]};\n`
+          for (const tokenName in primitiveTokens[groupName]) {
+            cssContents += `@value ${tokenName}: ${primitiveTokens[groupName][tokenName]};\n`
           }
 
           cssContents += `\n` // add a newline for readability
-        })
+        }
+
+        for (const groupName in aliasedTokens) {
+          cssContents += `/* ${groupName} */\n`
+
+          for (const tokenName in aliasedTokens[groupName]) {
+            const rootAlias = aliasedTokens[groupName][tokenName]
+            if (rootAlias) {
+              const [rootGroup, rootToken] = rootAlias.split('/')
+              if (
+                primitiveTokens[rootGroup] &&
+                primitiveTokens[rootGroup][rootToken]
+              ) {
+                const value = primitiveTokens[rootGroup][rootToken]
+                cssContents += `@value ${tokenName}: ${value};\n`
+              } else {
+                console.warn(
+                  `Warning: Token '${tokenName}' references an undefined primitive token '${rootAlias}'`
+                )
+              }
+            } else {
+              cssContents += `@value ${tokenName}: ${aliasedTokens[groupName][tokenName]};\n`
+            }
+          }
+
+          cssContents += `\n` // add a newline for readability
+        }
 
         fs.writeFileSync(
-          path.join(__dirname, '/src/lib/tokens.css'),
+          path.join(__dirname, '/src/lib/tokens-new.css'),
           cssContents
+        )
+      },
+    },
+    {
+      name: 'create-colors-ts',
+      apply: 'build',
+      writeBundle() {
+        const tokensJson = fs.readFileSync(TOKENS_FILE, 'utf-8')
+        const tokens = JSON.parse(tokensJson)
+
+        const primitiveTokens = {}
+        const aliasedTokens = {}
+
+        tokens.forEach((group) => {
+          const { name, values } = group
+          const groupedTokens =
+            name === 'Primitives' ? primitiveTokens : aliasedTokens
+
+          values.forEach((value) => {
+            for (const type in value) {
+              const tokens = Array.isArray(value[type]) ? value[type] : []
+              tokens.forEach((token) => {
+                const { name, value, rootAlias } = token
+                const [groupName, tokenName] = name.split('/')
+                if (!groupedTokens[groupName]) {
+                  groupedTokens[groupName] = {}
+                }
+                groupedTokens[groupName][tokenName] = rootAlias
+                  ? `${rootAlias.replace('/', '.')}`
+                  : value
+              })
+            }
+          })
+        })
+
+        let tsContents = ''
+
+        for (const groupName in primitiveTokens) {
+          tsContents += `export const ${groupName} = {\n`
+
+          for (const tokenName in primitiveTokens[groupName]) {
+            tsContents += `  ${tokenName}: '${primitiveTokens[groupName][tokenName]}',\n`
+          }
+
+          tsContents += `}\n\n`
+        }
+
+        for (const groupName in aliasedTokens) {
+          tsContents += `export const ${groupName} = {\n`
+
+          for (const tokenName in aliasedTokens[groupName]) {
+            tsContents += `  ${tokenName}: ${aliasedTokens[groupName][tokenName]},\n`
+          }
+
+          tsContents += `}\n\n`
+        }
+
+        fs.writeFileSync(
+          path.join(__dirname, '/src/lib/tokens-new.ts'),
+          tsContents
         )
       },
     },
     dts({
       entryRoot: 'src',
-      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
-      skipDiagnostics: true,
+      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
     }),
     react(),
     nxViteTsPaths(),
