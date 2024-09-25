ee52b3f1c65810a5f1ea14537f7ef078494da64d
PHIEKS <philip.hjalmrud.ekstromer@migrationsverket.se>
2024-09-25 14:33:40 +0200

    fix(checkbox): change to luide icon and fix css
    DS-423

--- packages/checkbox/src/lib/Checkbox.tsx
+++ packages/checkbox/src/lib/Checkbox.tsx
@@ -3,6 +3,8 @@
 import clsx from 'clsx'
 import { Checkbox as AriaCheckbox, CheckboxProps } from 'react-aria-components'
 import styles from './Checkbox.module.css'
+import { Check } from 'lucide-react'
+import { Minus } from 'lucide-react'
 
 export const Checkbox = ({ children, ...props }: CheckboxProps) => {
   return (
@@ -12,22 +14,11 @@ export const Checkbox = ({ children, ...props }: CheckboxProps) => {
     >
       {({ isIndeterminate }: { isIndeterminate: boolean }) => (
         <>
-          <div className={styles.checkboxInner}>
-            <svg
-              viewBox="0 0 18 18"
-              aria-hidden="true"
-            >
-              {isIndeterminate ? (
-                <rect
-                  x={1}
-                  y={7.5}
-                  width={15}
-                  height={3}
-                />
-              ) : (
-                <polyline points="1 9 7 14 15 4" />
-              )}
-            </svg>
+          <div
+            className={styles.checkboxInner}
+            aria-hidden="true"
+          >
+            {isIndeterminate ? <Minus></Minus> : <Check></Check>}
           </div>
           {children}
         </>
