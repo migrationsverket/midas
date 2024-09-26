a72f5174cf224f5db0a105fa0f99c21d348efc21
PHIEKS <philip.hjalmrud.ekstromer@migrationsverket.se>
2024-09-26 15:56:18 +0200

    fix(checkbox): change back to polyline for checkmark

--- packages/checkbox/src/lib/Checkbox.tsx
+++ packages/checkbox/src/lib/Checkbox.tsx
@@ -14,12 +14,26 @@ export const Checkbox = ({ children, ...props }: CheckboxProps) => {
     >
       {({ isIndeterminate }: { isIndeterminate: boolean }) => (
         <>
-          <div
-            className={styles.checkboxInner}
-            aria-hidden="true"
-          >
-            {isIndeterminate ? <Minus></Minus> : <Check></Check>}
+          <div className={styles.checkboxInner}>
+            <svg
+              width={16}
+              height={16}
+              viewBox="0 0 18 18"
+              aria-hidden="true"
+            >
+              {isIndeterminate ? (
+                <rect
+                  x={4}
+                  y={8}
+                  width={10}
+                  height={2}
+                />
+              ) : (
+                <polyline points="3,9 7,13 15,4" />
+              )}
+            </svg>
           </div>
+
           {children}
         </>
       )}
