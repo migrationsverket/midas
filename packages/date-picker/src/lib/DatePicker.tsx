6b48888369f5a639f450ec335c183a6b098bd733
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2024-10-01 10:19:53 +0200

    feat(date-picker): new datepicker

--- packages/date-picker/src/lib/DatePicker.tsx
+++ packages/date-picker/src/lib/DatePicker.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import type {
+  DatePickerProps,
   DateRangePickerProps,
   DateValue,
   ValidationResult,
@@ -10,7 +11,8 @@ import {
   CalendarCell,
   CalendarGrid,
   DateInput,
-  DateRangePicker,
+  DatePicker as AriaDatePicker,
+  DateRangePicker as AriaDateRangePicker,
   DateSegment,
   Dialog,
   Group,
@@ -30,20 +32,20 @@ interface MidasDateRangePickerProps<T extends DateValue>
   errorMessage?: string | ((validation: ValidationResult) => string)
 }
 
-export const DatePicker = <T extends DateValue>({
+export const DateRangePicker = <T extends DateValue>({
   label,
   description,
   errorMessage,
   ...props
 }: MidasDateRangePickerProps<T>) => {
   return (
-    <DateRangePicker
+    <AriaDateRangePicker
+      {...props}
       className={clsx(
         TextFieldStyles.inputField,
         styles.datePicker,
         props.className
       )}
-      {...props}
     >
       <InputWrapper
         label={label}
@@ -59,7 +61,7 @@ export const DatePicker = <T extends DateValue>({
           >
             {(segment) => <DateSegment segment={segment} />}
           </DateInput>
-          <span aria-hidden="true">–</span>
+          <span aria-hidden="true">-</span>
           <DateInput
             slot="end"
             className={styles.date}
@@ -96,6 +98,71 @@ export const DatePicker = <T extends DateValue>({
           </Dialog>
         </Popover>
       </InputWrapper>
-    </DateRangePicker>
+    </AriaDateRangePicker>
+  )
+}
+
+interface MidasDatePickerProps<T extends DateValue> extends DatePickerProps<T> {
+  label?: string
+  description?: string
+  errorMessage?: string | ((validation: ValidationResult) => string)
+}
+
+export const DatePicker = <T extends DateValue>({
+  label,
+  description,
+  errorMessage,
+  ...props
+}: MidasDatePickerProps<T>) => {
+  return (
+    <AriaDatePicker
+      {...props}
+      className={clsx(
+        TextFieldStyles.inputField,
+        styles.datePicker,
+        props.className
+      )}
+    >
+      <InputWrapper
+        label={label}
+        description={description}
+        errorMessage={errorMessage}
+      >
+        <Group
+          className={clsx(TextFieldStyles.input, styles.datePickerTextfield)}
+        >
+          <DateInput className={styles.date}>
+            {(segment) => <DateSegment segment={segment} />}
+          </DateInput>
+          <Button>
+            <CalendarDays size={22} />
+          </Button>
+        </Group>
+
+        <Popover>
+          <Dialog className={styles.dialog}>
+            <RangeCalendar>
+              <header className={styles.dialogHeader}>
+                <Button slot="previous">
+                  <ChevronLeft />
+                </Button>
+                <Heading className={styles.dialogHeading} />
+                <Button slot="next">
+                  <ChevronRight />
+                </Button>
+              </header>
+              <CalendarGrid className={styles.calendar}>
+                {(date) => (
+                  <CalendarCell
+                    date={date}
+                    className={styles.day}
+                  />
+                )}
+              </CalendarGrid>
+            </RangeCalendar>
+          </Dialog>
+        </Popover>
+      </InputWrapper>
+    </AriaDatePicker>
   )
 }
