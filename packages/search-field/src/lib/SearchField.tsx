de736c1b7b9f65c67f4052eeffb6537a9f96f911
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2024-10-01 08:41:58 +0200

    feat(searchfield): props rewrite

--- packages/search-field/src/lib/SearchField.tsx
+++ packages/search-field/src/lib/SearchField.tsx
@@ -5,9 +5,8 @@ import {
   FieldError,
   Input,
   Label,
-  Text,
   SearchField as AriaSearchField,
-  Form,
+  SearchFieldProps as AriaSearchFieldProps,
 } from 'react-aria-components'
 import { Search, X } from 'lucide-react'
 import { TextFieldStyles } from '@midas-ds/textfield'
@@ -16,45 +15,43 @@ import styles from './SearchField.module.css'
 import clsx from 'clsx'
 import React from 'react'
 
-export interface SearchFieldProps {
+export interface SearchFieldProps extends AriaSearchFieldProps {
   placeholder: string
-  description?: string
+  buttonText?: string
 }
 
 export const SearchField: React.FC<SearchFieldProps> = ({
   placeholder,
-  description,
+  buttonText,
+  ...props
 }) => {
   const [hasInput, setHasInput] = React.useState(false)
 
   return (
-    <Form className={styles.container}>
-      <AriaSearchField
-        name="search"
-        isRequired
-        onInput={() => setHasInput(true)}
-        onClear={() => setHasInput(false)}
-      >
-        <Label hidden>{placeholder}</Label>
-        <div className={styles.inputContainer}>
-          <Search
-            size={20}
-            className={styles.icon}
-          />
-          <Input
-            className={clsx(TextFieldStyles.input, styles.input)}
-            placeholder={placeholder}
-          />
-          {hasInput && (
-            <AriaButton className={styles.clear}>
-              <X size={16} />
-            </AriaButton>
-          )}
-        </div>
-        {description && <Text slot="description">{description}</Text>}
-        <FieldError />
-      </AriaSearchField>
-      <Button type="submit">Sök</Button>
-    </Form>
+    <AriaSearchField
+      onInput={() => setHasInput(true)}
+      onClear={() => setHasInput(false)}
+      className={styles.container}
+      {...props}
+    >
+      <Label hidden>{placeholder}</Label>
+      <div className={styles.inputContainer}>
+        <Search
+          size={20}
+          className={styles.icon}
+        />
+        <Input
+          className={clsx(TextFieldStyles.input, styles.input)}
+          placeholder={placeholder}
+        />
+        {hasInput && (
+          <AriaButton className={styles.clear}>
+            <X size={16} />
+          </AriaButton>
+        )}
+      </div>
+      <FieldError />
+      <Button type="submit">{buttonText ? buttonText : 'Sök'}</Button>
+    </AriaSearchField>
   )
 }
