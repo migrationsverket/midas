e5ab7d0fe792512710c5df540bc62616e0a10313
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2024-10-02 11:35:17 +0200

    feat(info-banner): dismissable banner

--- packages/info-banner/src/lib/InfoBanner.stories.tsx
+++ packages/info-banner/src/lib/InfoBanner.stories.tsx
@@ -49,3 +49,17 @@ export const Important = {
     type: 'important',
   },
 }
+
+export const Dismissable = {
+  args: {
+    title: 'Thank you!',
+    message:
+      'You are now done sharing all passports - alternatively you have \n' +
+      '        submitted a reply that you were not able or willing to share.\n' +
+      '          \n' +
+      '        You can close the e-service. We will contact you if we need more\n' +
+      '        information. You will hear from us when we have made a decision.',
+    type: 'success',
+    dismissable: true,
+  },
+}
