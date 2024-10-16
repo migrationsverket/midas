33ae364ae4fdc8aae5178ae0cbf5431a68f3fbf9
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2024-10-16 10:10:36 +0200

    feat(breadcrumbs)!: new build

--- packages/breadcrumbs/vite.config.ts
+++ packages/breadcrumbs/vite.config.ts
@@ -1,10 +1,10 @@
-/// <reference types='vitest' />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import * as path from 'path'
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 import preserveDirectives from 'rollup-preserve-directives'
 
 export default defineConfig({
@@ -14,33 +14,12 @@ export default defineConfig({
   plugins: [
     react(),
     nxViteTsPaths(),
+    nxCopyAssetsPlugin(['*.md']),
     dts({
       entryRoot: 'src',
-      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
-      skipDiagnostics: true,
-      insertTypesEntry: true,
+      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
     }),
-    cssInjectedByJsPlugin(),
-    {
-      name: 'custom-swap-directive',
-      generateBundle(_, bundle) {
-        for (const chunk of Object.values(bundle)) {
-          if (chunk.type === 'chunk') {
-            if ('code' in chunk) {
-              if (chunk.code.includes('use client')) {
-                chunk.code = chunk.code.replace(/['"]use client['"];/, '')
-                chunk.code = `'use client';\n${chunk.code}`
-              }
-              if (chunk.code.includes('use server')) {
-                chunk.code = chunk.code.replace(/['"]use server['"];/, '')
-                chunk.code = `'use server';\n${chunk.code}`
-              }
-            }
-          }
-        }
-      },
-      enforce: 'post',
-    },
+    libInjectCss(),
     preserveDirectives(),
   ],
 
@@ -48,10 +27,12 @@ export default defineConfig({
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
     outDir: '../../dist/packages/breadcrumbs',
+    emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
     },
+    cssCodeSplit: true,
     lib: {
       entry: 'src/index.ts',
       name: 'Breadcrumbs',
@@ -60,15 +41,9 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        preserveModules: false,
+      },
     },
-  },
-
-  test: {
-    globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 })
