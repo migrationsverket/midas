64a93e623bd3a80216ededb5fa0dee1daeeb73f7
jabir Khalil <jabir.saadi.khalil@migrationsverket.se>
2024-10-25 11:13:43 +0200

    fix(modal): delete trigger button and fix styling

--- packages/modal/src/lib/Modal.stories.tsx
+++ packages/modal/src/lib/Modal.stories.tsx
@@ -1,7 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Modal } from './Modal'
-import { Button } from '../../../../packages/button/src/lib/Button'
+import { DialogTrigger, Modal } from './Modal'
+import { Button } from '@midas-ds/button'
 import styles from './Modal.module.css'
+
 const meta: Meta<typeof Modal> = {
   component: Modal,
   title: 'Components/Modal',
@@ -17,21 +18,24 @@ export const Default: Story = {
   args: {},
   render: () => {
     return (
-      <Modal>
-        <h3>Vill du spara korrespondens?</h3>
-        <p>AB Förekommer</p>
-        <p>AC Förekommer ej</p>
-        <p>i ärendenummer 12345678</p>
-        <div className={styles.buttonContainer}>
-          <Button size="small">Unsubscribe</Button>
-          <Button
-            variant="secondary"
-            size="small"
-          >
-            Unsubscribe
-          </Button>
-        </div>
-      </Modal>
+      <DialogTrigger>
+        <Button>Öppna Modal</Button>
+        <Modal>
+          <h3>Vill du spara korrespondens?</h3>
+          <p>AB Förekommer</p>
+          <p>AC Förekommer ej</p>
+          <p>i ärendenummer 12345678</p>
+          <div className={styles.buttonContainer}>
+            <Button size="small">Unsubscribe</Button>
+            <Button
+              variant="secondary"
+              size="small"
+            >
+              Unsubscribe
+            </Button>
+          </div>
+        </Modal>
+      </DialogTrigger>
     )
   },
 }
--- packages/modal/src/lib/Modal.tsx
+++ packages/modal/src/lib/Modal.tsx
@@ -7,44 +7,38 @@ import {
   ModalOverlay,
 } from 'react-aria-components'
 import { X } from 'lucide-react'
-import { Button } from '../../../../packages/button/src/lib/Button'
+import { Button } from '@midas-ds/button'
+export { DialogTrigger }
 interface KeyboardModalTriggerProps {
   children?: React.ReactNode
 }
 
-export const Modal: React.FC<KeyboardModalTriggerProps> = ({
-  children,
-
-  ...props
-}) => {
+export const Modal: React.FC<KeyboardModalTriggerProps> = ({ children }) => {
   return (
-    <DialogTrigger>
-      <Button>Open dialog</Button>
-      <ModalOverlay className={styles.overlay}>
-        <ModalAria className={styles.modal}>
-          <Dialog className={styles.dialog}>
-            {({ close }: { close: () => void }) => (
-              <>
-                <Heading className={styles.header}>
-                  <Button
-                    onPress={close}
-                    variant="tertiary"
-                    size="small"
-                  >
-                    Stäng
-                    <X
-                      className={styles.icon}
-                      height={16}
-                      width={16}
-                    />
-                  </Button>
-                </Heading>
-                <div className={styles.divChildren}>{children}</div>
-              </>
-            )}
-          </Dialog>
-        </ModalAria>
-      </ModalOverlay>
-    </DialogTrigger>
+    <ModalOverlay className={styles.overlay}>
+      <ModalAria className={styles.modal}>
+        <Dialog className={styles.dialog}>
+          {({ close }: { close: () => void }) => (
+            <>
+              <Heading className={styles.header}>
+                <Button
+                  onPress={close}
+                  variant="tertiary"
+                  size="small"
+                >
+                  Stäng
+                  <X
+                    className={styles.icon}
+                    height={16}
+                    width={16}
+                  />
+                </Button>
+              </Heading>
+              <div className={styles.divChildren}>{children}</div>
+            </>
+          )}
+        </Dialog>
+      </ModalAria>
+    </ModalOverlay>
   )
 }
