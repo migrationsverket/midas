ae5d3cf2ac6a9979fcd29770b689f27046a01448
wilhjelmig <wilhjelmig@users.noreply.github.com>
2024-10-30 07:44:26 +0000

    Deploy preview for PR 108 🛫

---
+++ pr-preview/pr-108/assets/js/5e907c73.db4ec2ae.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[7990],{
+
+/***/ 69641:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"tag":{"label":"button","permalink":"/pr-preview/pr-108/tags/button","allTagsPath":"/pr-preview/pr-108/tags","count":1,"items":[{"id":"components/button","title":"Button","description":"Knappar används när användaren vill utföra en handling t.ex. spara ifylld information eller öppna ett formulär.","permalink":"/pr-preview/pr-108/components/button"}],"unlisted":false}}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-108/assets/js/a2cd1dd1.f06db1bb.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[5557],{
+
+/***/ 70565:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"tag":{"label":"accordion","permalink":"/pr-preview/pr-108/tags/accordion","allTagsPath":"/pr-preview/pr-108/tags","count":1,"items":[{"id":"components/accordion","title":"Accordion","description":"Dragspelskomponenten låter användare visa och dölja delar av relaterat innehåll på en sida.","permalink":"/pr-preview/pr-108/components/accordion"}],"unlisted":false}}');
+
+/***/ })
+
+}]);
