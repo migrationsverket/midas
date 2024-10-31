aa012e988f29645c3432b7c76ca6e337c0f09978
wilhjelmig <wilhjelmig@users.noreply.github.com>
2024-10-31 10:09:18 +0000

    Deploy preview for PR 118 🛫

---
+++ pr-preview/pr-118/assets/js/58374f21.ee7e6c43.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[7843],{
+
+/***/ 45251:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"tags":[{"label":"components","permalink":"/pr-preview/pr-118/tags/components","count":1},{"label":"menu","permalink":"/pr-preview/pr-118/tags/menu","count":2},{"label":"accordion","permalink":"/pr-preview/pr-118/tags/accordion","count":1},{"label":"button","permalink":"/pr-preview/pr-118/tags/button","count":1},{"label":"form","permalink":"/pr-preview/pr-118/tags/form","count":5},{"label":"grid","permalink":"/pr-preview/pr-118/tags/grid","count":2}]}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-118/assets/js/e8137900.e4f9d1b1.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[4580],{
+
+/***/ 78581:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"tags":[{"label":"releaser","permalink":"/pr-preview/pr-118/blog/tags/releaser","count":2},{"label":"midas","permalink":"/pr-preview/pr-118/blog/tags/midas","count":1}]}');
+
+/***/ })
+
+}]);
