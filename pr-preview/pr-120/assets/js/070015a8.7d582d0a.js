bcdebbb625f332bad4e8d2709e218135188337d9
wilhjelmig <wilhjelmig@users.noreply.github.com>
2024-10-31 13:22:29 +0000

    Deploy preview for PR 120 🛫

---
+++ pr-preview/pr-120/assets/js/070015a8.7d582d0a.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[1009],{
+
+/***/ 12398:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"tags":[{"label":"releaser","permalink":"/pr-preview/pr-120/blog/tags/releaser","count":2},{"label":"midas","permalink":"/pr-preview/pr-120/blog/tags/midas","count":1}]}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-120/assets/js/81dc3a08.4aa3f0b2.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[8857],{
+
+/***/ 67681:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"tag":{"label":"midas","permalink":"/pr-preview/pr-120/blog/tags/midas","allTagsPath":"/pr-preview/pr-120/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/pr-preview/pr-120/blog/tags/midas","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
