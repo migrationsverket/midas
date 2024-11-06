8374a83f6a949baa2b7653215f2c80c3d741a07d
wilhjelmig <wilhjelmig@users.noreply.github.com>
2024-11-06 12:43:30 +0000

    Remove preview for PR 127 🛬

--- pr-preview/pr-127/storybook/assets/Link.stories-Bui3AWiR.js
+++
@@ -1,6 +0,0 @@
-import{L as a}from"./Link-ByJDwbFz.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Link-Dnl6rA1_.js";import"./useFocusable-DvzZNYF2.js";import"./index-Drt3gf4w.js";import"./clsx-B-dksMZM.js";const u={component:a,title:"Components/Link",tags:["autodocs"],parameters:{layout:"centered"}},r={args:{children:"Länk",href:"#"}};var e,o,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    children: 'Länk',
-    href: '#'
-  }
-}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const L=["Primary"];export{r as Primary,L as __namedExportsOrder,u as default};
