91f5adaaa1cd5ccff3755e0743eca52bc187ba14
jabkha <jabkha@users.noreply.github.com>
2024-11-06 10:34:11 +0000

    Deploy preview for PR 126 🛫

---
+++ pr-preview/pr-126/storybook/assets/LinkButton.stories-BhhDRh3V.js
@@ -0,0 +1,17 @@
+import{j as i}from"./jsx-runtime-Nms4Y4qS.js";import{$ as k}from"./Link-DTBCvxNE.js";import{c as y}from"./clsx-B-dksMZM.js";import{k as c}from"./useFocusable-VocBF6K7.js";import{C as j}from"./chevron-right-CtBhK8oM.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Drt3gf4w.js";import"./createLucideIcon-WuqQdmgs.js";const x='"../../../theme/src/lib/tokens.css"',v='"Inter", sans-serif',B="1rem",E="500",N="#143c50",P="#f2f2f2",T="#bfbfbf",$="#2e7ca5",L="_linkButton_1m4in_4",R="_icon_1m4in_38",o={tokens:x,display:v,desktopSize:B,mediumWeight:E,blue150:N,gray10:P,gray50:T,blue100:$,linkButton:L,icon:R},n=({children:s,isDisabled:h,...a})=>i.jsxs(k,{isDisabled:h,className:y(o.linkButton,o.link,a.className),...a,children:[s,i.jsx(j,{size:20,className:o.icon})]});try{n.displayName="LinkButton",n.__docgenInfo={description:"A link to be used when a user expects a button but web technologies force us to use a a-tag",displayName:"LinkButton",props:{}}}catch{}try{c.displayName="RouterProvider",c.__docgenInfo={description:"A RouterProvider accepts a `navigate` function from a framework or client side router,\nand provides it to all nested React Aria links to enable client side navigation.",displayName:"RouterProvider",props:{}}}catch{}const q={component:n,title:"Components/LinkButton",tags:["autodocs"],argTypes:{}},e={args:{children:"Till E-tjänst",href:"#"}},r={args:{children:"Till E-tjänst",href:"#",isDisabled:!0}},t={args:{children:"Till E-tjänst",onPress:()=>alert("navigation fn")}};var l,d,p;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: 'Till E-tjänst',
+    href: '#'
+  }
+}`,...(p=(d=e.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var m,u,f;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: 'Till E-tjänst',
+    href: '#',
+    isDisabled: true
+  }
+}`,...(f=(u=r.parameters)==null?void 0:u.docs)==null?void 0:f.source}}};var g,_,b;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: 'Till E-tjänst',
+    onPress: () => alert('navigation fn')
+  }
+}`,...(b=(_=t.parameters)==null?void 0:_.docs)==null?void 0:b.source}}};const F=["Primary","Disabled","AppLink"];export{t as AppLink,r as Disabled,e as Primary,F as __namedExportsOrder,q as default};
