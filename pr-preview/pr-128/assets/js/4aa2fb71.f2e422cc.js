d9e453584c2dc33a11087b983dcf50ad5e6dd0f3
wilhjelmig <wilhjelmig@users.noreply.github.com>
2024-11-07 16:26:14 +0000

    Deploy preview for PR 128 🛫

---
+++ pr-preview/pr-128/assets/js/4aa2fb71.f2e422cc.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[5243],{
+
+/***/ 86102:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-128/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-128/storybook/assets/Flex.stories-C9EdMR4c.js
@@ -0,0 +1,31 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{C as e}from"./Card-CA5Tc-Xw.js";import{F as m,a as r}from"./FlexItem-DJyBMUzl.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Link-D9j-6iJN.js";import"./Link-XcigMV-L.js";import"./useFocusable-C6jNUHDg.js";import"./index-Drt3gf4w.js";import"./clsx-B-dksMZM.js";import"./createLucideIcon-WuqQdmgs.js";const k={component:m,title:"Components/Flex",tags:["autodocs"],parameters:{layout:"fullscreen"},argTypes:{fluid:{description:"Tar bort alla marginaler för nestad användning. Ha gärna marginal om den används över hela sidan",control:{type:"boolean"}}}},o={args:{},render:({...a})=>t.jsxs(m,{fluid:a.fluid,children:[t.jsx(r,{col:3,children:t.jsx(e,{title:"Ett kort",content:"Som är i ett grid"})}),t.jsx(r,{col:6,children:t.jsx(e,{title:"Ett kort",content:"Som är i ett grid"})}),t.jsx(r,{col:3,children:t.jsx(e,{title:"Ett kort",content:"Som är i ett grid"})}),t.jsx(r,{col:4,children:t.jsx(e,{title:"Ett kort",content:"Som är i ett grid"})}),t.jsx(r,{col:4,children:t.jsx(e,{title:"Ett kort",content:"Som är i ett grid"})}),t.jsx(r,{col:3,offset:1,children:t.jsx(e,{title:"Ett kort",content:"Som är i ett grid"})}),t.jsx(r,{col:"auto",children:t.jsx(e,{title:"Ett kort",content:"Som är i ett grid"})}),t.jsx(r,{children:t.jsx(e,{title:"Ett kort",content:"Som är i ett grid"})})]})};var n,i,l;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {},
+  render: ({
+    ...args
+  }) => <Flex fluid={args.fluid}>
+      <FlexItem col={3}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} />
+      </FlexItem>
+      <FlexItem col={6}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} />
+      </FlexItem>
+      <FlexItem col={3}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} />
+      </FlexItem>
+      <FlexItem col={4}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} />
+      </FlexItem>
+      <FlexItem col={4}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} />
+      </FlexItem>
+      <FlexItem col={3} offset={1}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} />
+      </FlexItem>
+      <FlexItem col={'auto'}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} />
+      </FlexItem>
+      <FlexItem>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} />
+      </FlexItem>
+    </Flex>
+}`,...(l=(i=o.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};const f=["Primary"];export{o as Primary,f as __namedExportsOrder,k as default};
