633f11bcc9589b4a06aca2bcd97c42e2af15f9c1
jabkha <jabkha@users.noreply.github.com>
2024-11-08 07:52:36 +0000

    Deploy preview for PR 126 🛫

---
+++ pr-preview/pr-126/storybook/assets/Link.stories-Cnm7gIQy.js
@@ -0,0 +1,23 @@
+import{L as h}from"./Link-CfWKNtUl.js";import{A as b}from"./arrow-right-BQ8kuuUB.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Link-DTBCvxNE.js";import"./useFocusable-VocBF6K7.js";import"./index-Drt3gf4w.js";import"./clsx-B-dksMZM.js";import"./createLucideIcon-WuqQdmgs.js";const R={component:h,title:"Components/Link",tags:["autodocs"],parameters:{layout:"centered"},argTypes:{isDisabled:{options:[!0,!1],control:{type:"radio"}}}},r={args:{children:"Länk",href:"#",icon:b}},a={args:{...r.args,isDisabled:!0}},e={args:{children:"En fristående länk",standalone:!0,href:"#"}},s={args:{...e.args,isDisabled:!0}};var o,n,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'Länk',
+    href: '#',
+    icon: ArrowRight
+  }
+}`,...(t=(n=r.parameters)==null?void 0:n.docs)==null?void 0:t.source}}};var i,c,d;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    isDisabled: true
+  }
+}`,...(d=(c=a.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var m,p,l;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: 'En fristående länk',
+    standalone: true,
+    href: '#'
+  }
+}`,...(l=(p=e.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var u,g,f;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    ...Standalone.args,
+    isDisabled: true
+  }
+}`,...(f=(g=s.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};const _=["Primary","PrimaryDisabled","Standalone","StandaloneDisabled"];export{r as Primary,a as PrimaryDisabled,e as Standalone,s as StandaloneDisabled,_ as __namedExportsOrder,R as default};
