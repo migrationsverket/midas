332ab2efcdc65727929826199eeb2781614a2100
jabkha <jabkha@users.noreply.github.com>
2024-11-13 15:30:18 +0000

    Deploy preview for PR 134 🛫

---
+++ pr-preview/pr-134/storybook/assets/Button.stories-DEcUDAhC.js
@@ -0,0 +1,54 @@
+import{j as k}from"./jsx-runtime-Nms4Y4qS.js";import{B as E}from"./Button-JCSNR-2w.js";import{c as F}from"./createLucideIcon-WuqQdmgs.js";import{X as M}from"./x-BNls5wfR.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./useFocusable-C6jNUHDg.js";import"./index-Drt3gf4w.js";import"./clsx-B-dksMZM.js";import"./Hidden-BdIcpH7f.js";/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const W=F("Plus",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]]),K={component:E,title:"Components/Button",tags:["autodocs"],args:{variant:"primary"},argTypes:{children:{name:"label"},variant:{options:["primary","secondary","tertiary","danger","icon"],control:{type:"radio"},defaultValue:"primary"},size:{options:["small","default (null)"],control:{type:"radio"},defaultValue:null},isDisabled:{options:[!0,!1],control:{type:"radio"}},iconPlacement:{options:["right","left"],control:{type:"radio"},defaultValue:"left"}}},r={args:{children:"Button"},parameters:{}},a={...r,args:{...r.args,variant:"secondary"}},e={args:{children:"Lägg till",variant:"tertiary",icon:W}},s={args:{variant:"primary",isDisabled:!0,children:"Lägg till"}},t={...r,args:{...r.args,variant:"secondary",isDisabled:!0}},n={args:{children:"Button",size:"small"},parameters:{}},o={args:{children:"Button",fullwidth:!0},parameters:{}},i={args:{children:k.jsx(M,{size:20,"aria-hidden":!0}),variant:"icon","aria-label":"Stäng"}};var c,l,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: 'Button'
+  },
+  parameters: {}
+}`,...(m=(l=r.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var d,p,u;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  ...Primary,
+  args: {
+    ...Primary.args,
+    variant: 'secondary'
+  }
+}`,...(u=(p=a.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var g,y,h;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: 'Lägg till',
+    variant: 'tertiary',
+    icon: Plus
+  }
+}`,...(h=(y=e.parameters)==null?void 0:y.docs)==null?void 0:h.source}}};var P,S,f;s.parameters={...s.parameters,docs:{...(P=s.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary',
+    isDisabled: true,
+    children: 'Lägg till'
+  }
+}`,...(f=(S=s.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var v,b,B;t.parameters={...t.parameters,docs:{...(v=t.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  ...Primary,
+  args: {
+    ...Primary.args,
+    variant: 'secondary',
+    isDisabled: true
+  }
+}`,...(B=(b=t.parameters)==null?void 0:b.docs)==null?void 0:B.source}}};var D,x,z;n.parameters={...n.parameters,docs:{...(D=n.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    size: 'small'
+  },
+  parameters: {}
+}`,...(z=(x=n.parameters)==null?void 0:x.docs)==null?void 0:z.source}}};var I,L,w;o.parameters={...o.parameters,docs:{...(I=o.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    fullwidth: true
+  },
+  parameters: {}
+}`,...(w=(L=o.parameters)==null?void 0:L.docs)==null?void 0:w.source}}};var j,T,V;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    children: <X size={20} aria-hidden />,
+    variant: 'icon',
+    'aria-label': 'Stäng'
+  }
+}`,...(V=(T=i.parameters)==null?void 0:T.docs)==null?void 0:V.source}}};const N=["Primary","Secondary","TertiaryWithIcon","PrimaryDisabled","SecondaryDisabled","SmallPrimary","PrimaryFullwidth","Icon"];export{i as Icon,r as Primary,s as PrimaryDisabled,o as PrimaryFullwidth,a as Secondary,t as SecondaryDisabled,n as SmallPrimary,e as TertiaryWithIcon,N as __namedExportsOrder,K as default};
---
+++ pr-preview/pr-134/storybook/assets/Select.stories-C2SlaV_X.js
@@ -0,0 +1,31 @@
+import{j as r}from"./jsx-runtime-Nms4Y4qS.js";import{S as i,I as s}from"./Select-BlMyHaX5.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Button-JCSNR-2w.js";import"./useFocusable-C6jNUHDg.js";import"./index-Drt3gf4w.js";import"./clsx-B-dksMZM.js";import"./Hidden-BdIcpH7f.js";import"./ListBox-BEKTHaqV.js";import"./useLocalizedStringFormatter-B6EHV5sq.js";import"./context-CkJ-GuNh.js";import"./SelectionManager-BUjlZe7l.js";import"./FocusScope-BobShdvL.js";import"./scrollIntoView-J620AJLa.js";import"./isScrollable-Bts8_1NI.js";import"./useDescription-6AUSMGLv.js";import"./useControlledState-DdsvHRJJ.js";import"./Dialog-V38XWmyV.js";import"./RSPContexts-Bc9pl35m.js";import"./PressResponder-DzwvMiQH.js";import"./getScrollParent-yMl4gzwT.js";import"./VisuallyHidden-tzU1VrE-.js";import"./Collection-S6q6hP5R.js";import"./index-Bb28N59q.js";import"./DragAndDrop-BDFo6_gz.js";import"./Form-CCYym-J0.js";import"./useCollator-MjvPODsq.js";import"./useSingleSelectListState-ua_CHISP.js";import"./intlStrings-ZMTSFH_M.js";import"./TextField-DufKFLXE.js";import"./triangle-alert-BkMwT_kE.js";import"./createLucideIcon-WuqQdmgs.js";import"./chevron-down-FOu3nidH.js";const te={component:i,title:"Components/Select",tags:["autodocs"],args:{label:"Etikett",description:"Beskrivning"},argTypes:{placeholder:{type:"string"}},parameters:{}};function I(e){const n=["Apple","Banana","Kiwi","Elderflower","Nectarine","Watermelon","Pineapple","Apricot","Cloudberry","Mango","Strawberry","Blueberry","Raspberry","Blackberry","Cherry","Peach","Plum","Grape","Orange","Lemon","Lime","Coconut","Fig","Papaya","Guava","Pomegranate","Dragonfruit","Starfruit","Passionfruit"];return Array.from({length:e},(j,b)=>{const y=n[Math.floor(Math.random()*n.length)];return{id:b+1,name:y}})}const S=I(300),t={args:{placeholder:"Many items..."},render:()=>r.jsx(i,{label:"Välj en frukt",items:S,children:e=>r.jsx(s,{children:e.name})})},o={args:{isInvalid:!0,errorMessage:"Fel!"},render:e=>r.jsxs(i,{...e,children:[r.jsx(s,{children:"Apple"}),r.jsx(s,{children:"Lemon"})]})},a={args:{isDisabled:!0},parameters:{docs:{description:{story:"Avoid using disabled"}}},render:e=>r.jsx(i,{...e,children:r.jsx(s,{children:"Hej"})})};var m,p,l;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Many items...'
+  },
+  render: () => <Select label="Välj en frukt" items={options}>
+      {item => <Item>{item.name}</Item>}
+    </Select>
+}`,...(l=(p=t.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var c,d,u;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    isInvalid: true,
+    errorMessage: 'Fel!'
+  },
+  render: args => <Select {...args}>
+      <Item>Apple</Item>
+      <Item>Lemon</Item>
+    </Select>
+}`,...(u=(d=o.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var g,h,f;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Avoid using disabled'
+      }
+    }
+  },
+  render: args => <Select {...args}>
+      <Item>Hej</Item>
+    </Select>
+}`,...(f=(h=a.parameters)==null?void 0:h.docs)==null?void 0:f.source}}};const oe=["Default","Invalid","disabled"];export{t as Default,o as Invalid,oe as __namedExportsOrder,te as default,a as disabled};
