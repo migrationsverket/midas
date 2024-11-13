5a76078b9d8942b1bde01b2a188eb1afb19988c6
phieksmig <phieksmig@users.noreply.github.com>
2024-11-13 07:46:14 +0000

    Deploy preview for PR 131 🛫

---
+++ pr-preview/pr-131/storybook/assets/TextArea.stories-9vu_k4U0.js
@@ -0,0 +1,27 @@
+import{T as S}from"./TextArea-DepHYOSL.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./TextField-B5IzZwet.js";import"./Form-CCYym-J0.js";import"./useFocusable-C6jNUHDg.js";import"./index-Drt3gf4w.js";import"./clsx-B-dksMZM.js";import"./useControlledState-DdsvHRJJ.js";import"./Hidden-BdIcpH7f.js";import"./Button-CnUdlKT0.js";import"./triangle-alert-BkMwT_kE.js";import"./createLucideIcon-WuqQdmgs.js";const k={component:S,title:"Components/TextArea",tags:["autodocs"],argTypes:{label:{type:"string",description:"Etikett"},description:{type:"string"}}},r={args:{label:"Label",description:"Description"}},a={args:{...r.args,isInvalid:!0,errorMessage:"Error"}},e={args:{...r.args,maxCharacters:50}},s={args:{...r.args,showCounter:!0}},o={args:{...r.args,isDisabled:!0}};var t,i,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    label: 'Label',
+    description: 'Description'
+  }
+}`,...(n=(i=r.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};var c,m,p;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    isInvalid: true,
+    errorMessage: 'Error'
+  }
+}`,...(p=(m=a.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var d,g,u;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    maxCharacters: 50
+  }
+}`,...(u=(g=e.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var l,b,y;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    showCounter: true
+  }
+}`,...(y=(b=s.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};var C,h,x;o.parameters={...o.parameters,docs:{...(C=o.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    isDisabled: true
+  }
+}`,...(x=(h=o.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};const O=["Primary","NotValid","MaxCharacters","ShowCounter","Disabled"];export{o as Disabled,e as MaxCharacters,a as NotValid,r as Primary,s as ShowCounter,O as __namedExportsOrder,k as default};
