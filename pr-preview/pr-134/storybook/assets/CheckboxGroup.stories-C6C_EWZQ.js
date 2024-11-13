a5586dfdb157e8914848d5dc7ef1af8415cdc5e1
jabkha <jabkha@users.noreply.github.com>
2024-11-13 14:23:15 +0000

    Deploy preview for PR 134 🛫

---
+++ pr-preview/pr-134/storybook/assets/CheckboxGroup.stories-C6C_EWZQ.js
@@ -0,0 +1,16 @@
+import{j as o}from"./jsx-runtime-Nms4Y4qS.js";import{C as g}from"./Checkbox-C-4OogD7.js";import{C as c}from"./CheckboxGroup-CG0ayTv4.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./RSPContexts-Bc9pl35m.js";import"./useFocusable-C6jNUHDg.js";import"./index-Drt3gf4w.js";import"./Form-CCYym-J0.js";import"./useControlledState-DdsvHRJJ.js";import"./Hidden-BdIcpH7f.js";import"./VisuallyHidden-tzU1VrE-.js";import"./Group-DPNhjRqE.js";import"./TextField-BbWya_Q_.js";import"./Button-BqGOo9bP.js";import"./triangle-alert-BkMwT_kE.js";import"./createLucideIcon-WuqQdmgs.js";const O={title:"Components/Checkbox/CheckboxGroup",component:c,parameters:{layout:"centered"},args:{isInvalid:!1,isDisabled:!1,isRequired:!1,isReadOnly:!1},argTypes:{isInvalid:{type:"boolean"},isDisabled:{type:"boolean"},isRequired:{type:"boolean"},isReadOnly:{type:"boolean"},errorMessage:{type:"string"}},tags:["autodocs"]},u=["Banan","Apple","Mango"],m={args:{},render:({...d})=>o.jsx(c,{...d,children:u.map(t=>o.jsx(g,{value:t,children:t},t))})},e={...m,args:{label:"Etikett",description:"Beskrivning",errorMessage:"här gick det snett"}},r={...m,args:{label:"Etikett",description:"Beskrivning",errorMessage:"här gick det snett",showSelectAll:!0}};var s,a,i;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  ...CheckboxGroupTemplate,
+  args: {
+    label: 'Etikett',
+    description: 'Beskrivning',
+    errorMessage: 'här gick det snett'
+  }
+}`,...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var n,p,l;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  ...CheckboxGroupTemplate,
+  args: {
+    label: 'Etikett',
+    description: 'Beskrivning',
+    errorMessage: 'här gick det snett',
+    showSelectAll: true
+  }
+}`,...(l=(p=r.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};const q=["Example","SelectAll"];export{e as Example,r as SelectAll,q as __namedExportsOrder,O as default};
