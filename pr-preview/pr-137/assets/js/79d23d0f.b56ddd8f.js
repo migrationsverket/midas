9be16c5c6afd273a9db45e910c21a1e88eeee759
wilhjelmig <wilhjelmig@users.noreply.github.com>
2024-11-15 07:45:16 +0000

    Deploy preview for PR 137 🛫

---
+++ pr-preview/pr-137/assets/js/79d23d0f.b56ddd8f.js
@@ -0,0 +1,111 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[3290],{
+
+/***/ 55118:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   assets: () => (/* binding */ assets),
+/* harmony export */   contentTitle: () => (/* binding */ contentTitle),
+/* harmony export */   "default": () => (/* binding */ MDXContent),
+/* harmony export */   frontMatter: () => (/* binding */ frontMatter),
+/* harmony export */   metadata: () => (/* binding */ metadata),
+/* harmony export */   toc: () => (/* binding */ toc)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(74848);
+/* harmony import */ var _mdx_js_react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(28453);
+
+
+const frontMatter = {
+	title: 'Ikoner',
+	sidebar_position: 4
+};
+const contentTitle = undefined;
+const metadata = {
+  "id": "basics/icons",
+  "title": "Ikoner",
+  "description": "Ikoner används på Migrationsverkets webbplatser, i e-tjänster och interna system.",
+  "source": "@site/docs/basics/icons.mdx",
+  "sourceDirName": "basics",
+  "slug": "/basics/icons",
+  "permalink": "/pr-preview/pr-137/basics/icons",
+  "draft": false,
+  "unlisted": false,
+  "tags": [],
+  "version": "current",
+  "sidebarPosition": 4,
+  "frontMatter": {
+    "title": "Ikoner",
+    "sidebar_position": 4
+  },
+  "sidebar": "sideBar",
+  "previous": {
+    "title": "Typografi",
+    "permalink": "/pr-preview/pr-137/basics/typography"
+  },
+  "next": {
+    "title": "Tillgänglighetsriktlinjer",
+    "permalink": "/pr-preview/pr-137/basics/accessibility"
+  }
+};
+const assets = {
+
+};
+
+
+
+const toc = [{
+  "value": "Generella riktlinjer",
+  "id": "generella-riktlinjer",
+  "level": 2
+}];
+function _createMdxContent(props) {
+  const _components = {
+    a: "a",
+    h2: "h2",
+    li: "li",
+    p: "p",
+    ul: "ul",
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+    children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_components.p, {
+      children: "Ikoner används på Migrationsverkets webbplatser, i e-tjänster och interna system.\nDe ska underlätta för användaren att navigera och bidra till en intuitiv förståelse av innehållet.\nDe ska därför ha ett utseende som användaren känner igen och förstår innebörden av."
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_components.h2, {
+      id: "generella-riktlinjer",
+      children: "Generella riktlinjer"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_components.ul, {
+      children: ["\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_components.li, {
+        children: ["Migrationsverket använder ikoner från ikonbiblioteket ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_components.a, {
+          href: "https://fontawesome.com/",
+          children: "Font Awesome"
+        }), ". Designsystemet använder dessutom ikoner från ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_components.a, {
+          href: "https://lucide.dev/",
+          children: "Lucide"
+        }), "."]
+      }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_components.li, {
+        children: "Alla ikoner ska alltid kompletteras med en förklarande text. Undantag är t.ex. pilar, extern länk eller kryss."
+      }), "\n"]
+    })]
+  });
+}
+function MDXContent(props = {}) {
+  const {wrapper: MDXLayout} = {
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return MDXLayout ? (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(MDXLayout, {
+    ...props,
+    children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-137/storybook/assets/Grid.stories-BJ71K3I1.js
@@ -0,0 +1,53 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{c as _}from"./clsx-B-dksMZM.js";import{C as r}from"./Card-CloyDLgj.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Link-CPSZLI5S.js";import"./Link-DNGGQ-aH.js";import"./useFocusable-FXxCDyRU.js";import"./index-Drt3gf4w.js";import"./createLucideIcon-WuqQdmgs.js";const k='"../../../theme/src/lib/tokens.css"',u="(min-width: 768px)",g="(min-width: 1200px)",p="(min-width: 1440px)",h="_container_18og4_4",x="_grid_18og4_16",j="_fluid_18og4_45",y="_col_18og4_52",l={tokens:k,mdBreakpoint:u,lgBreakpoint:g,xlBreakpoint:p,container:h,grid:x,fluid:j,col:y,"col-1":"_col-1_18og4_58","col-2":"_col-2_18og4_62","col-3":"_col-3_18og4_66","col-4":"_col-4_18og4_70","col-5":"_col-5_18og4_74","col-6":"_col-6_18og4_78","col-7":"_col-7_18og4_82","col-8":"_col-8_18og4_86","col-9":"_col-9_18og4_90","col-10":"_col-10_18og4_94","col-11":"_col-11_18og4_98","col-12":"_col-12_18og4_102","offset-1":"_offset-1_18og4_106","offset-2":"_offset-2_18og4_110","offset-3":"_offset-3_18og4_114","offset-4":"_offset-4_18og4_118","offset-5":"_offset-5_18og4_122","offset-6":"_offset-6_18og4_126","offset-7":"_offset-7_18og4_130","offset-8":"_offset-8_18og4_134","offset-9":"_offset-9_18og4_138","offset-10":"_offset-10_18og4_142","offset-11":"_offset-11_18og4_146"},s=({children:o,fluid:i,...n})=>t.jsx("div",{className:_(l.container,i&&l.fluid,n.className),...n,children:t.jsx("div",{className:l.grid,children:o})});try{s.displayName="Grid",s.__docgenInfo={description:`Grid baserat på display: grid;
+Använder grid-template för att bygga rätt antal kolumner per breakpoint.
+
+### Children
+Använd "grid-column: span $antalKollumner';" för att låta en child växa över flera kolumner.`,displayName:"Grid",props:{fluid:{defaultValue:null,description:"Tar bort alla marginaler för nestad användning",name:"fluid",required:!1,type:{name:"boolean"}}}}}catch{}const e=({children:o,col:i,...n})=>{const f=i?`col-${i}`:"";return t.jsx("div",{className:_(l.col,l[f],n.className),...n,children:o})};try{e.displayName="GridItem",e.__docgenInfo={description:"",displayName:"GridItem",props:{col:{defaultValue:null,description:"",name:"col",required:!1,type:{name:'"1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12'}}}}}catch{}const F={component:s,title:"Components/Grid",tags:["autodocs"],parameters:{layout:"fullscreen"},argTypes:{fluid:{description:"Tar bort alla marginaler för nestad användning. Ha gärna marginal om den används över hela sidan",control:{type:"boolean"}}}},a={args:{},render:({...o})=>t.jsxs(s,{fluid:o.fluid,children:[t.jsx(e,{col:3,children:t.jsx(r,{title:"Ett kort",content:"Som är i tre kolumner",link:{title:"Länk",href:"#"}})}),t.jsx(e,{col:3,children:t.jsx(r,{title:"Ett kort",content:"Som är i tre kolumner",link:{title:"Länk",href:"#"}})}),t.jsx(e,{col:6,children:t.jsx(r,{title:"Ett kort",content:"Som är i sex kolumner",link:{title:"Länk",href:"#"}})}),t.jsx(e,{col:4,children:t.jsx(r,{title:"Ett kort",content:"Som är i fyra kolumner",link:{title:"Länk",href:"#"}})}),t.jsx(e,{col:4,children:t.jsx(r,{title:"Ett kort",content:"Som är i fyra kolumner",link:{title:"Länk",href:"#"}})}),t.jsx(e,{col:4,children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid men just detta kort har väldigt mycket text för att illustrera att de andra också växer på höjden.",link:{title:"Länk",href:"#"}})}),t.jsx(e,{col:10,children:t.jsx(r,{title:"Ett kort",content:"Som är utan kolumner",link:{title:"Länk",href:"#"}})})]})};var c,d,m;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {},
+  render: ({
+    ...args
+  }) => <Grid fluid={args.fluid}>
+      <GridItem col={3}>
+        <Card title={'Ett kort'} content={'Som är i tre kolumner'} link={{
+        title: 'Länk',
+        href: '#'
+      }} />
+      </GridItem>
+      <GridItem col={3}>
+        <Card title={'Ett kort'} content={'Som är i tre kolumner'} link={{
+        title: 'Länk',
+        href: '#'
+      }} />
+      </GridItem>
+      <GridItem col={6}>
+        <Card title={'Ett kort'} content={'Som är i sex kolumner'} link={{
+        title: 'Länk',
+        href: '#'
+      }} />
+      </GridItem>
+      <GridItem col={4}>
+        <Card title={'Ett kort'} content={'Som är i fyra kolumner'} link={{
+        title: 'Länk',
+        href: '#'
+      }} />
+      </GridItem>
+      <GridItem col={4}>
+        <Card title={'Ett kort'} content={'Som är i fyra kolumner'} link={{
+        title: 'Länk',
+        href: '#'
+      }} />
+      </GridItem>
+      <GridItem col={4}>
+        <Card title={'Ett kort'} content={'Som är i ett grid men just detta kort har väldigt mycket text för att illustrera att de andra också växer på höjden.'} link={{
+        title: 'Länk',
+        href: '#'
+      }} />
+      </GridItem>
+      <GridItem col={10}>
+        <Card title={'Ett kort'} content={'Som är utan kolumner'} link={{
+        title: 'Länk',
+        href: '#'
+      }} />
+      </GridItem>
+    </Grid>
+}`,...(m=(d=a.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const w=["Primary"];export{a as Primary,w as __namedExportsOrder,F as default};
