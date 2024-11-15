dc52d04efcc6fcfbc9ce18d1251de1da3926467c
wilhjelmig <wilhjelmig@users.noreply.github.com>
2024-11-15 09:41:42 +0000

    Deploy preview for PR 134 🛫

---
+++ pr-preview/pr-134/storybook/assets/Tag.stories-p3b3qCdT.js
@@ -0,0 +1,27 @@
+import{j as i}from"./jsx-runtime-Nms4Y4qS.js";import{T as t,a as d}from"./Tag-BQKG_dAV.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Button-CqiFVUUu.js";import"./useFocusable-FXxCDyRU.js";import"./index-Drt3gf4w.js";import"./clsx-B-dksMZM.js";import"./Hidden-DXstef2u.js";import"./Collection-Bd6nTHYz.js";import"./index-Bb28N59q.js";import"./Text-Cg4FgRI1.js";import"./useControlledState-CQzcaGwO.js";import"./ListBox-q5ecyqAM.js";import"./DragAndDrop-D1my3whc.js";import"./isScrollable-Bts8_1NI.js";import"./SelectionManager-BjK4YV19.js";import"./FocusScope-DqX_BpuW.js";import"./scrollIntoView-BzRic4Uk.js";import"./context-DPkFGduH.js";import"./useDescription-D_qi9L6Z.js";import"./useCollator-DZ85_XvZ.js";import"./useListState-B-40T5bv.js";import"./useHighlightSelectionDescription-Bq9nZh10.js";import"./useLocalizedStringFormatter-CdgOzlVp.js";import"./useUpdateEffect-CCcm2-in.js";import"./useHasTabbableChild-BilymLPH.js";import"./getScrollParent-yMl4gzwT.js";import"./x-BNls5wfR.js";import"./createLucideIcon-WuqQdmgs.js";const L={component:t,title:"Components/Tag",tags:["autodocs"]},e={args:{isDisabled:!1,children:"Title",dismissable:!0},render:({...r})=>i.jsx(d,{"aria-label":"Taggar",children:i.jsx(t,{...r,children:r.children})})},a={args:{children:"Title",isDisabled:!0,dismissable:!0},render:({...r})=>i.jsx(d,{"aria-label":"Taggar",children:i.jsx(t,{...r,children:r.children})})};var s,o,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    isDisabled: false,
+    children: 'Title',
+    dismissable: true
+  },
+  render: ({
+    ...args
+  }) => {
+    return <TagGroup aria-label='Taggar'>
+        <Tag {...args}>{args.children}</Tag>
+      </TagGroup>;
+  }
+}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var m,p,l;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: 'Title',
+    isDisabled: true,
+    dismissable: true
+  },
+  render: ({
+    ...args
+  }) => {
+    return <TagGroup aria-label='Taggar'>
+        <Tag {...args}>{args.children}</Tag>
+      </TagGroup>;
+  }
+}`,...(l=(p=a.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};const M=["Default","Disabled"];export{e as Default,a as Disabled,M as __namedExportsOrder,L as default};
