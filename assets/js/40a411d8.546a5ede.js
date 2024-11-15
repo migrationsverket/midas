80ec26af897b527639abc8c42f0aef7e088d5543
wilhjelmig <165774426+wilhjelmig@users.noreply.github.com>
2024-11-15 14:30:51 +0000

    Deploying to gh-pages from @ migrationsverket/midas@c0ba68c1f7d4db82d2751843d00a5e76217cee77 🚀

---
+++ assets/js/40a411d8.546a5ede.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[1730],{
+
+/***/ 2414:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"tag":{"label":"releaser","permalink":"/blog/tags/releaser","allTagsPath":"/blog/tags","count":4,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/releaser","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
