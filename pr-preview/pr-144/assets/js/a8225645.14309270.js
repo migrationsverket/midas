f796ff61de3b33cba5b47a05689866a420e90c97
phieksmig <phieksmig@users.noreply.github.com>
2024-11-20 10:39:10 +0000

    Deploy preview for PR 144 🛫

---
+++ pr-preview/pr-144/assets/js/a8225645.14309270.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[2480],{
+
+/***/ 92538:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"tag":{"label":"components","permalink":"/pr-preview/pr-144/tags/components","allTagsPath":"/pr-preview/pr-144/tags","count":1,"items":[{"id":"components/accordion","title":"Accordion","description":"Dragspelskomponenten låter användare visa och dölja delar av relaterat innehåll på en sida.","permalink":"/pr-preview/pr-144/components/accordion"}],"unlisted":false}}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-144/assets/js/e108b786.a055a8a4.js
@@ -0,0 +1,133 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[8714],{
+
+/***/ 47271:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  assets: () => (/* binding */ assets),
+  contentTitle: () => (/* binding */ contentTitle),
+  "default": () => (/* binding */ MDXContent),
+  frontMatter: () => (/* binding */ frontMatter),
+  metadata: () => (/* reexport */ site_docs_basics_principles_mdx_e10_namespaceObject),
+  toc: () => (/* binding */ toc)
+});
+
+;// ./apps/docs/.docusaurus/docusaurus-plugin-content-docs/default/site-docs-basics-principles-mdx-e10.json
+const site_docs_basics_principles_mdx_e10_namespaceObject = /*#__PURE__*/JSON.parse('{"id":"basics/principles","title":"Designprinciper","description":"1. Utgå från målgruppens behov","source":"@site/docs/basics/principles.mdx","sourceDirName":"basics","slug":"/basics/principles","permalink":"/pr-preview/pr-144/basics/principles","draft":false,"unlisted":false,"tags":[],"version":"current","sidebarPosition":1,"frontMatter":{"title":"Designprinciper","sidebar_position":1},"sidebar":"sideBar","previous":{"title":"Design-DNA","permalink":"/pr-preview/pr-144/basics/design-dna"},"next":{"title":"Designtokens","permalink":"/pr-preview/pr-144/basics/tokens"}}');
+// EXTERNAL MODULE: ./node_modules/react/jsx-runtime.js
+var jsx_runtime = __webpack_require__(74848);
+// EXTERNAL MODULE: ./node_modules/@mdx-js/react/lib/index.js
+var lib = __webpack_require__(28453);
+;// ./apps/docs/docs/basics/principles.mdx
+
+
+const frontMatter = {
+	title: 'Designprinciper',
+	sidebar_position: 1
+};
+const contentTitle = undefined;
+
+const assets = {
+
+};
+
+
+
+const toc = [{
+  "value": "1. Utgå från målgruppens behov",
+  "id": "1-utgå-från-målgruppens-behov",
+  "level": 2
+}, {
+  "value": "2. Etisk och inkluderande design",
+  "id": "2-etisk-och-inkluderande-design",
+  "level": 2
+}, {
+  "value": "3. Helhet",
+  "id": "3-helhet",
+  "level": 2
+}, {
+  "value": "4. Iterera",
+  "id": "4-iterera",
+  "level": 2
+}, {
+  "value": "5. Datadrivet",
+  "id": "5-datadrivet",
+  "level": 2
+}, {
+  "value": "6. Arbeta enligt en designorienterad process",
+  "id": "6-arbeta-enligt-en-designorienterad-process",
+  "level": 2
+}, {
+  "value": "7. Enhetlighet",
+  "id": "7-enhetlighet",
+  "level": 2
+}];
+function _createMdxContent(props) {
+  const _components = {
+    h2: "h2",
+    p: "p",
+    ...(0,lib/* useMDXComponents */.R)(),
+    ...props.components
+  };
+  return (0,jsx_runtime.jsxs)(jsx_runtime.Fragment, {
+    children: [(0,jsx_runtime.jsx)(_components.h2, {
+      id: "1-utgå-från-målgruppens-behov",
+      children: "1. Utgå från målgruppens behov"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Om du inte vet vilka användarna är och vilka behov de har kommer du inte bygga rätt sak. Gör inga antaganden. Utforska, gör användarstudier och analysera data."
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "2-etisk-och-inkluderande-design",
+      children: "2. Etisk och inkluderande design"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Det vi bygger ska fungera för alla. Tillgänglighet och användbarhet står i första rummet. De som är i störst behov av våra tjänster är ofta de personer som har svårast att använda dem. Arbeta inkluderande och prioritera att våra system och tjänster ska kunna användas av alla."
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "3-helhet",
+      children: "3. Helhet"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Ha förståelse för kontexten. Vi designar flöden, kundresor och tjänster, inte enstaka webbplatser. En tjänst är något som hjälper människor att göra något. Vårt jobb är att ta reda på i vilka sammanhang våra användare befinner sig i och skapa tjänster som tar hänsyn till dessa."
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "4-iterera",
+      children: "4. Iterera"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Arbeta med ständiga förbättringar. Vi är aldrig klara med något; allt kan alltid bli lite bättre. Det bästa sättet att bygga bra tjänster är att börja smått, testa med användare och iterera. Om och om igen. Om en prototyp eller skiss inte fungerar, var inte rädd för att skrota den och börja om. Misslyckas tidigt och gör om."
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "5-datadrivet",
+      children: "5. Datadrivet"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Tillämpa alltid ett datadrivet arbetssätt. Gissa inte - ta beslut utifrån fakta. Detta gäller både under analysfasen och utvecklingsfasen, men också när en tjänst eller ett system är lanserat. Gör fältstudier, intervjua användare, analysera hur liknande tjänster ser ut och används. Testa skisser och prototyper med representanter ur målgruppen. Använd analysverktyg för att se hur en tjänst fungerar i drift och var eventuella problem eller flaskhalsar uppstår. "
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "6-arbeta-enligt-en-designorienterad-process",
+      children: "6. Arbeta enligt en designorienterad process"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Design bygger på att kontinuerligt involvera användare i designprocessen för att se till att lösningen blir lätt att använda för målgruppen. Det är en iterativ process som består av fyra delar: utforska målgruppen och användningssituationen, definiera målgruppen behov och mål med användningen, utforma och utforska lösningsidéer, specificera lösningar och testa med målgruppen."
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "7-enhetlighet",
+      children: "7. Enhetlighet"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Det är viktigt att våra användare känner igen sig i mötet med Migrationsverket. Det skapar förtroende och trygghet. Därför strävar vi efter enhetlighet. Var konsekvent när det gäller designmönster, grafiskt uttryck och språklig tonalitet - även mellan olika tjänster och system. "
+    })]
+  });
+}
+function MDXContent(props = {}) {
+  const {wrapper: MDXLayout} = {
+    ...(0,lib/* useMDXComponents */.R)(),
+    ...props.components
+  };
+  return MDXLayout ? (0,jsx_runtime.jsx)(MDXLayout, {
+    ...props,
+    children: (0,jsx_runtime.jsx)(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+
+
+/***/ })
+
+}]);
