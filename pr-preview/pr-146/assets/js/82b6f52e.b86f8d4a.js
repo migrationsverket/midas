893095927f16efe77481add8999d6acdae49230f
jabkha <jabkha@users.noreply.github.com>
2024-11-25 09:07:31 +0000

    Deploy preview for PR 146 🛫

---
+++ pr-preview/pr-146/assets/js/82b6f52e.b86f8d4a.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[2121],{
+
+/***/ 37410:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"tag":{"label":"releaser","permalink":"/pr-preview/pr-146/blog/tags/releaser","allTagsPath":"/pr-preview/pr-146/blog/tags","count":4,"unlisted":false},"listMetadata":{"permalink":"/pr-preview/pr-146/blog/tags/releaser","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
