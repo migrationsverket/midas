d9b85a2c5820b4e062c5f27b4a7a5fe6164e87f1
jabkha <jabkha@users.noreply.github.com>
2024-11-25 10:21:53 +0000

    Deploy preview for PR 147 🛫

---
+++ pr-preview/pr-147/assets/js/3a2b51d2.0ac6ab1c.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[3387],{
+
+/***/ 13416:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"tags":[{"label":"components","permalink":"/pr-preview/pr-147/tags/components","count":1},{"label":"menu","permalink":"/pr-preview/pr-147/tags/menu","count":2},{"label":"accordion","permalink":"/pr-preview/pr-147/tags/accordion","count":1},{"label":"button","permalink":"/pr-preview/pr-147/tags/button","count":1},{"label":"form","permalink":"/pr-preview/pr-147/tags/form","count":6},{"label":"grid","permalink":"/pr-preview/pr-147/tags/grid","count":2}]}');
+
+/***/ })
+
+}]);
