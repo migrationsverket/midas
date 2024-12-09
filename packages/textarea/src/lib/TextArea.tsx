04f05c5800a9fdb9b3f546c82830a32b0bffc705
PHIEKS <philip.hjalmrud.ekstromer@migrationsverket.se>
2024-12-09 12:16:55 +0100

    feat(textarea): change error message when characters in field > maxCharacter

--- packages/textarea/src/lib/TextArea.tsx
+++ packages/textarea/src/lib/TextArea.tsx
@@ -60,7 +60,7 @@ export const TextArea: React.FC<TextAreaProps> = ({
   const validateInput = (value: string) => {
     const maxCharactersError =
       maxCharacters && value.length > maxCharacters
-        ? `Just nu är det ${value.length - maxCharacters} tecken för mycket`
+        ? `Du har angett ${value.length - maxCharacters} tecken för mycket. Fältet är begränsat till ${maxCharacters} tecken.`
         : null
 
     const otherValidationError = validate ? validate(value) : null
