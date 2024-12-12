dc6f2a1b592d4b0bdbec25ed07d9e1163fd68385
phieksmig <phieksmig@users.noreply.github.com>
2024-12-12 08:26:16 +0000

    Deploy preview for PR 170 🛫

---
+++ pr-preview/pr-170/assets/js/d5a961a8.6e22fa13.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[4976],{
+
+/***/ 33218:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-170/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":6,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
