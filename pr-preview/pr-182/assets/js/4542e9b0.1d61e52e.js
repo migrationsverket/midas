996b22253124a715a953eabe4950478548b51284
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-01-13 15:00:34 +0000

    Deploy preview for PR 182 🛫

---
+++ pr-preview/pr-182/assets/js/4542e9b0.1d61e52e.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[3424],{
+
+/***/ 87209:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-182/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":7,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-182/storybook/assets/Checkbox.stories-Ca4im-E_.js
@@ -0,0 +1,10 @@
+import{j as n}from"./jsx-runtime-CfatFE5O.js";import{C as a}from"./Checkbox-B-6-YZVg.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./RSPContexts-Djf1EgA4.js";import"./useFocusable-CUvAewCR.js";import"./index-ZuzByk-F.js";import"./Form-Bnef0d36.js";import"./Text-CUSXFwf2.js";import"./useControlledState-DFHhQ_7Z.js";import"./Hidden-CB9z-rbJ.js";import"./VisuallyHidden-CU0vF0eT.js";const h={component:a,title:"Components/Checkbox",tags:["autodocs"],parameters:{layout:"centered"},args:{isInvalid:!1,isSelected:!1,isIndeterminate:!1,isDisabled:!1,isRequired:!1,isReadOnly:!1},argTypes:{isInvalid:{type:"boolean"},isSelected:{type:"boolean"},isIndeterminate:{type:"boolean"},isDisabled:{type:"boolean"},isRequired:{type:"boolean"},isReadOnly:{type:"boolean"}}},e={args:{},render:({...t})=>n.jsx(a,{value:"unsubscribe",...t,children:"Unsubscribe"})};var r,s,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {},
+  render: ({
+    ...args
+  }) => {
+    return <Checkbox value="unsubscribe" {...args}>
+        Unsubscribe
+      </Checkbox>;
+  }
+}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const R=["Example"];export{e as Example,R as __namedExportsOrder,h as default};
