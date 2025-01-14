e3cf21d9db2bb3a36efc8577977930c7c3cbe3b8
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-01-14 07:41:12 +0000

    Deploy preview for PR 178 🛫

---
+++ pr-preview/pr-178/storybook/assets/Layout.stories-Ds2sRsGi.js
@@ -0,0 +1,66 @@
+import{j as e}from"./jsx-runtime-CfatFE5O.js";import{L as x}from"./LinkButton-C-kdVVZj.js";import{B}from"./Button-BvTLClQx.js";import"./ButtonGroup-Duds0FV0.js";import{a as H}from"./FlexItem-CpQqq4BJ.js";import{L as R}from"./Logo-CUp1Ej5E.js";import{R as b}from"./index-ClcD9ViR.js";import{c}from"./clsx-B-dksMZM.js";/* empty css               */import{D as A,a as z}from"./Dropdown-Ce-BncA7.js";import{u as E}from"./useFocusable-CI7IKKVh.js";import{c as i}from"./createLucideIcon-DQK9Z80p.js";import{X as F}from"./x-BaIpobvV.js";import{P as O}from"./plus-lGmUxVE_.js";import"./Link-DKIb2naE.js";import"./chevron-right-CvgscaxW.js";import"./Hidden-Cj803rEv.js";import"./index-ZuzByk-F.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Dialog-BP7SazX_.js";import"./RSPContexts-Djf1EgA4.js";import"./Collection-C4MnC92I.js";import"./index-DvHoLfqc.js";import"./Separator-CIU4dNV7.js";import"./SelectionManager-DRGqbt9A.js";import"./FocusScope-DXS1oVaO.js";import"./context-WoXASG7e.js";import"./useDescription-g1HYx49D.js";import"./useControlledState-CccRFgkZ.js";import"./ListKeyboardDelegate-DaTzZypo.js";import"./Text-B5xeh108.js";import"./PressResponder-B8JDidd-.js";import"./useLocalizedStringFormatter-B63kKuQ6.js";import"./VisuallyHidden-CLrU_C9N.js";/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const G=i("Calendar",[["path",{d:"M8 2v4",key:"1cmpym"}],["path",{d:"M16 2v4",key:"4m81vk"}],["rect",{width:"18",height:"18",x:"3",y:"4",rx:"2",key:"1hopcy"}],["path",{d:"M3 10h18",key:"8toen8"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const $=i("ClipboardList",[["rect",{width:"8",height:"4",x:"8",y:"2",rx:"1",ry:"1",key:"tgr4d6"}],["path",{d:"M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2",key:"116196"}],["path",{d:"M12 11h4",key:"1jrz19"}],["path",{d:"M12 16h4",key:"n85exb"}],["path",{d:"M8 11h.01",key:"1dfujw"}],["path",{d:"M8 16h.01",key:"18s6g9"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const D=i("Gavel",[["path",{d:"m14.5 12.5-8 8a2.119 2.119 0 1 1-3-3l8-8",key:"15492f"}],["path",{d:"m16 16 6-6",key:"vzrcl6"}],["path",{d:"m8 8 6-6",key:"18bi4p"}],["path",{d:"m9 7 8 8",key:"5jnvq1"}],["path",{d:"m21 11-8-8",key:"z4y7zo"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const Y=i("House",[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"1d0kgt"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const q=i("Menu",[["line",{x1:"4",x2:"20",y1:"12",y2:"12",key:"1e0a9i"}],["line",{x1:"4",x2:"20",y1:"6",y2:"6",key:"1owob3"}],["line",{x1:"4",x2:"20",y1:"18",y2:"18",key:"yk5zj1"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const V=i("PanelLeftClose",[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",key:"afitv7"}],["path",{d:"M9 3v18",key:"fh3hqa"}],["path",{d:"m16 15-3-3 3-3",key:"14y99z"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const K=i("PanelRightClose",[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",key:"afitv7"}],["path",{d:"M15 3v18",key:"14nvp0"}],["path",{d:"m8 9 3 3-3 3",key:"12hl5m"}]]),X='"../../../theme/src/lib/tokens.css"',J='"Inter", sans-serif',Q="#143c50",U="#f2f2f2",W="#d9d9d9",Z="#b3b3b3",ee="#a6a6a6",ae="#5d5d5d",te="(min-width: 768px)",re="#b90835",ne="_baseLayout_udk0p_4",se="_mainContent_udk0p_13",ie="_header_udk0p_18",oe="_headerContent_udk0p_35",le="_headerItems_udk0p_52",de="_logo_udk0p_53",ce="_headerMenu_udk0p_61",pe="_toggleButton_udk0p_69",me="_sidebar_udk0p_77",he="_main_udk0p_13",ke="_app_udk0p_112",ue="_sidebarOpened_udk0p_121",be="_sidebarHeader_udk0p_125",ye="_sidebarFooter_udk0p_134",_e="_sidebarCollapsed_udk0p_148",ge="_userName_udk0p_165",xe="_abbr_udk0p_177",fe="_userTitle_udk0p_183",je="_title_udk0p_194",Ne="_sidebarNav_udk0p_205",ve="_list_udk0p_211",Ce="_listGroupTitle_udk0p_218",Le="_listItem_udk0p_227",we="_listItemCollapsed_udk0p_231",Me="_listLink_udk0p_235",Pe="_linkText_udk0p_242",Be="_linkTextCollapsed_udk0p_252",Ie="_listLinkCollapsed_udk0p_263",Te="_collapseButton_udk0p_267",Se="_backdrop_udk0p_272",He="_backdropOpened_udk0p_281",a={tokens:X,display:J,primaryBackground:Q,gray10:U,gray30:W,gray60:Z,gray70:ee,gray130:ae,mdBreakpoint:te,logoPrimary:re,baseLayout:ne,mainContent:se,header:ie,headerContent:oe,headerItems:le,logo:de,headerMenu:ce,toggleButton:pe,sidebar:me,main:he,app:ke,sidebarOpened:ue,sidebarHeader:be,sidebarFooter:ye,sidebarCollapsed:_e,userName:ge,abbr:xe,userTitle:fe,title:je,sidebarNav:Ne,list:ve,listGroupTitle:Ce,listItem:Le,listItemCollapsed:we,listLink:Me,linkText:Pe,linkTextCollapsed:Be,listLinkCollapsed:Ie,collapseButton:Te,backdrop:Se,backdropOpened:He},t={white:"#ffffff",black:"#000000",gray10:"#f2f2f2",gray20:"#e6e6e6",gray30:"#d9d9d9",gray40:"#cccccc",gray50:"#bfbfbf",gray60:"#b3b3b3",gray70:"#a6a6a6",gray80:"#999999",gray90:"#8c8c8c",gray100:"#808080",gray130:"#5d5d5d",gray150:"#1f1f1f",red10:"#f7e9e7",red20:"#f1d4d1",red30:"#e9beba",red40:"#e2a8a4",red50:"#d9918e",red60:"#d37a7a",red70:"#cc6467",red80:"#c54c56",red90:"#be3345",red100:"#b90835",red130:"#8b1327",red150:"#641018",blue10:"#eaf2f6",blue20:"#d5e5ed",blue30:"#c0d7e4",blue40:"#abcbdb",blue50:"#94bcd1",blue60:"#82b0c9",blue70:"#6ca3c0",blue80:"#5897b8",blue90:"#4289ad",blue100:"#2e7ca5",blue130:"#25607f",blue150:"#143c50"},Re={signalGreen10:"#d5f2d9",signalGreen100:"#008d3c",signalBlue10:t.blue10,signalBlue100:"#0066cc",signalYellow10:"#fff8e1",signalYellow100:"#f1c21b",signalRed10:"#ffdfdf",signalRed100:"#e62323",signalRed130:"#b31b1b",signalRed150:"#801313"},Ae={logoPrimary:t.red100,logoSecondary:t.white,textPrimary:t.gray150,textSecondary:t.white,textTertiary:t.red100,link:t.blue130,linkVisited:"#663366",backgroundPrimary:t.white,backgroundSecondary:t.gray10,backgroundFooter1:t.red100,backgroundFooter2:t.red130,backgroundInfo:t.blue10,backgroundImportant:Re.signalYellow10,borderPrimary:t.black,borderSecondary:t.gray60,borderFocusOuter:t.black,borderFocusInner:t.white,iconPrimary:t.black,iconInverted:t.white},ze=({items:y,title:p,user:m,app:o,children:l,headerChildren:h,clientSideRouter:d})=>{const[r,f]=b.useState(!1),[_,g]=b.useState(!1);b.useEffect(()=>{const n=s=>{s.key==="Escape"&&g(!1)};return window.addEventListener("keydown",n),()=>{window.removeEventListener("keydown",n)}},[]);const T=()=>e.jsx("div",{className:a.sidebarHeader,children:r?e.jsx("p",{className:a.name,children:o.shortName}):e.jsx("p",{className:a.abbr,children:o.name})}),j=({group:n})=>e.jsx("ul",{className:a.list,children:n.items.map((s,S)=>e.jsx("li",{className:c(a.listItem,r&&a.listItemCollapsed),children:e.jsx(x,{iconPlacement:"left",href:s.href,icon:s.icon,className:c(a.listLink,r&&a.listLinkCollapsed),variant:"tertiary",children:e.jsx("span",{className:c(a.linkText,r&&a.linkTextCollapsed),children:s.title})})},"link_"+S))});return e.jsxs("div",{className:a.baseLayout,children:[e.jsx(I,{title:p,headerChildren:h,user:m,app:o,isOpened:_,setIsOpened:g,setIsCollapsed:f}),e.jsxs("div",{className:a.mainContent,children:[e.jsxs("aside",{id:"midasMainMenu",className:c(a.sidebar,r&&a.sidebarCollapsed,_&&a.sidebarOpened),children:[e.jsx(T,{}),e.jsx("nav",{className:a.sidebarNav,children:e.jsx("ul",{className:a.list,children:y.map((n,s)=>e.jsxs("li",{children:[n.title&&!r&&e.jsx("p",{className:a.listGroupTitle,children:n.title}),d?e.jsx(E,{navigate:d,children:e.jsx(j,{group:n})}):e.jsx(j,{group:n})]},"list_"+s))})}),e.jsx("div",{className:a.sidebarFooter,children:e.jsx(B,{variant:"tertiary","aria-label":r?"Maximera sidomenyn":"Minimera sidomenyn",onPress:()=>f(!r),className:a.collapseButton,children:r?e.jsx(K,{}):e.jsx(V,{})})})]}),e.jsx(H,{children:e.jsx("main",{className:a.main,children:e.jsx("div",{className:a.app,children:l})})}),e.jsx("div",{className:c(a.backdrop,_&&a.backdropOpened),onClick:()=>g(!1),"aria-hidden":!0})]})]})},I=({title:y,user:p,app:m,headerChildren:o,isOpened:l,setIsOpened:h,setIsCollapsed:d})=>e.jsxs("header",{className:a.header,style:{borderTop:`solid 4px ${m.color?m.color:Ae.logoPrimary}`},children:[e.jsxs("div",{className:a.headerContent,children:[e.jsx(R,{padding:!1,size:"small",className:a.logo}),l!==void 0&&d&&h&&e.jsx(B,{variant:"icon",className:a.toggleButton,"aria-label":l?"Stäng meny":"Öppna meny","aria-controls":"midasMainMenu",onPress:()=>{d(!1),h(!l)},children:l?e.jsx(F,{size:20,"aria-hidden":!0}):e.jsx(q,{size:20,"aria-hidden":!0})}),e.jsxs("div",{children:[e.jsx("p",{className:a.userName,children:p.name}),e.jsx("p",{className:a.userTitle,children:p.title}),e.jsx("p",{className:a.title,children:y})]})]}),e.jsx("div",{className:a.headerItems,children:o}),e.jsx("div",{className:a.headerMenu,children:e.jsx(A,{children:b.Children.map(o,r=>e.jsx(z,{children:r}))})})]}),ga={component:ze,title:"Components/Layout",parameters:{layout:"fullscreen"}},Ee=[{items:[{title:"Översikt",href:"#",icon:Y}]},{title:"Ansökan",items:[{title:"Skapa ansökan",href:"#",icon:O},{title:"Beslut",href:"#",icon:D}]},{title:"Kort och konto",items:[{title:"LMA-kort",href:"#",icon:G},{title:"Avvikelser",href:"#",icon:$}]}],N=()=>e.jsx("div",{style:{background:t.white,display:"flex",justifyContent:"center",alignItems:"center",height:"90vh",border:`dotted 2px ${t.gray20}`},children:"Din applikation"}),k={args:{items:Ee,user:{name:"Namn Namnsson",title:"Roll eller behörighet"},app:{name:"Namn på applikationen",shortName:"NPA"},title:"Skapa ansökningar",children:e.jsxs(e.Fragment,{children:[e.jsx(N,{}),e.jsx(N,{})]}),headerChildren:e.jsx(x,{variant:"tertiary",target:"_blank",children:"Öppna annan tjänst"})}},u={render:()=>e.jsx(I,{title:"Skapa ansökningar",user:{name:"Namn Namnsson",title:"Roll eller behörighet"},app:{name:"Namn på applikationen",shortName:"NPA"},headerChildren:e.jsx(x,{variant:"tertiary",target:"_blank",children:"Öppna annan tjänst"})})};var v,C,L;k.parameters={...k.parameters,docs:{...(v=k.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    items: items,
+    user: {
+      name: 'Namn Namnsson',
+      title: 'Roll eller behörighet'
+    },
+    app: {
+      name: 'Namn på applikationen',
+      shortName: 'NPA'
+    },
+    title: 'Skapa ansökningar',
+    children: <>
+        <AppExample />
+        <AppExample />
+      </>,
+    headerChildren: <LinkButton variant='tertiary' target='_blank'>
+        Öppna annan tjänst
+      </LinkButton>
+  }
+}`,...(L=(C=k.parameters)==null?void 0:C.docs)==null?void 0:L.source}}};var w,M,P;u.parameters={...u.parameters,docs:{...(w=u.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: () => <Header title='Skapa ansökningar' user={{
+    name: 'Namn Namnsson',
+    title: 'Roll eller behörighet'
+  }} app={{
+    name: 'Namn på applikationen',
+    shortName: 'NPA'
+  }} headerChildren={<LinkButton variant='tertiary' target='_blank'>
+          Öppna annan tjänst
+        </LinkButton>} />
+}`,...(P=(M=u.parameters)==null?void 0:M.docs)==null?void 0:P.source}}};const xa=["Primary","OnlyHeader"];export{u as OnlyHeader,k as Primary,xa as __namedExportsOrder,ga as default};
