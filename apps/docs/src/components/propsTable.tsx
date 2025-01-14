cc938469928b4ae4a0413eb4c3bbabff8e9238b8
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2025-01-14 13:46:31 +0100

    chore: some table style and in accordion

--- apps/docs/src/components/propsTable.tsx
+++ apps/docs/src/components/propsTable.tsx
@@ -1,4 +1,10 @@
 import useGlobalData from '@docusaurus/useGlobalData'
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent
+} from '@midas-ds/accordion'
 import React from 'react'
 import { Props, ComponentDoc } from 'react-docgen-typescript'
 
@@ -19,39 +25,51 @@ export const PropTable = ({ name }) => {
   }
 
   return (
-    <>
-      <table style={{ fontSize: 'small' }}>
-        <thead style={{ textAlign: 'left' }}>
-          <tr>
-            <th>Namn</th>
-            <th>Typ</th>
-            <th>Default</th>
-            <th>Obligatorisk</th>
-            <th>Beskrivning</th>
-          </tr>
-        </thead>
-        <tbody style={{ fontSize: 'small' }}>
-          {Object.keys(props).map(key => {
-            return (
-              <tr key={key}>
-                <td>
-                  <p>{key}</p>
-                </td>
-                <td>
-                  <p>{props[key].type?.name}</p>
-                </td>
-                <td>
-                  {props[key].defaultValue && (
-                    <p>{props[key].defaultValue.value}</p>
-                  )}
-                </td>
-                <td>{props[key].required ? 'Yes' : 'No'}</td>
-                <td style={{ width: '30%' }}>{props[key].description}</td>
+    <Accordion
+      headingTag='h3'
+      type='multiple'
+    >
+      <AccordionItem value={'docs'}>
+        <AccordionTrigger>Visa props</AccordionTrigger>
+        <AccordionContent style={{ padding: 0 }}>
+          <table>
+            <thead style={{ textAlign: 'left' }}>
+              <tr>
+                <th>Namn</th>
+                <th>Typ</th>
+                <th>Default</th>
+                <th>Beskrivning</th>
+                <th>Obligatorisk</th>
               </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </>
+            </thead>
+            <tbody style={{ fontSize: '0.875rem' }}>
+              {Object.keys(props).map(key => {
+                return (
+                  <tr key={key}>
+                    <td style={{ verticalAlign: 'top' }}>
+                      <p>{key}</p>
+                    </td>
+                    <td style={{ verticalAlign: 'top' }}>
+                      <p>{props[key].type?.name}</p>
+                    </td>
+                    <td style={{ verticalAlign: 'top' }}>
+                      {props[key].defaultValue && (
+                        <p>{props[key].defaultValue.value}</p>
+                      )}
+                    </td>
+                    <td style={{ verticalAlign: 'top' }}>
+                      {props[key].description}
+                    </td>
+                    <td style={{ verticalAlign: 'top' }}>
+                      {props[key].required && 'Nej'}
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   )
 }
