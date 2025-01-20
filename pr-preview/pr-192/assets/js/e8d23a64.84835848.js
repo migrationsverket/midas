a40610d2f131f64052d9e216f444ee628f18c199
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-01-20 15:51:32 +0000

    Deploy preview for PR 192 🛫

---
+++ pr-preview/pr-192/assets/js/e8d23a64.84835848.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[8820],{
+
+/***/ 11421:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-192/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":8,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
