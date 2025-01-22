657d0f0b16cd2f1ae6820824b4e878aa63c3361b
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-01-22 20:54:31 +0100

    docs: update API and guidelines for Radio (#196)

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -59,7 +59,7 @@ const config: Config = {
             return true;
           },
           savePropValueAsString: true,
-          shouldExtractLiteralValuesFromEnum: true,
+          shouldExtractLiteralValuesFromEnum: false,
         },// see https://github.com/atomicpages/docusaurus-plugin-react-docgen-typescript for options
       }
     ],
