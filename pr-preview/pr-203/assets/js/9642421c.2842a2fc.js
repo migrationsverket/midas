65d063ae736591899c766429a95ba8981b8470e6
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-01-23 08:50:16 +0000

    Deploy preview for PR 203 🛫

---
+++ pr-preview/pr-203/assets/js/9642421c.2842a2fc.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[6366],{
+
+/***/ 3657:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-203/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":8,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
