7e3d71cb422731085175207e7eaf76972af2b6be
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2025-01-28 09:54:07 +0100

    chore: move storybook, upgrade nx, upgrade storybook

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -1,51 +1,32 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  core: {
-    disableTelemetry: true
-  },
-
   stories: [
-    './examples/*.stories.@(md|mdx|tsx)',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)'
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    './examples/*.stories.@(md|mdx|tsx)'
   ],
-
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
     '@chromatic-com/storybook'
   ],
-
+  core: {
+    disableTelemetry: true
+  },
   framework: {
     name: '@storybook/react-vite',
-    options: {}
-  },
-
-  staticDirs: ['./static'],
-
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      compilerOptions: {
-        allowSyntheticDefaultImports: true,
-        esModuleInterop: false
-      },
-      // Filter out third-party props from node_modules except @mui packages.
-      propFilter: prop =>
-        prop.parent
-          ? !/node_modules\/(?!react-aria-components)/.test(
-              prop.parent.fileName
-            )
-          : true
+    options: {
+      builder: {
+        viteConfigPath: 'packages/components/vite.config.ts'
+      }
     }
   },
-
-  docs: {}
+  staticDirs: ['./static']
 }
 
 export default config
 
 // To customize your Vite configuration you can use the viteFinal field.
 // Check https://storybook.js.org/docs/react/builders/vite#configuration
-// and https://nx.dev/packages/storybook/documents/custom-builder-configs
+// and https://nx.dev/recipes/storybook/custom-builder-configs
--- packages/components/.storybook/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
-
-export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/packages/components',
-  plugins: [react(), nxViteTsPaths()]
-})
