99e41d787245b7c8cc8d692572117f2288a951ad
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-01-29 10:44:01 +0000

    Deploy preview for PR 214 🛫

---
+++ pr-preview/pr-214/assets/js/9879ad05.479cae09.js
@@ -0,0 +1,75 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[26],{
+
+/***/ 40234:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   assets: () => (/* binding */ assets),
+/* harmony export */   contentTitle: () => (/* binding */ contentTitle),
+/* harmony export */   "default": () => (/* binding */ MDXContent),
+/* harmony export */   frontMatter: () => (/* binding */ frontMatter),
+/* harmony export */   metadata: () => (/* reexport default export from named module */ _site_docusaurus_docusaurus_plugin_content_blog_default_site_blog_single_package_mdx_407_json__WEBPACK_IMPORTED_MODULE_0__),
+/* harmony export */   toc: () => (/* binding */ toc)
+/* harmony export */ });
+/* harmony import */ var _site_docusaurus_docusaurus_plugin_content_blog_default_site_blog_single_package_mdx_407_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(52538);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(74848);
+/* harmony import */ var _mdx_js_react__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(28453);
+/* harmony import */ var _site_src_components_CodeBlock_CodeBlock__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(71372);
+
+
+const frontMatter = {
+	title: 'Ett komponentpaket',
+	description: 'I och med release 19 går vi över till ett komponentpaket.',
+	slug: 'one-package',
+	pagination_prev: null,
+	pagination_next: null
+};
+const contentTitle = undefined;
+
+const assets = {
+"authorsImageUrls": [],
+};
+
+
+
+
+
+
+const toc = [];
+function _createMdxContent(props) {
+  const _components = {
+    p: "p",
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.p, {
+    children: "I och med release 19 går vi över till ett komponentpaket."
+  });
+}
+function MDXContent(props = {}) {
+  const {wrapper: MDXLayout} = {
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_3__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return MDXLayout ? (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(MDXLayout, {
+    ...props,
+    children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+
+
+/***/ }),
+
+/***/ 52538:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"permalink":"/pr-preview/pr-214/blog/one-package","source":"@site/blog/single-package.mdx","title":"Ett komponentpaket","description":"I och med release 19 går vi över till ett komponentpaket.","date":"2025-01-27T12:21:31.000Z","tags":[],"readingTime":1.06,"hasTruncateMarker":true,"authors":[],"frontMatter":{"title":"Ett komponentpaket","description":"I och med release 19 går vi över till ett komponentpaket.","slug":"one-package","pagination_prev":null,"pagination_next":null},"unlisted":false,"nextItem":{"title":"Release 18","permalink":"/pr-preview/pr-214/blog/release-18"}}');
+
+/***/ })
+
+}]);
