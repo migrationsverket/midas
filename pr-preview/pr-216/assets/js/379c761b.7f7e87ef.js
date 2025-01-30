cfb1a90ec78546befa97ffe0411fc1b0256f34e6
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-01-30 08:04:38 +0000

    Deploy preview for PR 216 🛫

---
+++ pr-preview/pr-216/assets/js/379c761b.7f7e87ef.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[9047],{
+
+/***/ 12994:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-216/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":9,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
