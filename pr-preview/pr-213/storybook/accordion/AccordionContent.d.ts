96153fee904990ca3edcdc5e9d72e7bb75825ee6
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-01-30 07:00:30 +0000

    Remove preview for PR 213 🛬

--- pr-preview/pr-213/storybook/accordion/AccordionContent.d.ts
+++
@@ -1,3 +0,0 @@
-import { default as React } from '../../../../node_modules/react';
-import * as RadixAccordion from '@radix-ui/react-accordion';
-export declare const AccordionContent: React.ForwardRefExoticComponent<RadixAccordion.AccordionContentProps & React.RefAttributes<HTMLDivElement>>;
--- pr-preview/pr-213/storybook/accordion/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Accordion } from './Accordion';
-export { AccordionItem } from './AccordionItem';
-export { AccordionTrigger } from './AccordionTrigger';
-export { AccordionContent } from './AccordionContent';
-export { AccordionTagContext } from './AccordionTagContext';
