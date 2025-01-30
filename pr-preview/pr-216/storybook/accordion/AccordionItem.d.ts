b50d3063a3f4fe58267121af5d0be47ab15d3b92
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-01-30 10:49:23 +0000

    Remove preview for PR 216 🛬

--- pr-preview/pr-216/storybook/accordion/AccordionItem.d.ts
+++
@@ -1,3 +0,0 @@
-import { default as React } from '../../../../node_modules/react';
-import * as RadixAccordion from '@radix-ui/react-accordion';
-export declare const AccordionItem: React.ForwardRefExoticComponent<RadixAccordion.AccordionItemProps & React.RefAttributes<HTMLDivElement>>;
