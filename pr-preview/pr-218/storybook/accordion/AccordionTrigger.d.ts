6936da80372ac1897465a335b1ce05717cc8bf48
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-01-31 07:39:29 +0000

    Deploy preview for PR 218 🛫

--- pr-preview/pr-218/storybook/accordion/AccordionTrigger.d.ts
+++
@@ -1,3 +0,0 @@
-import { default as React } from '../../../../node_modules/react';
-import * as RadixAccordion from '@radix-ui/react-accordion';
-export declare const AccordionTrigger: React.ForwardRefExoticComponent<RadixAccordion.AccordionTriggerProps & React.RefAttributes<HTMLButtonElement>>;
---
+++ pr-preview/pr-218/storybook/assets/Flex-Di1QQQg5.js
@@ -0,0 +1,8 @@
+import{j as a}from"./jsx-runtime-DtgEGaVE.js";import{s as e}from"./FlexItem-DeKcLnIt.js";import{c as n}from"./clsx-B-dksMZM.js";const i=({children:t,fluid:o=!1,...s})=>a.jsx("div",{className:n(e.container,o&&e.fluid,s.className),...s,children:a.jsx("div",{className:e.flex,children:t})});i.__docgenInfo={description:`Grid based on display: flex;
+Calculates breakpoints and distributes columns according to MV specifications
+
+### Children
+Use FlexItem to manage each column.
+FlexItem accepts values of 1 through 12 and auto.
+
+@see {@link: https://migrationsverket.se/components/flex}`,methods:[],displayName:"Flex",props:{children:{required:!0,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""},fluid:{required:!1,tsType:{name:"boolean"},description:"Removes outer margins for nested use. First Flex on a page should have outer margins.",defaultValue:{value:"false",computed:!1}}}};export{i as F};
