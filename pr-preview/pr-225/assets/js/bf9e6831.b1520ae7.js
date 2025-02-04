188f7a329b239798017ddaf06e89edf0b0709998
derpbravely <derpbravely@users.noreply.github.com>
2025-02-04 17:59:33 +0000

    Deploy preview for PR 225 🛫

---
+++ pr-preview/pr-225/assets/js/bf9e6831.b1520ae7.js
@@ -0,0 +1,72 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[3143],{
+
+/***/ 53355:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   assets: () => (/* binding */ assets),
+/* harmony export */   contentTitle: () => (/* binding */ contentTitle),
+/* harmony export */   "default": () => (/* binding */ MDXContent),
+/* harmony export */   frontMatter: () => (/* binding */ frontMatter),
+/* harmony export */   metadata: () => (/* reexport default export from named module */ _site_docusaurus_docusaurus_plugin_content_blog_default_site_blog_open_source_md_726_json__WEBPACK_IMPORTED_MODULE_0__),
+/* harmony export */   toc: () => (/* binding */ toc)
+/* harmony export */ });
+/* harmony import */ var _site_docusaurus_docusaurus_plugin_content_blog_default_site_blog_open_source_md_726_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(86077);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(74848);
+/* harmony import */ var _mdx_js_react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(28453);
+
+
+const frontMatter = {
+	title: 'Midas har nu öppen källkod',
+	description: 'Designsystemet har nu öppen källkod och versionshanterat på GitHub. Detta för att vi enklare ska kunna dela med oss av vårt designsystem med andra myndigheter som också gått samma väg.',
+	slug: 'midas-open-source',
+	hide_table_of_contents: true,
+	pagination_prev: null,
+	pagination_next: null
+};
+const contentTitle = undefined;
+
+const assets = {
+"authorsImageUrls": [],
+};
+
+
+
+const toc = [];
+function _createMdxContent(props) {
+  const _components = {
+    p: "p",
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_2__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.p, {
+    children: "Designsystemet har nu öppen källkod och versionshanterat på GitHub. Detta för att vi enklare ska kunna dela med oss av vårt designsystem med andra myndigheter som också gått samma väg."
+  });
+}
+function MDXContent(props = {}) {
+  const {wrapper: MDXLayout} = {
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_2__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return MDXLayout ? (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(MDXLayout, {
+    ...props,
+    children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+
+
+/***/ }),
+
+/***/ 86077:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"permalink":"/pr-preview/pr-225/blog/midas-open-source","source":"@site/blog/open-source.md","title":"Midas har nu öppen källkod","description":"Designsystemet har nu öppen källkod och versionshanterat på GitHub. Detta för att vi enklare ska kunna dela med oss av vårt designsystem med andra myndigheter som också gått samma väg.","date":"2024-06-25T06:55:54.000Z","tags":[],"readingTime":0.94,"hasTruncateMarker":true,"authors":[],"frontMatter":{"title":"Midas har nu öppen källkod","description":"Designsystemet har nu öppen källkod och versionshanterat på GitHub. Detta för att vi enklare ska kunna dela med oss av vårt designsystem med andra myndigheter som också gått samma väg.","slug":"midas-open-source","hide_table_of_contents":true,"pagination_prev":null,"pagination_next":null},"unlisted":false,"prevItem":{"title":"Release 12","permalink":"/pr-preview/pr-225/blog/release-12"}}');
+
+/***/ })
+
+}]);
