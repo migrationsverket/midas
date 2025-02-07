9a11bc6c759af2720084da983183fff2db4c30db
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2025-02-07 15:50:38 +0100

    chore: fix accordion example

--- packages/components/src/tooltip/Tooltip.spec.tsx
+++ packages/components/src/tooltip/Tooltip.spec.tsx
@@ -1,10 +1,22 @@
 import { render } from '@testing-library/react'
 
-import Tooltip from './Tooltip'
+import { Tooltip, TooltipTrigger } from './'
+import { Button } from '../button'
+import { Save } from 'lucide-react'
 
 describe('Tooltip', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Tooltip />)
+    const { baseElement } = render(
+      <TooltipTrigger>
+        <Button
+          variant='tertiary'
+          aria-label='Spara'
+        >
+          <Save />
+        </Button>
+        <Tooltip>Spara</Tooltip>
+      </TooltipTrigger>
+    )
     expect(baseElement).toBeTruthy()
   })
 })
