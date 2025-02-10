d0ff496603b943c37618d544eee715b3fd596dda
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-02-10 14:42:32 +0000

    Deploy preview for PR 231 🛫

--- pr-preview/pr-231/storybook/layout/Layout.d.ts
+++ pr-preview/pr-231/storybook/layout/Layout.d.ts
@@ -1,5 +1,5 @@
 import { LucideIcon } from 'lucide-react';
-import { default as React } from '../../../../node_modules/react';
+import * as React from 'react';
 export interface SidebarLinkGroup {
     title?: string;
     items: SidebarLink[];
@@ -20,12 +20,21 @@ interface App {
     color?: HEX;
 }
 export interface MidasLayout {
+    /** The menu items/item groups */
     items: SidebarLinkGroup[];
+    /** Title displayed at the top of the application header */
     title: string;
     children: React.ReactNode;
+    /** List of links in the top right of the application header */
     headerChildren: React.ReactNode;
+    /** Current user details */
     user: SidebarUser;
+    /** Name of the app */
     app: App;
+    /** Provide the layout with your router for client side navigation
+     *
+     * @see {@link https://designsystem.migrationsverket.se/dev/client-side-routing/}
+     */
     clientSideRouter?: (path: string, routerOptions: undefined) => void;
 }
 export interface MidasHeader {
