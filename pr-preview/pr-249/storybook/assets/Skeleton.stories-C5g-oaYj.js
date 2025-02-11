a471b2e270482e8b748dc8b04d089db8968807f3
jabkha <jabkha@users.noreply.github.com>
2025-02-11 15:53:01 +0000

    Deploy preview for PR 249 🛫

---
+++ pr-preview/pr-249/storybook/assets/Skeleton.stories-C5g-oaYj.js
@@ -0,0 +1,15 @@
+import{j as k}from"./jsx-runtime-DtgEGaVE.js";import{c as S}from"./clsx-B-dksMZM.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";const y="_skeleton_1e0u1_11",h="_shimmer_1e0u1_1",T="_text_1e0u1_19",j="_circle_1e0u1_24",C="_rectangle_1e0u1_30",s={skeleton:y,shimmer:h,text:T,circle:j,rectangle:C},v=({variant:f="text"})=>k.jsx("div",{className:S(s.skeleton,s[f])});v.__docgenInfo={description:"",methods:[],displayName:"Skeleton",props:{variant:{required:!1,tsType:{name:"union",raw:'"text" | "circle" | "rectangle"',elements:[{name:"literal",value:'"text"'},{name:"literal",value:'"circle"'},{name:"literal",value:'"rectangle"'}]},description:"",defaultValue:{value:'"text"',computed:!1}}}};const q={component:v,title:"Components/Skeleton",tags:["autodocs"],argTypes:{variant:{options:["text","circle","rectangle"],control:{type:"radio"},defaultValue:void 0}}},e={args:{}},r={args:{variant:"text"}},t={args:{variant:"circle"}},a={args:{variant:"rectangle"}};var n,o,c;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {}
+}`,...(c=(o=e.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};var l,i,m;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    variant: 'text'
+  }
+}`,...(m=(i=r.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,u,d;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    variant: 'circle'
+  }
+}`,...(d=(u=t.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var g,_,x;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    variant: 'rectangle'
+  }
+}`,...(x=(_=a.parameters)==null?void 0:_.docs)==null?void 0:x.source}}};const w=["Default","Text","Circle","rectangle"];export{t as Circle,e as Default,r as Text,w as __namedExportsOrder,q as default,a as rectangle};
---
+++ pr-preview/pr-249/storybook/skeleton/Skeleton.d.ts
@@ -0,0 +1,6 @@
+import { default as React } from '../../../../node_modules/react';
+type SkeletonProps = {
+    variant?: "text" | "circle" | "rectangle";
+};
+export declare const Skeleton: React.FC<SkeletonProps>;
+export {};
