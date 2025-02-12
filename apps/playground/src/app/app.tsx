6775f2d008bb81c786e550996f0be97aa8121557
jabir Khalil <jabir.saadi.khalil@migrationsverket.se>
2025-02-12 14:33:41 +0100

    chore(skeleton): fix skeleton in app

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import styles from './app.module.css'
 import {
   Button,
@@ -8,14 +9,29 @@ import {
 } from '@midas-ds/components'
 
 export function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000); // Simulate API call
+  }, []);
+
   return (
     <div className={styles.container}>
       <Button variant={'secondary'}>Secondary Button</Button>
       <Button>Primary Button</Button>
       <SearchField placeholder={'Search...'} />
       <Logo/>
-      <TextField label={'Text'}/>
-      <Skeleton variant="rectangle"/>
+      {loading ? (
+         <>
+         <Skeleton variant='rectangular' height={10} width='40px'/>
+        <Skeleton variant="text" />
+        </>) : (
+          <div>
+          <TextField label={'Text'}/>
+          </div>
+        )
+}
+      
     </div>
   )
 }
