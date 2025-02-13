1ac5a769ca06029be4037c3892b7eb1cba1b8354
derpbravely <axel.engstrom@migrationsverket.se>
2025-02-13 14:38:35 +0100

    test(components): mock resizeobserver

--- packages/components/tests/setup.ts
+++ packages/components/tests/setup.ts
@@ -2,3 +2,9 @@ import '@testing-library/jest-dom'
 import { toHaveNoViolations } from 'jest-axe'
 
 expect.extend(toHaveNoViolations)
+
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}))
