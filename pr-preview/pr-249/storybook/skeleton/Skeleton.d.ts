0df895859183f1dd75dc54da208975fece8e4c6b
jabkha <jabkha@users.noreply.github.com>
2025-02-13 10:44:33 +0000

    Deploy preview for PR 249 🛫

--- pr-preview/pr-249/storybook/skeleton/Skeleton.d.ts
+++ pr-preview/pr-249/storybook/skeleton/Skeleton.d.ts
@@ -2,8 +2,8 @@ import { default as React } from '../../../../node_modules/react';
 type SkeletonProps = {
     width?: string | number;
     height?: string | number;
-    variant?: 'text' | 'circle' | 'rectangular' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
-    animation?: 'pulse' | 'wave';
+    variant?: 'circle' | 'rectangular';
+    animation?: 'wave';
 };
 export declare const Skeleton: React.FC<SkeletonProps>;
 export {};
