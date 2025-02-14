0dab2c32833059dd4828649ee912f3cb51e794d0
derpbravely <derpbravely@users.noreply.github.com>
2025-02-14 10:48:04 +0000

    Deploy preview for PR 256 🛫

---
+++ pr-preview/pr-256/storybook/assets/Checkbox.stories-JA_1AzRL.js
@@ -0,0 +1,10 @@
+import{j as i}from"./jsx-runtime-DtgEGaVE.js";import{C as t}from"./Checkbox-B6ZS6GNw.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./RSPContexts-CeCCo7XS.js";import"./useFocusable-BTV2lCc9.js";import"./Form-CdOY3XQC.js";import"./Text-DrzpM1Jb.js";import"./useField-DN-D1gXY.js";import"./Hidden-BxZRcyi_.js";import"./index-i7ZvCR4f.js";import"./useLabels-BlF-T0UY.js";import"./useControlledState-DGyHFLi9.js";import"./VisuallyHidden-CK0-3K8r.js";const k={component:t,title:"Components/Checkbox",tags:["autodocs"],parameters:{layout:"centered"},args:{isInvalid:!1,isSelected:!1,isIndeterminate:!1,isDisabled:!1,isRequired:!1,isReadOnly:!1},argTypes:{isInvalid:{type:"boolean"},isSelected:{type:"boolean"},isIndeterminate:{type:"boolean"},isDisabled:{type:"boolean"},isRequired:{type:"boolean"},isReadOnly:{type:"boolean"}}},e={args:{},render:({...a})=>i.jsx(t,{value:"unsubscribe",...a,children:"Unsubscribe"})};var r,s,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {},
+  render: ({
+    ...args
+  }) => {
+    return <Checkbox value='unsubscribe' {...args}>
+        Unsubscribe
+      </Checkbox>;
+  }
+}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const v=["Example"];export{e as Example,v as __namedExportsOrder,k as default};
