bc49a268cd4ffa2a4aae8995aa59b537af29be16
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-02-14 16:38:32 +0000

    Remove preview for PR 258 🛬

--- pr-preview/pr-258/storybook/dialog/Dialog.d.ts
+++
@@ -1,9 +0,0 @@
-import { AriaDialogProps } from 'react-aria';
-import { default as React } from '../../../../node_modules/react';
-interface DialogProps extends AriaDialogProps {
-    title?: React.ReactNode;
-    children: React.ReactNode;
-}
-export declare const Dialog: React.FC<DialogProps>;
-export declare const ModalTrigger: React.FC<any>;
-export {};
