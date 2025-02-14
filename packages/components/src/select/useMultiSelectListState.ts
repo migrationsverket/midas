f793f1e00151e3f25ffd4d93c70e4b41af5744cd
pixelrickdreamer <jakob.sanne-grubbstrom@migrationsverket.se>
2025-02-14 08:17:11 +0100

    Revert "feat(select): setup working prototype"
    This reverts commit 9e6ca604

--- packages/components/src/select/useMultiSelectListState.ts
+++ packages/components/src/select/useMultiSelectListState.ts
@@ -1,70 +1,69 @@
-import { ListState, useListState } from '@react-stately/list'
-import { CollectionBase, MultipleSelection, Node } from '@react-types/shared'
-import { Key } from 'react'
+import { ListState, useListState } from "@react-stately/list";
+import { CollectionBase, MultipleSelection, Node } from "@react-types/shared";
+import { Key } from "react";
 
 export interface MultiSelectListProps<T>
-  extends CollectionBase<T>,
-    MultipleSelection {}
+    extends CollectionBase<T>,
+        MultipleSelection {}
 
 export interface MultiSelectListState<T> extends ListState<T> {
-  /** The keys for the currently selected items. */
-  selectedKeys: Set<Key>
+    /** The keys for the currently selected items. */
+    selectedKeys: Set<Key>;
 
-  /** Sets the selected keys. */
-  setSelectedKeys(keys: Iterable<Key>): void
+    /** Sets the selected keys. */
+    setSelectedKeys(keys: Iterable<Key>): void;
 
-  /** The value of the currently selected items. */
-  selectedItems: Node<T>[] | null
+    /** The value of the currently selected items. */
+    selectedItems: Node<T>[] | null;
 
-  /** The type of selection. */
-  selectionMode: MultipleSelection['selectionMode']
+    /** The type of selection. */
+    selectionMode: MultipleSelection["selectionMode"];
 }
 
-export function useMultiSelectListState<T extends object>(
-  props: MultiSelectListProps<T>
+export function useMultiSelectListState<T extends {}>(
+    props: MultiSelectListProps<T>
 ): MultiSelectListState<T> {
-  const {
-    collection,
-    disabledKeys,
-    selectionManager,
-    selectionManager: { setSelectedKeys, selectedKeys, selectionMode }
-  } = useListState(props)
+    const {
+        collection,
+        disabledKeys,
+        selectionManager,
+        selectionManager: { setSelectedKeys, selectedKeys, selectionMode }
+    } = useListState(props);
 
-  const missingKeys: Key[] = []
+    const missingKeys: Key[] = [];
 
-  const selectedItems =
-    selectedKeys.size !== 0
-      ? Array.from(selectedKeys)
-          .map(key => {
-            const item = collection.getItem(key)
+    const selectedItems =
+        selectedKeys.size !== 0
+            ? Array.from(selectedKeys)
+                .map((key) => {
+                    const item = collection.getItem(key);
 
-            if (!item) {
-              missingKeys.push(key)
-            }
+                    if (!item) {
+                        missingKeys.push(key);
+                    }
 
-            return item
-          })
-          // Remove undefined values when some keys are not present in the collection
-          .filter(Boolean)
-      : null
+                    return item;
+                })
+                // Remove undefined values when some keys are not present in the collection
+                .filter(Boolean)
+            : null;
 
-  if (missingKeys.length) {
-    // eslint-disable-next-line no-console
-    console.warn(
-      `Select: Keys "${missingKeys.join(
-        ', '
-      )}" passed to "selectedKeys" are not present in the collection.`
-    )
-  }
+    if (missingKeys.length) {
+        // eslint-disable-next-line no-console
+        console.warn(
+            `Select: Keys "${missingKeys.join(
+                ", "
+            )}" passed to "selectedKeys" are not present in the collection.`
+        );
+    }
 
-  return {
-    collection,
-    disabledKeys,
-    selectionManager,
-    selectionMode,
-    selectedKeys,
-    setSelectedKeys: setSelectedKeys.bind(selectionManager),
-    /* @ts-ignore */
-    selectedItems
-  }
+    return {
+        collection,
+        disabledKeys,
+        selectionManager,
+        selectionMode,
+        selectedKeys,
+        setSelectedKeys: setSelectedKeys.bind(selectionManager),
+        selectedItems
+    };
 }
