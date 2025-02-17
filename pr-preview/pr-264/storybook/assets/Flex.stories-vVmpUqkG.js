788ed1ac865e9fd56cc95b0a94fa9d1bc06870a7
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-02-17 18:48:31 +0000

    Deploy preview for PR 264 🛫

---
+++ pr-preview/pr-264/storybook/assets/Flex.stories-vVmpUqkG.js
@@ -0,0 +1,55 @@
+import{j as t}from"./jsx-runtime-DtgEGaVE.js";import{F as d}from"./Flex-Di1QQQg5.js";import{F as e}from"./FlexItem-DeKcLnIt.js";import{C as n}from"./Card-C9YHtuYr.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./Link-y-PzNgKu.js";import"./Link-BFGTtvVZ.js";import"./useFocusable-l7NaYEdB.js";import"./createLucideIcon-8wDOqnHv.js";const u={component:d,title:"Components/Flex",tags:["autodocs"],parameters:{layout:"fullscreen"},argTypes:{fluid:{control:{type:"boolean"}}}},r={args:{},render:({...c})=>t.jsxs(d,{fluid:c.fluid,children:[t.jsx(e,{col:3,children:t.jsx(n,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:6,children:t.jsx(n,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:3,children:t.jsx(n,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:4,children:t.jsx(n,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:4,children:t.jsx(n,{title:"Ett kort",content:"Som är i ett grid men just detta kort har väldigt mycket text för att illustrera att de andra också växer på höjden.",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:3,offset:1,children:t.jsx(n,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:"auto",children:t.jsx(n,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{children:t.jsx(n,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})})]})};var i,l,o;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {},
+  render: ({
+    ...args
+  }) => <Flex fluid={args.fluid}>
+      <FlexItem col={3}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={6}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={3}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={4}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={4}>
+        <Card title={'Ett kort'} content={'Som är i ett grid men just detta kort har väldigt mycket text för att illustrera att de andra också växer på höjden.'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={3} offset={1}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={'auto'}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+    </Flex>
+}`,...(o=(l=r.parameters)==null?void 0:l.docs)==null?void 0:o.source}}};const E=["Primary"];export{r as Primary,E as __namedExportsOrder,u as default};
---
+++ pr-preview/pr-264/storybook/assets/Tag.stories-3dyRWmme.js
@@ -0,0 +1,27 @@
+import{j as i}from"./jsx-runtime-DtgEGaVE.js";import{T as t,a as d}from"./Tag-BNzoKy0O.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button-CcMfRhGg.js";import"./useFocusable-l7NaYEdB.js";import"./clsx-B-dksMZM.js";import"./Hidden-CwiKxbZj.js";import"./index-i7ZvCR4f.js";import"./Collection-DX0cA8sv.js";import"./index-DwWe6W7f.js";import"./useField-Cmo2p-9r.js";import"./useLabels-CDoWwmLU.js";import"./ListBox-eCYlcRDD.js";import"./DragAndDrop-sISFPnlb.js";import"./Separator-ZffSntDo.js";import"./SelectionManager-CjkpbfGY.js";import"./useEvent-BV3BzZuT.js";import"./scrollIntoView-DzftQ5Hl.js";import"./FocusScope-DgqfYpIJ.js";import"./context-CdxgAzm-.js";import"./useDescription-DsCB0LkR.js";import"./useControlledState-DGyHFLi9.js";import"./ListKeyboardDelegate-DmPpmGaI.js";import"./useCollator-BTTIni_k.js";import"./Text-Dyiz11Ec.js";import"./useListState-DEF2mLLn.js";import"./useHighlightSelectionDescription-4zbLvDDH.js";import"./useLocalizedStringFormatter-Dg5by4KY.js";import"./useUpdateEffect-DShQiA8p.js";import"./useHasTabbableChild-CJd1BsNz.js";import"./Button-CS2ZwkSb.js";import"./x-Cu6J0Y2u.js";import"./createLucideIcon-8wDOqnHv.js";const Q={component:t,title:"Components/Tag",tags:["autodocs"]},e={args:{isDisabled:!1,children:"Title",dismissable:!0},render:({...r})=>i.jsx(d,{"aria-label":"Taggar",children:i.jsx(t,{...r,children:r.children})})},a={args:{children:"Title",isDisabled:!0,dismissable:!0},render:({...r})=>i.jsx(d,{"aria-label":"Taggar",children:i.jsx(t,{...r,children:r.children})})};var o,s,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    isDisabled: false,
+    children: 'Title',
+    dismissable: true
+  },
+  render: ({
+    ...args
+  }) => {
+    return <TagGroup aria-label='Taggar'>
+        <Tag {...args}>{args.children}</Tag>
+      </TagGroup>;
+  }
+}`,...(n=(s=e.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var m,p,l;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: 'Title',
+    isDisabled: true,
+    dismissable: true
+  },
+  render: ({
+    ...args
+  }) => {
+    return <TagGroup aria-label='Taggar'>
+        <Tag {...args}>{args.children}</Tag>
+      </TagGroup>;
+  }
+}`,...(l=(p=a.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};const U=["Default","Disabled"];export{e as Default,a as Disabled,U as __namedExportsOrder,Q as default};
