15808b64f94ddc095a5ac75993380128ebae8481
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-02-18 07:21:40 +0000

    Deploy preview for PR 231 🛫

--- pr-preview/pr-231/storybook/table/Table.d.ts
+++ pr-preview/pr-231/storybook/table/Table.d.ts
@@ -1,11 +1,10 @@
 import { RowProps, TableHeaderProps, ColumnProps, TableProps as AriaTableProps, CellProps, TableBody } from 'react-aria-components';
-export interface TableProps<T> extends AriaTableProps {
+export interface TableProps extends AriaTableProps {
     narrow?: boolean;
     striped?: boolean;
-    rows: T[];
 }
-export declare const Table: <T extends object>({ narrow, striped, ...rest }: TableProps<T>) => import("react/jsx-runtime").JSX.Element;
-export declare const TableHeader: <T extends object>({ columns, children }: TableHeaderProps<T>) => import("react/jsx-runtime").JSX.Element;
+export declare const Table: ({ narrow, striped, ...rest }: TableProps) => import("react/jsx-runtime").JSX.Element;
+export declare const TableHeader: <T extends object>({ columns, children, }: TableHeaderProps<T>) => import("react/jsx-runtime").JSX.Element;
 export declare const Row: <T extends object>({ id, columns, children, ...rest }: RowProps<T>) => import("react/jsx-runtime").JSX.Element;
 export declare const Column: ({ children, ...rest }: ColumnProps) => import("react/jsx-runtime").JSX.Element;
 export declare const Cell: ({ ...rest }: CellProps) => import("react/jsx-runtime").JSX.Element;
