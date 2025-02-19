72a240e49b2707ee1965d0509532d03431f27508
derpbravely <axel.engstrom@migrationsverket.se>
2025-02-19 11:51:46 +0100

    test(textarea): refactor tests

--- packages/components/src/textarea/TextArea.spec.tsx
+++ packages/components/src/textarea/TextArea.spec.tsx
@@ -1,27 +1,37 @@
 import { render, screen } from '@testing-library/react'
-import { TextArea } from './TextArea'
+import { TextArea, TextAreaProps } from './TextArea'
 import { axe } from 'jest-axe'
 import user from '../../tests/utils/user'
 
-const label = 'Label for input'
+const label = 'Enter your text here'
+
+const TestForm = (props: TextAreaProps) => (
+  <form>
+    <TextArea
+      {...props}
+      label={label}
+    />
+    <button type='submit'>Submit</button>
+  </form>
+)
 
 describe('given a default TextArea', () => {
-  beforeEach(() => {
-    render(
-      <form>
-        <TextArea
-          label={label}
-          maxCharacters={4}
-          minLength={2}
-        />
-        <button type='submit'>Submit</button>
-      </form>,
-    )
-  })
+  beforeEach(() => render(<TestForm />))
 
   it('should have no accessibility violations', async () => {
     expect(await axe(screen.getByLabelText(label))).toHaveNoViolations()
   })
+})
+
+describe('given a TextArea with maxCharacters and minLength', () => {
+  beforeEach(() =>
+    render(
+      <TestForm
+        maxCharacters={4}
+        minLength={2}
+      />,
+    ),
+  )
 
   it('should give a validation error if the given text is too long', async () => {
     await user.tab()
@@ -49,24 +59,14 @@ describe('given a default TextArea', () => {
 })
 
 describe('given a required TextArea', () => {
-  beforeEach(() => {
-    render(
-      <form>
-        <TextArea
-          label={label}
-          isRequired
-        />
-        <button type='submit'>Submit</button>
-      </form>,
-    )
-  })
+  beforeEach(() => render(<TestForm isRequired />))
 
   it('should give a validation error if the user entered no text', async () => {
     await user.tab()
     await user.tab()
     await user.keyboard('[Enter]')
 
-    // Native validation message
+    // JSDOM Native required validation message
     expect(screen.getByText(/Constraints not satisfied/)).toBeInTheDocument()
   })
 })
@@ -74,21 +74,15 @@ describe('given a required TextArea', () => {
 describe('given a TextArea with custom validation', () => {
   const errorMessage = 'Only numbers are allowed'
 
-  beforeEach(() => {
-    const validate = (value: string) => {
-      return !/^\d+$/.test(value) ? errorMessage : undefined
-    }
+  beforeEach(() =>
     render(
-      <form>
-        <TextArea
-          label={label}
-          isRequired
-          validate={validate}
-        />
-        <button type='submit'>Submit</button>
-      </form>,
-    )
-  })
+      <TestForm
+        validate={(value: string) =>
+          !/^\d+$/.test(value) ? errorMessage : true
+        }
+      />,
+    ),
+  )
 
   it('should give a validation error if the user entered an unpermitted text', async () => {
     await user.tab()
