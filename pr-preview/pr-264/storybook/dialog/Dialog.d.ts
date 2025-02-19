9ceeddda1043cb3f70e62868e843ceb1aead09c4
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-02-19 09:21:47 +0000

    Deploy preview for PR 264 🛫

--- pr-preview/pr-264/storybook/dialog/Dialog.d.ts
+++ pr-preview/pr-264/storybook/dialog/Dialog.d.ts
@@ -1,9 +1,15 @@
 import { AriaDialogProps } from 'react-aria';
-import { default as React } from '../../../../node_modules/react';
+import { OverlayTriggerProps } from '@react-types/overlays';
+import * as React from 'react';
 interface DialogProps extends AriaDialogProps {
     title?: React.ReactNode;
     children: React.ReactNode;
 }
 export declare const Dialog: React.FC<DialogProps>;
-export declare const ModalTrigger: React.FC<any>;
+export declare const ModalTrigger: React.FC<OverlayTriggerProps & {
+    isDismissable?: boolean;
+} & {
+    children: (close: () => void) => any;
+    label?: string | undefined;
+}>;
 export {};
