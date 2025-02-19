ecf89b32df254612efc6c66cf960e18864e514a3
derpbravely <axel.engstrom@migrationsverket.se>
2025-02-19 18:55:09 +0100

    test(checkbox): add validation tests

--- packages/components/src/checkbox/Checkbox.spec.tsx
+++ packages/components/src/checkbox/Checkbox.spec.tsx
@@ -1,27 +1,43 @@
-import { render, RenderResult } from '@testing-library/react'
-import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
 import { Checkbox } from './Checkbox'
 import { CheckboxProps } from 'react-aria-components'
 import { axe } from 'jest-axe'
+import user from '../../tests/utils/user'
+
+const label = 'basic checkbox'
 
 describe('given a default Checkbox', () => {
-  // setup
-  let rendered: RenderResult
-
   beforeEach(() => {
-    rendered = render(<CheckboxTest></CheckboxTest>)
+    render(<TestForm />)
   })
 
   it('should have no accessibility violations', async () => {
-    expect(await axe(rendered.container)).toHaveNoViolations()
+    expect(await axe(screen.getByLabelText(label))).toHaveNoViolations()
   })
 })
 
-const CheckboxTest = (props: CheckboxProps) => (
-  <Checkbox
-    aria-label={'basic checkbox'}
-    {...props}
-  >
-    Click
-  </Checkbox>
+describe('given a required Checkbox', () => {
+  beforeEach(() => {
+    render(<TestForm isRequired />)
+  })
+
+  it('should be (aria) invalid if the user submitted without checking the box', async () => {
+    const checkbox = screen.getByLabelText(label)
+    await user.tab()
+    await user.tab()
+    await user.keyboard('[Enter]')
+    expect(checkbox).toBeInvalid()
+  })
+})
+
+const TestForm = (props: CheckboxProps) => (
+  <form>
+    <Checkbox
+      aria-label={label}
+      {...props}
+    >
+      Click
+    </Checkbox>
+    <button type='submit'>Submit</button>
+  </form>
 )
