362f039875b3d17e7e18eb202b4b9120ec20757d
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2025-02-21 11:43:27 +0100

    fix(textfield): update dossnr regex to support new delimiter

--- packages/components/src/textfield/TextField.tsx
+++ packages/components/src/textfield/TextField.tsx
@@ -191,4 +191,4 @@ export const ssnRegEx = new RegExp(
   '^(?:(?:19|20)?\\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\\d|3[01])(?:[-+ ]?\\d{4})?|\\d{4}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\\d|3[01])(?:[-+ ]?\\d{4}))$',
 )
 
-export const dossNrRegEx = new RegExp('\\d{1,2}[-+]?\\d{6}(/\\d{1,2})?$')
+export const dossNrRegEx = new RegExp('\\d{1,2}[-+]?\\d{6}([-/]\\d{1,2})?$')
