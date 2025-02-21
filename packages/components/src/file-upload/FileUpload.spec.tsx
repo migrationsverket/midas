63e94db8a944be91db5df95564fcdd5aa3b1863c
derpbravely <axel.engstrom@migrationsverket.se>
2025-02-21 15:13:20 +0100

    test(file-upload): add test for unhandled prop

--- packages/components/src/file-upload/FileUpload.spec.tsx
+++ packages/components/src/file-upload/FileUpload.spec.tsx
@@ -1,17 +1,41 @@
-import { FileUpload, FileTriggerProps } from './FileUpload'
-import '@testing-library/jest-dom'
+import { FileUpload } from './FileUpload'
 import { axe } from 'jest-axe'
-import { render, RenderResult } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import user from '../../tests/utils/user'
 
-describe('given default fileupload', () => {
-  let rendered: RenderResult
+const label = 'Etikett'
+
+describe('given default FileUpload', () => {
   beforeEach(() => {
-    rendered = render(<FileUploadExample label={'Etikett'} />)
+    render(
+      <FileUpload
+        label={label}
+        data-testid={label}
+      />,
+    )
   })
 
   it('should have no accessibility violations', async () => {
-    expect(await axe(rendered.container)).toHaveNoViolations()
+    expect(await axe(screen.getByTestId(label))).toHaveNoViolations()
   })
 })
 
-const FileUploadExample = (props: FileTriggerProps) => <FileUpload {...props} />
+describe('given a FileUpload with a custom select handler', () => {
+  const onSelect = jest.fn()
+
+  beforeEach(() => {
+    render(
+      <FileUpload
+        label={label}
+        data-testid={label}
+        onSelect={onSelect}
+      />,
+    )
+  })
+
+  it('should use the provided onSelect callback', async () => {
+    const testFile = new File(['derp'], 'derp.png', { type: 'image/png' })
+    await user.upload(screen.getByTestId(label), testFile)
+    expect(onSelect).toHaveBeenCalledTimes(1)
+  })
+})
