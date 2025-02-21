6bec6663a02a47f50919bd93d0d84edcb594ed16
derpbravely <derpbravely@users.noreply.github.com>
2025-02-21 15:08:49 +0000

    Deploy preview for PR 277 🛫

--- pr-preview/pr-277/storybook/file-upload/FileUpload.d.ts
+++ pr-preview/pr-277/storybook/file-upload/FileUpload.d.ts
@@ -1,11 +1,34 @@
-import { FileTriggerProps as AriaFileTriggerProps } from 'react-aria-components';
-import { default as React } from '../../../../node_modules/react';
-export interface FileTriggerProps extends AriaFileTriggerProps {
+import { FC, ReactNode } from '../../../../node_modules/react';
+export interface FileTriggerProps {
     /** Label for the file upload button */
     label?: string;
     /**  Additional description field */
     description?: string;
     /**  Use DropZone version */
     dropzone?: boolean;
+    /**
+     * Specifies what mime type of files are allowed.
+     */
+    acceptedFileTypes?: Array<string>;
+    /**
+     * Whether multiple files can be selected.
+     */
+    allowsMultiple?: boolean;
+    /**
+     * Specifies the use of a media capture mechanism to capture the media on the spot.
+     */
+    defaultCamera?: 'user' | 'environment';
+    /**
+     * Handler when a user selects a file.
+     */
+    onSelect?: (files: FileList | null) => void;
+    /**
+     * The children of the component.
+     */
+    children?: ReactNode;
+    /**
+     * Enables the selection of directories instead of individual files.
+     */
+    acceptDirectory?: boolean;
 }
-export declare const FileUpload: React.FC<FileTriggerProps>;
+export declare const FileUpload: FC<FileTriggerProps>;
