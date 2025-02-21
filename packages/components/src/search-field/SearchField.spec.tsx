3d650f314ff584a0632b5ede40c4dc1327a90dfb
derpbravely <axel.engstrom@migrationsverket.se>
2025-02-21 12:07:36 +0100

    test(search-field): add validation tests

--- packages/components/src/search-field/SearchField.spec.tsx
+++ packages/components/src/search-field/SearchField.spec.tsx
@@ -1,10 +1,58 @@
-import { render } from '@testing-library/react'
-
+import { screen } from '@testing-library/react'
+import { axe } from 'jest-axe'
 import { SearchField } from './'
+import user from '../../tests/utils/user'
+import { renderWithForm } from '../../tests/utils/browser'
 
-describe('SearchField', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<SearchField placeholder="Search" />)
-    expect(baseElement).toBeTruthy()
+const placeholder = 'Search'
+
+describe('given a default SearchField', () => {
+  beforeEach(() => {
+    renderWithForm(<SearchField placeholder={placeholder} />)
+  })
+
+  it('should have no accessibility violations', async () => {
+    expect(await axe(screen.getByLabelText(placeholder))).toHaveNoViolations()
+  })
+})
+
+describe('given a required SearchField', () => {
+  beforeEach(() => {
+    renderWithForm(
+      <SearchField
+        placeholder={placeholder}
+        isRequired
+      />,
+    )
+  })
+
+  it('should give a validation error if the user entered no text', async () => {
+    await user.tab()
+    await user.tab()
+    await user.keyboard('[Enter]')
+
+    // JSDOM Native required validation message
+    expect(screen.getByText(/Constraints not satisfied/)).toBeInTheDocument()
+  })
+})
+
+describe('given a SearchField with custom validation', () => {
+  const errorMessage = 'Dont search for secret documents'
+
+  beforeEach(() =>
+    renderWithForm(
+      <SearchField
+        placeholder={placeholder}
+        validate={value => (value === 'secret' ? errorMessage : true)}
+      />,
+    ),
+  )
+
+  it('should give a validation error if the user entered an unpermitted text', async () => {
+    await user.tab()
+    await user.keyboard('secret')
+    await user.tab()
+    await user.keyboard('[Enter]')
+    expect(screen.getByText(errorMessage)).toBeInTheDocument()
   })
 })
