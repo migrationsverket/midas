1f9a9cfc1583a6b7e5ac6fda705d44ba0a0c8927
derpbravely <axel.engstrom@migrationsverket.se>
2025-02-24 13:58:49 +0100

    fix(textfield): set initial counter value on render

--- packages/components/src/textfield/TextField.tsx
+++ packages/components/src/textfield/TextField.tsx
@@ -42,7 +42,9 @@ export const TextField: React.FC<TextFieldProps> = ({
   showCounter,
   ...props
 }) => {
-  const [value, setValue] = React.useState<string>(props.value || '')
+  const [value, setValue] = React.useState<string>(
+    props.defaultValue ?? props.value ?? '',
+  )
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = event.target.value
