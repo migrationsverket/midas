c17f99da641e5044a2e85d20d75cc8ebba896320
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-02-25 18:53:58 +0000

    Deploy preview for PR 255 🛫

--- pr-preview/pr-255/storybook/tooltip/Tooltip.d.ts
+++ pr-preview/pr-255/storybook/tooltip/Tooltip.d.ts
@@ -4,6 +4,6 @@ interface MidasTooltipProps extends Omit<TooltipProps, 'children'> {
     children: React.ReactNode;
     placement?: 'top' | 'right' | 'bottom' | 'left';
 }
-export declare function Tooltip({ children, placement, ...props }: MidasTooltipProps): import("react/jsx-runtime").JSX.Element;
+export declare function Tooltip({ children, placement, className, ...props }: MidasTooltipProps): import("react/jsx-runtime").JSX.Element;
 export declare function TooltipTrigger({ children, delay, ...props }: TooltipTriggerComponentProps): import("react/jsx-runtime").JSX.Element;
 export {};
