c882f32c825074dc5e3f577d250f9cf9889ab25c
derpbravely <derpbravely@users.noreply.github.com>
2025-02-26 18:09:10 +0000

    Deploy preview for PR 290 🛫

---
+++ pr-preview/pr-290/storybook/assets/Form.stories-Bj3mTrVA.js
@@ -0,0 +1,70 @@
+import{j as e}from"./jsx-runtime-DtgEGaVE.js";import{$ as b}from"./Form-CWi2Vnp4.js";import{F}from"./Flex-BcjRyzLV.js";import{F as n}from"./FlexItem-BmhdueUB.js";import{T as r}from"./TextField-BjUNfMuz.js";import{S as I,I as j}from"./Select-DIkBuEJs.js";import{B as m}from"./Button-wd7Nt-Ve.js";import{R as g,a as k}from"./Radio-EGa7aTW5.js";import{C as f}from"./CheckboxGroup-BPDInKZE.js";import{C as S}from"./Checkbox-CJy4yA4_.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./useFocusable-3zbZ-RaT.js";import"./clsx-B-dksMZM.js";import"./Text-DjlCanPO.js";import"./useFormValidation-CXImPhFu.js";import"./Hidden-Cu6-cfZ1.js";import"./index-i7ZvCR4f.js";import"./useField-BvVxJF6x.js";import"./useLabels-CouxmuYO.js";import"./TextField.module-DtPJIZYq.js";import"./useControlledState-DGyHFLi9.js";import"./Button-OaHVYHGx.js";import"./ListBox-B0H8Z5U7.js";import"./Collection-HK2tssQB.js";import"./index-DwWe6W7f.js";import"./DragAndDrop-sISFPnlb.js";import"./Separator-DEsxS4wB.js";import"./SelectionManager-Bgt2CNIh.js";import"./useEvent-Cu7AZejT.js";import"./FocusScope-CrTiNxai.js";import"./context-QyyerVwU.js";import"./useDescription-CxmrmoDT.js";import"./ListKeyboardDelegate-B8OEDIsW.js";import"./useListState-Dp7sQ2eW.js";import"./Dialog-CvXv7k8g.js";import"./RSPContexts-CeCCo7XS.js";import"./OverlayArrow-DBU6oQr0.js";import"./PressResponder-DE9hv8BO.js";import"./useLocalizedStringFormatter-wW5sL_ZF.js";import"./VisuallyHidden-BfEDOqwV.js";import"./intlStrings-ZMTSFH_M.js";import"./useSingleSelectListState-kR3Sada0.js";import"./chevron-down-U1dNK9nR.js";import"./createLucideIcon-8wDOqnHv.js";import"./Group-BhNTXCGH.js";const Ie={component:b,title:"Examples/Form",tags:["autodocs"],argTypes:{}},o={args:{},render:()=>{const a=["Banan","Apple","Mango"];return e.jsxs(F,{children:[e.jsx(n,{col:12,children:e.jsx(r,{label:"Ange ditt fullständiga namn",description:"Glöm inte dina eventuella mellannamn!"})}),e.jsx(n,{col:12,children:e.jsx(r,{label:"Personnummmer",description:"Anges på formen ÅÅMMDD-XXXX"})}),e.jsx(n,{col:12,children:e.jsx(I,{label:"Vilken är din favoritfrukt",placeholder:"Välj en frukt",children:a.map(l=>e.jsx(j,{children:l},l))})}),e.jsx(n,{children:e.jsx(m,{children:"Skicka"})})]})}},i={args:{},render:()=>{const a=["A","B","C"],l=["Anledning A","Anledning B","Anledning C"],h=["Man","Kvinna","Annat"];return e.jsx("div",{children:e.jsxs(F,{children:[e.jsx(n,{col:6,children:e.jsx(g,{label:"Anledning",description:"Ange anledning till att du söker i databasen",children:l.map(t=>e.jsx(k,{value:t,children:t},t))})}),e.jsx(n,{col:6,children:e.jsx(f,{label:"Databas",description:"Välj databas att söka i",children:a.map(t=>e.jsx(S,{value:t,children:t},t))})}),e.jsx(n,{col:6,children:e.jsx(r,{label:"Namn",description:""})}),e.jsx(n,{col:6,children:e.jsx(r,{label:"Personnummer"})}),e.jsx(n,{col:6,children:e.jsx(I,{label:"Kön",description:"Kan lämnas tomt",placeholder:"Välj kön",children:h.map(t=>e.jsx(j,{children:t},t))})}),e.jsx(n,{col:6,children:e.jsx(r,{label:"Ärendekod"})}),e.jsx(n,{col:"auto",children:e.jsx(m,{children:"Sök"})}),e.jsx(n,{col:"auto",children:e.jsx(m,{variant:"secondary",children:"Rensa"})})]})})}};var s,c,d;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {},
+  render: () => {
+    const ITEMS = ['Banan', 'Apple', 'Mango'];
+    return <Flex>
+        <FlexItem col={12}>
+          <TextField label='Ange ditt fullständiga namn' description='Glöm inte dina eventuella mellannamn!' />
+        </FlexItem>
+        <FlexItem col={12}>
+          <TextField label='Personnummmer' description='Anges på formen ÅÅMMDD-XXXX' />
+        </FlexItem>
+        <FlexItem col={12}>
+          <Select label='Vilken är din favoritfrukt' placeholder='Välj en frukt'>
+            {ITEMS.map((item: string) => <Item key={item}>{item}</Item>)}
+          </Select>
+        </FlexItem>
+
+        <FlexItem>
+          <Button>Skicka</Button>
+        </FlexItem>
+      </Flex>;
+  }
+}`,...(d=(c=o.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var p,x,u;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {},
+  render: () => {
+    const ITEMS = ['A', 'B', 'C'];
+    const RADIOITEMS = ['Anledning A', 'Anledning B', 'Anledning C'];
+    const SEX = ['Man', 'Kvinna', 'Annat'];
+    return <div>
+        <Flex>
+          <FlexItem col={6}>
+            <RadioGroup label='Anledning' description='Ange anledning till att du söker i databasen'>
+              {RADIOITEMS.map((item: string) => <Radio value={item} key={item}>
+                  {item}
+                </Radio>)}
+            </RadioGroup>
+          </FlexItem>
+          <FlexItem col={6}>
+            <CheckboxGroup label='Databas' description='Välj databas att söka i'>
+              {ITEMS.map((item: string) => <Checkbox value={item} key={item}>
+                  {item}
+                </Checkbox>)}
+            </CheckboxGroup>
+          </FlexItem>
+          <FlexItem col={6}>
+            <TextField label='Namn' description='' />
+          </FlexItem>
+          <FlexItem col={6}>
+            <TextField label='Personnummer' />
+          </FlexItem>
+
+          <FlexItem col={6}>
+            <Select label='Kön' description='Kan lämnas tomt' placeholder='Välj kön'>
+              {SEX.map((item: string) => <Item key={item}>{item}</Item>)}
+            </Select>
+          </FlexItem>
+          <FlexItem col={6}>
+            <TextField label='Ärendekod' />
+          </FlexItem>
+
+          <FlexItem col={'auto'}>
+            <Button>Sök</Button>
+          </FlexItem>
+          <FlexItem col={'auto'}>
+            <Button variant='secondary'>Rensa</Button>
+          </FlexItem>
+        </Flex>
+      </div>;
+  }
+}`,...(u=(x=i.parameters)==null?void 0:x.docs)==null?void 0:u.source}}};const je=["SimpleForm","TwoColumnForm"];export{o as SimpleForm,i as TwoColumnForm,je as __namedExportsOrder,Ie as default};
