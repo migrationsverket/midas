f248b0a7152531940acc11a4016efbf34e580b00
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-02-26 18:20:14 +0000

    Deploy preview for PR 264 🛫

---
+++ pr-preview/pr-264/storybook/assets/Select.stories-DkaF82vF.js
@@ -0,0 +1,62 @@
+import{S as _}from"./Select-2zwqiffF.js";import"./jsx-runtime-DtgEGaVE.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button-QTphRx4x.js";import"./useFocusable-C3Tvsz8-.js";import"./clsx-B-dksMZM.js";import"./Hidden-t2TOsKWR.js";import"./index-i7ZvCR4f.js";import"./useField-Cyo9MoZl.js";import"./useLabels-CtwdP7qT.js";import"./Tag-BORyegNc.js";import"./Collection-DSs8x1Wd.js";import"./index-DwWe6W7f.js";import"./ListBox-CXKdTALH.js";import"./DragAndDrop-sISFPnlb.js";import"./Separator-CjLqTao-.js";import"./SelectionManager-B6mzhZXY.js";import"./useEvent-B6d9-LYV.js";import"./scrollIntoView-DzftQ5Hl.js";import"./FocusScope-DOk3V7x2.js";import"./context-OSXAnMfc.js";import"./useDescription-BSbuOK9_.js";import"./useControlledState-DGyHFLi9.js";import"./ListKeyboardDelegate-DmPpmGaI.js";import"./useCollator-4QryF0sP.js";import"./Text-BVfnoLK6.js";import"./useListState-BsgHITYP.js";import"./useHighlightSelectionDescription-B0pAbar-.js";import"./useLocalizedStringFormatter-uDQfzgWc.js";import"./useUpdateEffect-DShQiA8p.js";import"./useHasTabbableChild-CBXIVSLK.js";import"./Button-DKJsPsGu.js";import"./x-Cu6J0Y2u.js";import"./createLucideIcon-8wDOqnHv.js";import"./TextField-CY9k8hi9.js";import"./Form-l2PcAF26.js";import"./Input-CgmTjY8Q.js";import"./useOverlayTriggerState-D97Z3G1I.js";import"./DismissButton-B8CqawU9.js";import"./VisuallyHidden-CFHSfYYo.js";import"./useModal-C7L4wT2l.js";import"./useMenuTrigger-75ikU0oH.js";import"./useMenuTriggerState-Dr4sU0pt.js";import"./chevron-down-U1dNK9nR.js";const Ie={component:_,title:"Components/Select",tags:["autodocs"]},F=["Apple","Banana","Kiwi","Elderflower","Nectarine","Watermelon","Pineapple","Apricot","Cloudberry","Mango","Strawberry","Blueberry","Raspberry","Blackberry","Cherry","Peach","Plum","Grape","Orange","Lemon","Lime","Coconut","Fig","Papaya","Guava","Pomegranate","Dragonfruit","Starfruit","Passionfruit"],R=F.map(p=>({name:p,id:p.toLocaleLowerCase()})),e={args:{label:"Label",selectionMode:"single",options:R,isDisabled:!1,isClearable:!0,isSelectableAll:!1,description:"Description",showTags:!1,placeholder:"Select an option"}},a={args:{...e.args,description:"Kiwi is pre-selected",defaultSelectedKeys:["kiwi"]}},s={args:{...e.args,selectionMode:"multiple",description:"All options are selected",defaultSelectedKeys:"all"}},o={args:{...e.args,isDisabled:!0}},t={args:{...e.args,description:"Kiwi is disabled",disabledKeys:["kiwi"]}},i={args:{...e.args,isInvalid:!0,errorMessage:"Error msg"}},l={args:{...e.args,selectionMode:"multiple",showTags:!0,defaultSelectedKeys:["apple","kiwi"]}},n={args:{...e.args,selectionMode:"multiple",isSelectableAll:!0}},r={args:{...e.args,selectionMode:"multiple",isClearable:!1}};var c,m,d;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: 'Label',
+    selectionMode: 'single',
+    options: options,
+    isDisabled: false,
+    isClearable: true,
+    isSelectableAll: false,
+    description: 'Description',
+    showTags: false,
+    placeholder: 'Select an option'
+  }
+}`,...(d=(m=e.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var u,g,b;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    ...Normal.args,
+    description: 'Kiwi is pre-selected',
+    defaultSelectedKeys: ['kiwi']
+  }
+}`,...(b=(g=a.parameters)==null?void 0:g.docs)==null?void 0:b.source}}};var S,f,y;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    ...Normal.args,
+    selectionMode: 'multiple',
+    description: 'All options are selected',
+    defaultSelectedKeys: 'all'
+  }
+}`,...(y=(f=s.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};var w,K,A;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    ...Normal.args,
+    isDisabled: true
+  }
+}`,...(A=(K=o.parameters)==null?void 0:K.docs)==null?void 0:A.source}}};var D,M,N;t.parameters={...t.parameters,docs:{...(D=t.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    ...Normal.args,
+    description: 'Kiwi is disabled',
+    disabledKeys: ['kiwi']
+  }
+}`,...(N=(M=t.parameters)==null?void 0:M.docs)==null?void 0:N.source}}};var C,h,k;i.parameters={...i.parameters,docs:{...(C=i.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    ...Normal.args,
+    isInvalid: true,
+    errorMessage: 'Error msg'
+  }
+}`,...(k=(h=i.parameters)==null?void 0:h.docs)==null?void 0:k.source}}};var E,L,P;l.parameters={...l.parameters,docs:{...(E=l.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    ...Normal.args,
+    selectionMode: 'multiple',
+    showTags: true,
+    defaultSelectedKeys: ['apple', 'kiwi']
+  }
+}`,...(P=(L=l.parameters)==null?void 0:L.docs)==null?void 0:P.source}}};var T,v,I;n.parameters={...n.parameters,docs:{...(T=n.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    ...Normal.args,
+    selectionMode: 'multiple',
+    isSelectableAll: true
+  }
+}`,...(I=(v=n.parameters)==null?void 0:v.docs)==null?void 0:I.source}}};var O,x,B,W,G;r.parameters={...r.parameters,docs:{...(O=r.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    ...Normal.args,
+    selectionMode: 'multiple',
+    isClearable: false
+  }
+}`,...(B=(x=r.parameters)==null?void 0:x.docs)==null?void 0:B.source},description:{story:"As default all options are clearable. `isClearable={false}` Experimental feature",...(G=(W=r.parameters)==null?void 0:W.docs)==null?void 0:G.description}}};const Oe=["Normal","DefaultSelectedKey","AllKeysSelected","Disabled","DisabledOption","Invalid","WithTags","SelectAllEnabled","NotClearable"];export{s as AllKeysSelected,a as DefaultSelectedKey,o as Disabled,t as DisabledOption,i as Invalid,e as Normal,r as NotClearable,n as SelectAllEnabled,l as WithTags,Oe as __namedExportsOrder,Ie as default};
