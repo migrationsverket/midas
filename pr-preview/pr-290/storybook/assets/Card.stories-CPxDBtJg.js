c2ffc7d6baeafc3f63cbef72dfb3a68d533c3062
derpbravely <derpbravely@users.noreply.github.com>
2025-02-26 17:43:53 +0000

    Deploy preview for PR 290 🛫

---
+++ pr-preview/pr-290/storybook/assets/Card.stories-CPxDBtJg.js
@@ -0,0 +1,13 @@
+import{C as i}from"./Card-V5zf1eOc.js";import"./jsx-runtime-DtgEGaVE.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Link-CzDyaeT0.js";import"./Link-DfyGSq1K.js";import"./useFocusable-3zbZ-RaT.js";import"./clsx-B-dksMZM.js";import"./createLucideIcon-8wDOqnHv.js";const a={source:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Pineapple_and_cross_section.jpg/640px-Pineapple_and_cross_section.jpg",description:"Pineapple"},u={component:i,title:"Components/Card",tags:["autodocs"]},e={args:{title:"Welcome to card",content:"Sometimes the default accessible name of an element is missing, or does not accurately describe its contents, and there is no content visible in the DOM that can be associated with the object to give it meaning. A common example is a button containing an SVG or icon font (which you shouldn't be using) without any text.",image:a,background:!1,link:{children:"Läs mer om Card",href:"#"},headingTag:"h2"}};var t,o,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    title: 'Welcome to card',
+    content: "Sometimes the default accessible name of an element is missing, or does not accurately describe its contents, and there is no content visible in the DOM that can be associated with the object to give it meaning. A common example is a button containing an SVG or icon font (which you shouldn't be using) without any text.",
+    image: testImage,
+    background: false,
+    link: {
+      children: 'Läs mer om Card',
+      href: '#'
+    },
+    headingTag: 'h2'
+  }
+}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const b=["Example"];export{e as Example,b as __namedExportsOrder,u as default};
---
+++ pr-preview/pr-290/storybook/search-field/SearchField.d.ts
@@ -0,0 +1,26 @@
+import { SearchFieldProps as AriaSearchFieldProps } from 'react-aria-components';
+import * as React from 'react';
+export interface SearchFieldProps extends AriaSearchFieldProps {
+    /** Placeholder text */
+    placeholder: string;
+    /**
+     *  Text displayed on the 'search' button
+     *  @default
+     *  'Sök'
+     */
+    buttonText?: string;
+    /**
+     * A function that returns an error message if a given value is invalid.
+     * Validation errors are displayed to the user when the form is submitted if validationBehavior="native".
+     * For realtime validation, use the isInvalid prop instead.
+     *
+     * To override the behavior of the isRequired prop you can instead use this property to return a custom error message.
+     */
+    validate?: AriaSearchFieldProps['validate'];
+    /**
+     * Whether user input is required on the input before form submission.
+     * Currently have troubles displaying an error message, please use the validate property if it's needed.
+     */
+    isRequired?: AriaSearchFieldProps['isRequired'];
+}
+export declare const SearchField: React.FC<SearchFieldProps>;
