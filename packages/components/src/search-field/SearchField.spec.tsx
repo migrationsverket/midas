2a6446cbf5a9b865d07be59c2799049dfe4be66b
derpbravely <axel.engstrom@migrationsverket.se>
2025-02-26 18:31:31 +0100

    test(search-field): add interaction tests

--- packages/components/src/search-field/SearchField.spec.tsx
+++ packages/components/src/search-field/SearchField.spec.tsx
@@ -1,24 +1,48 @@
-import { screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { axe } from 'jest-axe'
 import { SearchField } from './'
 import user from '../../tests/utils/user'
-import { renderWithForm } from '../../tests/utils/browser'
 
 const placeholder = 'Search'
 
 describe('given a default SearchField', () => {
+  const onChange = jest.fn()
+  const onSubmit = jest.fn()
+
   beforeEach(() => {
-    renderWithForm(<SearchField placeholder={placeholder} />)
+    render(
+      <SearchField
+        placeholder={placeholder}
+        onChange={onChange}
+        onSubmit={onSubmit}
+      />,
+    )
   })
 
   it('should have no accessibility violations', async () => {
     expect(await axe(screen.getByLabelText(placeholder))).toHaveNoViolations()
   })
+
+  it('should be possible to submit a search string using only the keyboard', async () => {
+    await user.tab()
+    await user.keyboard('hello')
+    await user.keyboard('[Enter]')
+    expect(onChange).toHaveBeenCalledWith('hello')
+    expect(onSubmit).toHaveBeenCalledWith('hello')
+  })
+
+  it('should be possible to submit a search string using the mouse', async () => {
+    await user.tab()
+    await user.keyboard('hello')
+    await user.click(screen.getByText('Sök'))
+    expect(onChange).toHaveBeenCalledWith('hello')
+    expect(onSubmit).toHaveBeenCalledWith('hello')
+  })
 })
 
 describe('given a required SearchField', () => {
   beforeEach(() => {
-    renderWithForm(
+    render(
       <SearchField
         placeholder={placeholder}
         isRequired
@@ -26,24 +50,40 @@ describe('given a required SearchField', () => {
     )
   })
 
-  it('should give a validation error if the user entered no text', async () => {
-    await user.tab()
-    await user.tab()
-    await user.keyboard('[Enter]')
+  it.failing(
+    'should give a validation error if the user entered no text',
+    async () => {
+      await user.tab()
+      await user.keyboard('[Enter]')
 
-    // JSDOM Native required validation message
-    expect(screen.getByText(/Constraints not satisfied/)).toBeInTheDocument()
-  })
+      // JSDOM Native required validation message
+      expect(screen.getByText(/Constraints not satisfied/)).toBeInTheDocument()
+    },
+  )
+
+  it.failing(
+    'should give a validation error if the user entered no text and used the mouse to click "search"',
+    async () => {
+      await user.click(screen.getByText('Sök'))
+
+      // JSDOM Native required validation message
+      expect(screen.getByText(/Constraints not satisfied/)).toBeInTheDocument()
+    },
+  )
 })
 
 describe('given a SearchField with custom validation', () => {
   const errorMessage = 'Dont search for secret documents'
+  const onChange = jest.fn()
+  const onSubmit = jest.fn()
 
   beforeEach(() =>
-    renderWithForm(
+    render(
       <SearchField
         placeholder={placeholder}
         validate={value => (value === 'secret' ? errorMessage : true)}
+        onChange={onChange}
+        onSubmit={onSubmit}
       />,
     ),
   )
@@ -53,6 +93,17 @@ describe('given a SearchField with custom validation', () => {
     await user.keyboard('secret')
     await user.tab()
     await user.keyboard('[Enter]')
+    expect(onChange).toHaveBeenCalledWith('secret')
+    expect(onSubmit).not.toHaveBeenCalled()
+    expect(screen.getByText(errorMessage)).toBeInTheDocument()
+  })
+
+  it('should give a validation error if the user entered an unpermitted text and used the mouse to click "search"', async () => {
+    await user.tab()
+    await user.keyboard('secret')
+    await user.click(screen.getByText('Sök'))
+    expect(onChange).toHaveBeenCalledWith('secret')
+    expect(onSubmit).not.toHaveBeenCalled()
     expect(screen.getByText(errorMessage)).toBeInTheDocument()
   })
 })
