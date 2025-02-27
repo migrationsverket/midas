e5397e662629758f21034dd1783128738d3e8f56
wilhjelmig <165774426+wilhjelmig@users.noreply.github.com>
2025-02-27 07:34:39 +0000

    Deploying to gh-pages from @ migrationsverket/midas@c377a6acbac84e1320b8484642375cc840bd912a 🚀

---
+++ storybook/assets/DatePicker.stories-B8Anbx0U.js
@@ -0,0 +1,12 @@
+import{D as s}from"./DatePicker-SG618sQo.js";import"./jsx-runtime-DtgEGaVE.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button-QmryGY2x.js";import"./useFocusable-DvoOiisW.js";import"./clsx-B-dksMZM.js";import"./Hidden-C-pamdWU.js";import"./index-cpIEhwLo.js";import"./RSPContexts-CeCCo7XS.js";import"./Text-EJwwmz-7.js";import"./context-DFhOKXSg.js";import"./useControlledState-DGyHFLi9.js";import"./useLocalizedStringFormatter-m925tTwS.js";import"./useLabels-CtxEn0bM.js";import"./useUpdateEffect-DShQiA8p.js";import"./useEvent-CA0YFu_S.js";import"./ListKeyboardDelegate-Qpuh_B0q.js";import"./SelectionManager-CrjNvHpx.js";import"./FocusScope-BEpvNgc1.js";import"./useDescription-Ct3UQeSC.js";import"./VisuallyHidden-DXoeczHE.js";import"./Group-ceAKJ6e-.js";import"./TextField.module-B2RiHuHW.js";import"./Form-Co3XUpyT.js";import"./useField-RzOmeqWZ.js";import"./useFilter-CPHv71Dg.js";import"./Dialog-Dd-vX3nh.js";import"./OverlayArrow-ErOZPrpP.js";import"./Collection-DXjGeaiG.js";import"./index-DwWe6W7f.js";import"./Separator-DXcJoRg9.js";import"./PressResponder-zWEAS-O-.js";import"./Heading-DqdRWzOb.js";import"./createLucideIcon-8wDOqnHv.js";import"./chevron-right-CjCwHShF.js";import"./TextField-y32xF91H.js";import"./Button-CMwrBylU.js";const N={component:s,title:"Components/DatePicker",tags:["autodocs"]},r={args:{label:"Välj datum",description:"YYYY-MM-DD"}},t={args:{label:"Välj datum och tid",description:"YYYY-MM-DD HH-MM-SS",granularity:"second"}};var o,i,m;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    label: 'Välj datum',
+    description: 'YYYY-MM-DD'
+  }
+}`,...(m=(i=r.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,e,a;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    label: 'Välj datum och tid',
+    description: 'YYYY-MM-DD HH-MM-SS',
+    granularity: 'second'
+  }
+}`,...(a=(e=t.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};const Q=["Primary","WithTime"];export{r as Primary,t as WithTime,Q as __namedExportsOrder,N as default};
---
+++ storybook/assets/Dropdown.stories-B-fJXia-.js
@@ -0,0 +1,26 @@
+import{j as a}from"./jsx-runtime-DtgEGaVE.js";import{D as T,a as f}from"./Dropdown-DXBwMi05.js";import{w as v,f as h,e as x}from"./index-frOfh_zr.js";import{L as E}from"./LinkButton-BjxvSrdu.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Dialog-Dd-vX3nh.js";import"./Button-QmryGY2x.js";import"./useFocusable-DvoOiisW.js";import"./clsx-B-dksMZM.js";import"./Hidden-C-pamdWU.js";import"./index-cpIEhwLo.js";import"./RSPContexts-CeCCo7XS.js";import"./OverlayArrow-ErOZPrpP.js";import"./context-DFhOKXSg.js";import"./useControlledState-DGyHFLi9.js";import"./Collection-DXjGeaiG.js";import"./index-DwWe6W7f.js";import"./Separator-DXcJoRg9.js";import"./SelectionManager-CrjNvHpx.js";import"./useEvent-CA0YFu_S.js";import"./FocusScope-BEpvNgc1.js";import"./useDescription-Ct3UQeSC.js";import"./ListKeyboardDelegate-Qpuh_B0q.js";import"./Text-EJwwmz-7.js";import"./PressResponder-zWEAS-O-.js";import"./useLocalizedStringFormatter-m925tTwS.js";import"./useLabels-CtxEn0bM.js";import"./VisuallyHidden-DXoeczHE.js";import"./Button-CMwrBylU.js";import"./createLucideIcon-8wDOqnHv.js";import"./Link-W1CcEl-a.js";import"./chevron-right-CjCwHShF.js";const er={component:T,title:"Components/Dropdown"},B=["Ett","Två","Tre"],w="Meny",r={args:{title:w,children:B.map((n,t)=>a.jsx(f,{children:a.jsx(E,{variant:"tertiary",children:n})},"dropdown_"+t))}},o={args:{...r.args},play:async({canvasElement:n})=>{const t=v(n),g=await t.findByText(w);await h.click(g),x(t).toBeTruthy()}},e={args:{...r.args,title:void 0,label:"Meny"}};var i,m,s;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    title: menuTitle,
+    children: ITEMS.map((item, i) => <DropdownItem key={'dropdown_' + i}>
+        <LinkButton variant='tertiary'>{item}</LinkButton>
+      </DropdownItem>)
+  }
+}`,...(s=(m=r.parameters)==null?void 0:m.docs)==null?void 0:s.source}}};var p,c,d;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const dropdown = await canvas.findByText(menuTitle);
+    await fireEvent.click(dropdown);
+    expect(canvas).toBeTruthy();
+  }
+}`,...(d=(c=o.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var l,u,y;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    title: undefined,
+    label: 'Meny'
+  }
+}`,...(y=(u=e.parameters)==null?void 0:u.docs)==null?void 0:y.source}}};const nr=["Primary","Open","WithoutTitle"];export{o as Open,r as Primary,e as WithoutTitle,nr as __namedExportsOrder,er as default};
