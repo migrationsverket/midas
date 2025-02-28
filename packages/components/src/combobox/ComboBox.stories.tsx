4dca32f24a916474ad2dfc0c7b35e11db2541dd7
derpbravely <axel.engstrom@migrationsverket.se>
2025-02-28 15:09:26 +0100

    test(combobox): migrate to storybook play

--- packages/components/src/combobox/ComboBox.stories.tsx
+++ packages/components/src/combobox/ComboBox.stories.tsx
@@ -3,6 +3,7 @@ import { ComboBox, ComboBoxItem } from './ComboBox'
 import { generateMockOptions } from './utils'
 import { Item } from './types'
 import { RunOptions } from 'axe-core'
+import { expect, userEvent, within } from '@storybook/test'
 
 const meta: Meta<typeof ComboBox> = {
   component: ComboBox,
@@ -29,15 +30,27 @@ export const Default: Story = {
     placeholder: 'Välj eller sök frukt',
     label: 'Välj en frukt',
     description: 'Description',
+    className: 'test',
   },
   render: (args: any) => (
     <ComboBox
+      data-testid='test'
       items={options}
       {...args}
     >
       {(item: Item) => <ComboBoxItem>{item.name}</ComboBoxItem>}
     </ComboBox>
   ),
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement)
+    step(
+      'it should preserve its classNames when being passed new ones',
+      async () => {
+        const comboBox = canvas.getByTestId('test')
+        expect(comboBox).toHaveClass(/combobox/, 'test')
+      },
+    )
+  },
 }
 
 export const Invalid: Story = {
@@ -80,5 +93,34 @@ export const Disabled: Story = {
         },
       } satisfies RunOptions,
     },
+  },
+}
+
+export const Required: Story = {
+  args: {
+    'aria-label': 'test',
+    isRequired: true,
+  },
+  render: (args: any) => (
+    <form>
+      <ComboBox {...args}>
+        <ComboBoxItem>Hej</ComboBoxItem>
+      </ComboBox>
+      <button type='submit'>Submit</button>
+    </form>
+  ),
+  play: async ({ canvasElement, step, args }) => {
+    const canvas = within(canvasElement)
+    step(
+      'it should be (aria) invalid and show a validation error message if the user submitted without selecting anything',
+      async () => {
+        const comboBox = canvas.getByLabelText(args['aria-label'] as string)
+        await userEvent.tab()
+        await userEvent.tab()
+        await userEvent.keyboard('[Enter]')
+        expect(comboBox).toBeInvalid()
+        expect(canvas.getByText(/Fyll i det här fältet/)).toBeInTheDocument()
+      },
+    )
   },
 }
