8ae08507e0f599f08896cbcaa36937585462183c
jabkha <jabkha@users.noreply.github.com>
2025-02-28 10:36:22 +0000

    Deploy preview for PR 292 🛫

---
+++ pr-preview/pr-292/storybook/assets/CheckboxGroup.stories-Cq3W-Azr.js
@@ -0,0 +1,16 @@
+import{j as o}from"./jsx-runtime-DtgEGaVE.js";import{C as g}from"./Checkbox-DQBYUQPT.js";import{C as m}from"./CheckboxGroup-rWtjrg4B.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./RSPContexts-CeCCo7XS.js";import"./useFocusable-DASL9ZMT.js";import"./Form-BTz3WNf3.js";import"./Text-DX8e4zrg.js";import"./useFormValidation-8Qy7QSzR.js";import"./useField-bJrG6qmG.js";import"./Hidden-BuU8W4f_.js";import"./index-cpIEhwLo.js";import"./useLabels-MT93WuwX.js";import"./useControlledState-DGyHFLi9.js";import"./VisuallyHidden-D11zA9Ye.js";import"./Group-Bxcv9fgd.js";import"./TextField-D77uAkYg.js";import"./TextField.module-CBAfyb2u.js";import"./Button-BCnPv5Qk.js";import"./Button-yfKCtftD.js";const _={title:"Components/Checkbox/CheckboxGroup",component:m,parameters:{layout:"centered"},args:{isInvalid:!1,isDisabled:!1,isRequired:!1,isReadOnly:!1},argTypes:{isInvalid:{type:"boolean"},isDisabled:{type:"boolean"},isRequired:{type:"boolean"},isReadOnly:{type:"boolean"},errorMessage:{type:"string"}},tags:["autodocs"]},u=["Banan","Apple","Mango"],c={args:{},render:({...d})=>o.jsx(m,{...d,children:u.map(t=>o.jsx(g,{value:t,children:t},t))})},e={...c,args:{label:"Etikett",description:"Beskrivning",errorMessage:"här gick det snett"}},r={...c,args:{label:"Etikett",description:"Beskrivning",errorMessage:"här gick det snett",showSelectAll:!0}};var s,a,i;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  ...CheckboxGroupTemplate,
+  args: {
+    label: 'Etikett',
+    description: 'Beskrivning',
+    errorMessage: 'här gick det snett'
+  }
+}`,...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var n,p,l;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  ...CheckboxGroupTemplate,
+  args: {
+    label: 'Etikett',
+    description: 'Beskrivning',
+    errorMessage: 'här gick det snett',
+    showSelectAll: true
+  }
+}`,...(l=(p=r.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};const z=["Example","SelectAll"];export{e as Example,r as SelectAll,z as __namedExportsOrder,_ as default};
