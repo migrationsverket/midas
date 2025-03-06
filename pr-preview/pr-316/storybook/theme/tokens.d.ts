218240542cca9e3044bc11821e5caf7d55ccbea2
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-03-06 07:16:56 +0000

    Deploy preview for PR 316 🛫

---
+++ pr-preview/pr-316/storybook/theme/tokens.d.ts
@@ -0,0 +1,135 @@
+export declare const baseColors: {
+    white: string;
+    black: string;
+    gray10: string;
+    gray20: string;
+    gray30: string;
+    gray40: string;
+    gray50: string;
+    gray60: string;
+    gray70: string;
+    gray80: string;
+    gray90: string;
+    gray100: string;
+    gray130: string;
+    gray150: string;
+    red10: string;
+    red20: string;
+    red30: string;
+    red40: string;
+    red50: string;
+    red60: string;
+    red70: string;
+    red80: string;
+    red90: string;
+    red100: string;
+    red130: string;
+    red150: string;
+    blue10: string;
+    blue20: string;
+    blue30: string;
+    blue40: string;
+    blue50: string;
+    blue60: string;
+    blue70: string;
+    blue80: string;
+    blue90: string;
+    blue100: string;
+    blue130: string;
+    blue150: string;
+};
+export declare const buttonColors: {
+    outline: string;
+    primaryBackground: string;
+    primaryText: string;
+    primaryHover: string;
+    primaryFocus: string;
+    primaryActive: string;
+    secondaryBackground: string;
+    secondaryText: string;
+    secondaryHover: string;
+    secondaryActive: string;
+};
+export declare const inputColor: {
+    inputText: string;
+};
+export declare const signalColors: {
+    signalGreen10: string;
+    signalGreen100: string;
+    signalBlue10: string;
+    signalBlue100: string;
+    signalYellow10: string;
+    signalYellow100: string;
+    signalRed10: string;
+    signalRed100: string;
+    signalRed130: string;
+    signalRed150: string;
+};
+export declare const borderRadius: {
+    sRadius: string;
+    mRadius: string;
+};
+export declare const borderWidths: {
+    xsWidth: string;
+    sWidth: string;
+    mWidth: string;
+};
+export declare const fontWeights: {
+    lightWeight: string;
+    regularWeight: string;
+    mediumWeight: string;
+    semiBoldWeight: string;
+    boldWeight: string;
+    blackWeight: string;
+};
+export declare const fontSizes: {
+    desktopSize: string;
+    mobileSize: string;
+    largeSize: string;
+    inputNormalSize: string;
+    inputMessageSize: string;
+    inputLabelSize: string;
+    inputDescriptionSize: string;
+};
+export declare const midasColors: {
+    logoPrimary: string;
+    logoSecondary: string;
+    textPrimary: string;
+    textSecondary: string;
+    textTertiary: string;
+    link: string;
+    linkVisited: string;
+    backgroundPrimary: string;
+    backgroundSecondary: string;
+    backgroundFooter1: string;
+    backgroundFooter2: string;
+    backgroundInfo: string;
+    backgroundImportant: string;
+    borderPrimary: string;
+    borderSecondary: string;
+    borderFocusOuter: string;
+    borderFocusInner: string;
+    iconPrimary: string;
+    iconInverted: string;
+};
+export declare const midasTransitions: {
+    slowTransition: string;
+    normalTransition: string;
+    fastTransition: string;
+};
+export declare const breakpoints: {
+    smBreakpoint: string;
+    mdBreakpoint: string;
+    lgBreakpoint: string;
+    xlBreakpoint: string;
+};
+export declare const accessibility: {
+    forcedColorsMode: string;
+    prefersReducedMotion: string;
+};
+export declare const font: {
+    display: string;
+};
+export declare const boxShadow: {
+    focus: string;
+};
