1ca06d840fd610266bf29bbd7193c11592711259
derpbravely <derpbravely@users.noreply.github.com>
2025-03-06 13:38:41 +0000

    Deploy preview for PR 293 🛫

---
+++ pr-preview/pr-293/storybook/assets/DatePicker.stories-CX5FjJ_C.js
@@ -0,0 +1,63 @@
+import{j as a}from"./jsx-runtime-DtgEGaVE.js";import{D as m}from"./DatePicker-CNDqABi6.js";import{u as t,e as D}from"./index-Bn1z2x26.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button-DlxIrWB_.js";import"./useFocusable-DTLHoeQe.js";import"./clsx-B-dksMZM.js";import"./Hidden-D9yAbrAR.js";import"./index-cpIEhwLo.js";import"./RSPContexts-CeCCo7XS.js";import"./Text-BorP-jFP.js";import"./context-DVpahnpO.js";import"./useControlledState-DGyHFLi9.js";import"./useLocalizedStringFormatter-BfPjM0Dg.js";import"./useLabels-Dt0KIlgT.js";import"./useUpdateEffect-DShQiA8p.js";import"./useEvent-B_cSPOEW.js";import"./ListKeyboardDelegate-dzYvetd0.js";import"./SelectionManager-Z9gqeiha.js";import"./FocusScope-DWsZ07QH.js";import"./useDescription-B0GcPv_I.js";import"./VisuallyHidden-3bHjY8GZ.js";import"./Group-BntAPbZu.js";import"./TextField-B1qUqIPy.js";import"./Form-Cq4QZx_r.js";import"./useFormValidation-D_zB_ZT8.js";import"./Label-B23VYN9V.js";import"./TextField.module-BSzw7ps9.js";import"./useField-lqkOjd2l.js";import"./Button-B61clMab.js";import"./useFilter-C6nYXwzR.js";import"./Dialog-DJFaj5QY.js";import"./OverlayArrow-B7QTCHgm.js";import"./Collection-B65vjQb4.js";import"./index-DwWe6W7f.js";import"./Separator-CFSVS3UH.js";import"./PressResponder-DogE8OEY.js";import"./Heading-CkRqaEjL.js";import"./createLucideIcon-8wDOqnHv.js";import"./chevron-right-CjCwHShF.js";const ut={component:m,title:"Components/DatePicker",tags:["autodocs"],args:{label:"Välj datum",description:"YYYY-MM-DD"},parameters:{a11y:{test:"todo"}}},r={},s={args:{label:"Välj datum och tid",description:"YYYY-MM-DD HH-MM-SS",granularity:"second"}},o={args:{isRequired:!0,errorMessage:"Var god ange ett datum"},render:e=>a.jsxs("form",{children:[a.jsx(m,{...e}),a.jsx("button",{type:"submit",children:"Submit"})]}),play:async({canvas:e,step:n,args:{errorMessage:x}})=>{await n("it should show an error message if submitted empty",async()=>{await t.tab(),await t.tab(),await t.tab(),await t.tab(),await t.tab(),await t.keyboard("[Enter]"),D(e.getByText(x)).toBeInTheDocument()})}},i={args:{isRequired:!0,validate:({year:e})=>e===new Date().getFullYear()?"Var god välj ett annat år":!0},render:e=>a.jsxs("form",{children:[a.jsx(m,{...e}),a.jsx("button",{type:"submit",children:"Submit"})]}),play:async({canvas:e,step:n})=>{await n("it should show a custom error message if invalid",async()=>{await t.tab(),await t.tab(),await t.tab(),await t.tab(),await t.keyboard("[Enter]"),await t.keyboard("[Enter]"),await t.tab(),D(e.getByText("Var god välj ett annat år")).toBeInTheDocument()})}};var p,u,c;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(c=(u=r.parameters)==null?void 0:u.docs)==null?void 0:c.source}}};var d,l,b;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    label: 'Välj datum och tid',
+    description: 'YYYY-MM-DD HH-MM-SS',
+    granularity: 'second'
+  }
+}`,...(b=(l=s.parameters)==null?void 0:l.docs)==null?void 0:b.source}}};var g,w,y;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    isRequired: true,
+    errorMessage: 'Var god ange ett datum'
+  },
+  render: args => <form>
+      <DatePicker {...args} />
+      <button type='submit'>Submit</button>
+    </form>,
+  play: async ({
+    canvas,
+    step,
+    args: {
+      errorMessage
+    }
+  }) => {
+    await step('it should show an error message if submitted empty', async () => {
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.keyboard('[Enter]');
+      expect(canvas.getByText(errorMessage as string)).toBeInTheDocument();
+    });
+  }
+}`,...(y=(w=o.parameters)==null?void 0:w.docs)==null?void 0:y.source}}};var v,h,E;i.parameters={...i.parameters,docs:{...(v=i.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    isRequired: true,
+    validate: ({
+      year
+    }) => year === new Date().getFullYear() ? 'Var god välj ett annat år' : true
+  },
+  render: args => <form>
+      <DatePicker {...args} />
+      <button type='submit'>Submit</button>
+    </form>,
+  play: async ({
+    canvas,
+    step
+  }) => {
+    /**
+     * This test is made to create an invalid DatePicker
+     * the current year is not allowed the "user" selects todays date and submits the form
+     */
+    await step('it should show a custom error message if invalid', async () => {
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.keyboard('[Enter]');
+      await userEvent.keyboard('[Enter]');
+      await userEvent.tab();
+      expect(canvas.getByText('Var god välj ett annat år')).toBeInTheDocument();
+    });
+  }
+}`,...(E=(h=i.parameters)==null?void 0:h.docs)==null?void 0:E.source}}};const ct=["Primary","WithTime","Required","CustomValiation"];export{i as CustomValiation,r as Primary,o as Required,s as WithTime,ct as __namedExportsOrder,ut as default};
---
+++ pr-preview/pr-293/storybook/assets/Radio.stories-BK-CtyJL.js
@@ -0,0 +1,81 @@
+import{j as a}from"./jsx-runtime-DtgEGaVE.js";import{R as g,a as u,s as M}from"./Radio-C7IKtn8J.js";import{e as m,u as r}from"./index-Bn1z2x26.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Group-BntAPbZu.js";import"./useFocusable-DTLHoeQe.js";import"./clsx-B-dksMZM.js";import"./Form-Cq4QZx_r.js";import"./Text-BorP-jFP.js";import"./useFormValidation-D_zB_ZT8.js";import"./Label-B23VYN9V.js";import"./Hidden-D9yAbrAR.js";import"./index-cpIEhwLo.js";import"./useControlledState-DGyHFLi9.js";import"./FocusScope-DWsZ07QH.js";import"./useField-lqkOjd2l.js";import"./useLabels-Dt0KIlgT.js";import"./context-DVpahnpO.js";import"./VisuallyHidden-3bHjY8GZ.js";import"./TextField-B1qUqIPy.js";import"./TextField.module-BSzw7ps9.js";import"./Button-B61clMab.js";import"./Button-DlxIrWB_.js";const oe={title:"Components/Radio",component:g,tags:["autodocs"],args:{label:"Group label",errorMessage:"Du måste välja en frukt?"},argTypes:{children:{table:{disable:!0}},label:{type:"string",control:{type:"text"}},isDisabled:{type:"boolean",options:[!1,!0],control:{type:"radio"}},isInvalid:{type:"boolean",options:[!1,!0],control:{type:"radio"}}}},C=["Äpple","Banan","Kiwi","Apelsin"],t=C.map(e=>a.jsx(u,{value:e,id:e.toLowerCase(),children:e},e)),F=[a.jsx(u,{value:"banan",children:"Banan"},"radio-banan"),a.jsx(u,{value:"apelsin",isDisabled:!0,children:"Apelsin"},"radio-apelsin"),a.jsx(u,{value:"kiwi",children:"Kiwi"},"radio-kiwi")],i={args:{label:"Frukt",description:"Välj en frukt",children:t,className:"test-class"},play:async({canvas:e,step:s,args:n})=>{await s("it should preserve its classNames when being passed new ones",async()=>{const V=e.getByLabelText(n.label);m(V).toHaveClass(M.radioGroup,n.className)})}},o={args:{children:t,isDisabled:!0}},l={args:{children:F}},d={args:{children:t,isInvalid:!0,errorMessage:"Det här stämmer inte!"}},c={args:{label:"Frukt",description:"Välj en frukt",children:t,isRequired:!0},render:e=>a.jsxs("form",{children:[a.jsx(g,{...e}),a.jsx("button",{type:"submit",children:"Submit"})]}),play:async({canvas:e,step:s,args:n})=>{await s("should show a validation error message if the user submitted without selecting anything",async()=>{await r.tab(),await r.tab(),await r.keyboard("[Enter]"),m(e.getByRole("radiogroup")).toBeInvalid(),m(e.getByText(n.errorMessage)).toBeInTheDocument()})}},p={args:{label:"Frukt",description:"Välj en frukt",children:t,errorMessage:void 0,validate:e=>e!=null&&e.includes("Äpple")?"Inga äpplen är tillåtna":!0},render:e=>a.jsxs("form",{children:[a.jsx(g,{...e}),a.jsx("button",{type:"submit",children:"Submit"})]}),play:async({canvas:e,step:s})=>{await s("it should show the custom error message if the constraints was not satisfied",async()=>{await r.tab(),await r.keyboard("[Enter]"),await r.tab(),await r.keyboard("[Enter]"),m(e.getByText("Inga äpplen är tillåtna")).toBeInTheDocument()})}};var b,h,y;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    label: 'Frukt',
+    description: 'Välj en frukt',
+    children: items,
+    className: 'test-class'
+  },
+  play: async ({
+    canvas,
+    step,
+    args
+  }) => {
+    await step('it should preserve its classNames when being passed new ones', async () => {
+      const radioGroup = canvas.getByLabelText(args.label as string);
+      expect(radioGroup).toHaveClass(styles.radioGroup, args.className as string);
+    });
+  }
+}`,...(y=(h=i.parameters)==null?void 0:h.docs)==null?void 0:y.source}}};var w,v,x;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    children: items,
+    isDisabled: true
+  }
+}`,...(x=(v=o.parameters)==null?void 0:v.docs)==null?void 0:x.source}}};var f,k,j;l.parameters={...l.parameters,docs:{...(f=l.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    children: radioItemsOneDisabled
+  }
+}`,...(j=(k=l.parameters)==null?void 0:k.docs)==null?void 0:j.source}}};var I,D,B;d.parameters={...d.parameters,docs:{...(I=d.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    children: items,
+    isInvalid: true,
+    errorMessage: 'Det här stämmer inte!'
+  }
+}`,...(B=(D=d.parameters)==null?void 0:D.docs)==null?void 0:B.source}}};var E,R,T;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    label: 'Frukt',
+    description: 'Välj en frukt',
+    children: items,
+    isRequired: true
+  },
+  render: args => <form>
+      <RadioGroup {...args} />
+      <button type='submit'>Submit</button>
+    </form>,
+  play: async ({
+    canvas,
+    step,
+    args
+  }) => {
+    await step('should show a validation error message if the user submitted without selecting anything', async () => {
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.keyboard('[Enter]');
+      expect(canvas.getByRole('radiogroup')).toBeInvalid();
+      expect(canvas.getByText(args.errorMessage as string)).toBeInTheDocument();
+    });
+  }
+}`,...(T=(R=c.parameters)==null?void 0:R.docs)==null?void 0:T.source}}};var S,G,N;p.parameters={...p.parameters,docs:{...(S=p.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    label: 'Frukt',
+    description: 'Välj en frukt',
+    children: items,
+    errorMessage: undefined,
+    validate: value => value?.includes('Äpple') ? 'Inga äpplen är tillåtna' : true
+  },
+  render: args => <form>
+      <RadioGroup {...args} />
+      <button type='submit'>Submit</button>
+    </form>,
+  play: async ({
+    canvas,
+    step
+  }) => {
+    await step('it should show the custom error message if the constraints was not satisfied', async () => {
+      await userEvent.tab();
+      await userEvent.keyboard('[Enter]');
+      await userEvent.tab();
+      await userEvent.keyboard('[Enter]');
+      expect(canvas.getByText('Inga äpplen är tillåtna')).toBeInTheDocument();
+    });
+  }
+}`,...(N=(G=p.parameters)==null?void 0:G.docs)==null?void 0:N.source}}};const le=["Normal","Disabled","OneItemDisabled","Invalid","Required","CustomValidation"];export{p as CustomValidation,o as Disabled,d as Invalid,i as Normal,l as OneItemDisabled,c as Required,le as __namedExportsOrder,oe as default};
