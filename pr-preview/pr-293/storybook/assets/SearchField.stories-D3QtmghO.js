0edd64fcf5925545018a0301352f126424f441bf
derpbravely <derpbravely@users.noreply.github.com>
2025-03-06 09:06:11 +0000

    Deploy preview for PR 293 🛫

---
+++ pr-preview/pr-293/storybook/assets/SearchField.stories-D3QtmghO.js
@@ -0,0 +1,16 @@
+import{S as g}from"./SearchField-B7u03TQz.js";import"./jsx-runtime-DtgEGaVE.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./TextField.module-BSzw7ps9.js";import"./useFocusable-DTLHoeQe.js";import"./clsx-B-dksMZM.js";import"./useFormValidation-D_zB_ZT8.js";import"./useControlledState-DGyHFLi9.js";import"./useField-lqkOjd2l.js";import"./useLabels-Dt0KIlgT.js";import"./useLocalizedStringFormatter-BfPjM0Dg.js";import"./context-DVpahnpO.js";import"./createLucideIcon-8wDOqnHv.js";import"./Button-BqibR3Mp.js";import"./Button-DlxIrWB_.js";import"./Hidden-D9yAbrAR.js";import"./index-cpIEhwLo.js";import"./x-Cu6J0Y2u.js";const j={component:g,title:"Components/SearchField",tags:["autodocs"],parameters:{layout:"centered"}},e={args:{placeholder:"Sök efter en person"}},r={args:{placeholder:'Sök efter "secret"',validate:l=>l==="secret"?"Sök inte efter hemligheter":!0}},t={args:{placeholder:"Sök efter dokument",isInvalid:!0,errorMessage:"Något gick fel, var god försök igen"}};var o,a,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Sök efter en person'
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var i,m,p;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Sök efter "secret"',
+    validate: (value: string) => value === 'secret' ? 'Sök inte efter hemligheter' : true
+  }
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var n,c,d;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Sök efter dokument',
+    isInvalid: true,
+    errorMessage: 'Något gick fel, var god försök igen'
+  }
+}`,...(d=(c=t.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const q=["Primary","CustomValidation","Invalid"];export{r as CustomValidation,t as Invalid,e as Primary,q as __namedExportsOrder,j as default};
