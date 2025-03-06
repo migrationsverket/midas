f0425ce30aa8aa3d7ff190149f0d13e7cb86540d
pixelrickdreamer <jakob.sanne-grubbstrom@migrationsverket.se>
2025-03-06 21:32:30 +0100

    feat(date-field): add datefield component

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -1,7 +1,6 @@
 import styles from './app.module.css'
 import {
-  Calendar,
-  DatePicker,
+  Calendar, DateField, DatePicker
 } from '@midas-ds/components'
 import React from 'react'
 
@@ -9,7 +8,12 @@ export function App() {
   const [date, setDate] = React.useState(null)
   return (
     <div className={styles.container}>
+      <DateField label={'label'} description={'description'} errorMessage={'hello'} onChange={setDate} value={date}/>
+      <DatePicker label={'label'} description={'description'}></DatePicker>
+      <DateField label={'label'} description={'description'} errorMessage={'hello'} isInvalid/>
+      <DatePicker label={'label'} description={'description'} errorMessage={'hello'} isInvalid></DatePicker>
       <Calendar
+        value={date}
         onChange={x => {
           setDate(x)
           console.log('change', x)
---
+++ packages/components/src/date-field/DateField.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {
+  DateField as AriaDateField,
+  DateFieldProps,
+  DateInput,
+  DateSegment,
+  DateValue,
+  Label,
+  ValidationResult
+} from 'react-aria-components'
+import {FieldError, Text} from 'react-aria-components';
+import styles from './DateField.module.css'
+import inputStyles from '../textfield/TextField.module.css'
+import { InputWrapper } from '../textfield'
+
+interface MyDateFieldProps<T extends DateValue> extends DateFieldProps<T> {
+  label?: string;
+  description?: string;
+  errorMessage?: string | ((validation: ValidationResult) => string);
+}
+
+export function DateField<T extends DateValue>  (
+  { label, description, errorMessage, ...props }: MyDateFieldProps<T>
+) {
+  return (
+    <AriaDateField {...props} className={styles.dateField}>
+      <Label className={styles.label}>{label}</Label>
+      {description && <Text slot="description" className={styles.description}>{description}</Text>}
+      <FieldError className={styles.fieldError}>{errorMessage}</FieldError>
+      <DateInput className={styles.inputField}>
+        {(segment) => <DateSegment segment={segment} className={styles.segment}/>}
+      </DateInput>
+    </AriaDateField>
+  );
+}
