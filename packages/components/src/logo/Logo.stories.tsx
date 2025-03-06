8b31101d6c4ced4e0d794e59a0cae9288a797dbd
derpbravely <axel.engstrom@migrationsverket.se>
2025-03-06 13:03:14 +0100

    test(logo): migrate to storybook play

--- packages/components/src/logo/Logo.stories.tsx
+++ packages/components/src/logo/Logo.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Logo } from './Logo'
+import { expect, within } from '@storybook/test'
 
 const meta: Meta<typeof Logo> = {
   component: Logo,
@@ -8,40 +9,52 @@ const meta: Meta<typeof Logo> = {
   argTypes: {
     size: {
       control: { type: 'radio' },
-      options: ['x-small', 'small', 'medium', 'large']
+      options: ['x-small', 'small', 'medium', 'large'],
     },
     background: {
-      control: { type: 'boolean' }
+      control: { type: 'boolean' },
     },
     primary: {
-      control: { type: 'boolean' }
+      control: { type: 'boolean' },
     },
     padding: {
-      control: { type: 'boolean' }
-    }
-  }
+      control: { type: 'boolean' },
+    },
+  },
 }
 export default meta
 
 type Story = StoryObj<typeof Logo>
 
-export const Primary: Story = {}
+export const Primary: Story = {
+  args: {
+    className: 'test-class',
+    // @ts-expect-error dont recognize this property
+    'data-testid': 'test',
+  },
+  play: async ({ canvasElement, args }) => {
+    const canvas = within(canvasElement)
+    // @ts-expect-error dont recognize this property
+    const logotype = canvas.getByTestId(args['data-testid'])
+    expect(logotype).toHaveClass(/container/, args.className as string)
+  },
+}
 
 export const Dark: Story = {
   args: {
-    primary: false
-  }
+    primary: false,
+  },
 }
 
 export const BackgroundPrimary: Story = {
   args: {
-    background: true
-  }
+    background: true,
+  },
 }
 
 export const BackgroundDark: Story = {
   args: {
     primary: false,
-    background: true
-  }
+    background: true,
+  },
 }
