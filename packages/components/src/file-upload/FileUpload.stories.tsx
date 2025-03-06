18d24739e587e96b002bf5399527401244c567ef
derpbravely <axel.engstrom@migrationsverket.se>
2025-03-06 10:02:48 +0100

    test(file-upload): migrate to storybook play

--- packages/components/src/file-upload/FileUpload.stories.tsx
+++ packages/components/src/file-upload/FileUpload.stories.tsx
@@ -1,19 +1,42 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { FileUpload } from './FileUpload'
+import { expect, fn, userEvent, within } from '@storybook/test'
 
 const meta: Meta<typeof FileUpload> = {
   component: FileUpload,
   title: 'Components/FileUpload',
   tags: ['autodocs'],
   argTypes: {},
-}
-export default meta
-type Story = StoryObj<typeof FileUpload>
-
-export const Primary: Story = {
   args: {
     label: 'Label',
     description: 'Description',
     allowsMultiple: true,
+  },
+}
+export default meta
+type Story = StoryObj<typeof FileUpload>
+
+export const Primary: Story = {}
+
+export const CustomSelectHandler: Story = {
+  args: {
+    // @ts-expect-error dont recognize this property
+    'data-testid': 'test',
+    onSelect: fn(),
+  },
+  play: async ({ canvasElement, step, args }) => {
+    const canvas = within(canvasElement)
+    step('it should use the provided onSelect callback', async () => {
+      const testFile = new File(['derp'], 'derp.png', { type: 'image/png' })
+      const fileUpload: HTMLInputElement = canvas.getByTestId(
+        // @ts-expect-error dont recognize this property
+        args['data-testid'] as string,
+      )
+      await userEvent.upload(fileUpload, testFile)
+      expect(args.onSelect).toHaveBeenCalledTimes(1)
+      expect(fileUpload?.files?.[0]).toStrictEqual(testFile)
+      expect(fileUpload?.files?.item(0)).toStrictEqual(testFile)
+      expect(fileUpload.files).toHaveLength(1)
+    })
   },
 }
