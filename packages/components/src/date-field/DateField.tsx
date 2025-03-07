1e1d1ec846e9a31380c74a3465f8bdfddeeefdca
pixelrickdreamer <jakob.sanne-grubbstrom@migrationsverket.se>
2025-03-07 08:26:53 +0100

    feat(date-field): add styles for invalid state

--- packages/components/src/date-field/DateField.tsx
+++ packages/components/src/date-field/DateField.tsx
@@ -10,8 +10,8 @@ import {
 } from 'react-aria-components'
 import {FieldError, Text} from 'react-aria-components';
 import styles from './DateField.module.css'
-import inputStyles from '../textfield/TextField.module.css'
-import { InputWrapper } from '../textfield'
+import clsx from 'clsx'
+
 
 interface MyDateFieldProps<T extends DateValue> extends DateFieldProps<T> {
   label?: string;
@@ -23,7 +23,7 @@ export function DateField<T extends DateValue>  (
   { label, description, errorMessage, ...props }: MyDateFieldProps<T>
 ) {
   return (
-    <AriaDateField {...props} className={styles.dateField}>
+    <AriaDateField {...props} className={clsx(styles.dateField, props.isDisabled && styles.isDisabled)}>
       <Label className={styles.label}>{label}</Label>
       {description && <Text slot="description" className={styles.description}>{description}</Text>}
       <FieldError className={styles.fieldError}>{errorMessage}</FieldError>
