350594fb7ed931b86ff7e52a126ee5727ef20e30
pixelrickdreamer <jakob.sanne-grubbstrom@migrationsverket.se>
2025-03-07 22:07:44 +0100

    feat(calendar): style `today`, remove parent flex

--- packages/components/src/date-field/DateField.stories.tsx
+++ packages/components/src/date-field/DateField.stories.tsx
@@ -1,40 +1,41 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { DateField } from './DateField'
-import {CalendarDate} from '@internationalized/date';
+import { CalendarDate } from '@internationalized/date'
 
 const meta: Meta<typeof DateField> = {
   component: DateField,
   title: 'Components/DateField',
-  tags: ['autodocs']
+  tags: ['autodocs'],
 }
 export default meta
 type Story = StoryObj<typeof DateField>
 
+/** Don't put format in description, it changes with browser language settings! */
 export const Default: Story = {
   args: {
     label: 'Välj datum',
-    description: 'YYYY-MM-DD'
-  }
+    description: 'vilket som helst',
+  },
 }
 
 export const Invalid: Story = {
   args: {
     ...Default.args,
     isInvalid: true,
-    errorMessage: 'Date must be tjugoniende maj'
-  }
+    errorMessage: 'Date must be tjugoniende maj',
+  },
 }
 export const Disabled: Story = {
   args: {
     ...Default.args,
-    isDisabled: true
-  }
+    isDisabled: true,
+  },
 }
 
 /** When using uncontrolled value */
 export const WithDefaultValue: Story = {
   args: {
     ...Default.args,
-    defaultValue: new CalendarDate(1999,5,29),
+    defaultValue: new CalendarDate(1999, 5, 29),
   },
 }
