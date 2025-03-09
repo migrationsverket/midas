851aa1c4288ceca96e07f4d2f7bd1b0589e1d778
pixelrickdreamer <jakob.sanne-grubbstrom@migrationsverket.se>
2025-03-09 20:24:50 +0100

    docs(datefield): add boilerplate docs

--- packages/components/src/date-field/DateField.tsx
+++ packages/components/src/date-field/DateField.tsx
@@ -1,35 +1,52 @@
-import * as React from 'react';
+import * as React from 'react'
 import {
   DateField as AriaDateField,
-  DateFieldProps,
+  DateFieldProps as AriaDateFieldProps,
   DateInput,
   DateSegment,
   DateValue,
   Label,
-  ValidationResult
+  ValidationResult,
 } from 'react-aria-components'
-import {FieldError, Text} from 'react-aria-components';
+import { FieldError, Text } from 'react-aria-components'
 import styles from './DateField.module.css'
 import clsx from 'clsx'
 
-
-interface MyDateFieldProps<T extends DateValue> extends DateFieldProps<T> {
-  label?: string;
-  description?: string;
-  errorMessage?: string | ((validation: ValidationResult) => string);
+interface DateFieldProps<T extends DateValue> extends AriaDateFieldProps<T> {
+  label?: string
+  description?: string
+  errorMessage?: string | ((validation: ValidationResult) => string)
 }
 
-export function DateField<T extends DateValue>  (
-  { label, description, errorMessage, ...props }: MyDateFieldProps<T>
-) {
+export function DateField<T extends DateValue>({
+  label,
+  description,
+  errorMessage,
+  ...props
+}: DateFieldProps<T>) {
   return (
-    <AriaDateField {...props} className={clsx(styles.dateField, props.isDisabled && styles.isDisabled)}>
+    <AriaDateField
+      {...props}
+      className={clsx(styles.dateField, props.isDisabled && styles.isDisabled)}
+    >
       <Label className={styles.label}>{label}</Label>
-      {description && <Text slot="description" className={styles.description}>{description}</Text>}
+      {description && (
+        <Text
+          slot='description'
+          className={styles.description}
+        >
+          {description}
+        </Text>
+      )}
       <FieldError className={styles.fieldError}>{errorMessage}</FieldError>
       <DateInput className={styles.inputField}>
-        {(segment) => <DateSegment segment={segment} className={styles.segment}/>}
+        {segment => (
+          <DateSegment
+            segment={segment}
+            className={styles.segment}
+          />
+        )}
       </DateInput>
     </AriaDateField>
-  );
+  )
 }
