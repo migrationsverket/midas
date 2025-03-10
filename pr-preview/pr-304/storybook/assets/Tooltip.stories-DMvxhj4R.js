66e62edbd51966cd54778fb8d6ef5463881ab99b
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-03-10 16:01:14 +0000

    Deploy preview for PR 304 🛫

---
+++ pr-preview/pr-304/storybook/assets/Tooltip.stories-DMvxhj4R.js
@@ -0,0 +1,14 @@
+import{j as r}from"./jsx-runtime-DtgEGaVE.js";import{T as i,a as p}from"./Tooltip-BkKX1GDr.js";import{B as s}from"./Button-DNaqWoGO.js";import{c as m}from"./createLucideIcon-8wDOqnHv.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./OverlayArrow-JJIn2pKb.js";import"./useFocusRing-Dvg3g2cU.js";import"./index-cpIEhwLo.js";import"./useFocusable-C9L8Uaaf.js";import"./useOverlayTriggerState-yt_HxB-E.js";import"./context-CuBBIohG.js";import"./useControlledState-DGyHFLi9.js";import"./useModal-Duhq1_Z8.js";import"./Button-BvljoDys.js";import"./LiveAnnouncer-DFDjXwI8.js";import"./Hidden-ClhOTPkO.js";/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=m("Save",[["path",{d:"M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z",key:"1c8476"}],["path",{d:"M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7",key:"1ydtos"}],["path",{d:"M7 3v4a1 1 0 0 0 1 1h7",key:"t51u73"}]]),H={component:i,title:"Components/Tooltip",tags:["autodocs"],argTypes:{}},t={args:{},render:()=>r.jsxs(p,{children:[r.jsx(s,{variant:"tertiary","aria-label":"Spara",children:r.jsx(n,{})}),r.jsx(i,{children:"Spara"})]})};var a,o,e;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {},
+  render: () => <TooltipTrigger>
+      <Button variant='tertiary' aria-label='Spara'>
+        <Save />
+      </Button>
+      <Tooltip>Spara</Tooltip>
+    </TooltipTrigger>
+}`,...(e=(o=t.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const P=["Primary"];export{t as Primary,P as __namedExportsOrder,H as default};
