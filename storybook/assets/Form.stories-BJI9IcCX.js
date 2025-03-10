c9efe29413b89b02552977028b31783f19f789cc
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-03-10 10:50:29 +0000

    Deploying to gh-pages from @ migrationsverket/midas@937cef7bf1d9374e4e6c0e91f8aa50b90ee9cb64 🚀

---
+++ storybook/assets/Form.stories-BJI9IcCX.js
@@ -0,0 +1,73 @@
+import{j as e}from"./jsx-runtime-DtgEGaVE.js";import{$ as j}from"./Form-BB8RM6iI.js";import{F}from"./Flex-D3szVBzO.js";import{F as n}from"./FlexItem-DsmKoLE5.js";import{T as o}from"./TextField-BmcCytUO.js";import{S as I}from"./Select-C8nCHYTH.js";import{B as a}from"./Button-BLHi0EwO.js";import{R as g,a as b}from"./Radio-ATukMafs.js";import{C as h}from"./CheckboxGroup-BPQI-e4x.js";import{C as k}from"./Checkbox-DnJP8IXB.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./useFocusRing-Dvg3g2cU.js";import"./clsx-B-dksMZM.js";import"./Text-DGilHHfL.js";import"./useFormValidation-c4o29Mgv.js";import"./Input-BZZpXOBL.js";import"./Hidden-ClhOTPkO.js";import"./index-cpIEhwLo.js";import"./useField-CBiuCiGl.js";import"./useLabels-BjpOYTdf.js";import"./TextField-U0IlVfQC.js";import"./useTextField-5XYY7WPm.js";import"./useControlledState-DGyHFLi9.js";import"./useFocusable-C9L8Uaaf.js";import"./TextField.module-RvTXLpy1.js";import"./Button-BvljoDys.js";import"./LiveAnnouncer-DFDjXwI8.js";import"./Tag-CI7DqmJp.js";import"./Collection-Cb_ranKc.js";import"./index-DwWe6W7f.js";import"./ListBox-VsRKlvF2.js";import"./DragAndDrop-sISFPnlb.js";import"./Separator-BxUipfud.js";import"./SelectionManager-DgK6Za11.js";import"./useEvent-BYWgjDNQ.js";import"./scrollIntoView-DzftQ5Hl.js";import"./FocusScope-D7AO3NHB.js";import"./context-CuBBIohG.js";import"./useDescription-CNBqbECa.js";import"./ListKeyboardDelegate-DmPpmGaI.js";import"./useCollator-Brx0Pzyc.js";import"./useListState-rEYBvldw.js";import"./useHighlightSelectionDescription-BjkPEEe8.js";import"./useLocalizedStringFormatter-a7gPYq3X.js";import"./useUpdateEffect-DShQiA8p.js";import"./useHasTabbableChild-DpCQfc91.js";import"./getScrollParent-CrrBdd7g.js";import"./x-Cu6J0Y2u.js";import"./createLucideIcon-8wDOqnHv.js";import"./useOverlayTriggerState-yt_HxB-E.js";import"./DismissButton-0vAQDsZr.js";import"./VisuallyHidden-D33sH8jm.js";import"./useModal-Duhq1_Z8.js";import"./useMenuTrigger-DRtks4Q9.js";import"./useMenuTriggerState-9DxRY-En.js";import"./chevron-down-U1dNK9nR.js";import"./Group-BIBGjc5-.js";import"./RSPContexts-CeCCo7XS.js";const ve={component:j,title:"Examples/Form",tags:["autodocs"],argTypes:{}},l={args:{},render:()=>{const r=["Banan","Apple","Mango"];return e.jsxs(F,{children:[e.jsx(n,{col:12,children:e.jsx(o,{label:"Ange ditt fullständiga namn",description:"Glöm inte dina eventuella mellannamn!"})}),e.jsx(n,{col:12,children:e.jsx(o,{label:"Personnummmer",description:"Anges på formen ÅÅMMDD-XXXX"})}),e.jsx(n,{col:12,children:e.jsx(I,{label:"Vilken är din favoritfrukt",placeholder:"Välj en frukt",selectionMode:"single",options:r.map(i=>({id:i,name:i}))})}),e.jsx(n,{children:e.jsx(a,{children:"Skicka"})})]})}},m={args:{},render:()=>{const r=["A","B","C"],i=["Anledning A","Anledning B","Anledning C"];return e.jsx("div",{children:e.jsxs(F,{children:[e.jsx(n,{col:6,children:e.jsx(g,{label:"Anledning",description:"Ange anledning till att du söker i databasen",children:i.map(t=>e.jsx(b,{value:t,children:t},t))})}),e.jsx(n,{col:6,children:e.jsx(h,{label:"Databas",description:"Välj databas att söka i",children:r.map(t=>e.jsx(k,{value:t,children:t},t))})}),e.jsx(n,{col:6,children:e.jsx(o,{label:"Namn",description:""})}),e.jsx(n,{col:6,children:e.jsx(o,{label:"Personnummer"})}),e.jsx(n,{col:6,children:e.jsx(I,{label:"Kön",description:"Kan lämnas tomt",placeholder:"Välj kön",selectionMode:"single",options:r.map(t=>({name:t,id:t}))})}),e.jsx(n,{col:6,children:e.jsx(o,{label:"Ärendekod"})}),e.jsx(n,{col:"auto",children:e.jsx(a,{children:"Sök"})}),e.jsx(n,{col:"auto",children:e.jsx(a,{variant:"secondary",children:"Rensa"})})]})})}};var s,p,d;l.parameters={...l.parameters,docs:{...(s=l.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {},
+  render: () => {
+    const ITEMS = ['Banan', 'Apple', 'Mango'];
+    return <Flex>
+        <FlexItem col={12}>
+          <TextField label='Ange ditt fullständiga namn' description='Glöm inte dina eventuella mellannamn!' />
+        </FlexItem>
+        <FlexItem col={12}>
+          <TextField label='Personnummmer' description='Anges på formen ÅÅMMDD-XXXX' />
+        </FlexItem>
+        <FlexItem col={12}>
+          <Select label='Vilken är din favoritfrukt' placeholder='Välj en frukt' selectionMode={'single'} options={ITEMS.map(i => ({
+          id: i,
+          name: i
+        }))}>
+          </Select>
+        </FlexItem>
+
+        <FlexItem>
+          <Button>Skicka</Button>
+        </FlexItem>
+      </Flex>;
+  }
+}`,...(d=(p=l.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var c,x,u;m.parameters={...m.parameters,docs:{...(c=m.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {},
+  render: () => {
+    const ITEMS = ['A', 'B', 'C'];
+    const RADIOITEMS = ['Anledning A', 'Anledning B', 'Anledning C'];
+    const SEX = ['Man', 'Kvinna', 'Annat'];
+    return <div>
+        <Flex>
+          <FlexItem col={6}>
+            <RadioGroup label='Anledning' description='Ange anledning till att du söker i databasen'>
+              {RADIOITEMS.map((item: string) => <Radio value={item} key={item}>
+                  {item}
+                </Radio>)}
+            </RadioGroup>
+          </FlexItem>
+          <FlexItem col={6}>
+            <CheckboxGroup label='Databas' description='Välj databas att söka i'>
+              {ITEMS.map((item: string) => <Checkbox value={item} key={item}>
+                  {item}
+                </Checkbox>)}
+            </CheckboxGroup>
+          </FlexItem>
+          <FlexItem col={6}>
+            <TextField label='Namn' description='' />
+          </FlexItem>
+          <FlexItem col={6}>
+            <TextField label='Personnummer' />
+          </FlexItem>
+
+          <FlexItem col={6}>
+            <Select label='Kön' description='Kan lämnas tomt' placeholder='Välj kön' selectionMode={'single'} options={ITEMS.map(i => ({
+            name: i,
+            id: i
+          }))} />
+          </FlexItem>
+          <FlexItem col={6}>
+            <TextField label='Ärendekod' />
+          </FlexItem>
+
+          <FlexItem col={'auto'}>
+            <Button>Sök</Button>
+          </FlexItem>
+          <FlexItem col={'auto'}>
+            <Button variant='secondary'>Rensa</Button>
+          </FlexItem>
+        </Flex>
+      </div>;
+  }
+}`,...(u=(x=m.parameters)==null?void 0:x.docs)==null?void 0:u.source}}};const Be=["SimpleForm","TwoColumnForm"];export{l as SimpleForm,m as TwoColumnForm,Be as __namedExportsOrder,ve as default};
---
+++ storybook/assets/SearchField.stories-D3wqsxd8.js
@@ -0,0 +1,16 @@
+import{S as g}from"./SearchField-BqnacMAA.js";import"./jsx-runtime-DtgEGaVE.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./useField-CBiuCiGl.js";import"./useFocusRing-Dvg3g2cU.js";import"./clsx-B-dksMZM.js";import"./Hidden-ClhOTPkO.js";import"./index-cpIEhwLo.js";import"./useLabels-BjpOYTdf.js";import"./TextField.module-RvTXLpy1.js";import"./useControlledState-DGyHFLi9.js";import"./useLocalizedStringFormatter-a7gPYq3X.js";import"./context-CuBBIohG.js";import"./useTextField-5XYY7WPm.js";import"./useFormValidation-c4o29Mgv.js";import"./useFocusable-C9L8Uaaf.js";import"./createLucideIcon-8wDOqnHv.js";import"./Button-BLHi0EwO.js";import"./Button-BvljoDys.js";import"./LiveAnnouncer-DFDjXwI8.js";import"./x-Cu6J0Y2u.js";const z={component:g,title:"Components/SearchField",tags:["autodocs"],parameters:{layout:"centered"}},e={args:{placeholder:"Sök efter en person"}},r={args:{placeholder:'Sök efter "secret"',validate:l=>l==="secret"?"Sök inte efter hemligheter":!0}},t={args:{placeholder:"Sök efter dokument",isInvalid:!0,errorMessage:"Något gick fel, var god försök igen"}};var o,a,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Sök efter en person'
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var i,m,p;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Sök efter "secret"',
+    validate: (value: string) => value === 'secret' ? 'Sök inte efter hemligheter' : true
+  }
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var n,c,d;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Sök efter dokument',
+    isInvalid: true,
+    errorMessage: 'Något gick fel, var god försök igen'
+  }
+}`,...(d=(c=t.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const A=["Primary","CustomValidation","Invalid"];export{r as CustomValidation,t as Invalid,e as Primary,A as __namedExportsOrder,z as default};
