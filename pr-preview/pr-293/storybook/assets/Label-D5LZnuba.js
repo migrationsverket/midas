9cf7c899a26ac32820870ac2b22d62538a1c7e36
derpbravely <derpbravely@users.noreply.github.com>
2025-03-10 18:43:36 +0000

    Remove preview for PR 293 🛬

--- pr-preview/pr-293/storybook/assets/Label-D5LZnuba.js
+++
@@ -1,1 +0,0 @@
-import{$ as o}from"./useFocusRing-CzvYi63w.js";import{$}from"./Hidden-Cwv2mliU.js";import{R as b,r as f}from"./index-BbAIUH2N.js";const l=f.createContext({}),p=$(function(e,a){[e,a]=o(e,a,l);let{elementType:t="label",...r}=e;return b.createElement(t,{className:"react-aria-Label",...r,ref:a})});export{p as $,l as a};
