53660a1bd735a29ceabfc594965c98dcf7339368
jabkha <jabkha@users.noreply.github.com>
2025-03-10 14:06:08 +0000

    Deploy preview for PR 326 🛫

---
+++ pr-preview/pr-326/storybook/assets/TextField.stories-DPHbrWcQ.js
@@ -0,0 +1,54 @@
+import{T as _}from"./TextField-C5Xv5jmA.js";import"./jsx-runtime-DtgEGaVE.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Form-BB8RM6iI.js";import"./useFocusRing-Dvg3g2cU.js";import"./clsx-B-dksMZM.js";import"./Text-DGilHHfL.js";import"./useFormValidation-c4o29Mgv.js";import"./Input-BZZpXOBL.js";import"./Hidden-ClhOTPkO.js";import"./index-cpIEhwLo.js";import"./useField-CBiuCiGl.js";import"./useLabels-BjpOYTdf.js";import"./TextField-U0IlVfQC.js";import"./useTextField-5XYY7WPm.js";import"./useControlledState-DGyHFLi9.js";import"./useFocusable-C9L8Uaaf.js";import"./TextField.module-RvTXLpy1.js";import"./Button-COJc5EXG.js";import"./Button-BvljoDys.js";import"./LiveAnnouncer-DFDjXwI8.js";const tr={component:_,title:"Components/TextField",tags:["autodocs"],argTypes:{label:{type:"string",description:"Etikett"},description:{type:"string"},type:{options:["password","text","email"],control:{type:"select"}}}},r={args:{label:"Label",description:"Description"}},e={args:{label:"Enter Password",type:"password"}},s={args:{...r.args,isInvalid:!0,errorMessage:"Fel i valideringen"}},a={args:{...r.args,isRequired:!0}},o={args:{...r.args,isDisabled:!0}},t={args:{...r.args,validationType:"ssn",label:"Personnummer",description:void 0,errorMessage:"Fel format för ett personnummer",maxLength:12}},n={args:{...r.args,validationType:"dossnr",label:"Dossiernummer",description:void 0,errorMessage:"Fel format för ett dossiernummer"}},i={args:{...r.args,maxLength:50}},m={args:{...r.args,showCounter:!0}};var p,c,d;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    label: 'Label',
+    description: 'Description'
+  }
+}`,...(d=(c=r.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var g,u,l;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    label: 'Enter Password',
+    type: 'password'
+  }
+}`,...(l=(u=e.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var y,P,b;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    isInvalid: true,
+    errorMessage: 'Fel i valideringen'
+  }
+}`,...(b=(P=s.parameters)==null?void 0:P.docs)==null?void 0:b.source}}};var f,w,x;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    isRequired: true
+  }
+}`,...(x=(w=a.parameters)==null?void 0:w.docs)==null?void 0:x.source}}};var S,h,D;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    isDisabled: true
+  }
+}`,...(D=(h=o.parameters)==null?void 0:h.docs)==null?void 0:D.source}}};var v,F,L;t.parameters={...t.parameters,docs:{...(v=t.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    validationType: 'ssn',
+    label: 'Personnummer',
+    description: undefined,
+    errorMessage: \`Fel format för ett personnummer\`,
+    maxLength: 12
+  }
+}`,...(L=(F=t.parameters)==null?void 0:F.docs)==null?void 0:L.source}}};var M,T,C;n.parameters={...n.parameters,docs:{...(M=n.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    validationType: 'dossnr',
+    label: 'Dossiernummer',
+    description: undefined,
+    errorMessage: \`Fel format för ett dossiernummer\`
+  }
+}`,...(C=(T=n.parameters)==null?void 0:T.docs)==null?void 0:C.source}}};var q,E,R;i.parameters={...i.parameters,docs:{...(q=i.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    maxLength: 50
+  }
+}`,...(R=(E=i.parameters)==null?void 0:E.docs)==null?void 0:R.source}}};var I,N,V;m.parameters={...m.parameters,docs:{...(I=m.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    showCounter: true
+  }
+}`,...(V=(N=m.parameters)==null?void 0:N.docs)==null?void 0:V.source}}};const nr=["Primary","Password","NotValid","Required","Disabled","Personnummer","Dossnr","MaxLength","ShowCounter"];export{o as Disabled,n as Dossnr,i as MaxLength,s as NotValid,e as Password,t as Personnummer,r as Primary,a as Required,m as ShowCounter,nr as __namedExportsOrder,tr as default};
---
+++ pr-preview/pr-326/storybook/assets/Tooltip.stories-CR70x0nC.js
@@ -0,0 +1,16 @@
+import{j as r}from"./jsx-runtime-DtgEGaVE.js";import{T as i,a as s}from"./Tooltip-CWCk_0Y_.js";import{B as n}from"./Button-COJc5EXG.js";import{c as m}from"./createLucideIcon-8wDOqnHv.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./OverlayArrow-JJIn2pKb.js";import"./useFocusRing-Dvg3g2cU.js";import"./index-cpIEhwLo.js";import"./useFocusable-C9L8Uaaf.js";import"./useOverlayTriggerState-yt_HxB-E.js";import"./context-CuBBIohG.js";import"./useControlledState-DGyHFLi9.js";import"./useModal-Duhq1_Z8.js";import"./Button-BvljoDys.js";import"./LiveAnnouncer-DFDjXwI8.js";import"./Hidden-ClhOTPkO.js";/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=m("Save",[["path",{d:"M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z",key:"1c8476"}],["path",{d:"M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7",key:"1ydtos"}],["path",{d:"M7 3v4a1 1 0 0 0 1 1h7",key:"t51u73"}]]),P={component:i,title:"Components/Tooltip",tags:["autodocs"],argTypes:{children:{control:"text"}}},t={args:{children:"Spara"},render:p=>r.jsxs(s,{children:[r.jsx(n,{variant:"tertiary","aria-label":"Spara",children:r.jsx(c,{})}),r.jsx(i,{...p})]})};var a,o,e;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: 'Spara'
+  },
+  render: args => <TooltipTrigger>
+      <Button variant='tertiary' aria-label='Spara'>
+        <Save />
+      </Button>
+      <Tooltip {...args}></Tooltip>
+    </TooltipTrigger>
+}`,...(e=(o=t.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const V=["Primary"];export{t as Primary,V as __namedExportsOrder,P as default};
