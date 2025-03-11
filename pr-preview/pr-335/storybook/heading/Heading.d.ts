9caee1c50c251565b8d5554553fd260a2d84756b
derpbravely <derpbravely@users.noreply.github.com>
2025-03-11 10:21:32 +0000

    Deploy preview for PR 335 🛫

---
+++ pr-preview/pr-335/storybook/heading/Heading.d.ts
@@ -0,0 +1,19 @@
+import * as React from 'react';
+type Variant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
+export interface HeadingProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement> {
+    children: React.ReactNode;
+    /**
+     * The heading element to render
+     */
+    variant?: Variant;
+    /**
+     * Override the semantic element of the component but keep the look of the variant property
+     */
+    component?: Variant;
+    /**
+     * Use the external/expressive look
+     */
+    isExternal?: boolean;
+}
+export declare const Heading: React.FC<HeadingProps>;
+export {};
