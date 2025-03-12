99888954fd6f0dc69947c992248e54138d9dd7b3
derpbravely <derpbravely@users.noreply.github.com>
2025-03-12 08:11:40 +0000

    Deploy preview for PR 335 🛫

--- pr-preview/pr-335/storybook/heading/Heading.d.ts
+++ pr-preview/pr-335/storybook/heading/Heading.d.ts
@@ -1,16 +1,10 @@
 import { HeadingProps as AriaHeadingProps } from 'react-aria-components';
 import * as React from 'react';
-export type Variant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
-export interface HeadingProps extends Omit<AriaHeadingProps, 'level'> {
-    children?: React.ReactNode;
+export interface HeadingProps extends AriaHeadingProps {
     /**
-     * The heading element to render
+     * Override the semantic element of the component but keep the look of the level property
      */
-    variant?: Variant;
-    /**
-     * Override the semantic element of the component but keep the look of the variant property
-     */
-    component?: Variant;
+    semanticLevel?: AriaHeadingProps['level'];
     /**
      * Use the external/expressive look
      */
