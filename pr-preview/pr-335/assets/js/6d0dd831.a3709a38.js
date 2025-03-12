5e145cf1fd0ae05378a7ed1e2fdf3c45811ea5a9
derpbravely <derpbravely@users.noreply.github.com>
2025-03-12 10:30:47 +0000

    Deploy preview for PR 335 🛫

---
+++ pr-preview/pr-335/assets/js/6d0dd831.a3709a38.js
@@ -0,0 +1,267 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[8197],{
+
+/***/ 62059:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  Example: () => (/* binding */ Example),
+  assets: () => (/* binding */ assets),
+  contentTitle: () => (/* binding */ contentTitle),
+  "default": () => (/* binding */ MDXContent),
+  frontMatter: () => (/* binding */ frontMatter),
+  metadata: () => (/* reexport */ site_docs_components_label_mdx_6d0_namespaceObject),
+  toc: () => (/* binding */ toc)
+});
+
+;// ./apps/docs/.docusaurus/docusaurus-plugin-content-docs/default/site-docs-components-label-mdx-6d0.json
+const site_docs_components_label_mdx_6d0_namespaceObject = /*#__PURE__*/JSON.parse('{"id":"components/label","title":"Label","description":"<ComponentHeader","source":"@site/docs/components/label.mdx","sourceDirName":"components","slug":"/components/label","permalink":"/pr-preview/pr-335/components/label","draft":false,"unlisted":false,"tags":[],"version":"current","frontMatter":{"title":"Label","pagination_prev":null,"pagination_next":null},"sidebar":"sideBar"}');
+// EXTERNAL MODULE: ./node_modules/react/jsx-runtime.js
+var jsx_runtime = __webpack_require__(74848);
+// EXTERNAL MODULE: ./node_modules/@mdx-js/react/lib/index.js
+var lib = __webpack_require__(28453);
+// EXTERNAL MODULE: ./node_modules/@docusaurus/theme-classic/lib/theme/Tabs/index.js + 2 modules
+var Tabs = __webpack_require__(65537);
+// EXTERNAL MODULE: ./node_modules/@docusaurus/theme-classic/lib/theme/TabItem/index.js + 1 modules
+var TabItem = __webpack_require__(79329);
+// EXTERNAL MODULE: ./node_modules/@babel/runtime/helpers/esm/objectWithoutPropertiesLoose.js
+var objectWithoutPropertiesLoose = __webpack_require__(98587);
+// EXTERNAL MODULE: ./node_modules/react/index.js
+var react = __webpack_require__(96540);
+// EXTERNAL MODULE: ./node_modules/clsx/dist/clsx.mjs
+var clsx = __webpack_require__(34164);
+// EXTERNAL MODULE: ./node_modules/react-aria-components/dist/Label.mjs
+var dist_Label = __webpack_require__(21704);
+;// ./packages/components/src/label/Label.module.css
+// extracted by mini-css-extract-plugin
+/* harmony default export */ const Label_module = ({"tokens":"\"../theme/tokens.css\"","black":"#000000","display":"\"Inter\", sans-serif","label-01":"label-01_rWfb","label-02":"label-02_LZ3P"});
+;// ./packages/components/src/label/Label.tsx
+var _excluded=["children","className","variant"];var Label=function Label(_ref){var children=_ref.children,className=_ref.className,_ref$variant=_ref.variant,variant=_ref$variant===void 0?'label-01':_ref$variant,rest=(0,objectWithoutPropertiesLoose/* default */.A)(_ref,_excluded);var classNames={'label-01':Label_module['label-01'],'label-02':Label_module['label-02']};var labelProps=Object.assign({className:(0,clsx/* default */.A)(classNames[variant],className)},rest);return/*#__PURE__*/(0,jsx_runtime.jsx)(dist_Label/* Label */.J,Object.assign({},labelProps,{children:children}));};
+// EXTERNAL MODULE: ./apps/docs/src/components/propsTable.tsx + 1 modules
+var propsTable = __webpack_require__(38355);
+// EXTERNAL MODULE: ./apps/docs/src/components/getComponentMetaData.tsx + 2 modules
+var getComponentMetaData = __webpack_require__(80059);
+// EXTERNAL MODULE: ./apps/docs/src/components/CodeBlock/CodeBlock.tsx + 1 modules
+var CodeBlock = __webpack_require__(71372);
+;// ./apps/docs/docs/components/label.mdx
+
+
+const frontMatter = {
+	title: 'Label',
+	pagination_prev: null,
+	pagination_next: null
+};
+const contentTitle = undefined;
+
+const assets = {
+
+};
+
+
+
+
+
+
+
+
+
+const Example = props => {
+  return (0,jsx_runtime.jsx)(CodeBlock/* default */.A, {
+    scope: {
+      Label: Label
+    },
+    ...props,
+    children: `<Label>Jag är ett en vanlig etikett</Label>`
+  });
+};
+const toc = [{
+  "value": "Installation",
+  "id": "installation",
+  "level": 2
+}, {
+  "value": "Varianter",
+  "id": "varianter",
+  "level": 2
+}, {
+  "value": "Stil",
+  "id": "stil",
+  "level": 3
+}, {
+  "value": "Val av element",
+  "id": "val-av-element",
+  "level": 3
+}, {
+  "value": "API",
+  "id": "api",
+  "level": 2
+}, {
+  "value": "Select",
+  "id": "select",
+  "level": 3
+}];
+function _createMdxContent(props) {
+  const _components = {
+    code: "code",
+    h2: "h2",
+    h3: "h3",
+    p: "p",
+    pre: "pre",
+    ...(0,lib/* useMDXComponents */.R)(),
+    ...props.components
+  };
+  return (0,jsx_runtime.jsxs)(jsx_runtime.Fragment, {
+    children: [(0,jsx_runtime.jsx)(getComponentMetaData/* ComponentHeader */.B, {
+      name: 'typography-label',
+      friendlyName: 'Etikett',
+      overrideHeadlessLink: "https://github.com/adobe/react-spectrum/tree/main/packages/@react-aria/label"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Etikett med fördefinerad stil"
+    }), "\n", (0,jsx_runtime.jsx)(Example, {
+      hideCode: true
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "installation",
+      children: "Installation"
+    }), "\n", (0,jsx_runtime.jsxs)(Tabs/* default */.A, {
+      groupId: "npm2yarn",
+      children: [(0,jsx_runtime.jsx)(TabItem/* default */.A, {
+        value: "npm",
+        children: (0,jsx_runtime.jsx)(_components.pre, {
+          children: (0,jsx_runtime.jsx)(_components.code, {
+            className: "language-bash",
+            children: "npm install @midas-ds/components\n"
+          })
+        })
+      }), (0,jsx_runtime.jsx)(TabItem/* default */.A, {
+        value: "yarn",
+        label: "Yarn",
+        children: (0,jsx_runtime.jsx)(_components.pre, {
+          children: (0,jsx_runtime.jsx)(_components.code, {
+            className: "language-bash",
+            children: "yarn add @midas-ds/components\n"
+          })
+        })
+      }), (0,jsx_runtime.jsx)(TabItem/* default */.A, {
+        value: "pnpm",
+        label: "pnpm",
+        children: (0,jsx_runtime.jsx)(_components.pre, {
+          children: (0,jsx_runtime.jsx)(_components.code, {
+            className: "language-bash",
+            children: "pnpm add @midas-ds/components\n"
+          })
+        })
+      })]
+    }), "\n", (0,jsx_runtime.jsx)(_components.pre, {
+      children: (0,jsx_runtime.jsx)(_components.code, {
+        className: "language-tsx",
+        children: "import { Label } from '@midas-ds/components'\n"
+      })
+    }), "\n", (0,jsx_runtime.jsx)(Example, {
+      hideExample: true
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "varianter",
+      children: "Varianter"
+    }), "\n", (0,jsx_runtime.jsx)(_components.h3, {
+      id: "stil",
+      children: "Stil"
+    }), "\n", (0,jsx_runtime.jsxs)(_components.p, {
+      children: ["Använd egenskapen ", (0,jsx_runtime.jsx)(_components.code, {
+        children: "variant"
+      }), " för att justera etikettens utseende"]
+    }), "\n", (0,jsx_runtime.jsx)(CodeBlock/* default */.A, {
+      scope: {
+        Label: Label
+      },
+      ...props,
+      children: `<Label variant="label-02">Jag är en etikett av varianten 'label-02'</Label>`
+    }), "\n", (0,jsx_runtime.jsx)(_components.h3, {
+      id: "val-av-element",
+      children: "Val av element"
+    }), "\n", (0,jsx_runtime.jsxs)(_components.p, {
+      children: ["Egenskapen ", (0,jsx_runtime.jsx)(_components.code, {
+        children: "elementType"
+      }), " kan användas för att ändra etikettens DOM-element."]
+    }), "\n", (0,jsx_runtime.jsx)(CodeBlock/* default */.A, {
+      scope: {
+        Label: Label
+      },
+      ...props,
+      children: `<Label elementType="div">Jag är en div</Label>`
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "api",
+      children: "API"
+    }), "\n", (0,jsx_runtime.jsx)(_components.h3, {
+      id: "select",
+      children: "Select"
+    }), "\n", (0,jsx_runtime.jsx)(propsTable/* PropTable */.U, {
+      name: "Label"
+    })]
+  });
+}
+function MDXContent(props = {}) {
+  const {wrapper: MDXLayout} = {
+    ...(0,lib/* useMDXComponents */.R)(),
+    ...props.components
+  };
+  return MDXLayout ? (0,jsx_runtime.jsx)(MDXLayout, {
+    ...props,
+    children: (0,jsx_runtime.jsx)(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+
+
+/***/ }),
+
+/***/ 21704:
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   I: () => (/* binding */ $01b77f81d0f07f68$export$75b6ee27786ba447),
+/* harmony export */   J: () => (/* binding */ $01b77f81d0f07f68$export$b04be29aa201d4f5)
+/* harmony export */ });
+/* harmony import */ var _utils_mjs__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5933);
+/* harmony import */ var _react_aria_collections__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(75127);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(96540);
+
+
+
+
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */ 
+
+
+const $01b77f81d0f07f68$export$75b6ee27786ba447 = /*#__PURE__*/ (0, react__WEBPACK_IMPORTED_MODULE_0__.createContext)({});
+const $01b77f81d0f07f68$export$b04be29aa201d4f5 = /*#__PURE__*/ (0, _react_aria_collections__WEBPACK_IMPORTED_MODULE_1__/* .createHideableComponent */ .U7)(function Label(props, ref) {
+    [props, ref] = (0, _utils_mjs__WEBPACK_IMPORTED_MODULE_2__/* .useContextProps */ .JT)(props, ref, $01b77f81d0f07f68$export$75b6ee27786ba447);
+    let { elementType: ElementType = 'label', ...labelProps } = props;
+    // @ts-ignore
+    return /*#__PURE__*/ (0, react__WEBPACK_IMPORTED_MODULE_0__).createElement(ElementType, {
+        className: "react-aria-Label",
+        ...labelProps,
+        ref: ref
+    });
+});
+
+
+
+//# sourceMappingURL=Label.module.js.map
+
+
+/***/ })
+
+}]);
