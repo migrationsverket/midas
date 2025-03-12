64331895f8423ce6473466e65f3f04feee039f84
derpbravely <derpbravely@users.noreply.github.com>
2025-03-12 10:55:11 +0000

    Deploy preview for PR 335 🛫

--- pr-preview/pr-335/storybook/label/Label.d.ts
+++ pr-preview/pr-335/storybook/label/Label.d.ts
@@ -1,10 +1,10 @@
 import { LabelProps as AriaLabelProps } from 'react-aria-components';
 import * as React from 'react';
-export type Variant = 'label-01' | 'label-02';
+export type LabelVariant = 'label-01' | 'label-02';
 export interface LabelProps extends AriaLabelProps {
     /**
      * The visual variant of the component
      */
-    variant?: Variant;
+    variant?: LabelVariant;
 }
 export declare const Label: React.FC<LabelProps>;
--- pr-preview/pr-335/storybook/text/Text.d.ts
+++ pr-preview/pr-335/storybook/text/Text.d.ts
@@ -1,11 +1,11 @@
 import { TextProps as AriaTextProps } from 'react-aria-components';
 import * as React from 'react';
-export type Variant = 'body-01' | 'body-02';
+export type TextVariant = 'body-01' | 'body-02';
 export interface TextProps extends AriaTextProps {
     /**
      * The visual variant of the component
      */
-    variant?: Variant;
+    variant?: TextVariant;
     /**
      * Use the external/expressive look
      */
