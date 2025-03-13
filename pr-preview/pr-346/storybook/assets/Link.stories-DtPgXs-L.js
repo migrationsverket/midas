7d7f15dbefe28a9beb289d42bb3d6f261b006ec0
derpbravely <derpbravely@users.noreply.github.com>
2025-03-13 12:00:06 +0000

    Deploy preview for PR 346 🛫

---
+++ pr-preview/pr-346/storybook/assets/Link.stories-DtPgXs-L.js
@@ -0,0 +1,23 @@
+import{L as k,A as y}from"./Link-Bxh9uuXp.js";import"./jsx-runtime-DtgEGaVE.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Link-Bcvh8UPc.js";import"./utils-DdXLQA3f.js";import"./clsx-B-dksMZM.js";import"./useFocusRing-CAIiVOBX.js";import"./useFocusable-C2jS3Qdx.js";import"./createLucideIcon-8wDOqnHv.js";const x={component:k,title:"Components/Link",tags:["autodocs"],parameters:{layout:"centered"},argTypes:{isDisabled:{options:[!0,!1],control:{type:"radio"}}}},r={args:{children:"Länk",icon:y,href:"https://designsystem.migrationsverket.se/components/link/"}},s={args:{...r.args,isDisabled:!0}},e={args:{children:"En fristående länk",standalone:!0,href:"https://designsystem.migrationsverket.se/components/link/"}},a={args:{...e.args,isDisabled:!0}};var n,t,o;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: 'Länk',
+    icon: ArrowRight,
+    href: 'https://designsystem.migrationsverket.se/components/link/'
+  }
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var i,m,c;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    isDisabled: true
+  }
+}`,...(c=(m=s.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var d,p,l;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    children: 'En fristående länk',
+    standalone: true,
+    href: 'https://designsystem.migrationsverket.se/components/link/'
+  }
+}`,...(l=(p=e.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var g,u,h;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    ...Standalone.args,
+    isDisabled: true
+  }
+}`,...(h=(u=a.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};const R=["Primary","PrimaryDisabled","Standalone","StandaloneDisabled"];export{r as Primary,s as PrimaryDisabled,e as Standalone,a as StandaloneDisabled,R as __namedExportsOrder,x as default};
