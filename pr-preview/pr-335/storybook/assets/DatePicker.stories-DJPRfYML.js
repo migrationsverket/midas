3cf6ad84ef613a8f2a046cadce66f7eed01284fc
derpbravely <derpbravely@users.noreply.github.com>
2025-03-13 07:51:22 +0000

    Deploy preview for PR 335 🛫

---
+++ pr-preview/pr-335/storybook/assets/DatePicker.stories-DJPRfYML.js
@@ -0,0 +1,63 @@
+import{j as a}from"./jsx-runtime-DtgEGaVE.js";import{D as m}from"./DatePicker-BufiaUkd.js";import{u as t,e as D}from"./index-DtSMq7G1.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button-BhwOwugC.js";import"./utils-DdXLQA3f.js";import"./clsx-B-dksMZM.js";import"./LiveAnnouncer-DFDjXwI8.js";import"./Hidden-0Xxd6rx0.js";import"./index-cpIEhwLo.js";import"./useFocusRing-CAIiVOBX.js";import"./useFocusable-C2jS3Qdx.js";import"./Calendar-DgJ7NQlB.js";import"./RSPContexts-CeCCo7XS.js";import"./Text-CV6Bluw9.js";import"./useDateFormatter-CI5bT3ml.js";import"./context-BR8A0IbO.js";import"./useControlledState-DGyHFLi9.js";import"./useLocalizedStringFormatter-CrofGC5X.js";import"./useLabels-BlWvxUw7.js";import"./useUpdateEffect-DShQiA8p.js";import"./useEvent-BB46z8qb.js";import"./getScrollParent-CrrBdd7g.js";import"./scrollIntoView-DzftQ5Hl.js";import"./useDescription-EqQzVjeZ.js";import"./VisuallyHidden-DZK_6M_C.js";import"./Heading-Bx0h7_1c.js";import"./chevron-right-CjCwHShF.js";import"./createLucideIcon-8wDOqnHv.js";import"./DateField-BtpSZfF1.js";import"./Form-DJSr-0L7.js";import"./useFormValidation-DerMACuP.js";import"./Group-CXqmBkpb.js";import"./Input-BPCyqAmH.js";import"./Label-oUcQSHlD.js";import"./FocusScope-Q9OnV_W8.js";import"./useField-_-T_Udov.js";import"./useFilter-DYBeGCRR.js";import"./useCollator-CsHORISF.js";import"./Dialog-Cjmp9FyO.js";import"./OverlayArrow-BGGzdKnh.js";import"./useDialog-84x-blKf.js";import"./PressResponder-BQj9d-QJ.js";import"./useOverlayTriggerState-Md-KFT5u.js";import"./DismissButton-Bk1YbeAq.js";import"./TextField.module-DBomeRQe.js";import"./TextField-DIBjoeRA.js";import"./TextField-CWcsO5dX.js";import"./useTextField-1zEqDY1L.js";import"./Button-dOT75icb.js";const Et={component:m,title:"Components/DatePicker",tags:["autodocs"],args:{label:"Välj datum",description:"YYYY-MM-DD"},parameters:{a11y:{test:"todo"}}},r={},o={args:{label:"Välj datum och tid",description:"YYYY-MM-DD HH-MM-SS",granularity:"second"}},i={args:{isRequired:!0,errorMessage:"Var god ange ett datum"},render:e=>a.jsxs("form",{children:[a.jsx(m,{...e}),a.jsx("button",{type:"submit",children:"Submit"})]}),play:async({canvas:e,step:n,args:{errorMessage:x}})=>{await n("it should show an error message if submitted empty",async()=>{await t.tab(),await t.tab(),await t.tab(),await t.tab(),await t.tab(),await t.keyboard("[Enter]"),D(e.getByText(x)).toBeInTheDocument()})}},s={args:{isRequired:!0,validate:({year:e})=>e===new Date().getFullYear()?"Var god välj ett annat år":!0},render:e=>a.jsxs("form",{children:[a.jsx(m,{...e}),a.jsx("button",{type:"submit",children:"Submit"})]}),play:async({canvas:e,step:n})=>{await n("it should show a custom error message if invalid",async()=>{await t.tab(),await t.tab(),await t.tab(),await t.tab(),await t.keyboard("[Enter]"),await t.keyboard("[Enter]"),await t.tab(),D(e.getByText("Var god välj ett annat år")).toBeInTheDocument()})}};var p,u,c;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(c=(u=r.parameters)==null?void 0:u.docs)==null?void 0:c.source}}};var d,l,b;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    label: 'Välj datum och tid',
+    description: 'YYYY-MM-DD HH-MM-SS',
+    granularity: 'second'
+  }
+}`,...(b=(l=o.parameters)==null?void 0:l.docs)==null?void 0:b.source}}};var g,w,y;i.parameters={...i.parameters,docs:{...(g=i.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    isRequired: true,
+    errorMessage: 'Var god ange ett datum'
+  },
+  render: args => <form>
+      <DatePicker {...args} />
+      <button type='submit'>Submit</button>
+    </form>,
+  play: async ({
+    canvas,
+    step,
+    args: {
+      errorMessage
+    }
+  }) => {
+    await step('it should show an error message if submitted empty', async () => {
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.keyboard('[Enter]');
+      expect(canvas.getByText(errorMessage as string)).toBeInTheDocument();
+    });
+  }
+}`,...(y=(w=i.parameters)==null?void 0:w.docs)==null?void 0:y.source}}};var v,h,E;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    isRequired: true,
+    validate: ({
+      year
+    }) => year === new Date().getFullYear() ? 'Var god välj ett annat år' : true
+  },
+  render: args => <form>
+      <DatePicker {...args} />
+      <button type='submit'>Submit</button>
+    </form>,
+  play: async ({
+    canvas,
+    step
+  }) => {
+    /**
+     * This test is made to create an invalid DatePicker
+     * the current year is not allowed the "user" selects todays date and submits the form
+     */
+    await step('it should show a custom error message if invalid', async () => {
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.keyboard('[Enter]');
+      await userEvent.keyboard('[Enter]');
+      await userEvent.tab();
+      expect(canvas.getByText('Var god välj ett annat år')).toBeInTheDocument();
+    });
+  }
+}`,...(E=(h=s.parameters)==null?void 0:h.docs)==null?void 0:E.source}}};const Dt=["Primary","WithTime","Required","CustomValiation"];export{s as CustomValiation,r as Primary,i as Required,o as WithTime,Dt as __namedExportsOrder,Et as default};
