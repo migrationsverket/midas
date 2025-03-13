820790f2c774c613f415e1d790f28ef0a3a08cdf
derpbravely <axel.engstrom@migrationsverket.se>
2025-03-13 08:53:23 +0100

    feat(typography) (#335)
    ## Description
    
    Adds new typography components `Heading`, `Text` and `Label`
    
    ## Changes
    
    Add baseline for components
    
    ## Additional Information
    
    Tokens yet to be implemented, wait for dark mode?
    **Margins** are defined by the browser at the moment. An example sketch
    of some text components used together might help us define them.
    
    ## Checklist
    
    - [x] Tests added if applicable
    - [x] Documentation updated
    - [x] Conventional commit messages
    - [x] Add complete typography example in Storybook

---
+++ packages/components/src/label/Label.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Label } from './Label'
+import { expect } from '@storybook/test'
+
+type Story = StoryObj<typeof Label>
+
+const meta: Meta<typeof Label> = {
+  component: Label,
+  title: 'Components/Typography/Label',
+  tags: ['autodocs'],
+}
+
+export const Label01: Story = {
+  args: {
+    variant: 'label-01',
+    children: 'I am a label with the variant of "label-01"',
+  },
+  play: async ({ canvas, args: { children } }) => {
+    expect(canvas.getByText(children as string)).toHaveStyle({
+      'font-size': '14px',
+      'line-height': '18px',
+      'font-weight': 400,
+    })
+  },
+}
+
+export const Label02: Story = {
+  args: {
+    variant: 'label-02',
+    children: 'I am a label with the variant of "label-02"',
+  },
+  play: async ({ canvas, args: { children } }) => {
+    expect(canvas.getByText(children as string)).toHaveStyle({
+      'font-size': '16px',
+      'line-height': '20px',
+      'font-weight': 500,
+    })
+  },
+}
+
+export default meta
---
+++ packages/components/src/text/Text.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import clsx from 'clsx'
+import {
+  Text as AriaText,
+  TextProps as AriaTextProps,
+} from 'react-aria-components'
+import styles from './Text.module.css'
+
+export type TextVariant = 'body-01' | 'body-02'
+
+export interface TextProps extends AriaTextProps {
+  /**
+   * The visual variant of the component
+   */
+  variant?: TextVariant
+  /**
+   * Use the external/expressive look
+   */
+  isExpressive?: boolean
+}
+
+const DEFAULT_ELEMENT = 'span'
+
+export const Text: React.FC<TextProps> = ({
+  children,
+  className,
+  variant = 'body-02',
+  isExpressive = false,
+  elementType = DEFAULT_ELEMENT,
+  ...rest
+}) => {
+  const classNames: Record<TextVariant, string> = {
+    'body-01': styles['body-01'],
+    'body-02': styles['body-02'],
+  }
+
+  const textProps: TextProps = {
+    className: clsx(classNames[variant], className),
+    elementType: elementType || DEFAULT_ELEMENT,
+    ...(isExpressive && { 'data-expressive': true }),
+    ...rest,
+  }
+
+  return <AriaText {...textProps}>{children}</AriaText>
+}
