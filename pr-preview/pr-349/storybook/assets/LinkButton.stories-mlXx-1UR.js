533384d62df313b67c7db291da75fdbdc6f432a9
phieksmig <phieksmig@users.noreply.github.com>
2025-03-14 15:12:18 +0000

    Deploy preview for PR 349 🛫

---
+++ pr-preview/pr-349/storybook/assets/LinkButton.stories-mlXx-1UR.js
@@ -0,0 +1,32 @@
+import{L as E}from"./LinkButton-8biLY2dl.js";import"./jsx-runtime-DtgEGaVE.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Link-Bcvh8UPc.js";import"./utils-DdXLQA3f.js";import"./clsx-B-dksMZM.js";import"./useFocusRing-CAIiVOBX.js";import"./useFocusable-C2jS3Qdx.js";import"./createLucideIcon-8wDOqnHv.js";import"./chevron-right-CjCwHShF.js";const q={component:E,title:"Components/LinkButton",tags:["autodocs"],parameters:{layout:"centered"},argTypes:{children:{name:"label"},variant:{options:["primary","secondary","tertiary","danger"],control:{type:"radio"},defaultValue:"primary"},isDisabled:{options:[!0,!1],control:{type:"radio"}},iconPlacement:{options:["right","left"],control:{type:"radio"},defaultValue:"left"}}},r={args:{children:"Till E-tjänst",href:"#"}},e={args:{...r.args,variant:"secondary"}},a={args:{...r.args,variant:"tertiary"}},s={args:{...r.args,children:"Föregående",iconPlacement:"left"}},t={args:{...r.args,isDisabled:!0}},n={args:{children:"Till E-tjänst",onPress:()=>alert("navigation fn")}};var o,i,c;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'Till E-tjänst',
+    href: '#'
+  }
+}`,...(c=(i=r.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};var m,p,d;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    variant: 'secondary'
+  }
+}`,...(d=(p=e.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var l,g,u;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    variant: 'tertiary'
+  }
+}`,...(u=(g=a.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var y,f,P;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    children: 'Föregående',
+    iconPlacement: 'left'
+  }
+}`,...(P=(f=s.parameters)==null?void 0:f.docs)==null?void 0:P.source}}};var h,v,S;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    isDisabled: true
+  }
+}`,...(S=(v=t.parameters)==null?void 0:v.docs)==null?void 0:S.source}}};var T,b,D;n.parameters={...n.parameters,docs:{...(T=n.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    children: 'Till E-tjänst',
+    onPress: () => alert('navigation fn')
+  }
+}`,...(D=(b=n.parameters)==null?void 0:b.docs)==null?void 0:D.source}}};const w=["Primary","Secondary","Tertiary","Previous","Disabled","AppLink"];export{n as AppLink,t as Disabled,s as Previous,r as Primary,e as Secondary,a as Tertiary,w as __namedExportsOrder,q as default};
