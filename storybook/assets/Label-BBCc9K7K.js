ac17c973ebc5c6428ed352754cd0dce5a03c214a
derpbravely <axel.engstrom@migrationsverket.se>
2025-03-14 14:18:20 +0000

    Deploying to gh-pages from @ migrationsverket/midas@f9ee4636b7b8d6cd365aa6c99cd9be6e5dec765b 🚀

---
+++ storybook/assets/Label-BBCc9K7K.js
@@ -0,0 +1,1 @@
+import{$ as o}from"./utils-BhWkjeqE.js";import{$}from"./Hidden-DrRnqvCY.js";import{R as b,r as f}from"./index-BbAIUH2N.js";const l=f.createContext({}),p=$(function(e,a){[e,a]=o(e,a,l);let{elementType:t="label",...r}=e;return b.createElement(t,{className:"react-aria-Label",...r,ref:a})});export{p as $,l as a};
