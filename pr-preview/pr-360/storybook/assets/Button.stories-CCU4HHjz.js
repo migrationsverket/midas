d024bd723cc50dc43a073221ba60037d38960772
derpbravely <derpbravely@users.noreply.github.com>
2025-03-18 13:24:04 +0000

    Deploy preview for PR 360 🛫

---
+++ pr-preview/pr-360/storybook/assets/Button.stories-CCU4HHjz.js
@@ -0,0 +1,49 @@
+import{j as E}from"./jsx-runtime-DtgEGaVE.js";import{B as F}from"./Button-rfFouqY9.js";import{P as W}from"./plus-DEoRU9eF.js";import{X}from"./x-Cu6J0Y2u.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button-AXOEyd3Z.js";import"./utils-C4wbeGu1.js";import"./clsx-B-dksMZM.js";import"./ProgressBar-DVVijuUB.js";import"./Label-BXmCKTLy.js";import"./Hidden-DizQ9ce0.js";import"./filterDOMProps-BSfnXAP7.js";import"./useLabel-Bdg4Lht-.js";import"./context-gxVqox2x.js";import"./NumberFormatter-DT0cDaax.js";import"./LiveAnnouncer-DFDjXwI8.js";import"./useFocusRing-BPh1BF7L.js";import"./index-cpIEhwLo.js";import"./useFocusable-BNsiIlBy.js";import"./createLucideIcon-8wDOqnHv.js";const tr={component:F,title:"Components/Button",tags:["autodocs"],args:{variant:"primary"},argTypes:{children:{type:"string"},variant:{options:["primary","secondary","tertiary","danger","icon"],control:{type:"radio"},defaultValue:"primary"},size:{options:["small","default (null)"],control:{type:"radio"},defaultValue:null},isDisabled:{options:[!0,!1],control:{type:"radio"}},iconPlacement:{options:["right","left"],control:{type:"radio"},defaultValue:"left"}}},r={args:{children:"Button"},parameters:{}},a={...r,args:{...r.args,variant:"secondary"}},e={args:{children:"Lägg till",variant:"tertiary",icon:W}},t={args:{variant:"primary",isDisabled:!0,children:"Lägg till"}},s={...r,args:{...r.args,variant:"secondary",isDisabled:!0}},o={args:{children:"Button",size:"small"},parameters:{}},n={args:{children:"Button",fullwidth:!0},parameters:{}},i={args:{children:E.jsx(X,{size:20,"aria-hidden":!0}),variant:"icon","aria-label":"Stäng"}};var c,m,l;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: 'Button'
+  },
+  parameters: {}
+}`,...(l=(m=r.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};var p,d,u;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  ...Primary,
+  args: {
+    ...Primary.args,
+    variant: 'secondary'
+  }
+}`,...(u=(d=a.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var g,y,h;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: 'Lägg till',
+    variant: 'tertiary',
+    icon: Plus
+  }
+}`,...(h=(y=e.parameters)==null?void 0:y.docs)==null?void 0:h.source}}};var P,S,f;t.parameters={...t.parameters,docs:{...(P=t.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary',
+    isDisabled: true,
+    children: 'Lägg till'
+  }
+}`,...(f=(S=t.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var v,b,B;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  ...Primary,
+  args: {
+    ...Primary.args,
+    variant: 'secondary',
+    isDisabled: true
+  }
+}`,...(B=(b=s.parameters)==null?void 0:b.docs)==null?void 0:B.source}}};var D,x,z;o.parameters={...o.parameters,docs:{...(D=o.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    size: 'small'
+  },
+  parameters: {}
+}`,...(z=(x=o.parameters)==null?void 0:x.docs)==null?void 0:z.source}}};var w,I,L;n.parameters={...n.parameters,docs:{...(w=n.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    fullwidth: true
+  },
+  parameters: {}
+}`,...(L=(I=n.parameters)==null?void 0:I.docs)==null?void 0:L.source}}};var j,T,V;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    children: <X size={20} aria-hidden />,
+    variant: 'icon',
+    'aria-label': 'Stäng'
+  }
+}`,...(V=(T=i.parameters)==null?void 0:T.docs)==null?void 0:V.source}}};const sr=["Primary","Secondary","TertiaryWithIcon","PrimaryDisabled","SecondaryDisabled","SmallPrimary","PrimaryFullwidth","Icon"];export{i as Icon,r as Primary,t as PrimaryDisabled,n as PrimaryFullwidth,a as Secondary,s as SecondaryDisabled,o as SmallPrimary,e as TertiaryWithIcon,sr as __namedExportsOrder,tr as default};
