3aeb7d796459166e76d0362fb9a9849e09b3b9d4
derpbravely <derpbravely@users.noreply.github.com>
2025-03-19 13:39:00 +0000

    Deploy preview for PR 360 🛫

---
+++ pr-preview/pr-360/storybook/assets/ButtonGroup.stories-MTtgetMu.js
@@ -0,0 +1,11 @@
+import{j as o}from"./jsx-runtime-DtgEGaVE.js";import{B as a}from"./Button-52iT9XjT.js";import{c}from"./clsx-B-dksMZM.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button-AXOEyd3Z.js";import"./utils-C4wbeGu1.js";import"./ProgressBar-DVVijuUB.js";import"./Label-BXmCKTLy.js";import"./Hidden-DizQ9ce0.js";import"./filterDOMProps-BSfnXAP7.js";import"./useLabel-Bdg4Lht-.js";import"./context-gxVqox2x.js";import"./NumberFormatter-DT0cDaax.js";import"./LiveAnnouncer-DFDjXwI8.js";import"./useFocusRing-BPh1BF7L.js";import"./index-cpIEhwLo.js";import"./useFocusable-BNsiIlBy.js";const l='"../theme/tokens.css"',d="undefined",g="_buttonGroup_5pd8y_4",y={tokens:l,smBreakpoint:d,buttonGroup:g},e=({children:t,className:p,"aria-label":u,...m})=>o.jsx("div",{role:"group","aria-label":u,className:c(y.buttonGroup,p),...m,children:t});try{e.displayName="ButtonGroup",e.__docgenInfo={description:"Group several buttons together.",displayName:"ButtonGroup",props:{"aria-label":{defaultValue:null,description:"Set a descriptive lable for screen readers",name:"aria-label",required:!0,type:{name:"string"}}}}}catch{}const I={component:e,title:"Components/Button/ButtonGroup",tags:["autodocs"],parameters:{layout:"centered"},args:{},argTypes:{}},r={args:{"aria-label":"Navigera i listan"},render:({...t})=>o.jsxs(e,{...t,children:[o.jsx(a,{children:"Fortsätt"}),o.jsx(a,{variant:"secondary",children:"Avbryt"})]})};var n,s,i;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    'aria-label': 'Navigera i listan'
+  },
+  render: ({
+    ...args
+  }) => <ButtonGroup {...args}>
+      <Button>Fortsätt</Button>
+      <Button variant='secondary'>Avbryt</Button>
+    </ButtonGroup>
+}`,...(i=(s=r.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};const O=["Primary"];export{r as Primary,O as __namedExportsOrder,I as default};
