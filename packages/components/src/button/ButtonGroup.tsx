42b5cdedfb772588734d4bf344b83f2aad881d48
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2025-03-20 10:46:36 +0100

    fix(button): clarify use of labels on button groups
    - Adds support for a visible title
    
    - Clarify docs to use either aria-label or title
    
    - Add documentation about using ButtonContext to control several buttons at once

--- packages/components/src/button/ButtonGroup.tsx
+++ packages/components/src/button/ButtonGroup.tsx
@@ -1,10 +1,15 @@
 import clsx from 'clsx'
 import styles from './ButtonGroup.module.css'
+import { ButtonContext } from 'react-aria-components'
+import { Label } from '../label'
+import * as React from 'react'
 
 export interface MidasButtonGroupProps
   extends React.HTMLAttributes<HTMLDivElement> {
+  /** Set a descriptive visible lable */
+  title?: string
   /** Set a descriptive lable for screen readers */
-  'aria-label': string
+  'aria-label'?: string
 }
 
 /**
@@ -18,17 +23,38 @@ export interface MidasButtonGroupProps
 export const ButtonGroup: React.FC<MidasButtonGroupProps> = ({
   children,
   className,
+  title,
   'aria-label': ariaLabel,
   ...rest
 }) => {
+  if (title && ariaLabel) {
+    console.warn(
+      'Choose either title and aria-label. Using both title and aria-label will create redundancy for screen reader users.',
+    )
+  }
+  const hasVisibleTitle = title ? true : false
+  const id = React.useId()
+
   return (
     <div
       role='group'
+      aria-labelledby={hasVisibleTitle ? id : undefined}
       aria-label={ariaLabel}
-      className={clsx(styles.buttonGroup, className)}
+      className={clsx(styles.buttonGroupWrapper, className)}
       {...rest}
     >
-      {children}
+      {hasVisibleTitle && (
+        <Label
+          id={id}
+          variant='label-02'
+          elementType='span'
+        >
+          {title}
+        </Label>
+      )}
+      <div className={styles.buttonGroup}>{children}</div>
     </div>
   )
 }
+
+export { ButtonContext }
