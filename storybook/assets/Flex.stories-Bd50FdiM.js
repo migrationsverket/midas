9422c36123d07c357b066fe3e1dd6ebdfe4d0335
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-03-20 11:36:55 +0000

    Deploying to gh-pages from @ migrationsverket/midas@e41c3a36bcea6ba7be8faa78255c69a53564169c 🚀

---
+++ storybook/assets/Flex.stories-Bd50FdiM.js
@@ -0,0 +1,55 @@
+import{j as t}from"./jsx-runtime-DtgEGaVE.js";import{F as d}from"./Flex-DDRHzxYt.js";import{F as e}from"./FlexItem-DjZzjSwe.js";import{C as r}from"./Card-hZ3oXDfy.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./Link-CXJOJes4.js";import"./Link-B6hcGuh8.js";import"./utils-C4wbeGu1.js";import"./filterDOMProps-BSfnXAP7.js";import"./useFocusRing-BPh1BF7L.js";import"./index-cpIEhwLo.js";import"./useFocusable-BNsiIlBy.js";import"./createLucideIcon-8wDOqnHv.js";const L={component:d,title:"Components/Flex",tags:["autodocs"],parameters:{layout:"fullscreen"},argTypes:{fluid:{control:{type:"boolean"}}}},n={args:{},render:({...c})=>t.jsxs(d,{fluid:c.fluid,children:[t.jsx(e,{col:3,children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:6,children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:3,children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:4,children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:4,children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid men just detta kort har väldigt mycket text för att illustrera att de andra också växer på höjden.",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:3,offset:1,children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:"auto",children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})})]})};var i,o,l;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {},
+  render: ({
+    ...args
+  }) => <Flex fluid={args.fluid}>
+      <FlexItem col={3}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={6}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={3}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={4}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={4}>
+        <Card title={'Ett kort'} content={'Som är i ett grid men just detta kort har väldigt mycket text för att illustrera att de andra också växer på höjden.'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={3} offset={1}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={'auto'}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+    </Flex>
+}`,...(l=(o=n.parameters)==null?void 0:o.docs)==null?void 0:l.source}}};const C=["Primary"];export{n as Primary,C as __namedExportsOrder,L as default};
