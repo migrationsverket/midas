1ae9015be0ebd9b749fdcf6cd508ffdbe1b1e55c
jabkha <jabkha@users.noreply.github.com>
2025-03-20 09:58:04 +0000

    Deploy preview for PR 365 🛫

---
+++ pr-preview/pr-365/storybook/assets/ButtonGroup.stories-ByayekZx.js
@@ -0,0 +1,9 @@
+import{j as o}from"./jsx-runtime-DtgEGaVE.js";import{B as a}from"./Button-Bc5fgnVD.js";import{c}from"./clsx-B-dksMZM.js";import{a as d}from"./Button-AXOEyd3Z.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./utils-C4wbeGu1.js";import"./ProgressBar-DVVijuUB.js";import"./Label-BXmCKTLy.js";import"./Hidden-DizQ9ce0.js";import"./filterDOMProps-BSfnXAP7.js";import"./useLabel-Bdg4Lht-.js";import"./context-gxVqox2x.js";import"./NumberFormatter-DT0cDaax.js";import"./LiveAnnouncer-DFDjXwI8.js";import"./useFocusRing-BPh1BF7L.js";import"./index-cpIEhwLo.js";import"./useFocusable-BNsiIlBy.js";const l='"../theme/tokens.css"',B="undefined",y="_buttonGroup_5pd8y_4",g={tokens:l,smBreakpoint:B,buttonGroup:y},e=({children:t,className:u,contextValue:n,...m})=>o.jsx("div",{role:"group",className:c(g.buttonGroup,u),...m,children:n?o.jsx(d.Provider,{value:n,children:t}):t});try{e.displayName="ButtonGroup",e.__docgenInfo={description:"Group several buttons together.",displayName:"ButtonGroup",props:{contextValue:{defaultValue:null,description:"Additional props to pass to child Buttons",name:"contextValue",required:!1,type:{name:"ContextValue<ButtonContextValue, HTMLButtonElement>"}}}}}catch{}const q={component:e,title:"Components/Button/ButtonGroup",tags:["autodocs"],parameters:{layout:"centered"},args:{},argTypes:{}},r={args:{"aria-label":"Navigera i listan"},render:t=>o.jsxs(e,{...t,children:[o.jsx(a,{children:"Fortsätt"}),o.jsx(a,{variant:"secondary",children:"Avbryt"})]})};var s,p,i;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    'aria-label': 'Navigera i listan'
+  },
+  render: args => <ButtonGroup {...args}>
+      <Button>Fortsätt</Button>
+      <Button variant='secondary'>Avbryt</Button>
+    </ButtonGroup>
+}`,...(i=(p=r.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};const H=["Primary"];export{r as Primary,H as __namedExportsOrder,q as default};
