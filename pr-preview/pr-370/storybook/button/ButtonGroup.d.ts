7be7a823b03fca54eab59a776bffe65622270b98
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-03-20 09:52:48 +0000

    Deploy preview for PR 370 🛫

---
+++ pr-preview/pr-370/storybook/button/ButtonGroup.d.ts
@@ -0,0 +1,17 @@
+import { ButtonContext } from 'react-aria-components';
+import * as React from 'react';
+export interface MidasButtonGroupProps extends React.HTMLAttributes<HTMLDivElement> {
+    /** Set a descriptive visible lable */
+    title?: string;
+    /** Set a descriptive lable for screen readers */
+    'aria-label'?: string;
+}
+/**
+ * Group several buttons together.
+ *
+ * @interface MidasButtonGroupProps
+ *
+ * @see {@link https://designsystem.migrationsverket.se/components/button}
+ */
+export declare const ButtonGroup: React.FC<MidasButtonGroupProps>;
+export { ButtonContext };
