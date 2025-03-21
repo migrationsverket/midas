7c5415421724c1a9835b69bf65d585a3470d40c8
derpbravely <axel.engstrom@migrationsverket.se>
2025-03-21 11:27:25 +0100

    test(select): add basic test cases

--- packages/components/src/select/Select.spec.tsx
+++ packages/components/src/select/Select.spec.tsx
@@ -3,42 +3,7 @@ import { axe } from 'jest-axe'
 import { Select } from './'
 import user from '../../tests/utils/user'
 import { renderWithForm } from '../../tests/utils/browser'
-
-const fruits = [
-  'Apple',
-  'Banana',
-  'Kiwi',
-  'Elderflower',
-  'Nectarine',
-  'Watermelon',
-  'Pineapple',
-  'Apricot',
-  'Cloudberry',
-  'Mango',
-  'Strawberry',
-  'Blueberry',
-  'Raspberry',
-  'Blackberry',
-  'Cherry',
-  'Peach',
-  'Plum',
-  'Grape',
-  'Orange',
-  'Lemon',
-  'Lime',
-  'Coconut',
-  'Fig',
-  'Papaya',
-  'Guava',
-  'Pomegranate',
-  'Dragonfruit',
-  'Starfruit',
-  'Passionfruit',
-]
-
-const options = fruits.map(fruit => {
-  return { name: fruit, id: fruit.toLocaleLowerCase() }
-})
+import { options } from './utils'
 
 const label = 'label for select'
 
