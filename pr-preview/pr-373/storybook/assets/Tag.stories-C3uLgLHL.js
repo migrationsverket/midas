9c4936ad5cd47cc0f245f5cd6bf925617c258780
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-03-21 20:42:02 +0000

    Deploy preview for PR 373 🛫

---
+++ pr-preview/pr-373/storybook/assets/Tag.stories-C3uLgLHL.js
@@ -0,0 +1,25 @@
+import{j as s}from"./jsx-runtime-DtgEGaVE.js";import{T as d,a as u,s as T}from"./Tag-i7bxFwdD.js";import{e as y}from"./index-DtSMq7G1.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button-BAmjpgnq.js";import"./utils-Cd6MiihZ.js";import"./clsx-B-dksMZM.js";import"./ProgressBar-Dh0Vg3qS.js";import"./Label-Bf-bZThx.js";import"./Hidden-lswqw32w.js";import"./filterDOMProps-BSfnXAP7.js";import"./useLabel-ClMtG9eF.js";import"./context-BSqmORe6.js";import"./NumberFormatter-DT0cDaax.js";import"./LiveAnnouncer-DFDjXwI8.js";import"./useFocusRing--RQdELqC.js";import"./index-cpIEhwLo.js";import"./useFocusable-5VAxOUE-.js";import"./Collection-CC9oGC21.js";import"./index-DwWe6W7f.js";import"./ListBox-CKwgwK3H.js";import"./DragAndDrop-sISFPnlb.js";import"./Separator-Q6oUd1rz.js";import"./SelectionManager-DeX5238t.js";import"./useEvent-Yxrkvbn7.js";import"./scrollIntoView-DzftQ5Hl.js";import"./FocusScope-B7ZOkgD-.js";import"./useDescription-CVNFK32G.js";import"./useControlledState-DGyHFLi9.js";import"./ListKeyboardDelegate-xVJLa59h.js";import"./useCollator-C3PRVItH.js";import"./Text-BkowhAon.js";import"./useListState-CMHXyEJa.js";import"./useField-9Cy_F6CL.js";import"./useHighlightSelectionDescription-CrqVFzKW.js";import"./useUpdateEffect-DVcG7G3u.js";import"./useLocalizedStringFormatter-CIgGmZTI.js";import"./useHasTabbableChild-CW7EX7UL.js";import"./getScrollParent-CrrBdd7g.js";import"./Button-S7ARy0fO.js";import"./x-Cu6J0Y2u.js";import"./createLucideIcon-8wDOqnHv.js";const ir={component:d,title:"Components/Tag",tags:["autodocs"],args:{children:"Title",dismissable:!0,className:"test-class"}},a="test",r={render:t=>s.jsx(u,{"aria-label":"Taggar",children:s.jsx(d,{...t,"data-testid":a,children:t.children})}),play:async({canvas:t,step:l,args:{className:g}})=>{await l("it should preserve its classNames when being passed new ones",async()=>{y(t.getByTestId(a)).toHaveClass(T.tag,g)})}},e={args:{isDisabled:!0},render:r.render};var o,i,p;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => {
+    return <TagGroup aria-label='Taggar'>
+        <Tag {...args} data-testid={testID}>
+          {args.children}
+        </Tag>
+      </TagGroup>;
+  },
+  play: async ({
+    canvas,
+    step,
+    args: {
+      className
+    }
+  }) => {
+    await step('it should preserve its classNames when being passed new ones', async () => {
+      expect(canvas.getByTestId(testID)).toHaveClass(styles.tag, className as string);
+    });
+  }
+}`,...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var m,n,c;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  },
+  render: Default.render
+}`,...(c=(n=e.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const pr=["Default","Disabled"];export{r as Default,e as Disabled,pr as __namedExportsOrder,ir as default};
