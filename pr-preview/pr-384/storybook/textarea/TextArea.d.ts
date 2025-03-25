7368f2ee7c78ca94f2ea5e85e19b67505a6f1ef9
derpbravely <derpbravely@users.noreply.github.com>
2025-03-25 10:36:24 +0000

    Remove preview for PR 384 🛬

--- pr-preview/pr-384/storybook/textarea/TextArea.d.ts
+++
@@ -1,3 +0,0 @@
-import { TextAreaProps } from 'react-aria-components';
-import * as React from 'react';
-export declare const TextArea: React.ForwardRefExoticComponent<TextAreaProps & React.RefAttributes<HTMLTextAreaElement>>;
--- pr-preview/pr-384/storybook/textfield/TextField.d.ts
+++
@@ -1,4 +0,0 @@
-import { TextFieldProps } from 'react-aria-components';
-import * as React from 'react';
-export declare const TextField: React.ForwardRefExoticComponent<TextFieldProps & React.RefAttributes<HTMLDivElement>>;
-export declare const InputWrapper: () => null;
