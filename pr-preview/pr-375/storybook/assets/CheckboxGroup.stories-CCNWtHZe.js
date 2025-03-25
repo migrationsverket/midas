78de16ef1fea333fe54d8348761a3aa29b1bcad9
jabkha <jabkha@users.noreply.github.com>
2025-03-25 08:12:50 +0000

    Deploy preview for PR 375 🛫

---
+++ pr-preview/pr-375/storybook/assets/CheckboxGroup.stories-CCNWtHZe.js
@@ -0,0 +1,16 @@
+import{j as o}from"./jsx-runtime-DtgEGaVE.js";import{C as g}from"./Checkbox-bYN73pm5.js";import{C as l}from"./CheckboxGroup-Iz6LzA1S.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./RSPContexts-CeCCo7XS.js";import"./utils-Cd6MiihZ.js";import"./Form-Cr76Dr7U.js";import"./Text-BkowhAon.js";import"./filterDOMProps-BSfnXAP7.js";import"./useFormValidation-BT6lxpQu.js";import"./useFocusRing--RQdELqC.js";import"./index-cpIEhwLo.js";import"./Label-DO1wrnkN.js";import"./Hidden-lswqw32w.js";import"./useControlledState-DGyHFLi9.js";import"./useField-9Cy_F6CL.js";import"./useLabel-ClMtG9eF.js";import"./useFocusable-5VAxOUE-.js";import"./VisuallyHidden-DRXW-gYG.js";import"./Group-DZHj0GkJ.js";import"./TextField-MmYVY00w.js";import"./Input-COAkbJ34.js";import"./TextField-Ff17dM54.js";import"./useTextField-CYsm1yTN.js";import"./TextField.module-BIg3UaME.js";import"./Button-CDEnfU2-.js";import"./Button-BriKy1No.js";import"./ProgressBar-Cgq2ntZX.js";import"./context-BSqmORe6.js";import"./NumberFormatter-DT0cDaax.js";import"./LiveAnnouncer-DFDjXwI8.js";import"./Label-BP8RLUVN.js";const W={title:"Components/Checkbox/CheckboxGroup",component:l,parameters:{layout:"centered"},args:{isInvalid:!1,isDisabled:!1,isRequired:!1,isReadOnly:!1},argTypes:{isInvalid:{type:"boolean"},isDisabled:{type:"boolean"},isRequired:{type:"boolean"},isReadOnly:{type:"boolean"},errorMessage:{type:"string"}},tags:["autodocs"]},u=["Banan","Apple","Mango"],c={args:{},render:({...d})=>o.jsx(l,{...d,children:u.map(t=>o.jsx(g,{value:t,children:t},t))})},e={...c,args:{label:"Etikett",description:"Beskrivning",errorMessage:"här gick det snett"}},r={...c,args:{label:"Etikett",description:"Beskrivning",errorMessage:"här gick det snett",showSelectAll:!0}};var s,i,p;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  ...CheckboxGroupTemplate,
+  args: {
+    label: 'Etikett',
+    description: 'Beskrivning',
+    errorMessage: 'här gick det snett'
+  }
+}`,...(p=(i=e.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var a,n,m;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  ...CheckboxGroupTemplate,
+  args: {
+    label: 'Etikett',
+    description: 'Beskrivning',
+    errorMessage: 'här gick det snett',
+    showSelectAll: true
+  }
+}`,...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const X=["Example","SelectAll"];export{e as Example,r as SelectAll,X as __namedExportsOrder,W as default};
