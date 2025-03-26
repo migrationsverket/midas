2ca0295d9a8cb7d233b619bad8ed2bf830133e37
derpbravely <derpbravely@users.noreply.github.com>
2025-03-26 19:41:06 +0000

    Deploy preview for PR 380 🛫

---
+++ pr-preview/pr-380/storybook/assets/CharacterCounter.stories-DFus6j28.js
@@ -0,0 +1,37 @@
+import{j as t}from"./jsx-runtime-DtgEGaVE.js";import{e as C}from"./index-DtSMq7G1.js";import{c as S}from"./clsx-B-dksMZM.js";import{r as _}from"./index-BbAIUH2N.js";import{$ as E}from"./utils-fGMpyvF6.js";import{$ as H}from"./Input-CMdrBSNU.js";import{F as I}from"./FieldError-BADsxAoD.js";import{I as T}from"./Input-CXoo8PD-.js";import{T as w}from"./TextField-N3CsHAuz.js";import{L as V}from"./Label-D7d8orDF.js";import{T as b}from"./Text-YbdszP_I.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Hidden-lswqw32w.js";import"./useFocusRing-D234eI5d.js";import"./index-cpIEhwLo.js";import"./Form-I0ljaAay.js";import"./Text-CLHHKsvT.js";import"./useFormValidation-BplRM21L.js";import"./Button-BW9mzf3F.js";import"./Button-CTiZg4rz.js";import"./ProgressBar-qio85TVe.js";import"./Label-C7YJVz0K.js";import"./context-gwlhO1hg.js";import"./LiveAnnouncer-A-XV3aHJ.js";import"./TextField-UckN7hDi.js";import"./useTextField-DQ33iIrk.js";import"./useControlledState-DGyHFLi9.js";import"./useField-DUxalVp6.js";import"./TextField.module-DZbAscKo.js";import"./useIsDisabled-Cy36feIv.js";import"./Checkbox-U40LUIMq.js";import"./RSPContexts-CeCCo7XS.js";import"./VisuallyHidden-CIX9y9is.js";import"./useDateFormatter-XsEnc7vv.js";import"./useLocalizedStringFormatter-D7bE6AYj.js";import"./FocusScope-BTvUdKNZ.js";import"./scrollIntoView-Br2KMoz-.js";import"./getScrollParent-DaqmRUg6.js";import"./useEvent-Ou8lCwV4.js";import"./useCollator-BwOLflG5.js";const j='"../theme/tokens.css"',P="_count_16y67_4",L="_countExceeded_16y67_15",c={tokens:j,"--font-family":'"Inter", sans-serif',"--line-height-02":"1.125rem","--size-02":"0.875rem","--text-invalid":"light-dark(#e62323, #e62323)","--text-primary":"light-dark(#171717, #f2f2f2)",count:P,countExceeded:L},m=_.forwardRef((e,a)=>{[e,a]=E(e,a,H);const{maxLength:r,value:i}=e,l=(i==null?void 0:i.toString())??"";return r!==void 0?t.jsxs("span",{className:S(c.count,l.length>r&&c.countExceeded),children:[l.length," / ",r]}):t.jsx("span",{className:c.count,children:l.length})});try{m.displayName="CharacterCounter",m.__docgenInfo={description:"",displayName:"CharacterCounter",props:{onHoverStart:{defaultValue:null,description:"Handler that is called when a hover interaction starts.",name:"onHoverStart",required:!1,type:{name:"(e: HoverEvent) => void"}},onHoverEnd:{defaultValue:null,description:"Handler that is called when a hover interaction ends.",name:"onHoverEnd",required:!1,type:{name:"(e: HoverEvent) => void"}},onHoverChange:{defaultValue:null,description:"Handler that is called when the hover state changes.",name:"onHoverChange",required:!1,type:{name:"(isHovering: boolean) => void"}},className:{defaultValue:null,description:"The CSS [className](https://developer.mozilla.org/en-US/docs/Web/API/Element/className) for the element. A function may be provided to compute the class based on component state.",name:"className",required:!1,type:{name:"string | ((values: InputRenderProps & { defaultClassName: string; }) => string)"}},style:{defaultValue:null,description:"The inline [style](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style) for the element. A function may be provided to compute the style based on component state.",name:"style",required:!1,type:{name:"CSSProperties | ((values: InputRenderProps & { defaultStyle: CSSProperties; }) => CSSProperties)"}}}}}catch{}const N="test",Ce={render:e=>t.jsxs(w,{...e,children:[t.jsx(V,{variant:"label-02",children:"Label"}),t.jsx(b,{slot:"description",children:"Description"}),t.jsx(m,{}),t.jsx(I,{}),t.jsx(T,{"data-testid":N})]}),title:"Components/CharacterCounter",tags:["autodocs"]},o={args:{value:"I love apples"},play:async({canvas:e,step:a,args:{value:r}})=>{await a("it should show the correct count for its initial value",async()=>{C(e.getByText(r.length)).toBeInTheDocument()})}},n={args:{...o.args,maxLength:15}},s={tags:["!dev","!autodocs"],args:{defaultValue:"HEJ"},play:async({canvas:e,step:a,args:{defaultValue:r}})=>{await a("it should show the correct count for its initial defaultValue",async()=>{C(e.getByText(r.length)).toBeInTheDocument()})}};var p,d,u;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    value: 'I love apples'
+  },
+  play: async ({
+    canvas,
+    step,
+    args: {
+      value
+    }
+  }) => {
+    await step('it should show the correct count for its initial value', async () => {
+      expect(canvas.getByText((value as string).length)).toBeInTheDocument();
+    });
+  }
+}`,...(u=(d=o.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var h,f,g;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    maxLength: 15
+  }
+}`,...(g=(f=n.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};var y,v,x;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  tags: ['!dev', '!autodocs'],
+  args: {
+    defaultValue: 'HEJ'
+  },
+  play: async ({
+    canvas,
+    step,
+    args: {
+      defaultValue
+    }
+  }) => {
+    await step('it should show the correct count for its initial defaultValue', async () => {
+      expect(canvas.getByText((defaultValue as string).length)).toBeInTheDocument();
+    });
+  }
+}`,...(x=(v=s.parameters)==null?void 0:v.docs)==null?void 0:x.source}}};const Se=["Primary","MaxLength","ShowCounterWithDefaultValue"];export{n as MaxLength,o as Primary,s as ShowCounterWithDefaultValue,Se as __namedExportsOrder,Ce as default};
