92ae888a25a72c137cbca1046e879e9a04f3a97d
derpbravely <axel.engstrom@migrationsverket.se>
2025-03-26 09:58:54 +0100

    test(character-counter): add tests

---
+++ packages/components/src/character-counter/CharacterCounter.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { expect } from '@storybook/test'
+import { TextField } from '../textfield'
+import { Label } from '../label'
+import { Text } from '../text'
+import { FieldError } from '../field-error'
+import { Input } from '../input'
+import { CharacterCounter } from './CharacterCounter'
+
+const testID = 'test'
+
+const fieldErrorTestID = 'error'
+
+export default {
+  render: args => (
+    <TextField {...args}>
+      <Label>Label</Label>
+      <Text slot='description'>Description</Text>
+      <CharacterCounter />
+      <Input data-testid={testID} />
+      <FieldError data-testid={fieldErrorTestID} />
+    </TextField>
+  ),
+  title: 'Components/CharacterCounter',
+  tags: ['autodocs'],
+} as Meta<typeof TextField>
+
+type Story = StoryObj<typeof TextField>
+
+export const Primary: Story = {
+  args: {
+    value: 'I love apples',
+  },
+  play: async ({ canvas, step, args: { value } }) => {
+    await step(
+      'it should show the correct count for its initial value',
+      async () => {
+        expect(canvas.getByText((value as string).length)).toBeInTheDocument()
+      },
+    )
+  },
+}
+
+export const MaxLength: Story = {
+  args: {
+    ...Primary.args,
+    maxLength: 15,
+  },
+}
+
+export const ShowCounterWithDefaultValue: Story = {
+  tags: ['!dev', '!autodocs'],
+  args: {
+    defaultValue: 'HEJ',
+  },
+  play: async ({ canvas, step, args: { defaultValue } }) => {
+    await step(
+      'it should show the correct count for its initial defaultValue',
+      async () => {
+        expect(
+          canvas.getByText((defaultValue as string).length),
+        ).toBeInTheDocument()
+      },
+    )
+  },
+}
