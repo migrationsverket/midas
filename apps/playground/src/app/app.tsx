9c8124bab2960d2e401706bb097501438a1e743e
derpbravely <axel.engstrom@migrationsverket.se>
2025-03-28 13:42:51 +0100

    chore: update playground

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -1,35 +1,21 @@
 import styles from './app.module.css'
-import {
-  CharacterCounter,
-  Input,
-  Label,
-  Text,
-  TextField,
-  FieldError,
-} from '@midas-ds/components'
+import { TextField } from '@midas-ds/components'
 
 export function App() {
   return (
     <div className={styles.container}>
-      <TextField maxLength={4}>
-        <Label>Label</Label>
-        <Text slot={'description'}>Description</Text>
-        <CharacterCounter />
-        <Input
-          placeholder={'placeholder'}
-          type={'email'}
-        />
-        <FieldError />
-      </TextField>
-      <TextField>
-        <Label>Label</Label>
-        <Text slot={'description'}>Description</Text>
-        <Input
-          placeholder={'placeholder'}
-          type={'password'}
-        />
-        <FieldError />
-      </TextField>
+      <TextField
+        label='Label'
+        description='Description'
+        showCounter
+        type='email'
+        maxLength={4}
+      />
+      <TextField
+        label='Label'
+        description='Description'
+        type='password'
+      />
     </div>
   )
 }
