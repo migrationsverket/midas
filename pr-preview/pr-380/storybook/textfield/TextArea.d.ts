9d012720f98237368a176f55657cce142280e9be
derpbravely <derpbravely@users.noreply.github.com>
2025-03-28 14:39:33 +0000

    Deploy preview for PR 380 🛫

--- pr-preview/pr-380/storybook/textfield/TextArea.d.ts
+++ pr-preview/pr-380/storybook/textfield/TextArea.d.ts
@@ -1,3 +1,3 @@
 import { TextFieldBaseProps } from './TextFieldBase';
 import * as React from 'react';
-export declare const TextArea: React.FC<TextFieldBaseProps>;
+export declare const TextArea: React.ForwardRefExoticComponent<TextFieldBaseProps & React.RefAttributes<HTMLTextAreaElement>>;
--- pr-preview/pr-380/storybook/textfield/TextField.d.ts
+++ pr-preview/pr-380/storybook/textfield/TextField.d.ts
@@ -1,3 +1,3 @@
 import { TextFieldBaseProps } from './TextFieldBase';
 import * as React from 'react';
-export declare const TextField: React.FC<TextFieldBaseProps>;
+export declare const TextField: React.ForwardRefExoticComponent<TextFieldBaseProps & React.RefAttributes<HTMLInputElement>>;
