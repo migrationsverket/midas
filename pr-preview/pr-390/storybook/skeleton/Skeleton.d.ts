69c0f937d7aaf61f8563e93a949bb631b50fdc16
phieksmig <phieksmig@users.noreply.github.com>
2025-03-28 14:05:15 +0000

    Remove preview for PR 390 🛬

--- pr-preview/pr-390/storybook/skeleton/Skeleton.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-type SkeletonProps = {
-    width?: string | number;
-    height?: string | number;
-    variant?: 'circle' | 'rectangular';
-    animation?: 'wave' | false;
-};
-export declare const Skeleton: React.FC<SkeletonProps>;
-export {};
