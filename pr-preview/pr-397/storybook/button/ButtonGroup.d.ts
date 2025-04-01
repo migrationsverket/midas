05eb074080158107778e5833c601fce4f74630f1
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-04-01 07:01:02 +0000

    Deploy preview for PR 397 🛫

---
+++ pr-preview/pr-397/storybook/button/ButtonGroup.d.ts
@@ -0,0 +1,9 @@
+import * as React from 'react';
+/**
+ * Group several buttons together.
+ *
+ * @interface React.HTMLAttributes<HTMLDivElement>
+ *
+ * @see {@link https://designsystem.migrationsverket.se/components/button}
+ */
+export declare const ButtonGroup: React.FC<React.HTMLAttributes<HTMLDivElement>>;
