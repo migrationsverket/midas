1734e6d0a9fffb854315c67fac5d7c0cd87887d0
wilhjelmig <165774426+wilhjelmig@users.noreply.github.com>
2025-04-01 07:37:51 +0000

    Deploying to gh-pages from @ migrationsverket/midas@4cbc73e604a3e6eb433bb89b71eb7cac4b0294a4 🚀

---
+++ storybook/assets/Checkbox.stories-BQuW3J1E.js
@@ -0,0 +1,48 @@
+import{j as r}from"./jsx-runtime-DtgEGaVE.js";import{C as n}from"./Checkbox-WUxjDmUa.js";import{u as i,e as y}from"./index-DtSMq7G1.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./RSPContexts-CeCCo7XS.js";import"./utils-Cd6MiihZ.js";import"./Form-Cr76Dr7U.js";import"./Text-BkowhAon.js";import"./filterDOMProps-BSfnXAP7.js";import"./useFormValidation-BT6lxpQu.js";import"./useFocusRing--RQdELqC.js";import"./index-cpIEhwLo.js";import"./Label-Bf-bZThx.js";import"./Hidden-lswqw32w.js";import"./useControlledState-DGyHFLi9.js";import"./useField-9Cy_F6CL.js";import"./useLabel-ClMtG9eF.js";import"./useFocusable-5VAxOUE-.js";import"./VisuallyHidden-DRXW-gYG.js";const G={component:n,title:"Components/Checkbox",tags:["autodocs"],parameters:{layout:"centered"},args:{isInvalid:!1,isSelected:!1,isIndeterminate:!1,isDisabled:!1,isRequired:!1,isReadOnly:!1},argTypes:{isInvalid:{type:"boolean"},isSelected:{type:"boolean"},isIndeterminate:{type:"boolean"},isDisabled:{type:"boolean"},isRequired:{type:"boolean"},isReadOnly:{type:"boolean"}}},s={render:({...e})=>r.jsx(n,{value:"unsubscribe",...e,children:"Unsubscribe"})},t={args:{isDisabled:!0},render:({...e})=>r.jsx(n,{value:"unsubscribe",...e,children:"Unsubscribe"})},a={args:{isRequired:!0,"aria-label":"test"},tags:["!dev"],render:({...e})=>r.jsxs("form",{children:[r.jsx(n,{value:"unsubscribe",...e,children:"Unsubscribe"}),r.jsx("button",{type:"submit",children:"submit"})]}),play:async({canvas:e,step:h})=>{await h("It should be (aria) invalid if the user submitted without checking the box",async()=>{const g=e.getByLabelText("test");await i.tab(),await i.tab(),await i.keyboard("[Enter]"),y(g).toBeInvalid()})}};var o,c,u;s.parameters={...s.parameters,docs:{...(o=s.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: ({
+    ...args
+  }) => {
+    return <Checkbox value='unsubscribe' {...args}>
+        Unsubscribe
+      </Checkbox>;
+  }
+}`,...(u=(c=s.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};var b,d,l;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  },
+  render: ({
+    ...args
+  }) => {
+    return <Checkbox value='unsubscribe' {...args}>
+        Unsubscribe
+      </Checkbox>;
+  }
+}`,...(l=(d=t.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var p,m,x;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    isRequired: true,
+    'aria-label': 'test'
+  },
+  tags: ['!dev'],
+  render: ({
+    ...args
+  }) => {
+    return <form>
+        <Checkbox value='unsubscribe' {...args}>
+          Unsubscribe
+        </Checkbox>
+        <button type='submit'>submit</button>
+      </form>;
+  },
+  play: async ({
+    canvas,
+    step
+  }) => {
+    await step('It should be (aria) invalid if the user submitted without checking the box', async () => {
+      const checkbox = canvas.getByLabelText('test');
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.keyboard('[Enter]');
+      expect(checkbox).toBeInvalid();
+    });
+  }
+}`,...(x=(m=a.parameters)==null?void 0:m.docs)==null?void 0:x.source}}};const H=["Example","Disabled","Required"];export{t as Disabled,s as Example,a as Required,H as __namedExportsOrder,G as default};
