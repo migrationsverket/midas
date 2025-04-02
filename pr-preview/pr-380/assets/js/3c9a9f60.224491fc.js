f4bb008448623f0b10cf9df74fc5012a0e72fc97
derpbravely <derpbravely@users.noreply.github.com>
2025-04-02 08:02:16 +0000

    Deploy preview for PR 380 🛫

---
+++ pr-preview/pr-380/assets/js/3c9a9f60.224491fc.js
@@ -0,0 +1,175 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[2488],{
+
+/***/ 9756:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  assets: () => (/* binding */ assets),
+  contentTitle: () => (/* binding */ contentTitle),
+  "default": () => (/* binding */ MDXContent),
+  frontMatter: () => (/* binding */ frontMatter),
+  metadata: () => (/* reexport */ site_docs_design_patterns_buttons_and_links_mdx_3c9_namespaceObject),
+  toc: () => (/* binding */ toc)
+});
+
+;// ./apps/docs/.docusaurus/docusaurus-plugin-content-docs/default/site-docs-design-patterns-buttons-and-links-mdx-3c9.json
+const site_docs_design_patterns_buttons_and_links_mdx_3c9_namespaceObject = /*#__PURE__*/JSON.parse('{"id":"design-patterns/buttons-and-links","title":"Knappar och länkar","description":"Detta mönster är till för beskriva i vilka situationer Knapp, Länkknapp och Länk ska användas","source":"@site/docs/design-patterns/buttons-and-links.mdx","sourceDirName":"design-patterns","slug":"/design-patterns/buttons-and-links","permalink":"/pr-preview/pr-380/design-patterns/buttons-and-links","draft":false,"unlisted":false,"tags":[],"version":"current","frontMatter":{"pagination_prev":null,"pagination_next":null},"sidebar":"sideBar"}');
+// EXTERNAL MODULE: ./node_modules/react/jsx-runtime.js
+var jsx_runtime = __webpack_require__(74848);
+// EXTERNAL MODULE: ./node_modules/@mdx-js/react/lib/index.js
+var lib = __webpack_require__(28453);
+// EXTERNAL MODULE: ./packages/components/src/button/Button.tsx + 1 modules
+var Button = __webpack_require__(86559);
+// EXTERNAL MODULE: ./packages/components/src/link-button/LinkButton.tsx + 2 modules
+var LinkButton = __webpack_require__(1353);
+// EXTERNAL MODULE: ./packages/components/src/link/Link.tsx + 2 modules
+var Link = __webpack_require__(46964);
+// EXTERNAL MODULE: ./apps/docs/src/components/CodeBlock/CodeBlock.tsx + 1 modules
+var CodeBlock = __webpack_require__(71372);
+;// ./apps/docs/docs/design-patterns/buttons-and-links.mdx
+
+
+const frontMatter = {
+	pagination_prev: null,
+	pagination_next: null
+};
+const contentTitle = 'Knappar och länkar';
+
+const assets = {
+
+};
+
+
+
+
+
+const toc = [{
+  "value": "Knappar",
+  "id": "knappar",
+  "level": 2
+}, {
+  "value": "Länkar",
+  "id": "länkar",
+  "level": 2
+}, {
+  "value": "Länkknapp",
+  "id": "länkknapp",
+  "level": 3
+}, {
+  "value": "Fristående länk",
+  "id": "fristående-länk",
+  "level": 3
+}, {
+  "value": "Länk",
+  "id": "länk",
+  "level": 3
+}];
+function _createMdxContent(props) {
+  const _components = {
+    h1: "h1",
+    h2: "h2",
+    h3: "h3",
+    header: "header",
+    li: "li",
+    p: "p",
+    ul: "ul",
+    ...(0,lib/* useMDXComponents */.R)(),
+    ...props.components
+  };
+  return (0,jsx_runtime.jsxs)(jsx_runtime.Fragment, {
+    children: [(0,jsx_runtime.jsx)(_components.header, {
+      children: (0,jsx_runtime.jsx)(_components.h1, {
+        id: "knappar-och-länkar",
+        children: "Knappar och länkar"
+      })
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Detta mönster är till för beskriva i vilka situationer Knapp, Länkknapp och Länk ska användas"
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "knappar",
+      children: "Knappar"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Knappar används för att initiera en aktivitet. När användaren interagerar med en knapp händer något på sidan.\nExempel på situationer där det är lämpligt med en knapp:"
+    }), "\n", (0,jsx_runtime.jsxs)(_components.ul, {
+      children: ["\n", (0,jsx_runtime.jsx)(_components.li, {
+        children: "När ett formulär ska skickas in."
+      }), "\n", (0,jsx_runtime.jsx)(_components.li, {
+        children: "För att initiera en sökning"
+      }), "\n", (0,jsx_runtime.jsx)(_components.li, {
+        children: "För att öppna en modal"
+      }), "\n"]
+    }), "\n", (0,jsx_runtime.jsx)(CodeBlock/* default */.A, {
+      hideCode: true,
+      scope: {
+        Button: Button/* Button */.$
+      },
+      children: `<Button onPress={() => { alert('Du tryckte på knappen och här är din popup');}}>Visa en popup</Button>`
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "länkar",
+      children: "Länkar"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Länkar används för att ta användaren till en ny sida eller en annan del av en sida/applikation. På en webbsida eller i en webbapplikation syns detta som en ny URL."
+    }), "\n", (0,jsx_runtime.jsx)(_components.h3, {
+      id: "länkknapp",
+      children: "Länkknapp"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Länkknappen är tekniskt en länk men visuellt lik en knapp. Den används vid navigering när det behövs en tydligare visuell signal till användaren att välja länken, än vad en vanlig länk ger.\nAnvändaren ska uppfatta att en handling utförs. Exempel på situationer där det är lämpligt med en länkknapp:"
+    }), "\n", (0,jsx_runtime.jsxs)(_components.ul, {
+      children: ["\n", (0,jsx_runtime.jsx)(_components.li, {
+        children: "Vid navigering inom en applikation"
+      }), "\n", (0,jsx_runtime.jsx)(_components.li, {
+        children: "Som en startpunkt, t.ex. en länk från webben till en e-tjänst"
+      }), "\n"]
+    }), "\n", (0,jsx_runtime.jsx)(CodeBlock/* default */.A, {
+      hideCode: true,
+      scope: {
+        LinkButton: LinkButton/* LinkButton */.z
+      },
+      children: `<LinkButton href="/components/link-button">{'Till sidan för Länkknapp'}</LinkButton>`
+    }), "\n", (0,jsx_runtime.jsx)(_components.h3, {
+      id: "fristående-länk",
+      children: "Fristående länk"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Fristående länk är en variant av länk som används när en länk ligger utanför ett textstycke."
+    }), "\n", (0,jsx_runtime.jsx)(CodeBlock/* default */.A, {
+      hideCode: true,
+      scope: {
+        Link: Link/* Link */.N
+      },
+      children: `<Link standalone href="/components/link">{'Läs mer om länkar'}</Link>`
+    }), "\n", (0,jsx_runtime.jsx)(_components.h3, {
+      id: "länk",
+      children: "Länk"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Länk används när länken är i ett textstycke."
+    }), "\n", (0,jsx_runtime.jsx)(CodeBlock/* default */.A, {
+      hideCode: true,
+      scope: {
+        Link: Link/* Link */.N
+      },
+      children: `<p>Designsystemet har två olika varianter av <Link href="/components/link">{'länkar'}</Link>.</p>`
+    })]
+  });
+}
+function MDXContent(props = {}) {
+  const {wrapper: MDXLayout} = {
+    ...(0,lib/* useMDXComponents */.R)(),
+    ...props.components
+  };
+  return MDXLayout ? (0,jsx_runtime.jsx)(MDXLayout, {
+    ...props,
+    children: (0,jsx_runtime.jsx)(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+
+
+/***/ })
+
+}]);
