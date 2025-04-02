8317d3da5cfc75ef38a00f6f80e323f69dfdc969
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-04-02 10:03:28 +0200

    refactor(textfield): ♻️💥 code separation of textfield and textarea components (#380)
    BREAKING CHANGE: domain specific regex validation removed

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -1,7 +1,39 @@
 import styles from './app.module.css'
+import { TextArea, TextField } from '@midas-ds/components'
+import { I18nProvider } from 'react-aria-components'
 
 export function App() {
-  return <div className={styles.container}></div>
+  return (
+    <div className={styles.container}>
+      <TextField
+        label='Label'
+        description='Description'
+        showCounter
+        type='email'
+        maxLength={4}
+      />
+      <TextField
+        label='Label'
+        description='Description'
+        type='password'
+        validate={value => (value === 'Skriv inte hej' ? 'error' : true)}
+      />
+      <TextArea
+        label='Label'
+        description='Skriv hej'
+        validate={value => (value === 'Skriv inte hej' ? 'error' : true)}
+      />
+      <I18nProvider locale={'de-DE'}>
+        <TextField type={'password'}></TextField>
+      </I18nProvider>
+      <I18nProvider locale={'ko'}>
+        <TextField type={'password'}></TextField>
+      </I18nProvider>
+      <I18nProvider locale={'ar'}>
+        <TextField type={'password'}></TextField>
+      </I18nProvider>
+    </div>
+  )
 }
 
 export default App
---
+++ packages/components/src/utils/intl/index.ts
@@ -0,0 +1,3 @@
+export * from './useLocalizedStringFormatter'
+export * from './useMessageFormatter'
+
