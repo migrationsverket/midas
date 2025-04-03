65e20e1fdd424f6865834c9d18f35de485a194c7
derpbravely <axel.engstrom@migrationsverket.se>
2025-04-03 14:16:08 +0200

    chore: use docusaurus link

--- apps/docs/src/components/Link.tsx
+++ apps/docs/src/components/Link.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import { Link as MidasLink, type LinkProps } from '@midas-ds/components'
-import useBrokenLinks from '@docusaurus/useBrokenLinks'
+import DocusaurusLink from '@docusaurus/Link'
 
 /**
  * A wrapper to implement docusaurus broken link collector around Midas Link
  */
-export const Link: React.FC<LinkProps<typeof MidasLink>> = props => {
-  const brokenLinks = useBrokenLinks()
-
-  // Only check internal links
-  if (!props.href.includes('//')) {
-    brokenLinks.collectLink(props.href)
-  }
-
-  return <MidasLink {...props} />
-}
+export const Link: React.FC<LinkProps<typeof DocusaurusLink>> = props => (
+  <MidasLink
+    as={DocusaurusLink}
+    {...props}
+  />
+)
