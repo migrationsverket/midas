554e3ae8df39fc344d889497e592abf3e19d5956
derpbravely <axel.engstrom@migrationsverket.se>
2025-04-03 10:17:31 +0200

    test: ignore util files in jest unit tests

--- packages/components/jest.config.ts
+++ packages/components/jest.config.ts
@@ -3,10 +3,11 @@ export default {
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }]
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/components',
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   testEnvironment: 'jsdom',
+  modulePathIgnorePatterns: ['<rootDir>/src/utils'],
 }
