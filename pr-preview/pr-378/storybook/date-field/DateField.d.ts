51cc00a85dd435a8001fd895d55056088708a072
derpbravely <derpbravely@users.noreply.github.com>
2025-04-04 07:22:26 +0000

    Remove preview for PR 378 🛬

--- pr-preview/pr-378/storybook/date-field/DateField.d.ts
+++
@@ -1,8 +0,0 @@
-import { DateFieldProps as AriaDateFieldProps, DateValue, ValidationResult } from 'react-aria-components';
-interface DateFieldProps<T extends DateValue> extends AriaDateFieldProps<T> {
-    label?: string;
-    description?: string;
-    errorMessage?: string | ((validation: ValidationResult) => string);
-}
-export declare function DateField<T extends DateValue>({ label, description, errorMessage, className, ...props }: DateFieldProps<T>): import("react/jsx-runtime").JSX.Element;
-export {};
