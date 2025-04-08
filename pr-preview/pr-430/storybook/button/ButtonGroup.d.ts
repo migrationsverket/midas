7ffdcca4846990877946350825415dce87ccf21c
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-04-08 06:43:22 +0000

    Remove preview for PR 430 🛬

--- pr-preview/pr-430/storybook/button/ButtonGroup.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-/**
- * Group several buttons together.
- *
- * @interface React.HTMLAttributes<HTMLDivElement>
- *
- * @see {@link https://designsystem.migrationsverket.se/components/button}
- */
-export declare const ButtonGroup: React.FC<React.HTMLAttributes<HTMLDivElement>>;
