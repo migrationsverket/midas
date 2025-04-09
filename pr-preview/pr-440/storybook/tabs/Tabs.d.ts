0bf64f912781bca20aa31dddf4ac942a22cb8723
derpbravely <derpbravely@users.noreply.github.com>
2025-04-09 08:00:28 +0000

    Deploy preview for PR 440 🛫

--- pr-preview/pr-440/storybook/tabs/Tabs.d.ts
+++ pr-preview/pr-440/storybook/tabs/Tabs.d.ts
@@ -1,12 +1,20 @@
 import * as React from 'react';
-export interface TabsProps<T extends string> {
-    /** An array of tab titles */
-    tabs: T[];
-    /** Lable for accecibility */
+export interface TabsProps {
+    /**
+     * An array of tab titles
+     */
+    tabs: string[];
+    /**
+     * Label for accessibility
+     */
     label: string;
-    /** Choose another than the first tab to be selected by default. Name must match one of the tabs */
-    defaultSelected?: T;
-    /** Amount of children must match the amount of tabs */
+    /**
+     * Choose another than the first tab to be selected by default. Name must match one of the tabs
+     */
+    defaultSelected?: string;
+    /**
+     * Amount of children must match the amount of tabs
+     */
     children: React.ReactNode;
 }
-export declare const Tabs: <T extends string>({ tabs, label, defaultSelected, children }: TabsProps<T>) => import("react/jsx-runtime").JSX.Element;
+export declare const Tabs: React.FC<TabsProps>;
