024726e8f18d460dd887b61c89fe6796cf558b81
derpbravely <axel.engstrom@migrationsverket.se>
2025-04-09 14:15:30 +0200

    refactor(spinner): update comments and set default values

--- packages/components/src/spinner/Spinner.tsx
+++ packages/components/src/spinner/Spinner.tsx
@@ -3,24 +3,26 @@ import { LoaderCircle } from 'lucide-react'
 import clsx from 'clsx'
 
 export interface SpinnerProps {
-  /** Displays a smaller when set to true
+  /** Displays a smaller spinner when set to true
    * @default false
-   * */
+   */
   small?: boolean
   /** For use on coloured background
    * @default false
-   * */
+   */
   isOnColor?: boolean
-
   /** For use on dark background
    * @default false
    * @deprecated please use `isOnColor` instead
-   * */
-
+   */
   dark?: boolean
 }
 
-export const Spinner: React.FC<SpinnerProps> = ({ small, isOnColor, dark }) => {
+export const Spinner: React.FC<SpinnerProps> = ({
+  small,
+  isOnColor = false,
+  dark = false,
+}) => {
   return (
     <div
       className={styles.container}
