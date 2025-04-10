ef518415876a8fce4387a8c3cea5e5b2f1198ab9
derpbravely <derpbravely@users.noreply.github.com>
2025-04-10 13:20:27 +0000

    Deploy preview for PR 450 🛫

--- pr-preview/pr-450/storybook/select/useMultiSelectState.d.ts
+++ pr-preview/pr-450/storybook/select/useMultiSelectState.d.ts
@@ -1,9 +1,14 @@
 import { MenuTriggerState } from '@react-stately/menu';
+import { Key } from 'react-aria';
 import { MultiSelectListState } from './useMultiSelectListState';
 import { OverlayTriggerProps } from '@react-types/overlays';
 import { AsyncLoadable, CollectionBase, FocusableProps, InputBase, LabelableProps, MultipleSelection, TextInputBase, Validation } from '@react-types/shared';
 import { FormValidationState } from '@react-stately/form';
-export interface MultiSelectProps<T> extends CollectionBase<T>, AsyncLoadable, Omit<InputBase, 'isReadOnly'>, Validation, LabelableProps, TextInputBase, MultipleSelection, FocusableProps, OverlayTriggerProps {
+/** Added this for a better output, will see how this plays out */
+interface ArraySelection extends Omit<MultipleSelection, 'onSelectionChange'> {
+    onSelectionChange?: (value: Set<Key>) => void;
+}
+export interface MultiSelectProps<T> extends CollectionBase<T>, AsyncLoadable, Omit<InputBase, 'isReadOnly'>, Validation, LabelableProps, TextInputBase, ArraySelection, FocusableProps, OverlayTriggerProps {
     /**
      * Whether the menu should automatically flip direction when space is limited.
      * @default true
@@ -17,3 +22,4 @@ export interface MultiSelectState<T> extends MultiSelectListState<T>, MenuTrigge
     setFocused(isFocused: boolean): void;
 }
 export declare function useMultiSelectState<T extends object>(props: MultiSelectProps<T>): MultiSelectState<T>;
+export {};
