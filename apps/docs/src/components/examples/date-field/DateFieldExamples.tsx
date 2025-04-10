47bb4f4dd6647fb582bdab4410f498ccd8f2b0b3
pixelrickdreamer <jakob.sanne-grubbstrom@migrationsverket.se>
2025-04-10 19:21:46 +0200

    docs(date-field): fix lint error

--- apps/docs/src/components/examples/date-field/DateFieldExamples.tsx
+++ apps/docs/src/components/examples/date-field/DateFieldExamples.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { DateField } from '@midas-ds/components'
-import { now, toCalendarDate, getLocalTimeZone } from '@internationalized/date'
+import { now, toCalendarDate, getLocalTimeZone, CalendarDate } from '@internationalized/date'
 
 export const DateFieldControlled = () => {
   const today = toCalendarDate(now(getLocalTimeZone()))
-  const [date, setDate] = React.useState(today)
+  const [date, setDate] = React.useState<CalendarDate | null>(today)
   return (
     <>
       <DateField
         value={date}
-        onChange={value => (value!! ? setDate(value) : () => {})}
+        onChange={setDate}
       ></DateField>
       <pre>Du har valt datumet: {date?.toString()}</pre>
     </>
