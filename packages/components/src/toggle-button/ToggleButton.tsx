85eb06f322d015692b0279acc8a2d2ea6604dd8e
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2025-04-10 11:29:46 +0200

    feat(color-scheme-switch): new component to let users easily switch color-scheme in ui

---
+++ packages/components/src/toggle-button/ToggleButton.tsx
@@ -0,0 +1,21 @@
+import {
+  ToggleButton as AriaToggleButton,
+  ToggleButtonProps,
+} from 'react-aria-components'
+import ButtonStyles from '../button/Button.module.css'
+import clsx from 'clsx'
+
+export const ToggleButton: React.FC<
+  ToggleButtonProps & React.RefAttributes<HTMLButtonElement>
+> = ({ ...rest }) => {
+  return (
+    <AriaToggleButton
+      {...rest}
+      className={clsx(
+        ButtonStyles.button,
+        ButtonStyles.iconBtn,
+        rest.className,
+      )}
+    />
+  )
+}
