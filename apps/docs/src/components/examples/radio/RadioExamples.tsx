80e1062d93b11294ea8e7d78c4d4a6894f8064c1
jabir Khalil <jabir.saadi.khalil@migrationsverket.se>
2025-04-10 15:51:34 +0200

    docs(radio): add control mode

--- apps/docs/src/components/examples/radio/RadioExamples.tsx
+++ apps/docs/src/components/examples/radio/RadioExamples.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RadioGroup, Radio } from '@midas-ds/components'
 
 const fruits = ['Äpple', 'Banan', 'Kiwi', 'Apelsin']
@@ -19,3 +19,25 @@ export const BasicExample: React.FC = () => (
     ))}
   </RadioGroup>
 )
+
+export const ControlledExample = () => {
+  const [selectedFruit, setSelectedFruit] = useState(fruits[2])
+
+  return (
+    <RadioGroup
+      label='Välj frukt'
+      description='Du kan bara välja en'
+      value={selectedFruit}
+      onChange={(value: string) => setSelectedFruit(value)}
+    >
+      {fruits.slice(0, 4).map(fruit => (
+        <Radio
+          key={fruit}
+          value={fruit}
+        >
+          {fruit}
+        </Radio>
+      ))}
+    </RadioGroup>
+  )
+}
