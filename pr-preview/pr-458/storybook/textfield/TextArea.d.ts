2734d5962df177458b93f8fc66c4250185c591d4
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-04-11 17:55:52 +0000

    Remove preview for PR 458 🛬

--- pr-preview/pr-458/storybook/textfield/TextArea.d.ts
+++
@@ -1,7 +0,0 @@
-import { TextFieldBaseProps } from './TextFieldBase';
-import { TextAreaProps as AriaTextAreaProps } from 'react-aria-components';
-import * as React from 'react';
-export interface TextAreaProps extends Omit<TextFieldBaseProps, 'children' | 'type'> {
-    className?: AriaTextAreaProps['className'];
-}
-export declare const TextArea: React.ForwardRefExoticComponent<TextAreaProps & React.RefAttributes<HTMLTextAreaElement>>;
