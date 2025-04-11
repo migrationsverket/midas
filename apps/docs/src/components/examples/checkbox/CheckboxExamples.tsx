db851396540ca22846a09616eea9c02572073470
derpbravely <axel.engstrom@migrationsverket.se>
2025-04-11 11:04:06 +0200

    docs(checkbox): add controlled and uncontrolled examples

--- apps/docs/src/components/examples/checkbox/CheckboxExamples.tsx
+++ apps/docs/src/components/examples/checkbox/CheckboxExamples.tsx
@@ -1,15 +1,38 @@
-import { Checkbox } from '@midas-ds/components'
-import React, { useState } from 'react'
+import React from 'react'
+import { Checkbox, CheckboxGroup } from '@midas-ds/components'
 
 export const ControlledExample = () => {
-  const [isChecked, setIsChecked] = useState(true)
+  const [isSelected, setIsSelected] = React.useState(false)
 
   return (
-    <Checkbox
-      isSelected={isChecked}
-      onChange={(checked: boolean) => setIsChecked(checked)}
-    >
-      Jag godkänner villkoren
-    </Checkbox>
+    <>
+      <Checkbox
+        isSelected={isSelected}
+        onChange={setIsSelected}
+      >
+        Jag godkänner villkoren
+      </Checkbox>
+      <pre>Du har{isSelected ? '' : ' inte'} godkänt villkoren</pre>
+    </>
+  )
+}
+
+export const ControlledGroupExample = () => {
+  const [value, setValue] = React.useState<string[]>()
+
+  return (
+    <>
+      <CheckboxGroup
+        label='Favoritfrukt'
+        value={value}
+        onChange={setValue}
+      >
+        <Checkbox value='banan'>Banan</Checkbox>
+        <Checkbox value='melon'>Melon</Checkbox>
+        <Checkbox value='kiwi'>Kiwi</Checkbox>
+        <Checkbox value='satsumas'>Satsumas</Checkbox>
+      </CheckboxGroup>
+      <pre>Valda alternativ: {value?.join(', ')}</pre>
+    </>
   )
 }
