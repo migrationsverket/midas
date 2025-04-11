d8f11f881874e2e6bc77e0a7206d9c58c6e0d0ad
derpbravely <axel.engstrom@migrationsverket.se>
2025-04-11 13:50:36 +0200

    docs(combobox): update controlled example

--- apps/docs/src/components/examples/combobox/ComboboxExamples.tsx
+++ apps/docs/src/components/examples/combobox/ComboboxExamples.tsx
@@ -1,59 +1,37 @@
 import { ComboBox, ComboBoxItem } from '@midas-ds/components'
-import React, { useState } from 'react'
+import React from 'react'
+import { Key } from 'react-aria-components'
 
 export const BasicExample: React.FC = () => (
   <ComboBox
     label='Välj din favoritfrukt'
     description='Du kan bara välja en'
   >
-    <ComboBoxItem key='Äpple'>Äpple</ComboBoxItem>
-    <ComboBoxItem key='Banan'>Banan</ComboBoxItem>
-    <ComboBoxItem key='Kiwi'>Kiwi</ComboBoxItem>
-    <ComboBoxItem key='Apelsin'>Apelsin</ComboBoxItem>
+    <ComboBoxItem id='Äpple'>Äpple</ComboBoxItem>
+    <ComboBoxItem id='Banan'>Banan</ComboBoxItem>
+    <ComboBoxItem id='Kiwi'>Kiwi</ComboBoxItem>
+    <ComboBoxItem id='Apelsin'>Apelsin</ComboBoxItem>
   </ComboBox>
 )
 
 export const ControlledExample = () => {
-  const [selectedFruit, setSelectedFruit] = useState<string>('Kiwi')
+  const [selectedFruit, setSelectedFruit] = React.useState<Key | null>(null)
 
   return (
     <>
       <ComboBox
         label='Välj din favoritfrukt'
         description='Du kan bara välja en'
-        placeholder={selectedFruit}
-        onSelectionChange={key => {
-          console.log('Selected key:', key as string)
-          setSelectedFruit(key as string)
-        }}
+        selectedKey={selectedFruit}
+        onSelectionChange={setSelectedFruit}
       >
-        <ComboBoxItem
-          key='Äpple'
-          textValue='Äpple'
-        >
-          Äpple
-        </ComboBoxItem>
-        <ComboBoxItem
-          key='Banan'
-          textValue='Banan'
-        >
-          Banan
-        </ComboBoxItem>
-        <ComboBoxItem
-          key='Kiwi'
-          textValue='Kiwi'
-        >
-          Kiwi
-        </ComboBoxItem>
-        <ComboBoxItem
-          key='Apelsin'
-          textValue='Apelsin'
-        >
-          Apelsin
-        </ComboBoxItem>
+        <ComboBoxItem id='Äpple'>Äpple</ComboBoxItem>
+        <ComboBoxItem id='Banan'>Banan</ComboBoxItem>
+        <ComboBoxItem id='Kiwi'>Kiwi</ComboBoxItem>
+        <ComboBoxItem id='Apelsin'>Apelsin</ComboBoxItem>
       </ComboBox>
 
-      <div style={{ marginTop: '1rem' }}>selectedFruit: {selectedFruit}</div>
+      <pre style={{ marginTop: '1rem' }}>Vald frukt: {selectedFruit}</pre>
     </>
   )
 }
