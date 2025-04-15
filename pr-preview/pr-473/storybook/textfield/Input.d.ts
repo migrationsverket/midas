92c01d1bd75e79510619a1607a1c51b2891bd422
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-04-15 07:37:47 +0000

    Deploy preview for PR 473 🛫

---
+++ pr-preview/pr-473/storybook/textfield/Input.d.ts
@@ -0,0 +1,4 @@
+import { InputProps } from 'react-aria-components';
+import * as React from 'react';
+export type { InputProps };
+export declare const Input: React.ForwardRefExoticComponent<InputProps & React.RefAttributes<HTMLInputElement>>;
