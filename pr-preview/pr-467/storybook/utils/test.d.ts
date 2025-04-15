c67a7784ea19ac340774487d3025edc8dcc844a1
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-04-15 07:08:30 +0000

    Remove preview for PR 467 🛬

--- pr-preview/pr-467/storybook/utils/test.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * A utility for making visual assertions in Storybook play tests based on the browsers "prefers-colors-scheme" setting
- */
-export declare const lightDark: (light: string, dark: string) => string;
