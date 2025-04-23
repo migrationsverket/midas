78dbac75c79bea9e0dd5a696aca0702d39cc4192
derpbravely <derpbravely@users.noreply.github.com>
2025-04-23 09:26:31 +0000

    Deploy preview for PR 499 🛫

---
+++ pr-preview/pr-499/storybook/textfield/TextArea.d.ts
@@ -0,0 +1,6 @@
+import { TextFieldBaseProps } from './TextFieldBase';
+import { TextAreaProps as AriaTextAreaProps } from 'react-aria-components';
+import * as React from 'react';
+export interface TextAreaProps extends Omit<TextFieldBaseProps, 'children'>, Omit<AriaTextAreaProps, 'autoComplete' | 'children' | 'defaultValue' | 'onBeforeInput' | 'onBlur' | 'onChange' | 'onCompositionEnd' | 'onCompositionStart' | 'onCompositionUpdate' | 'onCopy' | 'onCut' | 'onFocus' | 'onInput' | 'onKeyDown' | 'onKeyUp' | 'onPaste' | 'onSelect' | 'slot' | 'spellCheck' | 'style' | 'type' | 'value'> {
+}
+export declare const TextArea: React.ForwardRefExoticComponent<TextAreaProps & React.RefAttributes<HTMLTextAreaElement>>;
