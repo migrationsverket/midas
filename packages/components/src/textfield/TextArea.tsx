8e780ff53a0c06c0d79f4a2f200434c36e2c7793
derpbravely <axel.engstrom@migrationsverket.se>
2025-04-23 11:22:16 +0200

    fix(textfield): merge textfield and input/textarea native dom props

--- packages/components/src/textfield/TextArea.tsx
+++ packages/components/src/textfield/TextArea.tsx
@@ -10,14 +10,58 @@ import clsx from 'clsx'
 import styles from './TextField.module.css'
 
 export interface TextAreaProps
-  extends Omit<TextFieldBaseProps, 'children' | 'type'> {
-  className?: AriaTextAreaProps['className']
-}
+  extends Omit<TextFieldBaseProps, 'children' | 'type'>,
+    Omit<
+      AriaTextAreaProps,
+      | 'autoComplete'
+      | 'children'
+      | 'defaultValue'
+      | 'onBeforeInput'
+      | 'onBlur'
+      | 'onChange'
+      | 'onCompositionEnd'
+      | 'onCompositionStart'
+      | 'onCompositionUpdate'
+      | 'onCopy'
+      | 'onCut'
+      | 'onFocus'
+      | 'onInput'
+      | 'onKeyDown'
+      | 'onKeyUp'
+      | 'onPaste'
+      | 'onSelect'
+      | 'slot'
+      | 'spellCheck'
+      | 'style'
+      | 'type'
+      | 'value'
+    > {}
 
 export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
   ({ className, ...props }, ref) => (
     <TextFieldBase {...props}>
       <AriaTextArea
+        {...props}
+        autoComplete={undefined}
+        defaultValue={undefined}
+        onBeforeInput={undefined}
+        onBlur={undefined}
+        onChange={undefined}
+        onCompositionEnd={undefined}
+        onCompositionStart={undefined}
+        onCompositionUpdate={undefined}
+        onCopy={undefined}
+        onCut={undefined}
+        onFocus={undefined}
+        onInput={undefined}
+        onKeyDown={undefined}
+        onKeyUp={undefined}
+        onPaste={undefined}
+        onSelect={undefined}
+        slot={undefined}
+        spellCheck={undefined}
+        style={undefined}
+        value={undefined}
         className={clsx(styles.textArea, className)}
         ref={ref}
       />
--- packages/components/src/textfield/TextField.tsx
+++ packages/components/src/textfield/TextField.tsx
@@ -4,14 +4,60 @@ import * as React from 'react'
 import { TextFieldBase, type TextFieldBaseProps } from './TextFieldBase'
 import { Input, type InputProps } from './Input'
 
-export interface TextFieldProps extends Omit<TextFieldBaseProps, 'children'> {
-  className?: InputProps['className']
-}
+export interface TextFieldProps
+  extends Omit<TextFieldBaseProps, 'children'>,
+    Omit<
+      InputProps,
+      | 'autoComplete'
+      | 'children'
+      | 'defaultValue'
+      | 'onBeforeInput'
+      | 'onBlur'
+      | 'onChange'
+      | 'onCompositionEnd'
+      | 'onCompositionStart'
+      | 'onCompositionUpdate'
+      | 'onCopy'
+      | 'onCut'
+      | 'onFocus'
+      | 'onInput'
+      | 'onKeyDown'
+      | 'onKeyUp'
+      | 'onPaste'
+      | 'onSelect'
+      | 'slot'
+      | 'spellCheck'
+      | 'style'
+      | 'type'
+      | 'value'
+    > {}
 
 export const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
   ({ className, ...props }, ref) => (
     <TextFieldBase {...props}>
       <Input
+        {...props}
+        autoComplete={undefined}
+        defaultValue={undefined}
+        onBeforeInput={undefined}
+        onBlur={undefined}
+        onChange={undefined}
+        onCompositionEnd={undefined}
+        onCompositionStart={undefined}
+        onCompositionUpdate={undefined}
+        onCopy={undefined}
+        onCut={undefined}
+        onFocus={undefined}
+        onInput={undefined}
+        onKeyDown={undefined}
+        onKeyUp={undefined}
+        onPaste={undefined}
+        onSelect={undefined}
+        slot={undefined}
+        spellCheck={undefined}
+        style={undefined}
+        type={undefined}
+        value={undefined}
         className={className}
         ref={ref}
       />
