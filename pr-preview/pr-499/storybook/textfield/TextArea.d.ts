07e7d75e19965628bd2701034c4fef3b5d6001b1
derpbravely <derpbravely@users.noreply.github.com>
2025-04-23 11:53:10 +0000

    Deploy preview for PR 499 🛫

--- pr-preview/pr-499/storybook/textfield/TextArea.d.ts
+++ pr-preview/pr-499/storybook/textfield/TextArea.d.ts
@@ -1,6 +1,7 @@
 import { TextFieldBaseProps } from './TextFieldBase';
 import { TextAreaProps as AriaTextAreaProps } from 'react-aria-components';
 import * as React from 'react';
-export interface TextAreaProps extends Omit<TextFieldBaseProps, 'children' | 'type'>, Omit<AriaTextAreaProps, 'autoComplete' | 'children' | 'defaultValue' | 'onBeforeInput' | 'onBlur' | 'onChange' | 'onCompositionEnd' | 'onCompositionStart' | 'onCompositionUpdate' | 'onCopy' | 'onCut' | 'onFocus' | 'onInput' | 'onKeyDown' | 'onKeyUp' | 'onPaste' | 'onSelect' | 'slot' | 'spellCheck' | 'style' | 'type' | 'value'> {
+export interface TextAreaProps extends Omit<TextFieldBaseProps, 'children' | 'type'> {
+    inputProps?: AriaTextAreaProps;
 }
 export declare const TextArea: React.ForwardRefExoticComponent<TextAreaProps & React.RefAttributes<HTMLTextAreaElement>>;
--- pr-preview/pr-499/storybook/textfield/TextField.d.ts
+++ pr-preview/pr-499/storybook/textfield/TextField.d.ts
@@ -1,6 +1,7 @@
 import { TextFieldBaseProps } from './TextFieldBase';
 import { InputProps } from './Input';
 import * as React from 'react';
-export interface TextFieldProps extends Omit<TextFieldBaseProps, 'children'>, Omit<InputProps, 'autoComplete' | 'children' | 'defaultValue' | 'onBeforeInput' | 'onBlur' | 'onChange' | 'onCompositionEnd' | 'onCompositionStart' | 'onCompositionUpdate' | 'onCopy' | 'onCut' | 'onFocus' | 'onInput' | 'onKeyDown' | 'onKeyUp' | 'onPaste' | 'onSelect' | 'slot' | 'spellCheck' | 'style' | 'type' | 'value'> {
+export interface TextFieldProps extends Omit<TextFieldBaseProps, 'children'> {
+    inputProps?: InputProps;
 }
 export declare const TextField: React.ForwardRefExoticComponent<TextFieldProps & React.RefAttributes<HTMLInputElement>>;
