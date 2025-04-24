1c704b714d342fa38469b88009fa3a04f0103c31
derpbravely <axel.engstrom@migrationsverket.se>
2025-04-24 09:58:42 +0200

    fix(accordion): remove disabled styling if component has disabled children

--- packages/components/src/utils/test.ts
+++ packages/components/src/utils/test.ts
@@ -5,3 +5,17 @@ export const lightDark = (light: string, dark: string) =>
   window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
     ? dark
     : light
+
+/**
+ * Convert a CSS hex color to CSS rgb color
+ * ```
+ * '#f2f2f2' => 'rgb(242, 242, 242)'
+ * ```
+ */
+export const hexToRgb = (hex: string) => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+
+  return result
+    ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})`
+    : ''
+}
