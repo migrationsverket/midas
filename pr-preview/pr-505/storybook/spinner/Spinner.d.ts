fa946cad10c3941287752f2e5108076f823ae260
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-04-25 06:30:27 +0000

    Deploy preview for PR 505 🛫

---
+++ pr-preview/pr-505/storybook/spinner/Spinner.d.ts
@@ -0,0 +1,16 @@
+export interface SpinnerProps {
+    /** Displays a smaller spinner when set to true
+     * @default false
+     */
+    small?: boolean;
+    /** For use on coloured background
+     * @default false
+     */
+    isOnColor?: boolean;
+    /** For use on dark background
+     * @default false
+     * @deprecated please use `isOnColor` instead
+     */
+    dark?: boolean;
+}
+export declare const Spinner: React.FC<SpinnerProps>;
