451fdb8f667dc4c5e03dae2a632cb4a86f25f2e9
derpbravely <derpbravely@users.noreply.github.com>
2025-04-25 09:21:39 +0000

    Deploy preview for PR 504 🛫

--- pr-preview/pr-504/storybook/calendar/components/CalendarHeader.d.ts
+++ pr-preview/pr-504/storybook/calendar/components/CalendarHeader.d.ts
@@ -1,7 +1,7 @@
 import { CalendarGridProps } from 'react-aria-components';
 import * as React from 'react';
 export interface CalendarHeaderProps {
-    weekdayStyle?: CalendarGridProps['weekdayStyle'];
     errorMessage?: string;
+    weekdayStyle?: CalendarGridProps['weekdayStyle'];
 }
 export declare const CalendarHeader: React.FC;
