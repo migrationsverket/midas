a60f3a3bcfee1b8abd51c016dcbe520e838971d4
derpbravely <derpbravely@users.noreply.github.com>
2025-04-25 12:46:26 +0000

    Deploy preview for PR 508 🛫

---
+++ pr-preview/pr-508/storybook/field-error/FieldError.d.ts
@@ -0,0 +1,3 @@
+import { FieldErrorProps } from 'react-aria-components';
+import * as React from 'react';
+export declare const FieldError: React.ForwardRefExoticComponent<FieldErrorProps & React.RefAttributes<HTMLElement>>;
