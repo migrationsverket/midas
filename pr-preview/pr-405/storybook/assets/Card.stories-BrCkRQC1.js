eb241a0e76713216ccebf75c5a290ed73b5cf6cf
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-04-28 11:35:42 +0000

    Deploy preview for PR 405 🛫

---
+++ pr-preview/pr-405/storybook/assets/Card.stories-BrCkRQC1.js
@@ -0,0 +1,138 @@
+import{j as n}from"./jsx-runtime-DtgEGaVE.js";import{C as O,a as t,b as r,c as g,d as q,e as b,f as X}from"./Card-BwuN3AoJ.js";import{e,u as C}from"./index-DtSMq7G1.js";import{B as d}from"./Button-DytQ-ITy.js";import{c as G}from"./createLucideIcon-8wDOqnHv.js";import{X as M}from"./x-Cu6J0Y2u.js";import{T as s}from"./Text-BO1MQlio.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./Button-C6eeUJZ7.js";import"./utils-Dv9Ux5aE.js";import"./ProgressBar-DJC3JvQN.js";import"./Label-Cr3pFc-x.js";import"./Hidden-lswqw32w.js";import"./filterDOMProps-BSfnXAP7.js";import"./useLabel-BAqrTMfP.js";import"./context-oTIQb5ND.js";import"./NumberFormatter-DT0cDaax.js";import"./LiveAnnouncer-DFDjXwI8.js";import"./useButton-BH_mgHDo.js";import"./usePress-COtC8Zcb.js";import"./useFocusRing-CPQALXIu.js";import"./index-cpIEhwLo.js";import"./useFocusable-b_0ijIRa.js";import"./Link-slNGPAHu.js";import"./arrow-right-DnGf4Axc.js";import"./Heading-DmTsGMz3.js";import"./RSPContexts-CeCCo7XS.js";import"./Button.module-kL9Mhzoi.js";import"./Text-D0cNLqf0.js";/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const j=G("Pen",[["path",{d:"M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z",key:"1a8usu"}]]),kn={component:O,title:"Components/Card",tags:["autodocs"]},c={args:{children:n.jsxs(t,{children:[n.jsx(r,{children:"Dina uppgifter"}),n.jsx(s,{children:"Namn: Namn Namnsson"}),n.jsx(g,{children:n.jsx(d,{variant:"icon",icon:j,children:"Redigera"})})]})}},p={args:{...c.args,horizontal:!0}},m={args:{children:n.jsxs(t,{children:[n.jsx(r,{children:"Dina uppgifter"}),n.jsx(s,{children:"Namn: Namn Namnsson"}),n.jsxs(g,{children:[n.jsx(d,{variant:"icon",icon:j,children:"Ändra"}),n.jsx(d,{variant:"icon",icon:M,children:"Avbryt"})]})]})},play:async({canvas:o,step:i})=>{await i("It should be possible to focus the button",async()=>{const a=o.getByText("Avbryt");e(a).not.toHaveFocus(),await C.tab(),await C.tab(),e(a).toHaveFocus()})}},l={args:{children:n.jsxs(n.Fragment,{children:[n.jsx(q,{onPress:()=>{},"data-testid":"card-action-area",children:n.jsxs(t,{children:[n.jsx(r,{children:"Dina uppgifter"}),n.jsx(s,{children:"Namn: Namn Namnsson"})]})}),n.jsx(t,{children:n.jsxs(g,{children:[n.jsx(d,{variant:"icon",icon:j,children:"Ändra"}),n.jsx(d,{variant:"icon",icon:M,children:"Avbryt"})]})})]})},play:async({canvas:o,step:i})=>{await i("It should be possible to focus the primary action area",async()=>{const a=o.getByTestId("card-action-area");e(a).not.toHaveFocus(),await C.tab(),e(a).toHaveFocus()})}},u={args:{children:n.jsxs(t,{children:[n.jsx(b,{href:"#","data-testid":"card-link",children:n.jsx(r,{children:"Dina uppgifter"})}),n.jsx(s,{children:"Namn: Namn Namnsson"})]})},play:async({canvas:o,step:i})=>{await i("It should be possible to focus the link",async()=>{const a=o.getByTestId("card-link");e(a).not.toHaveFocus(),await C.tab(),e(a).toHaveFocus()})}},h={args:{style:{maxWidth:300},children:n.jsxs(n.Fragment,{children:[n.jsx(X,{src:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Pineapple_and_cross_section.jpg/640px-Pineapple_and_cross_section.jpg",alt:"Ananas"}),n.jsxs(t,{children:[n.jsx(b,{href:"#","data-testid":"card-link",children:n.jsx(r,{children:"Dina uppgifter"})}),n.jsx(s,{children:"Namn: Namn Namnsson"})]})]})}},x={args:{style:{maxWidth:300},children:n.jsxs(t,{children:[n.jsx(b,{href:"#","data-testid":"card-link",children:n.jsx(r,{children:"Dina uppgifter"})})," ",n.jsx(X,{src:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Pineapple_and_cross_section.jpg/640px-Pineapple_and_cross_section.jpg",alt:"Ananas"}),n.jsx(s,{children:"Namn: Namn Namnsson"})]})}};var v,y,f;c.parameters={...c.parameters,docs:{...(v=c.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    children: <CardContent>
+        <CardTitle>Dina uppgifter</CardTitle>
+        <Text>Namn: Namn Namnsson</Text>
+        <CardActions>
+          <Button variant='icon' icon={Pen}>
+            Redigera
+          </Button>
+        </CardActions>
+      </CardContent>
+  }
+}`,...(f=(y=c.parameters)==null?void 0:y.docs)==null?void 0:f.source}}};var N,k,T;p.parameters={...p.parameters,docs:{...(N=p.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    ...Example.args,
+    horizontal: true
+  }
+}`,...(T=(k=p.parameters)==null?void 0:k.docs)==null?void 0:T.source}}};var A,_,w;m.parameters={...m.parameters,docs:{...(A=m.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    children: <CardContent>
+        <CardTitle>Dina uppgifter</CardTitle>
+        <Text>Namn: Namn Namnsson</Text>
+        <CardActions>
+          <Button variant='icon' icon={Pen}>
+            Ändra
+          </Button>
+          <Button variant='icon' icon={X}>
+            Avbryt
+          </Button>
+        </CardActions>
+      </CardContent>
+  },
+  play: async ({
+    canvas,
+    step
+  }) => {
+    await step('It should be possible to focus the button', async () => {
+      const button = canvas.getByText('Avbryt');
+      expect(button).not.toHaveFocus();
+
+      // focus the link
+      await userEvent.tab();
+      await userEvent.tab();
+      expect(button).toHaveFocus();
+    });
+  }
+}`,...(w=(_=m.parameters)==null?void 0:_.docs)==null?void 0:w.source}}};var B,I,P;l.parameters={...l.parameters,docs:{...(B=l.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <CardActionArea onPress={() => {
+        return;
+      }} data-testid='card-action-area'>
+          <CardContent>
+            <CardTitle>Dina uppgifter</CardTitle>
+            <Text>Namn: Namn Namnsson</Text>
+          </CardContent>
+        </CardActionArea>
+        <CardContent>
+          <CardActions>
+            <Button variant='icon' icon={Pen}>
+              Ändra
+            </Button>
+            <Button variant='icon' icon={X}>
+              Avbryt
+            </Button>
+          </CardActions>
+        </CardContent>
+      </>
+  },
+  play: async ({
+    canvas,
+    step
+  }) => {
+    await step('It should be possible to focus the primary action area', async () => {
+      const button = canvas.getByTestId('card-action-area');
+      expect(button).not.toHaveFocus();
+
+      // focus the link
+      await userEvent.tab();
+      expect(button).toHaveFocus();
+    });
+  }
+}`,...(P=(I=l.parameters)==null?void 0:I.docs)==null?void 0:P.source}}};var F,H,W;u.parameters={...u.parameters,docs:{...(F=u.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    children: <CardContent>
+        <CardLink href='#' data-testid='card-link'>
+          <CardTitle>Dina uppgifter</CardTitle>
+        </CardLink>
+        <Text>Namn: Namn Namnsson</Text>
+      </CardContent>
+  },
+  play: async ({
+    canvas,
+    step
+  }) => {
+    await step('It should be possible to focus the link', async () => {
+      const link = canvas.getByTestId('card-link');
+      expect(link).not.toHaveFocus();
+
+      // focus the link
+      await userEvent.tab();
+      expect(link).toHaveFocus();
+    });
+  }
+}`,...(W=(H=u.parameters)==null?void 0:H.docs)==null?void 0:W.source}}};var D,L,E;h.parameters={...h.parameters,docs:{...(D=h.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    style: {
+      maxWidth: 300
+    },
+    children: <>
+        <CardImage src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Pineapple_and_cross_section.jpg/640px-Pineapple_and_cross_section.jpg' alt='Ananas' />
+        <CardContent>
+          <CardLink href='#' data-testid='card-link'>
+            <CardTitle>Dina uppgifter</CardTitle>
+          </CardLink>
+          <Text>Namn: Namn Namnsson</Text>
+        </CardContent>
+      </>
+  }
+}`,...(E=(L=h.parameters)==null?void 0:L.docs)==null?void 0:E.source}}};var S,z,R;x.parameters={...x.parameters,docs:{...(S=x.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    style: {
+      maxWidth: 300
+    },
+    children: <CardContent>
+        <CardLink href='#' data-testid='card-link'>
+          <CardTitle>Dina uppgifter</CardTitle>
+        </CardLink>{' '}
+        <CardImage src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Pineapple_and_cross_section.jpg/640px-Pineapple_and_cross_section.jpg' alt='Ananas' />
+        <Text>Namn: Namn Namnsson</Text>
+      </CardContent>
+  }
+}`,...(R=(z=x.parameters)==null?void 0:z.docs)==null?void 0:R.source}}};const Tn=["Example","Horizontal","WithActions","WithPrimaryAction","WithLink","WithImage","WithContainedImage"];export{c as Example,p as Horizontal,m as WithActions,x as WithContainedImage,h as WithImage,u as WithLink,l as WithPrimaryAction,Tn as __namedExportsOrder,kn as default};
---
+++ pr-preview/pr-405/storybook/assets/Select.stories-QKkAVKVT.js
@@ -0,0 +1,216 @@
+import{j as re}from"./jsx-runtime-DtgEGaVE.js";import{S as ie}from"./Select-By4gl3zm.js";import{u as r,e as n}from"./index-DtSMq7G1.js";import{r as le}from"./index-BbAIUH2N.js";import"./useButton-BH_mgHDo.js";import"./utils-Dv9Ux5aE.js";import"./clsx-B-dksMZM.js";import"./filterDOMProps-BSfnXAP7.js";import"./usePress-COtC8Zcb.js";import"./useFocusRing-CPQALXIu.js";import"./index-cpIEhwLo.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./useFocusable-b_0ijIRa.js";import"./Tag-L0BliC0m.js";import"./Button-C6eeUJZ7.js";import"./ProgressBar-DJC3JvQN.js";import"./Label-Cr3pFc-x.js";import"./Hidden-lswqw32w.js";import"./useLabel-BAqrTMfP.js";import"./context-oTIQb5ND.js";import"./NumberFormatter-DT0cDaax.js";import"./LiveAnnouncer-DFDjXwI8.js";import"./Collection-Af87zMws.js";import"./index-DwWe6W7f.js";import"./ListBox-CDlT0gOm.js";import"./DragAndDrop-sISFPnlb.js";import"./Separator-vJnokFEK.js";import"./SelectionManager-B6WaAg_e.js";import"./useEvent-DnmuJhSu.js";import"./scrollIntoView-DzftQ5Hl.js";import"./FocusScope-C0EWTSbR.js";import"./useDescription-TyCFs068.js";import"./useControlledState-DGyHFLi9.js";import"./ListKeyboardDelegate-6F1ur1To.js";import"./useCollator-5m9ED1_d.js";import"./Text-D0cNLqf0.js";import"./useListState-Q19L1Hvv.js";import"./useField-BWvtu4tG.js";import"./useHighlightSelectionDescription-bMNPNBBZ.js";import"./useUpdateEffect-DVcG7G3u.js";import"./useLocalizedStringFormatter-DGtYtDnp.js";import"./LocalizedStringFormatter-BB0Nk989.js";import"./useHasTabbableChild-Bh3e4XK9.js";import"./getScrollParent-CrrBdd7g.js";import"./Button-DytQ-ITy.js";import"./Button.module-kL9Mhzoi.js";import"./x-Cu6J0Y2u.js";import"./createLucideIcon-8wDOqnHv.js";import"./TextField-DtKlQFf3.js";import"./Form-BMyPeP3B.js";import"./useFormValidation-BrFQy5aA.js";import"./Input-CAfzXJk2.js";import"./useTextField-zhCHv51w.js";import"./check-B_S0S3VY.js";import"./Label--BCDKGtH.js";import"./useOverlayTriggerState-CnVHlh1Q.js";import"./useMenuTriggerState-Dn_TR51k.js";import"./VisuallyHidden-DiGk2ECL.js";import"./useModal-j8uWYn4E.js";import"./useObserveElement-BrriYDGX.js";import"./chevron-down-U1dNK9nR.js";import"./Text-BO1MQlio.js";import"./FieldError-BI5lou7e.js";const o=["Äpple","Banan","Kiwi","Fläder","Nektarin","Vattenmelon","Ananas","Aprikos","Hjortron","Mango","Jordgubbe","Blåbär","Hallon","Björnbär","Körsbär","Persika","Plommon","Druva","Apelsin","Citron","Lime","Kokosnöt","Fikon","Papaya","Guava","Granatäpple","Drakfrukt","Stjärnfrukt","Passionsfrukt"].map(a=>({name:a,id:a.toLocaleLowerCase()})),oe=[{name:"Grönsaker",children:[{name:"Morot",id:"morot"},{name:"Broccoli",id:"broccoli"},{name:"Spenat",id:"spenat"},{name:"Potatis",id:"potatis"},{name:"Tomat",id:"tomat"}]},{name:"Bär",children:[{name:"Jordgubbe",id:"jordgubbe"},{name:"Blåbär",id:"blåbär"},{name:"Hallon",id:"hallon"},{name:"Björnbär",id:"björnbär"}]},{name:"Citrusfrukter",children:[{name:"Apelsin",id:"apelsin"},{name:"Citron",id:"citron"},{name:"Lime",id:"lime"},{name:"Grapefrukt",id:"grapefrukt"}]},{name:"Tropiska Frukter",children:[{name:"Ananas",id:"ananas"},{name:"Mango",id:"mango"},{name:"Papaya",id:"papaya"},{name:"Guava",id:"guava"}]},{name:"Stenfrukter",children:[{name:"Persika",id:"persika"},{name:"Plommon",id:"plommon"},{name:"Körsbär",id:"körsbär"},{name:"Aprikos",id:"aprikos"}]},{name:"Meloner",children:[{name:"Vattenmelon",id:"vattenmelon"},{name:"Cantaloupe",id:"cantaloupe"},{name:"Honungsmelon",id:"honungsmelon"}]},{name:"Exotiska Frukter",children:[{name:"Drakfrukt",id:"drakfrukt"},{name:"Stjärnfrukt",id:"stjärnfrukt"},{name:"Passionsfrukt",id:"passionsfrukt"}]},{name:"Torkade Frukter",children:[{name:"Russin",id:"russin"},{name:"Fikon",id:"fikon"},{name:"Dadlar",id:"dadlar"}]},{name:"Nötter",children:[{name:"Mandel",id:"mandel"},{name:"Valnöt",id:"valnöt"},{name:"Pistage",id:"pistage"}]},{name:"Örter",children:[{name:"Basilika",id:"basilika"},{name:"Mynta",id:"mynta"},{name:"Koriander",id:"koriander"}]},{name:"Kryddor",children:[{name:"Kanel",id:"kanel"},{name:"Ingefära",id:"ingefära"},{name:"Gurkmeja",id:"gurkmeja"}]},{name:"Baljväxter",children:[{name:"Linser",id:"linser"},{name:"Kikärtor",id:"kikartor"},{name:"Svarta Bönor",id:"svarta-bönor"}]}],Sa={component:ie,title:"Components/Select",tags:["autodocs"],args:{description:"Description",isClearable:!0,isDisabled:!1,isSelectableAll:!1,label:"Label",options:o,placeholder:"Select an option",selectionMode:"single",showTags:!1,errorPosition:"top"}},l={play:async({args:a,canvas:e,step:t})=>{await t("It should be possible to select an item using the keyboard",async()=>{await r.tab(),await r.keyboard("[Space]"),await r.keyboard("[Space]");const s=e.getByLabelText(`${a.label}-hidden`),i=e.getByText(o[0].name,{selector:"span"});n(s).toHaveDisplayValue([o[0].name]),n(i).toBeVisible()})}},c={args:{description:"Kiwi is pre-selected",defaultSelectedKeys:new Set(["kiwi"])},play:async({args:a,canvas:e,step:t})=>{await t("It should display and reflect the pre-selected value",async()=>{const s=e.getByLabelText(`${a.label}-hidden`),i=e.getByText("Kiwi",{selector:"span"});n(s).toHaveDisplayValue(["Kiwi"]),n(i).toBeVisible()})}},p={args:{description:"Kiwi and banana are pre-selected",defaultSelectedKeys:new Set(["kiwi","banan"]),selectionMode:"multiple"},play:async({args:a,canvas:e,step:t})=>{await t("It should display and reflect the pre-selected value",async()=>{const s=e.getByLabelText(`${a.label}-hidden`),i=e.getByText("2 valda",{selector:"span"});n(s).toHaveDisplayValue(["Banan","Kiwi"]),n(i).toBeVisible()})}},m={args:{selectionMode:"multiple",description:"All options are selected",defaultSelectedKeys:"all"},play:async({args:a,canvas:e,step:t})=>{await t("It should display and reflect the pre-selected values",async()=>{const s=e.getByLabelText(`${a.label}-hidden`),i=e.getByText(/valda/,{selector:"span"});n(s).toHaveDisplayValue(o.map(({name:w})=>w)),n(i).toBeVisible()})}},u={parameters:{docs:{description:{story:"Avoid using disabled"}},a11y:{element:"body",config:{rules:[{id:"color-contrast",enabled:!1}]},options:{rules:{"color-contrast":{enabled:!1}}}}},args:{isDisabled:!0}},b={args:{description:"Kiwi is disabled",disabledKeys:["kiwi"]}},y={args:{isInvalid:!0,errorMessage:"Error msg"}},g={args:{selectionMode:"multiple",showTags:!0,defaultSelectedKeys:new Set(["äpple","kiwi"])},play:async({args:a,canvas:e,step:t})=>{await t("It should display and reflect the pre-selected values",async()=>{const s=e.getByLabelText(`${a.label}-hidden`),i=e.getByText("2 valda",{selector:"span"});n(s).toHaveDisplayValue(["Äpple","Kiwi"]),n(i).toBeVisible(),n(e.getByText("Äpple",{selector:"div"})).toBeVisible(),n(e.getByText("Kiwi",{selector:"div"})).toBeVisible()})}},v={args:{selectionMode:"multiple",isSelectableAll:!0},play:async({args:a,canvas:e,step:t})=>{await t("It should be possible to select all items",async()=>{await r.tab(),await r.keyboard("[Space]"),await r.tab({shift:!0}),await r.keyboard("[Space]"),await r.keyboard("[Escape]");const s=e.getByLabelText(`${a.label}-hidden`),i=e.getByText(`${o.length} valda`,{selector:"span"});n(s).toHaveDisplayValue(o.map(({name:w})=>w)),n(i).toBeVisible()})}},d={args:{selectionMode:"multiple",isClearable:!1}},S={tags:["!dev","!autodocs"],args:{...l.args,selectionMode:"single",isClearable:!1,label:"Ärende",placeholder:"Välj ärende"},render:a=>{const[e,t]=le.useState(new Set);return re.jsx(ie,{...a,selectedKeys:e,onSelectionChange:t,options:[{id:"12",name:"tolv"},{id:"1",name:"ett"},{id:"2",name:"två"}]})},play:async({args:a,step:e,canvas:t})=>{await e("It should be possible to select an item with an ID greater than 9",async()=>{await r.tab(),await r.keyboard("[Space]"),await r.keyboard("[Space]"),n(t.getByLabelText(a.label+"-hidden")).toHaveDisplayValue(["tolv"])})}},h={args:{...l.args,options:oe}};var k,f,B;l.parameters={...l.parameters,docs:{...(k=l.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  play: async ({
+    args,
+    canvas,
+    step
+  }) => {
+    await step('It should be possible to select an item using the keyboard', async () => {
+      await userEvent.tab();
+      await userEvent.keyboard('[Space]');
+      await userEvent.keyboard('[Space]');
+      const hiddenSelect = canvas.getByLabelText(\`\${args.label}-hidden\`);
+      const visibleValue = canvas.getByText(options[0].name, {
+        selector: 'span'
+      });
+      expect(hiddenSelect).toHaveDisplayValue([options[0].name]);
+      expect(visibleValue).toBeVisible();
+    });
+  }
+}`,...(B=(f=l.parameters)==null?void 0:f.docs)==null?void 0:B.source}}};var x,V,K;c.parameters={...c.parameters,docs:{...(x=c.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    description: 'Kiwi is pre-selected',
+    defaultSelectedKeys: new Set(['kiwi'])
+  },
+  play: async ({
+    args,
+    canvas,
+    step
+  }) => {
+    await step('It should display and reflect the pre-selected value', async () => {
+      const hiddenSelect = canvas.getByLabelText(\`\${args.label}-hidden\`);
+      const visibleValue = canvas.getByText('Kiwi', {
+        selector: 'span'
+      });
+      expect(hiddenSelect).toHaveDisplayValue(['Kiwi']);
+      expect(visibleValue).toBeVisible();
+    });
+  }
+}`,...(K=(V=c.parameters)==null?void 0:V.docs)==null?void 0:K.source}}};var T,D,E;p.parameters={...p.parameters,docs:{...(T=p.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    description: 'Kiwi and banana are pre-selected',
+    defaultSelectedKeys: new Set(['kiwi', 'banan']),
+    selectionMode: 'multiple'
+  },
+  play: async ({
+    args,
+    canvas,
+    step
+  }) => {
+    await step('It should display and reflect the pre-selected value', async () => {
+      const hiddenSelect = canvas.getByLabelText(\`\${args.label}-hidden\`);
+      const visibleValue = canvas.getByText('2 valda', {
+        selector: 'span'
+      });
+      expect(hiddenSelect).toHaveDisplayValue(['Banan', 'Kiwi']);
+      expect(visibleValue).toBeVisible();
+    });
+  }
+}`,...(E=(D=p.parameters)==null?void 0:D.docs)==null?void 0:E.source}}};var I,M,L;m.parameters={...m.parameters,docs:{...(I=m.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    selectionMode: 'multiple',
+    description: 'All options are selected',
+    defaultSelectedKeys: 'all'
+  },
+  play: async ({
+    args,
+    canvas,
+    step
+  }) => {
+    await step('It should display and reflect the pre-selected values', async () => {
+      const hiddenSelect = canvas.getByLabelText(\`\${args.label}-hidden\`);
+      const visibleValue = canvas.getByText(/valda/, {
+        selector: 'span'
+      });
+      expect(hiddenSelect).toHaveDisplayValue(options.map(({
+        name
+      }) => name));
+      expect(visibleValue).toBeVisible();
+    });
+  }
+}`,...(L=(M=m.parameters)==null?void 0:M.docs)==null?void 0:L.source}}};var A,H,j;u.parameters={...u.parameters,docs:{...(A=u.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: 'Avoid using disabled'
+      }
+    },
+    a11y: {
+      element: 'body',
+      config: {
+        rules: [{
+          // Dont check for color contrast on disabled elements
+          id: 'color-contrast',
+          enabled: false
+        }]
+      },
+      options: {
+        rules: {
+          'color-contrast': {
+            enabled: false
+          }
+        }
+      } satisfies RunOptions
+    }
+  },
+  args: {
+    isDisabled: true
+  }
+}`,...(j=(H=u.parameters)==null?void 0:H.docs)==null?void 0:j.source}}};var C,$,P;b.parameters={...b.parameters,docs:{...(C=b.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    description: 'Kiwi is disabled',
+    disabledKeys: ['kiwi']
+  }
+}`,...(P=($=b.parameters)==null?void 0:$.docs)==null?void 0:P.source}}};var N,F,G;y.parameters={...y.parameters,docs:{...(N=y.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    isInvalid: true,
+    errorMessage: 'Error msg'
+  }
+}`,...(G=(F=y.parameters)==null?void 0:F.docs)==null?void 0:G.source}}};var O,W,R;g.parameters={...g.parameters,docs:{...(O=g.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    selectionMode: 'multiple',
+    showTags: true,
+    defaultSelectedKeys: new Set(['äpple', 'kiwi'])
+  },
+  play: async ({
+    args,
+    canvas,
+    step
+  }) => {
+    await step('It should display and reflect the pre-selected values', async () => {
+      const hiddenSelect = canvas.getByLabelText(\`\${args.label}-hidden\`);
+      const visibleValue = canvas.getByText('2 valda', {
+        selector: 'span'
+      });
+      expect(hiddenSelect).toHaveDisplayValue(['Äpple', 'Kiwi']);
+      expect(visibleValue).toBeVisible();
+      expect(canvas.getByText('Äpple', {
+        selector: 'div'
+      })).toBeVisible();
+      expect(canvas.getByText('Kiwi', {
+        selector: 'div'
+      })).toBeVisible();
+    });
+  }
+}`,...(R=(W=g.parameters)==null?void 0:W.docs)==null?void 0:R.source}}};var J,_,q;v.parameters={...v.parameters,docs:{...(J=v.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  args: {
+    selectionMode: 'multiple',
+    isSelectableAll: true
+  },
+  play: async ({
+    args,
+    canvas,
+    step
+  }) => {
+    await step('It should be possible to select all items', async () => {
+      await userEvent.tab();
+      await userEvent.keyboard('[Space]');
+      await userEvent.tab({
+        shift: true
+      });
+      await userEvent.keyboard('[Space]');
+      await userEvent.keyboard('[Escape]');
+      const hiddenSelect = canvas.getByLabelText(\`\${args.label}-hidden\`);
+      const visibleValue = canvas.getByText(\`\${options.length} valda\`, {
+        selector: 'span'
+      });
+      expect(hiddenSelect).toHaveDisplayValue(options.map(({
+        name
+      }) => name));
+      expect(visibleValue).toBeVisible();
+    });
+  }
+}`,...(q=(_=v.parameters)==null?void 0:_.docs)==null?void 0:q.source}}};var z,Q,U,X,Y;d.parameters={...d.parameters,docs:{...(z=d.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    selectionMode: 'multiple',
+    isClearable: false
+  }
+}`,...(U=(Q=d.parameters)==null?void 0:Q.docs)==null?void 0:U.source},description:{story:"As default all options are clearable. `isClearable={false}` Experimental feature",...(Y=(X=d.parameters)==null?void 0:X.docs)==null?void 0:Y.description}}};var Z,ee,ae;S.parameters={...S.parameters,docs:{...(Z=S.parameters)==null?void 0:Z.docs,source:{originalSource:`{
+  tags: ['!dev', '!autodocs'],
+  args: {
+    ...Normal.args,
+    selectionMode: 'single',
+    isClearable: false,
+    label: 'Ärende',
+    placeholder: 'Välj ärende'
+  },
+  render: args => {
+    const [selectedKey, setSelectedKey] = useState<Selection>(new Set());
+    return <Select {...args} selectedKeys={selectedKey} onSelectionChange={setSelectedKey} options={[{
+      id: '12',
+      name: 'tolv'
+    }, {
+      id: '1',
+      name: 'ett'
+    }, {
+      id: '2',
+      name: 'två'
+    }]} />;
+  },
+  play: async ({
+    args,
+    step,
+    canvas
+  }) => {
+    await step('It should be possible to select an item with an ID greater than 9', async () => {
+      await userEvent.tab();
+      await userEvent.keyboard('[Space]');
+      await userEvent.keyboard('[Space]');
+      expect(canvas.getByLabelText(args.label + '-hidden')).toHaveDisplayValue(['tolv']);
+    });
+  }
+}`,...(ae=(ee=S.parameters)==null?void 0:ee.docs)==null?void 0:ae.source}}};var te,ne,se;h.parameters={...h.parameters,docs:{...(te=h.parameters)==null?void 0:te.docs,source:{originalSource:`{
+  args: {
+    ...Normal.args,
+    options: optionsWithSections
+  }
+}`,...(se=(ne=h.parameters)==null?void 0:ne.docs)==null?void 0:se.source}}};const ha=["Normal","DefaultSelectedKey","DefaultSelectedKeys","AllKeysSelected","Disabled","DisabledOption","Invalid","WithTags","SelectAllEnabled","NotClearable","DS872","Sectioned"];export{m as AllKeysSelected,S as DS872,c as DefaultSelectedKey,p as DefaultSelectedKeys,u as Disabled,b as DisabledOption,y as Invalid,l as Normal,d as NotClearable,h as Sectioned,v as SelectAllEnabled,g as WithTags,ha as __namedExportsOrder,Sa as default};
