614b67d82ebf9605998e1ac43dfd119e9a9d048f
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-04-28 08:05:56 +0000

    Deploy preview for PR 405 🛫

--- pr-preview/pr-405/storybook/card/Card.d.ts
+++ pr-preview/pr-405/storybook/card/Card.d.ts
@@ -21,12 +21,12 @@ interface MidasCardLink<C extends React.ElementType> {
     children: React.ReactNode;
     as?: C;
 }
-export type LinkProps<C extends React.ElementType> = MidasCardLink<C> & Omit<React.ComponentProps<C>, keyof MidasCardLink<C>>;
+export type MidasCardLinkProps<C extends React.ElementType> = MidasCardLink<C> & Omit<React.ComponentProps<C>, keyof MidasCardLink<C>>;
 export declare const Card: React.FC<MidasCard>;
 export declare const CardContent: React.FC<React.HTMLAttributes<HTMLDivElement>>;
 export declare const CardTitle: React.FC<HeadingProps>;
 export declare const CardActions: React.FC<React.HTMLAttributes<HTMLDivElement>>;
 export declare const CardActionArea: React.FC<ButtonProps & React.RefAttributes<HTMLButtonElement>>;
 export declare const CardImage: React.FC<MidasCardImage>;
-export declare const CardLink: <C extends React.ElementType = (props: import('react-aria-components').LinkProps & React.RefAttributes<HTMLAnchorElement>) => React.ReactElement | null>({ children, as, ...rest }: LinkProps<C>) => import("react/jsx-runtime").JSX.Element;
+export declare const CardLink: <C extends React.ElementType = (props: import('react-aria-components').LinkProps & React.RefAttributes<HTMLAnchorElement>) => React.ReactElement | null>({ children, as, ...rest }: MidasCardLinkProps<C>) => import("react/jsx-runtime").JSX.Element;
 export {};
