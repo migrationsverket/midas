a75456b49f303fe380bae3f3cafa0e55ce0693cc
derpbravely <axel.engstrom@migrationsverket.se>
2025-04-28 08:29:49 +0200

    refactor(calendar): remove unused interface

--- packages/components/src/calendar/components/CalendarHeader.tsx
+++ packages/components/src/calendar/components/CalendarHeader.tsx
@@ -1,14 +1,8 @@
 import * as React from 'react'
-import { CalendarGridProps } from 'react-aria-components'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { Button } from '../../button'
 import { Heading } from '../../heading'
 import styles from '../Calendar.module.css'
-
-export interface CalendarHeaderProps {
-  errorMessage?: string
-  weekdayStyle?: CalendarGridProps['weekdayStyle']
-}
 
 export const CalendarHeader: React.FC = () => (
   <header className={styles.header}>
