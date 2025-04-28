cefcfe6158159c7d928583f81aaea3730e3f6f2e
phieksmig <phieksmig@users.noreply.github.com>
2025-04-28 11:58:41 +0000

    Deploy preview for PR 517 🛫

---
+++ pr-preview/pr-517/storybook/date-picker/DatePickerInputField.d.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+interface DatePickerInputFieldProps {
+    children?: React.ReactNode;
+    isDisabled?: boolean;
+    isInvalid?: boolean;
+}
+export declare const DatePickerInputField: React.FC<DatePickerInputFieldProps>;
+export {};
