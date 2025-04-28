7147412643b409a84957ebb092998abc11f9045d
jabkha <jabkha@users.noreply.github.com>
2025-04-28 08:22:07 +0000

    Deploy preview for PR 514 🛫

---
+++ pr-preview/pr-514/storybook/calendar/CalendarGrid.d.ts
@@ -0,0 +1,6 @@
+import { CalendarGridProps as AriaCalendarGridProps } from 'react-aria-components';
+import * as React from 'react';
+export interface CalendarGridProps {
+    weekdayStyle?: AriaCalendarGridProps['weekdayStyle'];
+}
+export declare const CalendarGrid: React.FC<CalendarGridProps>;
