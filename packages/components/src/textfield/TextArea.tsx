2643927ef248e4ee031e4da37eef617a1a970ddf
derpbravely <axel.engstrom@migrationsverket.se>
2025-04-28 15:03:25 +0200

    fix(textfield): remove dir and dirname, not handled correctly by react aria

--- packages/components/src/textfield/TextArea.tsx
+++ packages/components/src/textfield/TextArea.tsx
@@ -12,20 +12,16 @@ import styles from './TextField.module.css'
 export interface TextAreaProps
   extends Omit<TextFieldBaseProps, 'children' | 'type' | 'pattern'> {
   className?: AriaTextAreaProps['className']
-  dir?: string
-  dirName?: string
   form?: string
   rows?: number
   wrap?: string
 }
 
 export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
-  ({ className, dir, dirName, form, rows, wrap, ...rest }, ref) => (
+  ({ className, form, rows, wrap, ...rest }, ref) => (
     <TextFieldBase {...rest}>
       <AriaTextArea
         className={clsx(styles.textArea, className)}
-        dir={dir}
-        dirName={dirName}
         form={form}
         ref={ref}
         rows={rows}
--- packages/components/src/textfield/TextField.tsx
+++ packages/components/src/textfield/TextField.tsx
@@ -7,20 +7,15 @@ import clsx from 'clsx'
 
 export interface TextFieldProps extends Omit<TextFieldBaseProps, 'children'> {
   className?: InputProps['className']
-  dir?: string
-  dirName?: string
   form?: string
   list?: string
 }
 
 export const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
-  ({ className, dir, dirName, form, list, ...rest }, ref) => (
+  ({ className, form, list, ...rest }, ref) => (
     <TextFieldBase {...rest}>
       <Input
         className={clsx(className)}
-        dir={dir}
-        // @ts-expect-error @types/react doesn't recognize this valid attribute
-        dirName={dirName}
         form={form}
         list={list}
         ref={ref}
