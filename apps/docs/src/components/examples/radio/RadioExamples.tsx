8975a22903a1471d7db35922efbc69dff19d42ca
PHIEKS <philip.hjalmrud.ekstromer@migrationsverket.se>
2025-04-28 13:50:31 +0200

    docs(radio): add new examples

--- apps/docs/src/components/examples/radio/RadioExamples.tsx
+++ apps/docs/src/components/examples/radio/RadioExamples.tsx
@@ -44,3 +44,27 @@ export const HorizontalExample = () => (
     <Radio value='banan'>Banan</Radio>
   </RadioGroup>
 )
+
+export const DefaultValueExample = () => (
+  <RadioGroup
+    label='Vill du lägga till din favoritfrukt i fruktkorgen?'
+    description='Det blir gott!'
+    defaultValue='yes'
+  >
+    <Radio value='yes'>Ja</Radio>
+    <Radio value='no'>Nej</Radio>
+  </RadioGroup>
+)
+
+export const NoneOfTheAboveExample = () => (
+  <RadioGroup
+    label='Välj din favoritfrukt'
+    description='Du kan bara välja en'
+  >
+    <Radio value='apple'>Äpple</Radio>
+    <Radio value='banan'>Banan</Radio>
+    <Radio value='kiwi'>Kiwi</Radio>
+    <Radio value='apelsin'>Apelsin</Radio>
+    <Radio value='none'>Inget av givna alternativ</Radio>
+  </RadioGroup>
+)
