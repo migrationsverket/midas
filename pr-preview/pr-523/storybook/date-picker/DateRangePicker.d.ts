115150658540aec5c0d92ef75fb4cd4b12058504
derpbravely <derpbravely@users.noreply.github.com>
2025-04-30 09:53:35 +0000

    Remove preview for PR 523 🛬

--- pr-preview/pr-523/storybook/date-picker/DateRangePicker.d.ts
+++
@@ -1,9 +0,0 @@
-import { DateRangePickerProps as AriaDateRangePickerProps, DateValue, ValidationResult } from 'react-aria-components';
-import * as React from 'react';
-export interface DateRangePickerProps extends AriaDateRangePickerProps<DateValue> {
-    description?: string;
-    errorMessage?: string | ((validation: ValidationResult) => string);
-    errorPosition?: 'top' | 'bottom';
-    label?: string;
-}
-export declare const DateRangePicker: React.FC<DateRangePickerProps>;
