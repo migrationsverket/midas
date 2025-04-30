e84f1604eccd47988992820d2ccf163c5981eb00
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-04-30 18:21:01 +0200

    feat(textfield): add medium size variant (#520)

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -1,37 +1,23 @@
 import styles from './app.module.css'
-import { TextArea, TextField } from '@midas-ds/components'
-import { I18nProvider } from 'react-aria-components'
+import { TextField } from '@midas-ds/components'
 
 export function App() {
   return (
     <div className={styles.container}>
-      <TextField
-        label='Label'
-        description='Description'
-        showCounter
-        type='email'
-        maxLength={4}
-      />
-      <TextField
-        label='Label'
-        description='Description'
-        type='password'
-        validate={value => (value === 'Skriv inte hej' ? 'error' : true)}
-      />
-      <TextArea
-        label='Label'
-        description='Skriv hej'
-        validate={value => (value === 'Skriv inte hej' ? 'error' : true)}
-      />
-      <I18nProvider locale={'de-DE'}>
-        <TextField type={'password'}></TextField>
-      </I18nProvider>
-      <I18nProvider locale={'ko'}>
-        <TextField type={'password'}></TextField>
-      </I18nProvider>
-      <I18nProvider locale={'ar'}>
-        <TextField type={'password'}></TextField>
-      </I18nProvider>
+      <div style={{ display: 'flex', alignItems: 'start', gap: '2rem', border: '1px solid salmon' }}>
+        <TextField
+          label='Medium'
+          description='size component'
+          size={'medium'}
+          defaultValue={'this is medium'}
+        />
+        <TextField
+          size={'large'}
+          label='Large'
+          defaultValue={'this is large'}
+          description='size component'
+        />
+      </div>
     </div>
   )
 }
