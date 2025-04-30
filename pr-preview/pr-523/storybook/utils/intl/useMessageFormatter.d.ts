e06f568a1220ad4abf2d6fdf4f0e9ed8f51eaa91
derpbravely <derpbravely@users.noreply.github.com>
2025-04-30 08:27:17 +0000

    Deploy preview for PR 523 🛫

--- pr-preview/pr-523/storybook/utils/intl/useMessageFormatter.d.ts
+++ pr-preview/pr-523/storybook/utils/intl/useMessageFormatter.d.ts
@@ -6,6 +6,6 @@ export type FormatMessage = (key: string, variables?: {
  * Handles formatting ICU Message strings to create localized strings for the current locale.
  * Automatically updates when the locale changes, and handles caching of messages for performance.
  * @param strings - A mapping of languages to strings by key.
- * @deprecated - use useLocalizedStringFormatter instead.
+ * @deprecated since v7.0.0 - use useLocalizedStringFormatter instead.
  */
 export declare function useMessageFormatter(strings: LocalizedStrings): FormatMessage;
