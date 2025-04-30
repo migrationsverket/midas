450393add407757be2075ebd63a298a84dcd2d6f
derpbravely <derpbravely@users.noreply.github.com>
2025-04-30 09:04:43 +0000

    Remove preview for PR 528 🛬

--- pr-preview/pr-528/storybook/label/Label.d.ts
+++
@@ -1,9 +0,0 @@
-import { LabelProps as AriaLabelProps } from 'react-aria-components';
-import * as React from 'react';
-export interface LabelProps extends AriaLabelProps {
-    /**
-     *  @deprecated Please use Label without variant for label-02, and for label-01 use Text component with slot: 'description'
-     */
-    variant?: 'label-01' | 'label-02';
-}
-export declare const Label: React.FC<LabelProps>;
