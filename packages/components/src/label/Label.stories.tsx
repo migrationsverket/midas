5e226966fbabb466d2859136aba73ea43866c6fa
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2025-05-07 15:09:12 +0200

    docs(label): remove label-01, 02 as examples

--- packages/components/src/label/Label.stories.tsx
+++ packages/components/src/label/Label.stories.tsx
@@ -10,37 +10,9 @@ const meta: Meta<typeof Label> = {
   tags: ['autodocs'],
 }
 
-export const LabelBase: Story = {
+export const _Label: Story = {
   args: {
-    children: 'I am a label.',
-  },
-  play: async ({ canvas, args: { children } }) => {
-    expect(canvas.getByText(children as string)).toHaveStyle({
-      'font-size': '16px',
-      'line-height': '20px',
-      'font-weight': 500,
-    })
-  },
-}
-
-export const Label01: Story = {
-  args: {
-    variant: 'label-01',
-    children: 'I am a label with the variant of "label-01"',
-  },
-  play: async ({ canvas, args: { children } }) => {
-    expect(canvas.getByText(children as string)).toHaveStyle({
-      'font-size': '14px',
-      'line-height': '18px',
-      'font-weight': 400,
-    })
-  },
-}
-
-export const Label02: Story = {
-  args: {
-    variant: 'label-02',
-    children: 'I am a label with the variant of "label-02"',
+    children: 'I am a label',
   },
   play: async ({ canvas, args: { children } }) => {
     expect(canvas.getByText(children as string)).toHaveStyle({
