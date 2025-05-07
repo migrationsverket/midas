17d2b8834513b80a1141a444909dc5dba77b0ae4
phieksmig <phieksmig@users.noreply.github.com>
2025-05-07 19:00:07 +0000

    Deploy preview for PR 536 🛫

--- pr-preview/pr-536/storybook/assets/Flex-CxD7Y6jI.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./jsx-runtime-DtgEGaVE.js";import{s as e}from"./FlexItem-C4ikU2nm.js";import{c as t}from"./clsx-B-dksMZM.js";const l=({children:a,fluid:r=!1,...s})=>o.jsx("div",{...s,className:t(e.container,r&&e.fluid,s.className),children:o.jsx("div",{className:e.flex,children:a})});try{l.displayName="Flex",l.__docgenInfo={description:`Grid based on display: flex;
-Calculates breakpoints and distributes columns according to MV specifications
-
-### Children
-Use FlexItem to manage each column.
-FlexItem accepts values of 1 through 12 and auto.`,displayName:"Flex",props:{fluid:{defaultValue:{value:"false"},description:"Removes outer margins for nested use. First Flex on a page should have outer margins.",name:"fluid",required:!1,type:{name:"boolean"}}}}}catch{}export{l as F};
