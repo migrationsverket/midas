3f587c77fd1a2a5825fcea601400773db3004b3a
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-05-07 12:26:18 +0000

    Deploy preview for PR 540 🛫

---
+++ pr-preview/pr-540/storybook/assets/Flex.stories-AWIEPT5a.js
@@ -0,0 +1,55 @@
+import{j as t}from"./jsx-runtime-DtgEGaVE.js";import{F as m}from"./Flex-BvqrhWvD.js";import{F as e}from"./FlexItem-CZuJD__Y.js";import{C as r}from"./Card-DHXmZe5E.js";import"./index-BbAIUH2N.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./Button-C6eeUJZ7.js";import"./utils-Dv9Ux5aE.js";import"./ProgressBar-DJC3JvQN.js";import"./Label-Cr3pFc-x.js";import"./Hidden-lswqw32w.js";import"./filterDOMProps-BSfnXAP7.js";import"./useLabel-BAqrTMfP.js";import"./context-oTIQb5ND.js";import"./NumberFormatter-DT0cDaax.js";import"./LiveAnnouncer-DFDjXwI8.js";import"./useButton-BH_mgHDo.js";import"./usePress-COtC8Zcb.js";import"./useFocusRing-CPQALXIu.js";import"./index-cpIEhwLo.js";import"./useFocusable-b_0ijIRa.js";import"./Link-slNGPAHu.js";import"./arrow-right-DnGf4Axc.js";import"./createLucideIcon-8wDOqnHv.js";import"./Heading-QrS6DDsr.js";import"./RSPContexts-CeCCo7XS.js";const z={component:m,title:"Components/Flex",tags:["autodocs"],parameters:{layout:"fullscreen",a11y:{test:"todo"}},argTypes:{fluid:{control:{type:"boolean"}}}},n={args:{},render:({...d})=>t.jsxs(m,{fluid:d.fluid,children:[t.jsx(e,{col:3,children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:6,children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:3,children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:4,children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:4,children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid men just detta kort har väldigt mycket text för att illustrera att de andra också växer på höjden.",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:3,offset:1,children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{col:"auto",children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})}),t.jsx(e,{children:t.jsx(r,{title:"Ett kort",content:"Som är i ett grid",link:{children:"Länk",href:"#"}})})]})};var i,o,l;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {},
+  render: ({
+    ...args
+  }) => <Flex fluid={args.fluid}>
+      <FlexItem col={3}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={6}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={3}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={4}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={4}>
+        <Card title={'Ett kort'} content={'Som är i ett grid men just detta kort har väldigt mycket text för att illustrera att de andra också växer på höjden.'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={3} offset={1}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem col={'auto'}>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+      <FlexItem>
+        <Card title={'Ett kort'} content={'Som är i ett grid'} link={{
+        children: 'Länk',
+        href: '#'
+      }} />
+      </FlexItem>
+    </Flex>
+}`,...(l=(o=n.parameters)==null?void 0:o.docs)==null?void 0:l.source}}};const A=["Primary"];export{n as Primary,A as __namedExportsOrder,z as default};
