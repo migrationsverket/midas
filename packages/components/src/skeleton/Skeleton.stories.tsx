b8d31927d373afbf6a383068ae42ac5668febc76
jabir Khalil <jabir.saadi.khalil@migrationsverket.se>
2025-05-08 13:10:56 +0200

    test(skeleton): add tests to skeleton

--- packages/components/src/skeleton/Skeleton.stories.tsx
+++ packages/components/src/skeleton/Skeleton.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Skeleton } from './Skeleton'
-
+import { expect } from '@storybook/test'
+import styles from './Skeleton.module.css'
 const meta: Meta<typeof Skeleton> = {
   component: Skeleton,
   title: 'Components/Skeleton',
@@ -40,12 +41,32 @@ export const Rectangle: Story = {
     width: '100px',
     height: '40px',
   },
+  render: args => (
+    <Skeleton
+      {...args}
+      data-testid='skeleton'
+    />
+  ),
+  play: async ({ canvas }) => {
+    const skeleton = canvas.getByTestId('skeleton')
+    await expect(skeleton).toHaveClass(styles.wave, styles.skeleton)
+  },
 }
 
 export const Circle: Story = {
   args: {
     variant: 'circle',
     width: '50px',
+  },
+  render: args => (
+    <Skeleton
+      {...args}
+      data-testid='skeleton'
+    />
+  ),
+  play: async ({ canvas }) => {
+    const skeleton = canvas.getByTestId('skeleton')
+    await expect(skeleton).toHaveClass(styles.wave, styles.circle)
   },
 }
 
@@ -60,7 +81,6 @@ export const Rectangular: Story = {
 export const NoAnimation: Story = {
   args: {
     ...Rectangle.args,
-    animation: false,
     isAnimated: false,
   },
   parameters: {
@@ -75,12 +95,32 @@ export const NoAnimation: Story = {
       },
     },
   },
+  render: args => (
+    <Skeleton
+      {...args}
+      data-testid='skeleton'
+    />
+  ),
+  play: async ({ canvas }) => {
+    const skeleton = canvas.getByTestId('skeleton')
+    await expect(skeleton).not.toHaveClass(styles.wave)
+  },
 }
 
 export const Animation: Story = {
   args: {
     ...Rectangle.args,
     isAnimated: true,
+  },
+  render: args => (
+    <Skeleton
+      {...args}
+      data-testid='skeleton'
+    />
+  ),
+  play: async ({ canvas }) => {
+    const skeleton = canvas.getByTestId('skeleton')
+    await expect(skeleton).toHaveClass(styles.wave)
   },
 }
 
