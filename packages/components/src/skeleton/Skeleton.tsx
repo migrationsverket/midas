6d92212fedc7fbf05dda19f65926417f70d1e151
jabir Khalil <jabir.saadi.khalil@migrationsverket.se>
2025-05-09 10:42:56 +0200

    refactor(skeleton): extend SkeletonProps

--- packages/components/src/skeleton/Skeleton.tsx
+++ packages/components/src/skeleton/Skeleton.tsx
@@ -2,19 +2,23 @@ import * as React from 'react'
 import styles from './Skeleton.module.css'
 import clsx from 'clsx'
 
-type SkeletonProps = {
+export interface SkeletonProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
   width?: React.CSSProperties['width']
   height?: React.CSSProperties['height']
   variant?:
     | 'circle'
     | 'rectangle'
     /**
-     * @deprecated since v8.4.0 - Use 'rectangle' instead. This will be removed in future versions.
+     * @deprecated after v9.0.0 - Use 'rectangle' instead. This will be removed in future versions.
      */
     | 'rectangular'
 
   /**
-   * @deprecated since v8.4.0 - Use 'isAnimated' instead. This will be removed in future versions.
+   * @deprecated after v9.0.0 - Use 'isAnimated' instead. This will be removed in future versions.
    */
   animation?: 'wave' | false
   /**
