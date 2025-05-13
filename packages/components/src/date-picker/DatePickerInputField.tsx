c1e1a0444409fd4b5bcf33964f68c13febbae0bc
derpbravely <axel.engstrom@migrationsverket.se>
2025-05-13 11:48:07 +0200

    feat(date-picker): add size prop

--- packages/components/src/date-picker/DatePickerInputField.tsx
+++ packages/components/src/date-picker/DatePickerInputField.tsx
@@ -3,22 +3,31 @@ import { Button, Group } from 'react-aria-components'
 import { CalendarDays } from 'lucide-react'
 import { clsx } from 'clsx'
 import styles from './DatePicker.module.css'
+import { Size } from '../common/types'
 
 interface DatePickerInputFieldProps {
   children?: React.ReactNode
   isDisabled?: boolean
   isInvalid?: boolean
+  size?: Size
 }
 
 export const DatePickerInputField: React.FC<DatePickerInputFieldProps> = ({
   children,
   isDisabled,
   isInvalid,
+  size = 'large',
 }) => (
-  <Group className={clsx(styles.inputField)}>
+  <Group
+    className={clsx(styles.inputField, {
+      [styles.medium]: size === 'medium',
+    })}
+  >
     {children}
     <Button
-      className={styles.calendarButton}
+      className={clsx(styles.calendarButton, {
+        [styles.medium]: size === 'medium',
+      })}
       data-invalid={isInvalid || undefined}
       isDisabled={isDisabled}
     >
