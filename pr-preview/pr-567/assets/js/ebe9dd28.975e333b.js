90358449cbad39f8c0c50d30654af52c958d6854
jabkha <jabkha@users.noreply.github.com>
2025-05-14 10:06:06 +0000

    Deploy preview for PR 567 🛫

---
+++ pr-preview/pr-567/assets/js/ebe9dd28.975e333b.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[118],{
+
+/***/ 86574:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-567/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
