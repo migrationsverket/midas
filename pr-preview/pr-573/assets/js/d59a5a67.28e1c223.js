1072865a0ed8a315edee60d4fcfe449a9c28fff2
phieksmig <phieksmig@users.noreply.github.com>
2025-05-14 14:03:08 +0000

    Deploy preview for PR 573 🛫

---
+++ pr-preview/pr-573/assets/js/d59a5a67.28e1c223.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[1579],{
+
+/***/ 88102:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"archive":{"blogPosts":[{"id":"/releases/9.0.0","metadata":{"permalink":"/pr-preview/pr-573/blog/releases/9.0.0","source":"@site/blog/releases/9.0.0/index.mdx","title":"Release 9.0.0","description":"Migrera från 8.x.x till 9.0.0","date":"2025-05-08T00:00:00.000Z","tags":[],"readingTime":0.68,"hasTruncateMarker":true,"authors":[],"frontMatter":{"title":"Release 9.0.0","date":"2025-05-08T00:00:00.000Z","pagination_prev":null,"pagination_next":null},"unlisted":false},"content":"## Migrera från 8.x.x till 9.0.0\\n\\nStrukturen på tokens för `lineHeight` och `size` under `typography` har ändrats.\\nImporterar du dessa värden behöver du justera din kod enligt:\\n\\n### lineHeight\\n\\n```tsx\\nimport { typography } from \'@midas-ds/components/theme\'\\n```\\n\\n```diff\\n-   typography.lineHeight01\\n+   typography.lineHeight10\\n-   typography.lineHeight02\\n+   typography.lineHeight20\\n-   typography.lineHeight03\\n+   typography.lineHeight30\\n-   typography.lineHeight04\\n+   typography.lineHeight40\\n-   typography.lineHeight05\\n+   typography.lineHeight50\\n-   typography.lineHeight06\\n+   typography.lineHeight60\\n-   typography.lineHeight07\\n+   typography.lineHeight70\\n-   typography.lineHeight08\\n+   typography.lineHeight80\\n-   typography.lineHeight09\\n+   typography.lineHeight90\\n-   typography.lineHeight10\\n+   typography.lineHeight100\\n```\\n\\n### size\\n\\n```tsx\\nimport { typography } from \'@midas-ds/components/theme\'\\n```\\n\\n```diff\\n\\n-   typography.size01\\n+   typography.fontSize10\\n-   typography.size02\\n+   typography.fontSize20\\n-   typography.size03\\n+   typography.fontSize30\\n-   typography.size04\\n+   typography.fontSize40\\n-   typography.size05\\n+   typography.fontSize50\\n-   typography.size06\\n+   typography.fontSize60\\n-   typography.size07\\n+   typography.fontSize70\\n-   typography.size08\\n+   typography.fontSize80\\n-   typography.size09\\n+   typography.fontSize90\\n-   typography.size10\\n+   typography.fontSize100\\n\\n```\\n\\nKolla [changelog](/changelog/9.0.0)\\n\\n{/* truncate */}"}]}}');
+
+/***/ })
+
+}]);
