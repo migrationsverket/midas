b5d70d6ee4d013a7a7c26685f37a8629bd256698
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-05-15 09:25:02 +0000

    Deploy preview for PR 571 🛫

---
+++ pr-preview/pr-571/assets/js/a51206e1.41beb3ea.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[7632],{
+
+/***/ 48201:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-571/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
