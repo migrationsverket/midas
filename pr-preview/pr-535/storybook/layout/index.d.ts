eb06e668d24d4a398c0974d8f41ea5e0629a262d
phieksmig <phieksmig@users.noreply.github.com>
2025-05-15 08:56:06 +0000

    Remove preview for PR 535 🛬

--- pr-preview/pr-535/storybook/layout/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './Layout';
-export * from './components/Header';
-export * from './components/Sidebar';
-export * from './components/SidebarLink';
-export * from './components/Navbar';
