81cf5762c4889a2900da92bd38e39514f0cb169f
derpbravely <derpbravely@users.noreply.github.com>
2025-05-16 09:32:51 +0000

    Deploy preview for PR 580 🛫

---
+++ pr-preview/pr-580/assets/js/03801e7d.eebea0fe.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[9647],{
+
+/***/ 72337:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-580/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":9,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-580/assets/js/f0d41d07.0404d977.js
@@ -0,0 +1,276 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[389],{
+
+/***/ 22954:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   assets: () => (/* binding */ assets),
+/* harmony export */   contentTitle: () => (/* binding */ contentTitle),
+/* harmony export */   "default": () => (/* binding */ MDXContent),
+/* harmony export */   frontMatter: () => (/* binding */ frontMatter),
+/* harmony export */   metadata: () => (/* reexport default export from named module */ _site_docusaurus_docusaurus_plugin_content_blog_default_site_blog_releases_10_0_0_index_mdx_f0d_json__WEBPACK_IMPORTED_MODULE_0__),
+/* harmony export */   toc: () => (/* binding */ toc)
+/* harmony export */ });
+/* harmony import */ var _site_docusaurus_docusaurus_plugin_content_blog_default_site_blog_releases_10_0_0_index_mdx_f0d_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(55075);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(74848);
+/* harmony import */ var _mdx_js_react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(28453);
+
+
+const frontMatter = {
+	title: 'Release 10.0.0',
+	date: new Date('2025-05-15T00:00:00.000Z'),
+	pagination_prev: null,
+	pagination_next: null
+};
+const contentTitle = undefined;
+
+const assets = {
+"authorsImageUrls": [],
+};
+
+/*truncate*/
+
+
+const toc = [{
+  "value": "Nya funktioner",
+  "id": "nya-funktioner",
+  "level": 2
+}, {
+  "value": "Migrera från 9.x.x till 10.0.0",
+  "id": "migrera-från-9xx-till-1000",
+  "level": 2
+}, {
+  "value": "Accordion",
+  "id": "accordion",
+  "level": 3
+}, {
+  "value": "Button",
+  "id": "button",
+  "level": 3
+}, {
+  "value": "Modal, DialogTrigger",
+  "id": "modal-dialogtrigger",
+  "level": 3
+}, {
+  "value": "Spinner",
+  "id": "spinner",
+  "level": 3
+}, {
+  "value": "Tabs",
+  "id": "tabs",
+  "level": 3
+}, {
+  "value": "Tokens",
+  "id": "tokens",
+  "level": 3
+}, {
+  "value": "useMessageFormatter",
+  "id": "usemessageformatter",
+  "level": 3
+}, {
+  "value": "Label",
+  "id": "label",
+  "level": 3
+}, {
+  "value": "Skeleton",
+  "id": "skeleton",
+  "level": 3
+}];
+function _createMdxContent(props) {
+  const _components = {
+    a: "a",
+    code: "code",
+    h2: "h2",
+    h3: "h3",
+    p: "p",
+    pre: "pre",
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_2__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.Fragment, {
+    children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h2, {
+      id: "nya-funktioner",
+      children: "Nya funktioner"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.p, {
+      children: "Medium size components"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h2, {
+      id: "migrera-från-9xx-till-1000",
+      children: "Migrera från 9.x.x till 10.0.0"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h3, {
+      id: "accordion",
+      children: "Accordion"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.p, {
+      children: ["Accordion har inte längre stöd för prop ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "type"
+      }), " för att ha flera AccordionItems öppna samtidigt. Använd\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "allowsMultipleExpanded"
+      }), " istället"]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.pre, {
+      children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        className: "language-diff",
+        children: "- <Accordion type='multiple'>/* items */</Accordion>\n+ <Accordion allowsMultipleExpanded>/* items */</Accordion>\n"
+      })
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h3, {
+      id: "button",
+      children: "Button"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.p, {
+      children: ["Button har inte längre stöd för ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "size='small'"
+      }), ". Däremot introduceras storleken ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "medium"
+      }), " och det går att välja\nmellan ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "'large'"
+      }), " och ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "'medium'"
+      }), ", där large är default."]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.pre, {
+      children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        className: "language-diff",
+        children: "//highlight-next-line\n-   <Button size='small'></Button> // this is no longer valid\n+   <Button size='medium'></Button>\n\n"
+      })
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h3, {
+      id: "modal-dialogtrigger",
+      children: "Modal, DialogTrigger"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.p, {
+      children: ["Varianten med ModalTrigger och Dialog är nu utfasad och istället ska DialogTrigger och Modal användas. En aning\nförvirrande men principen är liknande och den uppdaterade versionen innehåller många förbättringar.\nSe dokumentationen för ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.a, {
+        href: "/components/modal",
+        children: "modal"
+      }), " för hur DialogTrigger används."]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.pre, {
+      children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        className: "language-diff",
+        children: "- import {Dialog, ModalTrigger} from '@midas-ds/components' // no longer works\n+ import {DialogTrigger, Modal} from '@midas-ds/components'\n"
+      })
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h3, {
+      id: "spinner",
+      children: "Spinner"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.p, {
+      children: ["Från och med 10.0.0 går det inte längre att använda prop ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "dark"
+      }), ", använd ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "ìsOnColor"
+      }), " istället."]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.pre, {
+      children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        className: "language-diff",
+        children: "- <Spinner dark/>\n+ <Spinner isOnColor/>\n"
+      })
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h3, {
+      id: "tabs",
+      children: "Tabs"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.p, {
+      children: ["Använd ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "defaultSelectedKey"
+      }), " istället för ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "defaultSelected"
+      }), "."]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.pre, {
+      children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        className: "language-diff",
+        children: "- <Tabs defaultSelected={1}> /* tab content */ </Tabs>\n+ <Tabs defaultSelectedKey={1}> /* tab content */ </Tabs>\n"
+      })
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h3, {
+      id: "tokens",
+      children: "Tokens"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.p, {
+      children: ["Tokens ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "borderInvalid"
+      }), " ersätts med ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "supportBorderWarning"
+      }), " och ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "textInvalid"
+      }), " ersätts med ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "textWarning"
+      }), "."]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.pre, {
+      children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        className: "language-diff",
+        children: "- semantic.borderInvalid\n+ semantic.supportBorderWarning\n- semantic.textInvalid\n+ semantic.textWarning\n\n"
+      })
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h3, {
+      id: "usemessageformatter",
+      children: "useMessageFormatter"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.p, {
+      children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "useMessageFormatter"
+      }), " försvinner till förmån för ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "useLocalizedStringFormatter"
+      }), " eftersom den är deprecated hos React Aria."]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.pre, {
+      children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        className: "language-diff",
+        children: "- useMessageFormatter()\n+ useLocalizedStringFormatter()\n"
+      })
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h3, {
+      id: "label",
+      children: "Label"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.p, {
+      children: ["Label variant försvinner i version 10.0.0, det går alltså inte längre att använda ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "variant='label-01'"
+      }), " eller ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "label-02"
+      }), ".\nDefault är numera ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "label-02"
+      }), " och för att uppnå variant ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "label-01"
+      }), ", använd komponent ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "Text"
+      }), " med ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "slot='description'"
+      }), "."]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.pre, {
+      children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        className: "language-diff",
+        children: "- <Label variant='label-01'>Label</Label>\n+ <Text slot='description'>Label</Label>\n"
+      })
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h3, {
+      id: "skeleton",
+      children: "Skeleton"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.p, {
+      children: ["Namnfix på prop ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "rectangular"
+      }), " som numera heter ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "rectangle"
+      }), " för en mer konsekvent semantik. Animering är numera endast en\nboolean, ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "isAnimated=true|false"
+      }), "."]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.pre, {
+      children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        className: "language-diff",
+        children: "- <Skeleton variant='rectangular' animation='wave'/>\n+ <Skeleton variant='rectangle' isAnimated />\n"
+      })
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.p, {
+      children: ["Se alla förändringar i detalj på ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.a, {
+        href: "/changelog",
+        children: "changelog"
+      })]
+    }), "\n"]
+  });
+}
+function MDXContent(props = {}) {
+  const {wrapper: MDXLayout} = {
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_2__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return MDXLayout ? (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(MDXLayout, {
+    ...props,
+    children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+
+
+/***/ }),
+
+/***/ 55075:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"permalink":"/pr-preview/pr-580/blog/releases/10.0.0","source":"@site/blog/releases/10.0.0/index.mdx","title":"Release 10.0.0","description":"Nya funktioner","date":"2025-05-15T00:00:00.000Z","tags":[],"readingTime":1.6,"hasTruncateMarker":true,"authors":[],"frontMatter":{"title":"Release 10.0.0","date":"2025-05-15T00:00:00.000Z","pagination_prev":null,"pagination_next":null},"unlisted":false,"nextItem":{"title":"Release 9.0.0","permalink":"/pr-preview/pr-580/blog/releases/9.0.0"}}');
+
+/***/ })
+
+}]);
