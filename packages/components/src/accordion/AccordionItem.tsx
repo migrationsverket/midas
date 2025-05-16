cd168bb32cbad50bdbf47d613049c225b47212e4
jabir Khalil <jabir.saadi.khalil@migrationsverket.se>
2025-05-16 10:47:56 +0200

    feat(accordion): add variant `hasBackground`

--- packages/components/src/accordion/AccordionItem.tsx
+++ packages/components/src/accordion/AccordionItem.tsx
@@ -21,10 +21,10 @@ interface MidasAccordionItem extends Omit<DisclosureProps, 'children'> {
   /** Display an accordion item in different status states */
   type?: 'default' | 'success' | 'warning'
   /**
-   * To not use white background in types
-   * @default false
+   * Adds a background element to the content, set to false for a transparent look
+   * @default true
    **/
-  hasWhiteBackground?: boolean
+  hasBackground?: boolean
 }
 
 export const AccordionItem: React.FC<MidasAccordionItem> = ({
@@ -33,7 +33,7 @@ export const AccordionItem: React.FC<MidasAccordionItem> = ({
   className,
   headingLevel = 'h2',
   type,
-  hasWhiteBackground = false,
+  hasBackground = true,
   ...props
 }) => {
   const titleIsReactNode = typeof title === 'object'
@@ -92,7 +92,7 @@ export const AccordionItem: React.FC<MidasAccordionItem> = ({
         <div
           className={clsx(
             styles.content,
-            hasWhiteBackground && styles.hasWhiteBackground,
+            hasBackground && styles.hasBackground,
           )}
         >
           {children}
