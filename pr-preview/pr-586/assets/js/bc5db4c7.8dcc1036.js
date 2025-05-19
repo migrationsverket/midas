4af2d5326ac4d7858234ccac237fc1cd4b65c1ac
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-05-19 08:04:25 +0000

    Deploy preview for PR 586 🛫

---
+++ pr-preview/pr-586/assets/js/bc5db4c7.8dcc1036.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[8589],{
+
+/***/ 19902:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-586/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":9,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
