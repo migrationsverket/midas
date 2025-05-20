6e9efc55cc86604a16e207a5dd1391ca9b540d2d
derpbravely <derpbravely@users.noreply.github.com>
2025-05-20 11:50:27 +0000

    Deploy preview for PR 590 🛫

---
+++ pr-preview/pr-590/assets/js/acecf23e.a49ad23b.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[1903],{
+
+/***/ 92621:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"blogBasePath":"/pr-preview/pr-590/blog","blogTitle":"Blog","authorsListPath":"/pr-preview/pr-590/blog/authors"}');
+
+/***/ })
+
+}]);
