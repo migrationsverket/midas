d9d179e3e8b1cf13ad671cc7f79a93352b707a0e
derpbravely <derpbravely@users.noreply.github.com>
2025-05-21 10:57:45 +0000

    Deploy preview for PR 593 🛫

---
+++ pr-preview/pr-593/assets/js/1941f20e.faa2ba19.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[8157],{
+
+/***/ 69759:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-593/changelog/page/2","page":2,"postsPerPage":20,"totalPages":2,"totalCount":24,"previousPage":"/pr-preview/pr-593/changelog","blogDescription":"Håll dig uppdaterad med allt som händer i varje release","blogTitle":"Midas changelog"}}');
+
+/***/ })
+
+}]);
