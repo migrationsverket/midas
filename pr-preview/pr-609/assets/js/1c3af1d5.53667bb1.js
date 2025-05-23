0108800e819a3272ab82270dbbb546e718f4855d
jabkha <jabkha@users.noreply.github.com>
2025-05-23 12:44:51 +0000

    Deploy preview for PR 609 🛫

---
+++ pr-preview/pr-609/assets/js/1c3af1d5.53667bb1.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[6661],{
+
+/***/ 10181:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-609/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":9,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
