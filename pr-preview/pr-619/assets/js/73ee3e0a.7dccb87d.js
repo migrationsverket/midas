17e1a4378f88263ad6541a81774af5f184b55820
phieksmig <phieksmig@users.noreply.github.com>
2025-05-27 18:46:30 +0000

    Deploy preview for PR 619 🛫

---
+++ pr-preview/pr-619/assets/js/73ee3e0a.7dccb87d.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[730],{
+
+/***/ 44617:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-619/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":10,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
