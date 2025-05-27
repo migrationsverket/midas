31ea0181e87e0a015288360f0f6360ed4e656c58
phieksmig <phieksmig@users.noreply.github.com>
2025-05-27 06:39:12 +0000

    Deploy preview for PR 614 🛫

---
+++ pr-preview/pr-614/assets/js/b70c3472.9791886e.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[7803],{
+
+/***/ 26630:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-614/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":10,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
