a718e09d291391ae52dcd289c220d66100f7cb80
derpbravely <derpbravely@users.noreply.github.com>
2025-06-02 09:09:33 +0000

    Deploy preview for PR 621 🛫

---
+++ pr-preview/pr-621/assets/js/405b77f1.78314a62.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[6003],{
+
+/***/ 12324:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-621/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":10,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
