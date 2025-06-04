aea3f43d3760ae7840e455fb030d284d605d7a85
derpbravely <derpbravely@users.noreply.github.com>
2025-06-04 07:22:55 +0000

    Deploy preview for PR 628 🛫

---
+++ pr-preview/pr-628/assets/js/1a5ebeda.3dcc2141.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[7201],{
+
+/***/ 68473:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-628/changelog","page":1,"postsPerPage":20,"totalPages":2,"totalCount":25,"nextPage":"/pr-preview/pr-628/changelog/page/2","blogDescription":"Håll dig uppdaterad med allt som händer i varje release","blogTitle":"Midas changelog"}}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-628/assets/js/55a455d5.5accdef9.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[361],{
+
+/***/ 84345:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-628/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":10,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
