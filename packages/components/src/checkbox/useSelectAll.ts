d595e55cb5ef06ff444a1c1fc6699197792f8328
derpbravely <197733689+derpbravely@users.noreply.github.com>
2025-06-05 14:55:57 +0200

    refactor(checkbox): rename variables

--- packages/components/src/checkbox/useSelectAll.ts
+++ packages/components/src/checkbox/useSelectAll.ts
@@ -1,24 +1,31 @@
-import { CheckboxGroupState } from 'react-stately';
-import * as React from 'react';
-import { Checkbox } from './Checkbox';
+import * as React from 'react'
+import { CheckboxGroupState } from 'react-stately'
+import { Checkbox } from './Checkbox'
 
-export const useSelectAll = (children: React.ReactNode, state: CheckboxGroupState) => {
+export const useSelectAll = (
+  children: React.ReactNode,
+  { value: selectedItems }: CheckboxGroupState,
+) => {
+  const checkboxes = React.Children.toArray(children).filter(
+    child => React.isValidElement(child) && child.type === Checkbox,
+  )
 
-  const actualChildren = React.Children.toArray(children).filter(child => React.isValidElement(child) && child.type === Checkbox);
-  const numberOfCheckboxes = React.Children.count(actualChildren);
-  const checkboxValues: string[] = React.Children
-    .toArray(children)
+  const numberOfCheckboxes = React.Children.count(checkboxes)
+
+  const checkboxValues: string[] = React.Children.toArray(children)
     .filter(child => React.isValidElement(child))
     .filter(child => !!child && child.type === Checkbox)
-    .map((child) => child.props.value);
-  const noneSelected = state.value.length === 0
-  const allSelected = state.value.length === numberOfCheckboxes
+    .map(child => child.props.value)
+
+  const noneSelected = selectedItems.length === 0
+
+  const allSelected = selectedItems.length === numberOfCheckboxes
+
   const someSelected = !noneSelected && !allSelected
 
   return {
     allSelected,
     someSelected,
-    checkboxValues
+    checkboxValues,
   }
-
 }
