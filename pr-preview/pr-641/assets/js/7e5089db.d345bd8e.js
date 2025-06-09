a879fab71e4253475f42d414b90b5dfce82b2fe6
derpbravely <derpbravely@users.noreply.github.com>
2025-06-09 11:57:57 +0000

    Deploy preview for PR 641 🛫

---
+++ pr-preview/pr-641/assets/js/7e5089db.d345bd8e.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[4827],{
+
+/***/ 75704:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-641/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":11,"nextPage":"/pr-preview/pr-641/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
