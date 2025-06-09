eb0ca5dd81bb8a2e62096b05b2348f786cb0d833
derpbravely <derpbravely@users.noreply.github.com>
2025-06-09 10:27:10 +0000

    Deploy preview for PR 640 🛫

---
+++ pr-preview/pr-640/assets/js/dfbeae55.93002939.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[2426],{
+
+/***/ 35865:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-640/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":11,"nextPage":"/pr-preview/pr-640/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
