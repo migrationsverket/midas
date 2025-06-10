73a4d707fe9204748782c5ac0b78b20dff7160c7
phieksmig <phieksmig@users.noreply.github.com>
2025-06-10 13:57:48 +0000

    Deploy preview for PR 646 🛫

---
+++ pr-preview/pr-646/assets/js/48263c5a.9dfc86ef.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[2835],{
+
+/***/ 86135:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-646/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":11,"nextPage":"/pr-preview/pr-646/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
