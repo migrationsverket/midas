6f3cc36071438766d41f456cb2d3eac2a9b998b2
derpbravely <197733689+derpbravely@users.noreply.github.com>
2025-06-10 09:39:04 +0200

    feat(table): add size prop

--- packages/components/src/table/Table.tsx
+++ packages/components/src/table/Table.tsx
@@ -28,22 +28,35 @@ import {
   GripVertical,
 } from 'lucide-react'
 import clsx from 'clsx'
+import { Size } from '../common/types'
 
 export interface TableProps extends AriaTableProps {
   /**
    *  A more compact version of the table
+   *  @deprecated since v10.1.1, please use the `size` prop instead.
    */
   narrow?: boolean
+  /** Row and column height (large: 48px, medium: 40px)
+   *  @default 'large'
+   * */
+  size?: Size
   /**
    * Alternating colors for rows
    */
   striped?: boolean
 }
 
-export const Table = ({ narrow, striped, className, ...rest }: TableProps) => {
+export const Table = ({
+  narrow,
+  size,
+  striped,
+  className,
+  ...rest
+}: TableProps) => {
   const classNames = clsx(
     styles.table,
     narrow && styles.narrow,
+    size === 'medium' && styles.medium,
     striped && styles.striped,
     className,
   )
