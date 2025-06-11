c2b26403029bba671247b227d6d7e2fd34ac551f
jabkha <jabkha@users.noreply.github.com>
2025-06-11 09:03:45 +0000

    Deploy preview for PR 651 🛫

---
+++ pr-preview/pr-651/assets/js/8ecb7e4e.095cbbea.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[6212],{
+
+/***/ 66745:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-651/changelog/page/2","page":2,"postsPerPage":20,"totalPages":2,"totalCount":26,"previousPage":"/pr-preview/pr-651/changelog","blogDescription":"Håll dig uppdaterad med allt som händer i varje release","blogTitle":"Midas changelog"}}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-651/assets/js/d0000e63.8948d4f2.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[1607],{
+
+/***/ 10376:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-651/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":11,"nextPage":"/pr-preview/pr-651/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
