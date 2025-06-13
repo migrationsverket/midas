7dfc7fb83259c6511f6db224a846f367d744313b
Wilhelm Hjelm <wilhelm.hjelm@consid.se>
2025-06-13 10:26:31 +0200

    parent d4935c1e572cc0de11b361f0039a309195c27083 author Wilhelm Hjelm <wilhelm.hjelm@consid.se> 1749803191 +0200 committer Wilhelm Hjelm <wilhelm.hjelm@consid.se> 1750064419 +0200
    feat(grid): new grid v2
    
    - flex and grid is replaced with new grid components
    - updated breakpoints
    - updated breakpoints in some components
    - adjust usage of Grid across docweb and storybook
    
    docs: remove grid from use page
    
    Apply suggestions from code review
    
    Co-authored-by: derpbravely <197733689+derpbravely@users.noreply.github.com>
    
    fix(grid): change magrin prop to removeMargins
    
    chore: pr comments
    
    docs: adjust some stories
    
    docs: fix tokens table in mobile
    
    fix(grid): export flex and flexitem as deprecated components
    
    feat(grid)!: new grid v2
    
    - flex and grid is replaced with new grid components
    - updated breakpoints
    - updated breakpoints in some components
    - adjust usage of Grid across docweb and storybook
    
    docs: remove grid from use page
    
    Apply suggestions from code review
    
    Co-authored-by: derpbravely <197733689+derpbravely@users.noreply.github.com>
    
    fix(grid): change magrin prop to removeMargins
    
    chore: pr comments
    
    docs: fix tokens table in mobile
    
    fix(grid): export flex and flexitem as deprecated components
    
    feat(grid): new grid v2
    
    - flex and grid is replaced with new grid components
    - updated breakpoints
    - updated breakpoints in some components
    - adjust usage of Grid across docweb and storybook
    
    docs: remove grid from use page
    
    Apply suggestions from code review
    
    Co-authored-by: derpbravely <197733689+derpbravely@users.noreply.github.com>
    
    fix(grid): change magrin prop to removeMargins
    
    chore: pr comments
    
    docs: adjust some stories
    
    docs: fix tokens table in mobile
    
    fix(grid): export flex and flexitem as deprecated components
    
    feat(grid)!: new grid v2
    
    - flex and grid is replaced with new grid components
    - updated breakpoints
    - updated breakpoints in some components
    - adjust usage of Grid across docweb and storybook
    
    docs: remove grid from use page
    
    Apply suggestions from code review
    
    Co-authored-by: derpbravely <197733689+derpbravely@users.noreply.github.com>
    
    fix(grid): change magrin prop to removeMargins
    
    docs: fix tokens table in mobile
    
    fix(grid): export flex and flexitem as deprecated components

--- packages/components/.storybook/custom-viewports.ts
+++ packages/components/.storybook/custom-viewports.ts
@@ -1,43 +1,39 @@
+import { windowSizes } from '../src/theme'
+
 export const customViewports = {
-  smallest: {
-    name: 'Smallest',
-    styles: {
-      width: '320px',
-      height: '568px',
-    },
-  },
+  smallest: { name: 'Smallest', styles: { width: '320px', height: '568px' } },
   'x-small': {
     name: 'X-small',
     styles: {
-      width: '440px',
+      width: `${windowSizes.sm - 1}px`, // 479px
       height: '100%',
     },
   },
   small: {
     name: 'Small',
     styles: {
-      width: '767px',
+      width: `${windowSizes.sm}px`, // 480px
       height: '100%',
     },
   },
   medium: {
     name: 'Medium',
     styles: {
-      width: '768px',
+      width: `${windowSizes.md}px`, // 768px
       height: '100%',
     },
   },
   large: {
     name: 'Large',
     styles: {
-      width: '1200px',
+      width: `${windowSizes.lg}px`, // 1024px
       height: '100%',
     },
   },
   xLarge: {
     name: 'X-Large',
     styles: {
-      width: '1440px',
+      width: `${windowSizes.xl}px`, // 1280px
       height: '100%',
     },
   },
