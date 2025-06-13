a15f985afb128f0400bc1c6fa139480eb24d00c8
derpbravely <derpbravely@users.noreply.github.com>
2025-06-13 10:47:41 +0000

    Deploy preview for PR 658 🛫

---
+++ pr-preview/pr-658/assets/js/325f5f02.ab28d21b.js
@@ -0,0 +1,113 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[414],{
+
+/***/ 28262:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   assets: () => (/* binding */ assets),
+/* harmony export */   contentTitle: () => (/* binding */ contentTitle),
+/* harmony export */   "default": () => (/* binding */ MDXContent),
+/* harmony export */   frontMatter: () => (/* binding */ frontMatter),
+/* harmony export */   metadata: () => (/* reexport default export from named module */ _site_docusaurus_docusaurus_plugin_content_blog_default_site_blog_releases_3_0_0_index_mdx_325_json__WEBPACK_IMPORTED_MODULE_0__),
+/* harmony export */   toc: () => (/* binding */ toc)
+/* harmony export */ });
+/* harmony import */ var _site_docusaurus_docusaurus_plugin_content_blog_default_site_blog_releases_3_0_0_index_mdx_325_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(81269);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(74848);
+/* harmony import */ var _mdx_js_react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(28453);
+
+
+const frontMatter = {
+	title: 'Release 3.0.0',
+	date: new Date('2025-02-28T00:00:00.000Z'),
+	pagination_prev: null,
+	pagination_next: null
+};
+const contentTitle = undefined;
+
+const assets = {
+"authorsImageUrls": [],
+};
+
+/*truncate*/
+
+
+const toc = [{
+  "value": "Migrera från 2.x.x till 3.0.0",
+  "id": "migrera-från-2xx-till-300",
+  "level": 2
+}, {
+  "value": "SearchField",
+  "id": "searchfield",
+  "level": 3
+}];
+function _createMdxContent(props) {
+  const _components = {
+    a: "a",
+    code: "code",
+    h2: "h2",
+    h3: "h3",
+    p: "p",
+    pre: "pre",
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_2__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.Fragment, {
+    children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h2, {
+      id: "migrera-från-2xx-till-300",
+      children: "Migrera från 2.x.x till 3.0.0"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h3, {
+      id: "searchfield",
+      children: "SearchField"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.p, {
+      children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "SearchField"
+      }), " stödjer inte längre ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "isRequired"
+      }), ". Beteendet kan efterliknas med hjälp av ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        children: "validate"
+      })]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.pre, {
+      children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+        className: "language-tsx",
+        children: "<SearchField validate={v => !!v.length || 'Var god ange åtminstone ett tecken' />\n"
+      })
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.p, {
+      children: ["Se mer information om ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.a, {
+        href: "/components/search-field",
+        children: "hur du använder SearchField"
+      })]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.p, {
+      children: ["Kolla ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.a, {
+        href: "/changelog/3.0.0",
+        children: "changelog"
+      })]
+    }), "\n"]
+  });
+}
+function MDXContent(props = {}) {
+  const {wrapper: MDXLayout} = {
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_2__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return MDXLayout ? (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(MDXLayout, {
+    ...props,
+    children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+
+
+/***/ }),
+
+/***/ 81269:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"permalink":"/pr-preview/pr-658/blog/releases/3.0.0","source":"@site/blog/releases/3.0.0/index.mdx","title":"Release 3.0.0","description":"Migrera från 2.x.x till 3.0.0","date":"2025-02-28T00:00:00.000Z","tags":[],"readingTime":0.23,"hasTruncateMarker":true,"authors":[],"frontMatter":{"title":"Release 3.0.0","date":"2025-02-28T00:00:00.000Z","pagination_prev":null,"pagination_next":null},"unlisted":false,"prevItem":{"title":"Release 4.0.0","permalink":"/pr-preview/pr-658/blog/releases/4.0.0"},"nextItem":{"title":"Release 2.0.0","permalink":"/pr-preview/pr-658/blog/releases/2.0.0"}}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-658/assets/js/6c1310e1.d198e003.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[3992],{
+
+/***/ 78799:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-658/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":11,"nextPage":"/pr-preview/pr-658/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
