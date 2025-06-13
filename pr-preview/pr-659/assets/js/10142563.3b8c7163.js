23e2cb2a56cc5c6cc0dd6d348915ed67a68ca337
derpbravely <derpbravely@users.noreply.github.com>
2025-06-13 11:52:27 +0000

    Deploy preview for PR 659 🛫

---
+++ pr-preview/pr-659/assets/js/10142563.3b8c7163.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[2205],{
+
+/***/ 5533:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-659/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":11,"previousPage":"/pr-preview/pr-659/blog","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
