036cc4539497d3eecf0a14ad4432a1e7c71681ba
derpbravely <197733689+derpbravely@users.noreply.github.com>
2025-06-16 08:27:53 +0000

    Deploying to gh-pages from @ migrationsverket/midas@39ab5afca3236db3666432ab2bf5d1c5472f0fac 🚀

---
+++ dev-docs/assets/js/c4da33b9.5d7bcf1f.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[2262],{
+
+/***/ 98502:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/dev-docs/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":11,"nextPage":"/dev-docs/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
---
+++ dev-docs/assets/js/f986d618.f517397e.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[2815],{
+
+/***/ 41951:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/dev-docs/changelog","page":1,"postsPerPage":20,"totalPages":2,"totalCount":26,"nextPage":"/dev-docs/changelog/page/2","blogDescription":"Håll dig uppdaterad med allt som händer i varje release","blogTitle":"Midas changelog"}}');
+
+/***/ })
+
+}]);
