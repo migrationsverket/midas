acbe2fe9873fc26ec4661a9d01a23050a9285352
derpbravely <derpbravely@users.noreply.github.com>
2025-06-16 14:06:17 +0000

    Deploy preview for PR 662 🛫

---
+++ pr-preview/pr-662/assets/js/b6ba8940.33ec4473.js
@@ -0,0 +1,65 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[413],{
+
+/***/ 35450:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   assets: () => (/* binding */ assets),
+/* harmony export */   contentTitle: () => (/* binding */ contentTitle),
+/* harmony export */   "default": () => (/* binding */ MDXContent),
+/* harmony export */   frontMatter: () => (/* binding */ frontMatter),
+/* harmony export */   metadata: () => (/* reexport default export from named module */ _site_docusaurus_changelog_plugin_default_site_changelog_source_6_1_0_md_903_json__WEBPACK_IMPORTED_MODULE_0__),
+/* harmony export */   toc: () => (/* binding */ toc)
+/* harmony export */ });
+/* harmony import */ var _site_docusaurus_changelog_plugin_default_site_changelog_source_6_1_0_md_903_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(52958);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(74848);
+/* harmony import */ var _mdx_js_react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(28453);
+
+
+const frontMatter = {
+	mdx: {
+		format: 'md'
+	},
+	date: '2025-03-28T20:00',
+	pagination_prev: null,
+	pagination_next: null
+};
+const contentTitle = '6.1.0';
+
+const assets = {
+"authorsImageUrls": [],
+};
+
+
+
+const toc = [];
+function _createMdxContent(props) {
+  return (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.Fragment, {});
+}
+function MDXContent(props = {}) {
+  const {wrapper: MDXLayout} = {
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_2__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return MDXLayout ? (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(MDXLayout, {
+    ...props,
+    children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+
+
+/***/ }),
+
+/***/ 52958:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"permalink":"/pr-preview/pr-662/changelog/6.1.0","source":"@site/changelog/source/6.1.0.md","title":"6.1.0","description":"- text: add description slot (73d308a13)","date":"2025-03-28T20:00:00.000Z","tags":[],"hasTruncateMarker":true,"authors":[],"frontMatter":{"mdx":{"format":"md"},"date":"2025-03-28T20:00","pagination_prev":null,"pagination_next":null},"unlisted":false,"prevItem":{"title":"6.2.0","permalink":"/pr-preview/pr-662/changelog/6.2.0"},"nextItem":{"title":"6.0.1","permalink":"/pr-preview/pr-662/changelog/6.0.1"},"listPageLink":"/pr-preview/pr-662/changelog/"}');
+
+/***/ })
+
+}]);
