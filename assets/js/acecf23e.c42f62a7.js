435deb41819025329ccd96d60afd6b52472ed089
derpbravely <197733689+derpbravely@users.noreply.github.com>
2025-06-18 08:48:02 +0000

    Deploying to gh-pages from @ migrationsverket/midas@06f7d43a96e9f326ab641d2b58100ab08240bfea 🚀

---
+++ assets/js/acecf23e.c42f62a7.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[1903],{
+
+/***/ 92621:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"blogBasePath":"/release-notes","blogTitle":"Blog","authorsListPath":"/release-notes/authors"}');
+
+/***/ })
+
+}]);
