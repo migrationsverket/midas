35453a0855d98d5b9c02cbef05812bd570f347f5
wilhjelmig <wilhjelmig@users.noreply.github.com>
2025-06-19 07:34:46 +0000

    Deploy preview for PR 671 🛫

---
+++ pr-preview/pr-671/assets/js/2a24a634.6e074b93.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[5334],{
+
+/***/ 16300:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-671/release-notes/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":12,"previousPage":"/pr-preview/pr-671/release-notes","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
