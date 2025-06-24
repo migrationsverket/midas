07359e0f0481654e15e6fb5dd848906596065819
derpbravely <derpbravely@users.noreply.github.com>
2025-06-24 12:02:43 +0000

    Deploy preview for PR 676 🛫

---
+++ pr-preview/pr-676/assets/js/9109ef7c.e09ca6e8.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[267],{
+
+/***/ 30404:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-676/changelog","page":1,"postsPerPage":20,"totalPages":2,"totalCount":27,"nextPage":"/pr-preview/pr-676/changelog/page/2","blogDescription":"Håll dig uppdaterad med allt som händer i varje release","blogTitle":"Midas changelog"}}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-676/assets/js/c5311ee5.24760d8e.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[7083],{
+
+/***/ 80729:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-676/changelog/page/2","page":2,"postsPerPage":20,"totalPages":2,"totalCount":27,"previousPage":"/pr-preview/pr-676/changelog","blogDescription":"Håll dig uppdaterad med allt som händer i varje release","blogTitle":"Midas changelog"}}');
+
+/***/ })
+
+}]);
