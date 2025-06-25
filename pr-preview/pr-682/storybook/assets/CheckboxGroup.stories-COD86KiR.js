c2f56be2dcc73f9d4faa4e5e47c44e93ee5eae0c
derpbravely <derpbravely@users.noreply.github.com>
2025-06-25 12:48:54 +0000

    Deploy preview for PR 682 🛫

---
+++ pr-preview/pr-682/storybook/assets/CheckboxGroup.stories-COD86KiR.js
@@ -0,0 +1,83 @@
+import{j as s}from"./jsx-runtime-BYYWji4R.js";import{C as u}from"./Checkbox-Bl6EfnlF.js";import{C as j}from"./CheckboxGroup-BJHYA_c8.js";import{u as a,e as o}from"./index-CqmFxPum.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./RSPContexts-HQcWPJyv.js";import"./utils-BLyg3OOq.js";import"./clsx-B-dksMZM.js";import"./Form-CLBgFqGB.js";import"./useFormValidation-Cwkq_bn0.js";import"./useFocusRing-eGNMqT99.js";import"./_class_private_field_init-BOtJyCjG.js";import"./index-DQEgcCId.js";import"./VisuallyHidden-DWQ0FsMN.js";import"./useToggleState-DcHMzLVj.js";import"./useControlledState-H1tiOFpU.js";import"./FieldError-Bc4GcTNT.js";import"./Text-w6ZeLcO4.js";import"./Text-ObKd9RkE.js";import"./Label-yzxHIfXU.js";import"./Button-D5YfTa-t.js";import"./Hidden-BC9qER92.js";import"./context-d6PNTTSe.js";import"./useButton-rs5PgjfN.js";import"./Dialog-yN6YlsCK.js";import"./OverlayArrow-BZcOvAjo.js";import"./Collection-nQ4RBMPG.js";import"./index-NDK0uC61.js";import"./Separator-u34oIBUJ.js";import"./SelectionManager-sKkAcYRw.js";import"./useEvent-B0l3XC2n.js";import"./scrollIntoView-BivqL63c.js";import"./FocusScope-C-SsV_ba.js";import"./ListKeyboardDelegate-Bkg2cFa9.js";import"./useLocalizedStringFormatter-TrhiTCnH.js";import"./getScrollParent-FdZGTYO5.js";import"./Dialog-B7jnc85T.js";import"./Button-ICAWD0Qi.js";import"./Button.module-AG3JZ-l4.js";import"./x-BaIpobvV.js";import"./createLucideIcon-DQK9Z80p.js";import"./Heading-DncQzqB_.js";import"./info-DTm9uzms.js";import"./useLocalizedStringFormatter-1I2iWIMg.js";import"./useField-BBXNKLtY.js";const Se={title:"Components/Checkbox/CheckboxGroup",component:j,parameters:{layout:"centered"},args:{isInvalid:!1,isDisabled:!1,isRequired:!1,isReadOnly:!1,label:"Etikett",description:"Beskrivning",errorMessage:"Du måste välja en frukt",errorPosition:"top",children:["Banan","Apple","Mango"].map(e=>s.jsx(u,{value:e,children:e},e))},tags:["autodocs"]},i={},c={args:{showSelectAll:!0}},l={args:{showSelectAll:!0,selectAllLabel:"SELECT ALL"},tags:["!dev","!autodocs"],parameters:{chromatic:{disableSnapshot:!0}},render:({...e})=>s.jsxs(j,{...e,children:[s.jsx(u,{value:"banana",children:"Banana"}),s.jsx(u,{value:"apple",children:"Apple"}),s.jsx("div",{children:"I'm not a checkbox"})]}),play:async({canvas:e,step:t})=>{const n=e.getByText("SELECT ALL");await t("It should show all selected as checked when all options are checked",async()=>{await a.tab(),await a.tab(),await a.keyboard("[Enter]"),await a.tab(),await a.keyboard("[Enter]"),await o(n).toHaveAttribute("data-selected","true")}),await t("It should show select all as indeterminate when 0 > check < max is selected",async()=>{const r=e.getByLabelText("Banana");await a.click(r),await o(n).toHaveAttribute("data-indeterminate","true")})}},p={args:{isDisabled:!0}},d={args:{isRequired:!0,isInvalid:void 0,label:"Invalid (by required)",description:"This is a working test",validationBehavior:"aria",errorMessage:"Du måste välja en av frukterna"},tags:["!dev","!autodocs"],parameters:{chromatic:{disableSnapshot:!0}},play:async({canvas:e,step:t,args:n})=>{await t("It should display correct error message when invalid",async()=>{await o(e.getByText(n.errorMessage)).toBeVisible()}),await t("All checkboxes should be required",async()=>{e.getAllByRole("checkbox").forEach(async r=>await o(r).toBeRequired())}),await t("Validation should be satisfied when checking one checkbox",async()=>{await a.click(e.getAllByRole("checkbox")[0]),e.getAllByRole("checkbox").forEach(async r=>await o(r).toBeValid())})}},m={args:{isInvalid:!0}};var h,b,x;i.parameters={...i.parameters,docs:{...(h=i.parameters)==null?void 0:h.docs,source:{originalSource:"{}",...(x=(b=i.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var k,w,g;c.parameters={...c.parameters,docs:{...(k=c.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    showSelectAll: true
+  }
+}`,...(g=(w=c.parameters)==null?void 0:w.docs)==null?void 0:g.source}}};var v,y,A;l.parameters={...l.parameters,docs:{...(v=l.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    showSelectAll: true,
+    selectAllLabel: 'SELECT ALL'
+  },
+  tags: ['!dev', '!autodocs'],
+  parameters: {
+    chromatic: {
+      disableSnapshot: true
+    }
+  },
+  render: ({
+    ...args
+  }) => {
+    return <CheckboxGroup {...args}>
+        <Checkbox value='banana'>Banana</Checkbox>
+        <Checkbox value='apple'>Apple</Checkbox>
+        <div>I'm not a checkbox</div>
+      </CheckboxGroup>;
+  },
+  play: async ({
+    canvas,
+    step
+  }) => {
+    const selectAllCheckbox = canvas.getByText('SELECT ALL');
+    await step('It should show all selected as checked when all options are checked', async () => {
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.keyboard('[Enter]');
+      await userEvent.tab();
+      await userEvent.keyboard('[Enter]');
+      await expect(selectAllCheckbox).toHaveAttribute('data-selected', 'true');
+    });
+    await step('It should show select all as indeterminate when 0 > check < max is selected', async () => {
+      const banana = canvas.getByLabelText('Banana');
+      await userEvent.click(banana);
+      await expect(selectAllCheckbox).toHaveAttribute('data-indeterminate', 'true');
+    });
+  }
+}`,...(A=(y=l.parameters)==null?void 0:y.docs)==null?void 0:A.source}}};var E,B,S;p.parameters={...p.parameters,docs:{...(E=p.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(S=(B=p.parameters)==null?void 0:B.docs)==null?void 0:S.source}}};var I,C,f;d.parameters={...d.parameters,docs:{...(I=d.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    isRequired: true,
+    isInvalid: undefined,
+    label: 'Invalid (by required)',
+    description: 'This is a working test',
+    validationBehavior: 'aria',
+    errorMessage: 'Du måste välja en av frukterna'
+  },
+  tags: ['!dev', '!autodocs'],
+  parameters: {
+    chromatic: {
+      disableSnapshot: true
+    }
+  },
+  play: async ({
+    canvas,
+    step,
+    args
+  }) => {
+    await step('It should display correct error message when invalid', async () => {
+      await expect(canvas.getByText(args.errorMessage as string)).toBeVisible();
+    });
+    await step('All checkboxes should be required', async () => {
+      canvas.getAllByRole('checkbox').forEach(async checkbox => await expect(checkbox).toBeRequired());
+    });
+    await step('Validation should be satisfied when checking one checkbox', async () => {
+      await userEvent.click(canvas.getAllByRole('checkbox')[0]);
+      canvas.getAllByRole('checkbox').forEach(async checkbox => await expect(checkbox).toBeValid());
+    });
+  }
+}`,...(f=(C=d.parameters)==null?void 0:C.docs)==null?void 0:f.source}}};var L,R,T;m.parameters={...m.parameters,docs:{...(L=m.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    isInvalid: true
+  }
+}`,...(T=(R=m.parameters)==null?void 0:R.docs)==null?void 0:T.source}}};const Ie=["Example","ShowSelectAll","SelectAllInteraction","Disabled","InvalidInteraction","Invalid"];export{p as Disabled,i as Example,m as Invalid,d as InvalidInteraction,l as SelectAllInteraction,c as ShowSelectAll,Ie as __namedExportsOrder,Se as default};
