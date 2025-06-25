2bf3f89b292ba8eba449b7a65c3734dc209c33ba
derpbravely <197733689+derpbravely@users.noreply.github.com>
2025-06-25 12:20:06 +0000

    Deploying to gh-pages from @ migrationsverket/midas@9ea323915db59880d16fb395095a2b326c97f825 🚀

---
+++ storybook/assets/Text-w6ZeLcO4.js
@@ -0,0 +1,1 @@
+import{$ as o}from"./utils-BLyg3OOq.js";import{r as a,R as $}from"./index-ClcD9ViR.js";const f=a.createContext({}),x=a.forwardRef(function(e,t){[e,t]=o(e,t,f);let{elementType:r="span",...c}=e;return $.createElement(r,{className:"react-aria-Text",...c,ref:t})});export{f as $,x as a};
---
+++ storybook/common/types.d.ts
@@ -0,0 +1,3 @@
+export type Size = 'large' | 'medium';
+export type ColumnSize = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' | 'auto' | 'grow' | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 'full' | 'half' | 'third' | 'quarter';
+export type OffsetSize = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 'auto' | 'half' | 'third' | 'quarter';
