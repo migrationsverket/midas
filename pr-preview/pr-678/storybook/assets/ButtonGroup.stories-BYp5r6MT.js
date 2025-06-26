2e0a705649deab06356332951a518b8c69fefe2e
jabkha <jabkha@users.noreply.github.com>
2025-06-26 13:18:54 +0000

    Deploy preview for PR 678 🛫

---
+++ pr-preview/pr-678/storybook/assets/ButtonGroup.stories-BYp5r6MT.js
@@ -0,0 +1,11 @@
+import{j as o}from"./jsx-runtime-BYYWji4R.js";import{B as a}from"./Button-BJdOFndd.js";import{c as m}from"./clsx-B-dksMZM.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button.module-AG3JZ-l4.js";import"./Button-D5YfTa-t.js";import"./utils-BLyg3OOq.js";import"./Hidden-BC9qER92.js";import"./useFocusRing-eGNMqT99.js";import"./_class_private_field_init-BOtJyCjG.js";import"./index-DQEgcCId.js";import"./context-d6PNTTSe.js";import"./useButton-rs5PgjfN.js";const l="_buttonGroup_fh6r8_4",d={buttonGroup:l},e=({children:r,className:i,"aria-label":u,...c})=>o.jsx("div",{role:"group","aria-label":u,className:m(d.buttonGroup,i),...c,children:r});try{e.displayName="ButtonGroup",e.__docgenInfo={description:"Group several buttons together.",displayName:"ButtonGroup",props:{}}}catch{}const F={component:e,title:"Components/Button/ButtonGroup",tags:["autodocs"],parameters:{layout:"centered"},args:{},argTypes:{}},t={args:{"aria-label":"Vill du acceptera ändringen?"},render:({...r})=>o.jsxs(e,{...r,children:[o.jsx(a,{children:"Fortsätt"}),o.jsx(a,{variant:"secondary",children:"Avbryt"})]})};var n,s,p;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    'aria-label': 'Vill du acceptera ändringen?'
+  },
+  render: ({
+    ...args
+  }) => <ButtonGroup {...args}>
+      <Button>Fortsätt</Button>
+      <Button variant='secondary'>Avbryt</Button>
+    </ButtonGroup>
+}`,...(p=(s=t.parameters)==null?void 0:s.docs)==null?void 0:p.source}}};const P=["Primary"];export{t as Primary,P as __namedExportsOrder,F as default};
