eb4c28e9a02401a8cb8c2cb6dbefe2f1d8df4e51
derpbravely <derpbravely@users.noreply.github.com>
2025-06-26 08:48:05 +0000

    Deploy preview for PR 685 🛫

---
+++ pr-preview/pr-685/assets/js/3a3814a2.04e7ff95.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[7888],{
+
+/***/ 34035:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-685/release-notes","page":1,"postsPerPage":10,"totalPages":2,"totalCount":13,"nextPage":"/pr-preview/pr-685/release-notes/page/2","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-685/storybook/checkbox/Checkbox.d.ts
@@ -0,0 +1,2 @@
+import * as React from 'react';
+export declare const Checkbox: React.ForwardRefExoticComponent<import('react-aria').AriaCheckboxProps & import('react-aria-components').SlotProps & React.RefAttributes<HTMLLabelElement>>;
