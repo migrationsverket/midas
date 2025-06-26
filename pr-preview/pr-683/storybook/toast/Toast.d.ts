008783a288d83f535cf8f6bb2372e29bccafa059
derpbravely <derpbravely@users.noreply.github.com>
2025-06-26 08:12:45 +0000

    Deploy preview for PR 683 🛫

---
+++ pr-preview/pr-683/storybook/toast/Toast.d.ts
@@ -0,0 +1,30 @@
+import { AriaToastProps, AriaToastRegionProps } from 'react-aria';
+import { QueuedToast, ToastQueue, ToastState, ToastStateProps } from 'react-stately';
+import { default as React } from '../../../../node_modules/react';
+export interface MidasToast {
+    message: string;
+    type: 'success' | 'info' | 'warning' | 'important';
+    children?: React.ReactNode;
+}
+interface ToastRegionProps<T> extends AriaToastRegionProps {
+    state: ToastState<T>;
+}
+export interface ToastProps<T> extends AriaToastProps<T> {
+    state: ToastState<T>;
+    toast: QueuedToast<T>;
+    children?: React.ReactNode;
+}
+interface ToastProviderProps extends AriaToastRegionProps {
+    children?: ((state: ToastState<MidasToast>) => React.ReactNode) | React.ReactNode;
+}
+/**
+ * Temporary implementation of https://github.com/adobe/react-spectrum/blob/main/packages/%40react-stately/toast/src/useToastState.ts#L59
+ * TODO: Erase this as soon as react-stately is released
+ */
+export declare function useToastState<T>(props?: ToastStateProps): ToastState<T>;
+export declare const toastQueue: ToastQueue<MidasToast>;
+export declare const GlobalToastRegion: (props: ToastProviderProps) => React.ReactPortal | null;
+export declare const ToastProvider: ({ children, ...props }: ToastProviderProps) => import("react/jsx-runtime").JSX.Element;
+export declare function ToastRegion<T extends MidasToast>({ state, ...props }: ToastRegionProps<T>): import("react/jsx-runtime").JSX.Element;
+export declare function Toast<T extends MidasToast>({ state, ...props }: ToastProps<T>): import("react/jsx-runtime").JSX.Element;
+export {};
