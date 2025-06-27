3d6609b12bee2e558776de9053a4560569d0597a
derpbravely <197733689+derpbravely@users.noreply.github.com>
2025-06-27 10:08:47 +0200

    fix(breadcrumbs): 💄 accept custom className (#683)
    ## Description
    
    Component did not allow any custom styling.
    
    ## Changes
    
    - Extend react aria breadcrumbs
    - Deprecate and rename prop type

--- packages/components/src/breadcrumbs/Breadcrumbs.tsx
+++ packages/components/src/breadcrumbs/Breadcrumbs.tsx
@@ -1,32 +1,44 @@
 import {
   Breadcrumbs as AriaBreadcrumbs,
+  type BreadcrumbsProps as AriaBreadcrumbsProps,
   Breadcrumb,
 } from 'react-aria-components'
+import clsx from 'clsx'
 import { Link } from '../link'
-
 import styles from './Breadcrumbs.module.css'
 
-export interface BreadcrumbProps {
-  items: {
-    href: string
-    title: string
-    isDisabled?: boolean
-  }[]
+export interface BreadcrumbItem {
+  href: string
+  title: string
+  isDisabled?: boolean
 }
 
-export const Breadcrumbs: React.FC<BreadcrumbProps> = ({ items }) => {
-  return (
-    <AriaBreadcrumbs className={styles.container}>
-      {items.map((item, index) => (
-        <Breadcrumb key={item.title.toLocaleLowerCase() + index}>
-          <Link
-            href={item.href}
-            isDisabled={item.isDisabled}
-          >
-            {item.title}
-          </Link>
-        </Breadcrumb>
-      ))}
-    </AriaBreadcrumbs>
-  )
+export interface BreadcrumbsProps<T extends BreadcrumbItem>
+  extends AriaBreadcrumbsProps<T> {
+  items: T[]
 }
+
+export const Breadcrumbs = <T extends BreadcrumbItem>({
+  items,
+  className,
+  ...rest
+}: BreadcrumbsProps<T>) => (
+  <AriaBreadcrumbs
+    className={clsx(styles.container, className)}
+    {...rest}
+  >
+    {items.map(({ title, href, isDisabled }, index) => (
+      <Breadcrumb key={title.toLocaleLowerCase() + index}>
+        <Link
+          href={href}
+          isDisabled={isDisabled}
+        >
+          {title}
+        </Link>
+      </Breadcrumb>
+    ))}
+  </AriaBreadcrumbs>
+)
+
+/** @deprecated since v10.3.0 please use `BreadcrumbsProps` instead */
+export type BreadcrumbProps<T extends BreadcrumbItem> = BreadcrumbsProps<T>
