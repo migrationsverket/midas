2d6fb7bc025fd690f997e5080bf4dc695884b5ea
derpbravely <derpbravely@users.noreply.github.com>
2025-06-27 07:32:15 +0000

    Deploy preview for PR 678 🛫

---
+++ pr-preview/pr-678/storybook/assets/Button.stories-Bf55uDTB.js
@@ -0,0 +1,57 @@
+import{j as X}from"./jsx-runtime-BYYWji4R.js";import{B as u}from"./Button-CC7nL3b9.js";import{u as p,e as t}from"./index-CqmFxPum.js";import{s as C}from"./modes-BX-MOWoB.js";import{P as M}from"./plus-lGmUxVE_.js";import{X as O}from"./x-BaIpobvV.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button.module-AG3JZ-l4.js";import"./Button-D5YfTa-t.js";import"./utils-BLyg3OOq.js";import"./clsx-B-dksMZM.js";import"./Hidden-BC9qER92.js";import"./useFocusRing-eGNMqT99.js";import"./_class_private_field_init-BOtJyCjG.js";import"./index-DQEgcCId.js";import"./context-d6PNTTSe.js";import"./useButton-rs5PgjfN.js";import"./createLucideIcon-DQK9Z80p.js";const ca={component:u,title:"Components/Button",tags:["autodocs"],args:{variant:"primary",children:"Button"},argTypes:{children:{type:"string"},isDisabled:{options:[!0,!1],control:{type:"radio"}}},render:(r,{globals:{size:a}})=>X.jsx(u,{...r,size:a}),parameters:{chromatic:{modes:C}},play:async({canvas:r,step:a,globals:{size:_}})=>{await a("it should have focus when clicked",async()=>{const e=r.getByRole("button");await p.click(e),await t(e).toBeEnabled(),e.focus(),await p.keyboard("{Enter}"),await t(e).toHaveFocus()}),await a("it should change size according to size prop",async()=>{await t(r.getByRole("button")).toHaveStyle({height:_==="large"?"48px":"40px"})})}},s={args:{children:"Button",className:"test-class"}},n={args:{variant:"secondary"}},o={args:{children:"Lägg till",variant:"tertiary",icon:M}},i={args:{variant:"primary",isDisabled:!0,children:"Lägg till"},play:async({canvas:r})=>{const a=r.getByRole("button");await p.click(a),await t(a).toBeDisabled()}},c={args:{variant:"secondary",isDisabled:!0},play:async({canvas:r})=>{const a=r.getByRole("button");await p.click(a),await t(a).toBeDisabled()}},l={args:{children:"Button",fullwidth:!0}},d={args:{children:X.jsx(O,{size:20,"aria-hidden":!0}),variant:"icon","aria-label":"Stäng"}},m={args:{children:"Button",variant:"danger"}};var g,y,b;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    className: 'test-class'
+  }
+}`,...(b=(y=s.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};var h,v,B;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary'
+  }
+}`,...(B=(v=n.parameters)==null?void 0:v.docs)==null?void 0:B.source}}};var w,D,S;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    children: 'Lägg till',
+    variant: 'tertiary',
+    icon: Plus
+  }
+}`,...(S=(D=o.parameters)==null?void 0:D.docs)==null?void 0:S.source}}};var f,x,P;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary',
+    isDisabled: true,
+    children: 'Lägg till'
+  },
+  play: async ({
+    canvas
+  }) => {
+    const button = canvas.getByRole('button');
+    await userEvent.click(button);
+    await expect(button).toBeDisabled();
+  }
+}`,...(P=(x=i.parameters)==null?void 0:x.docs)==null?void 0:P.source}}};var k,E,R;c.parameters={...c.parameters,docs:{...(k=c.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary',
+    isDisabled: true
+  },
+  play: async ({
+    canvas
+  }) => {
+    const button = canvas.getByRole('button');
+    await userEvent.click(button);
+    await expect(button).toBeDisabled();
+  }
+}`,...(R=(E=c.parameters)==null?void 0:E.docs)==null?void 0:R.source}}};var z,j,I;l.parameters={...l.parameters,docs:{...(z=l.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    fullwidth: true
+  }
+}`,...(I=(j=l.parameters)==null?void 0:j.docs)==null?void 0:I.source}}};var L,F,T;d.parameters={...d.parameters,docs:{...(L=d.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    children: <X size={20} aria-hidden />,
+    variant: 'icon',
+    'aria-label': 'Stäng'
+  }
+}`,...(T=(F=d.parameters)==null?void 0:F.docs)==null?void 0:T.source}}};var H,N,W;m.parameters={...m.parameters,docs:{...(H=m.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    children: 'Button',
+    variant: 'danger'
+  }
+}`,...(W=(N=m.parameters)==null?void 0:N.docs)==null?void 0:W.source}}};const la=["Primary","Secondary","TertiaryWithIcon","PrimaryDisabled","SecondaryDisabled","PrimaryFullwidth","Icon","Danger"];export{m as Danger,d as Icon,s as Primary,i as PrimaryDisabled,l as PrimaryFullwidth,n as Secondary,c as SecondaryDisabled,o as TertiaryWithIcon,la as __namedExportsOrder,ca as default};
---
+++ pr-preview/pr-678/storybook/assets/Spinner.stories-BONa03Jj.js
@@ -0,0 +1,21 @@
+import{j as e}from"./jsx-runtime-BYYWji4R.js";import{c as v}from"./clsx-B-dksMZM.js";import{c as y}from"./createLucideIcon-DQK9Z80p.js";import{B as S}from"./Button-CC7nL3b9.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button.module-AG3JZ-l4.js";import"./Button-D5YfTa-t.js";import"./utils-BLyg3OOq.js";import"./Hidden-BC9qER92.js";import"./useFocusRing-eGNMqT99.js";import"./_class_private_field_init-BOtJyCjG.js";import"./index-DQEgcCId.js";import"./context-d6PNTTSe.js";import"./useButton-rs5PgjfN.js";/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=y("LoaderCircle",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]),h="_container_32v8g_4",k="_spinner_32v8g_8",C="_dark_32v8g_18",j="_vsHidden_32v8g_22",s={container:h,spinner:k,dark:C,vsHidden:j},t=({small:r,isOnColor:f=!1})=>e.jsxs("div",{className:s.container,role:"status",children:[e.jsx(x,{className:v(s.spinner,f&&s.dark),size:r?16:32,strokeWidth:r?2:3,absoluteStrokeWidth:!0}),e.jsx("span",{className:s.vsHidden,children:"Laddar, vänligen vänta."})]});try{t.displayName="Spinner",t.__docgenInfo={description:"",displayName:"Spinner",props:{small:{defaultValue:{value:"false"},description:"Displays a smaller spinner when set to true",name:"small",required:!1,type:{name:"boolean"}},isOnColor:{defaultValue:{value:"false"},description:"For use on coloured background",name:"isOnColor",required:!1,type:{name:"boolean"}}}}}catch{}const F={component:t,title:"Components/Spinner",tags:["autodocs"],parameters:{layout:"centered"},args:{small:!1,isOnColor:!1}},a={args:{}},n={args:{small:!0}},o={args:{small:!0,isOnColor:!0},render:r=>e.jsxs(S,{children:[e.jsx(t,{...r}),"Laddar"]})};var i,l,c;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {}
+}`,...(c=(l=a.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var d,m,p;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    small: true
+  }
+}`,...(p=(m=n.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var u,_,g;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    small: true,
+    isOnColor: true
+  },
+  render: args => <Button>
+      <Spinner {...args} />
+      Laddar
+    </Button>
+}`,...(g=(_=o.parameters)==null?void 0:_.docs)==null?void 0:g.source}}};const M=["Primary","Small","InButton"];export{o as InButton,a as Primary,n as Small,M as __namedExportsOrder,F as default};
