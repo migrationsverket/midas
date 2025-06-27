f612517f10f4e005c2ab5f185fa16f63a3ece570
derpbravely <derpbravely@users.noreply.github.com>
2025-06-27 07:36:34 +0000

    Remove preview for PR 692 🛬

--- pr-preview/pr-692/storybook/popover/Popover.d.ts
+++
@@ -1,7 +0,0 @@
-import { PopoverProps } from 'react-aria-components';
-import * as React from 'react';
-interface MidasPopoverProps extends Omit<PopoverProps, 'children'> {
-    children: React.ReactNode;
-}
-export declare function Popover({ children, className, offset, ...props }: MidasPopoverProps): import("react/jsx-runtime").JSX.Element;
-export {};
