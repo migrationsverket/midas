2896918ba956c8cee28e755e1f5dac38942d043b
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-07-01 13:50:56 +0000

    Deploy preview for PR 694 🛫

---
+++ pr-preview/pr-694/storybook/calendar/CalendarHeader.d.ts
@@ -0,0 +1,3 @@
+import { CalendarProps } from './Calendar';
+import * as React from 'react';
+export declare const CalendarHeader: React.FC<CalendarProps>;
---
+++ pr-preview/pr-694/storybook/checkbox/Checkbox.d.ts
@@ -0,0 +1,4 @@
+import * as React from 'react';
+export declare const Checkbox: React.ForwardRefExoticComponent<import('react-aria').AriaCheckboxProps & import('react-aria-components').SlotProps & {
+    className?: string;
+} & React.RefAttributes<HTMLLabelElement>>;
