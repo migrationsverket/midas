a36311a9824314f82ae236c5d9c1596ce0df7dcf
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-07-02 09:55:14 +0000

    Deploy preview for PR 698 🛫

---
+++ pr-preview/pr-698/assets/js/56cb68f4.1e0a4650.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[4485],{
+
+/***/ 51902:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-698/release-notes/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":14,"previousPage":"/pr-preview/pr-698/release-notes","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
