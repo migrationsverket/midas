1a1479741a5d9d862dfa234dbb1a0b43a9757f5f
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-07-03 05:53:42 +0000

    Deploy preview for PR 702 🛫

---
+++ pr-preview/pr-702/assets/js/184.43ac065d.js
@@ -0,0 +1,19 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[184],{
+
+/***/ 40184:
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   createInfoServices: () => (/* reexport safe */ _chunk_MU4RMLXT_mjs__WEBPACK_IMPORTED_MODULE_0__.v)
+/* harmony export */ });
+/* harmony import */ var _chunk_MU4RMLXT_mjs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(14882);
+/* harmony import */ var _chunk_O6L2WK4J_mjs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(21028);
+
+
+
+
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-702/assets/js/83cb8021.2e7b4a2f.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[268],{
+
+/***/ 87798:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-702/release-notes","page":1,"postsPerPage":10,"totalPages":2,"totalCount":14,"nextPage":"/pr-preview/pr-702/release-notes/page/2","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
