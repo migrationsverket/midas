413b68c4a9818848e7e88af5c412fd08eec28f85
phieksmig <phieksmig@users.noreply.github.com>
2025-07-03 14:18:36 +0000

    Deploy preview for PR 705 🛫

---
+++ pr-preview/pr-705/assets/js/b909a18b.6d017ded.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[3185],{
+
+/***/ 36655:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-705/changelog/page/2","page":2,"postsPerPage":20,"totalPages":2,"totalCount":29,"previousPage":"/pr-preview/pr-705/changelog","blogDescription":"Håll dig uppdaterad med allt som händer i varje release","blogTitle":"Midas changelog"}}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-705/storybook/logo/intl/translations.json.d.ts
@@ -0,0 +1,11 @@
+declare const _default: {
+  "en": {
+    "logotype": "Logotype of the Swedish Migration Agency"
+  },
+  "sv": {
+    "logotype": "Migrationsverkets logotyp"
+  }
+}
+;
+
+export default _default;
