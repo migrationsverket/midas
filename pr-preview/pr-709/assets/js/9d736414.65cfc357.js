22119718a71398921b01473b3e571b14fceae22f
phieksmig <phieksmig@users.noreply.github.com>
2025-07-14 12:41:34 +0000

    Deploy preview for PR 709 🛫

---
+++ pr-preview/pr-709/assets/js/9d736414.65cfc357.js
@@ -0,0 +1,138 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[5831],{
+
+/***/ 56851:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   assets: () => (/* binding */ assets),
+/* harmony export */   contentTitle: () => (/* binding */ contentTitle),
+/* harmony export */   "default": () => (/* binding */ MDXContent),
+/* harmony export */   frontMatter: () => (/* binding */ frontMatter),
+/* harmony export */   metadata: () => (/* reexport default export from named module */ _site_docusaurus_changelog_plugin_default_site_changelog_source_4_0_0_md_9d7_json__WEBPACK_IMPORTED_MODULE_0__),
+/* harmony export */   toc: () => (/* binding */ toc)
+/* harmony export */ });
+/* harmony import */ var _site_docusaurus_changelog_plugin_default_site_changelog_source_4_0_0_md_9d7_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(62021);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(74848);
+/* harmony import */ var _mdx_js_react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(28453);
+
+
+const frontMatter = {
+	mdx: {
+		format: 'md'
+	},
+	date: '2025-03-10T20:00',
+	pagination_prev: null,
+	pagination_next: null
+};
+const contentTitle = '4.0.0';
+
+const assets = {
+"authorsImageUrls": [],
+};
+
+
+
+const toc = [{
+  "value": "🚀 Features",
+  "id": "-features",
+  "level": 2
+}, {
+  "value": "🩹 Fixes",
+  "id": "-fixes",
+  "level": 2
+}, {
+  "value": "Documentation Changes",
+  "id": "documentation-changes",
+  "level": 2
+}, {
+  "value": "⚠️ Breaking Changes",
+  "id": "️-breaking-changes",
+  "level": 2
+}];
+function _createMdxContent(props) {
+  const _components = {
+    code: "code",
+    h2: "h2",
+    li: "li",
+    strong: "strong",
+    ul: "ul",
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_2__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.Fragment, {
+    children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h2, {
+      id: "-features",
+      children: "🚀 Features"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.ul, {
+      children: ["\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.li, {
+        children: "⚠️ updated select, multi-select and modal components"
+      }), "\n"]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h2, {
+      id: "-fixes",
+      children: "🩹 Fixes"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.ul, {
+      children: ["\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.li, {
+        children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.strong, {
+          children: "layout:"
+        }), " design adjustments"]
+      }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.li, {
+        children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.strong, {
+          children: "radio:"
+        }), " change cursor to pointer"]
+      }), "\n"]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h2, {
+      id: "documentation-changes",
+      children: "Documentation Changes"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.ul, {
+      children: ["\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.li, {
+        children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.strong, {
+          children: "date-picker:"
+        }), " add style unavailable and code example for Unavailable Date in docs"]
+      }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.li, {
+        children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.strong, {
+          children: "docs:"
+        }), " add new UIkit"]
+      }), "\n"]
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.h2, {
+      id: "️-breaking-changes",
+      children: "⚠️ Breaking Changes"
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.ul, {
+      children: ["\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(_components.li, {
+        children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+          children: "Select"
+        }), ", ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+          children: "MultiSelect"
+        }), " and ", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.code, {
+          children: "Modal"
+        }), " now have updated API:s, expect things not to work as before. Make sure to check the API:s before upgrading."]
+      }), "\n"]
+    })]
+  });
+}
+function MDXContent(props = {}) {
+  const {wrapper: MDXLayout} = {
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_2__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return MDXLayout ? (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(MDXLayout, {
+    ...props,
+    children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+
+
+/***/ }),
+
+/***/ 62021:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"permalink":"/pr-preview/pr-709/changelog/4.0.0","source":"@site/changelog/source/4.0.0.md","title":"4.0.0","description":"🚀 Features","date":"2025-03-10T20:00:00.000Z","tags":[],"hasTruncateMarker":true,"authors":[],"frontMatter":{"mdx":{"format":"md"},"date":"2025-03-10T20:00","pagination_prev":null,"pagination_next":null},"unlisted":false,"prevItem":{"title":"5.0.0","permalink":"/pr-preview/pr-709/changelog/5.0.0"},"nextItem":{"title":"3.1.0","permalink":"/pr-preview/pr-709/changelog/3.1.0"},"listPageLink":"/pr-preview/pr-709/changelog/page/2"}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-709/storybook/assets/RangeCalendar.stories-CIvwD7rx.js
@@ -0,0 +1,70 @@
+import{R as A}from"./RangeCalendar-Cc-JNtTn.js";import{u as e,e as i}from"./index-CqmFxPum.js";import{m as t}from"./storybook-DLHntVpb.js";import"./jsx-runtime-BYYWji4R.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./CalendarHeader-zbvztDqQ.js";import"./Button-Dw7lljiq.js";import"./utils-CT8CL8Qy.js";import"./SSRProvider-BhYbDCf7.js";import"./clsx-B-dksMZM.js";import"./Hidden-BC9qER92.js";import"./useFocusRing-CiRxO26c.js";import"./_class_private_field_init-BOtJyCjG.js";import"./index-DQEgcCId.js";import"./context-zNjZoWML.js";import"./useButton-DFlERivp.js";import"./RSPContexts-HQcWPJyv.js";import"./Text-BDGh9UD8.js";import"./useDateFormatter-DzWwJ2YB.js";import"./CalendarDate-Lfx2W0H2.js";import"./useControlledState-H1tiOFpU.js";import"./useLocalizedStringFormatter-DiUIqqhg.js";import"./LocalizedStringFormatter-BB0Nk989.js";import"./useUpdateEffect-BzF61qSB.js";import"./useEvent-YbNcfHeo.js";import"./getScrollParent-D3zukwiL.js";import"./scrollIntoView-D-J2YGNX.js";import"./VisuallyHidden-Bwo2Sxwr.js";import"./Button-D6otDre8.js";import"./Button.module-Bmb634uG.js";import"./createLucideIcon-uZw3gxGR.js";import"./Heading-BqhVQUP5.js";import"./Text-B94wSZIf.js";const se={component:A,title:"Components/Calendar/RangeCalendar",tags:["autodocs"]},a={},r={args:{defaultValue:{start:t.subtract({days:2}),end:t.add({days:2})}}},o={args:{isDisabled:!0},parameters:{a11y:{element:"body",config:{rules:[{id:"color-contrast",enabled:!1}]},options:{rules:{"color-contrast":{enabled:!1}}}}}},s={tags:["!dev","!autodocs"],parameters:{chromatic:{disableSnapshot:!0}},play:async({canvas:n,step:S})=>{await S("it should be possible to select today and two days ahead with the keyboard",async()=>{const h=t.day.toString(),k=t.add({days:1}).day.toString(),R=t.add({days:2}).day.toString();await e.tab(),await e.tab(),await e.tab(),await e.keyboard("[Space]"),await e.keyboard("[ArrowRight]"),await i(n.getByRole("gridcell",{name:h})).toHaveAttribute("aria-selected","true"),await i(n.getByRole("gridcell",{name:k})).toHaveAttribute("aria-selected","true"),await i(n.getByRole("gridcell",{name:R})).toHaveAttribute("aria-selected","true")})}};var d,c,m;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:"{}",...(m=(c=a.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var l,p,u;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    defaultValue: {
+      start: mockedNow.subtract({
+        days: 2
+      }),
+      end: mockedNow.add({
+        days: 2
+      })
+    }
+  }
+}`,...(u=(p=r.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var y,b,g;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  },
+  parameters: {
+    a11y: {
+      element: 'body',
+      config: {
+        rules: [{
+          // Dont check for color contrast on disabled elements
+          id: 'color-contrast',
+          enabled: false
+        }]
+      },
+      options: {
+        rules: {
+          'color-contrast': {
+            enabled: false
+          }
+        }
+      } satisfies RunOptions
+    }
+  }
+}`,...(g=(b=o.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var w,f,v;s.parameters={...s.parameters,docs:{...(w=s.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  tags: ['!dev', '!autodocs'],
+  parameters: {
+    chromatic: {
+      disableSnapshot: true
+    }
+  },
+  play: async ({
+    canvas,
+    step
+  }) => {
+    await step('it should be possible to select today and two days ahead with the keyboard', async () => {
+      const todaysDate = mockedNow.day.toString();
+      const tomorrow = mockedNow.add({
+        days: 1
+      }).day.toString();
+      const dayAfterTomorrow = mockedNow.add({
+        days: 2
+      }).day.toString();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.keyboard('[Space]');
+      await userEvent.keyboard('[ArrowRight]');
+      await expect(canvas.getByRole('gridcell', {
+        name: todaysDate
+      })).toHaveAttribute('aria-selected', 'true');
+      await expect(canvas.getByRole('gridcell', {
+        name: tomorrow
+      })).toHaveAttribute('aria-selected', 'true');
+      await expect(canvas.getByRole('gridcell', {
+        name: dayAfterTomorrow
+      })).toHaveAttribute('aria-selected', 'true');
+    });
+  }
+}`,...(v=(f=s.parameters)==null?void 0:f.docs)==null?void 0:v.source}}};const ne=["Primary","SelectedDates","Disabled","KeyboardTest"];export{o as Disabled,s as KeyboardTest,a as Primary,r as SelectedDates,ne as __namedExportsOrder,se as default};
