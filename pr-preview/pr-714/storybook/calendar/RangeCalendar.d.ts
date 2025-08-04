e238008818c8ff3f3ca72dc8247446bf1f7a75c9
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-08-04 16:59:58 +0000

    Remove preview for PR 714 🛬

--- pr-preview/pr-714/storybook/calendar/RangeCalendar.d.ts
+++
@@ -1,6 +0,0 @@
-import { DateValue, RangeCalendarProps as AriaRangeCalendarProps } from 'react-aria-components';
-import * as React from 'react';
-export interface RangeCalendarProps extends AriaRangeCalendarProps<DateValue> {
-    errorMessage?: string;
-}
-export declare const RangeCalendar: React.FC<RangeCalendarProps>;
