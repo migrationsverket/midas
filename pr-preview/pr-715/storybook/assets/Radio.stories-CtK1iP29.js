4d9fd9ad88647766e9d30ab3655e20e222a48ab6
jabkha <jabkha@users.noreply.github.com>
2025-08-05 12:48:35 +0000

    Deploy preview for PR 715 🛫

---
+++ pr-preview/pr-715/storybook/assets/Radio.stories-CtK1iP29.js
@@ -0,0 +1,112 @@
+import{j as a}from"./jsx-runtime-BYYWji4R.js";import{a as r,R as h,s as b}from"./Radio-D4wp7eVO.js";import{e as o,u as t}from"./index-CqmFxPum.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Group-CrqH1lRX.js";import"./utils-CT8CL8Qy.js";import"./SSRProvider-BhYbDCf7.js";import"./clsx-B-dksMZM.js";import"./useFocusRing-CiRxO26c.js";import"./_class_private_field_init-BOtJyCjG.js";import"./index-DQEgcCId.js";import"./FieldError-Ax3oEaat.js";import"./Text-BDGh9UD8.js";import"./Form-C688dPfu.js";import"./useFormValidation-DOw2d6ip.js";import"./Button-Dw7lljiq.js";import"./Hidden-BC9qER92.js";import"./context-zNjZoWML.js";import"./useButton-DFlERivp.js";import"./FocusScope-DFjHfTMc.js";import"./useField-DCM7HehW.js";import"./VisuallyHidden-Bwo2Sxwr.js";import"./useControlledState-H1tiOFpU.js";import"./Label-BZKUkVhy.js";import"./Dialog-BQhmOU3o.js";import"./RSPContexts-HQcWPJyv.js";import"./OverlayArrow-B5tMncCy.js";import"./Collection-CAY-ghkk.js";import"./index-NDK0uC61.js";import"./Separator-BuNd-mI_.js";import"./SelectionManager-B7CR_Zkr.js";import"./useEvent-YbNcfHeo.js";import"./scrollIntoView-D-J2YGNX.js";import"./ListKeyboardDelegate-DdGMYXDu.js";import"./useLocalizedStringFormatter-DiUIqqhg.js";import"./LocalizedStringFormatter-BB0Nk989.js";import"./getScrollParent-D3zukwiL.js";import"./Dialog-CPDcCti4.js";import"./useLocalizedStringFormatter-9M_dEwOo.js";import"./Button-BlOWLqcE.js";import"./Button.module-DOo3SpJX.js";import"./x-Dz2R9y6b.js";import"./createLucideIcon-uZw3gxGR.js";import"./Heading-BqhVQUP5.js";import"./info-CSjK64PU.js";import"./Text-B94wSZIf.js";import"./FieldError-DzhuwbDv.js";const He={title:"Components/Radio",component:h,subcomponents:{Radio:r},tags:["autodocs"],args:{label:"Välj frukt",description:"Valfri beskrivning",errorMessage:"Du måste välja en frukt?",errorPosition:"top"},argTypes:{children:{table:{disable:!0}},label:{type:"string",control:{type:"text"}},isDisabled:{type:"boolean",options:[!1,!0],control:{type:"radio"}},isInvalid:{type:"boolean",options:[!1,!0],control:{type:"radio"}}}},O=["Äpple","Banan","Kiwi","Apelsin"],u=O.map(e=>a.jsx(r,{value:e,id:e.toLowerCase(),children:e},e)),A=[a.jsx(r,{value:"banan",children:"Banan"},"radio-banan"),a.jsx(r,{value:"apelsin",isDisabled:!0,children:"Apelsin"},"radio-apelsin"),a.jsx(r,{value:"kiwi",children:"Kiwi"},"radio-kiwi")],n={args:{label:"Frukt",description:"Välj en frukt",children:O.map(e=>a.jsx(r,{value:e,id:e.toLowerCase(),className:"test-radio-class",children:e},e)),className:"test-class"},play:async({canvas:e,step:s,args:g})=>{await s("it should preserve its classNames when being passed new ones",async()=>{const q=e.getByRole("radiogroup"),z=e.getByRole("group").childNodes;await o(q).toHaveClass(b.radioGroup,g.className),z.forEach(async L=>{await o(L).toHaveClass(b.radio,"test-radio-class")})})}},i={args:{children:u,isDisabled:!0}},l={args:{children:A}},p={args:{children:u,isInvalid:!0,errorMessage:"Det här stämmer inte!"}},d={tags:["!dev","!autodocs"],parameters:{chromatic:{disableSnapshot:!0}},args:{label:"Frukt",description:"Välj en frukt",children:u,isRequired:!0},render:e=>a.jsxs("form",{children:[a.jsx(h,{...e}),a.jsx("button",{type:"submit",children:"Submit"})]}),play:async({canvas:e,step:s,args:g})=>{await s("should show a validation error message if the user submitted without selecting anything",async()=>{await t.tab(),await t.tab(),await t.keyboard("[Enter]"),o(e.getByRole("radiogroup")).toBeInvalid(),o(e.getByText(g.errorMessage)).toBeInTheDocument()})}},c={tags:["!dev","!autodocs"],parameters:{chromatic:{disableSnapshot:!0}},args:{label:"Frukt",description:"Välj en frukt",children:u,errorMessage:void 0,validate:e=>e!=null&&e.includes("Äpple")?"Inga äpplen är tillåtna":!0},render:e=>a.jsxs("form",{children:[a.jsx(h,{...e}),a.jsx("button",{type:"submit",children:"Submit"})]}),play:async({canvas:e,step:s})=>{await s("it should show the custom error message if the constraints was not satisfied",async()=>{await t.tab(),await t.keyboard("[Enter]"),await t.tab(),await t.keyboard("[Enter]"),o(e.getByText("Inga äpplen är tillåtna")).toBeInTheDocument()})}},m={args:{...n.args,children:a.jsxs(a.Fragment,{children:[a.jsx(r,{value:"apple",children:"Äpple"},"radio-apple"),a.jsx(r,{value:"banan",children:"Banan"},"radio-banan")]}),orientation:"horizontal"}};var y,v,w;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    label: 'Frukt',
+    description: 'Välj en frukt',
+    children: fruits.map(fruit => <Radio key={fruit} value={fruit} id={fruit.toLowerCase()} className='test-radio-class'>
+        {fruit}
+      </Radio>),
+    className: 'test-class'
+  },
+  play: async ({
+    canvas,
+    step,
+    args
+  }) => {
+    await step('it should preserve its classNames when being passed new ones', async () => {
+      const radioGroup = canvas.getByRole('radiogroup');
+      const radios = canvas.getByRole('group').childNodes;
+      await expect(radioGroup).toHaveClass(styles.radioGroup, args.className as string);
+      radios.forEach(async radio => {
+        await expect(radio).toHaveClass(styles.radio, 'test-radio-class');
+      });
+    });
+  }
+}`,...(w=(v=n.parameters)==null?void 0:v.docs)==null?void 0:w.source}}};var f,x,k;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    children: items,
+    isDisabled: true
+  }
+}`,...(k=(x=i.parameters)==null?void 0:x.docs)==null?void 0:k.source}}};var j,R,B;l.parameters={...l.parameters,docs:{...(j=l.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    children: radioItemsOneDisabled
+  }
+}`,...(B=(R=l.parameters)==null?void 0:R.docs)==null?void 0:B.source}}};var I,E,D;p.parameters={...p.parameters,docs:{...(I=p.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    children: items,
+    isInvalid: true,
+    errorMessage: 'Det här stämmer inte!'
+  }
+}`,...(D=(E=p.parameters)==null?void 0:E.docs)==null?void 0:D.source}}};var S,N,C;d.parameters={...d.parameters,docs:{...(S=d.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  tags: ['!dev', '!autodocs'],
+  parameters: {
+    chromatic: {
+      disableSnapshot: true
+    }
+  },
+  args: {
+    label: 'Frukt',
+    description: 'Välj en frukt',
+    children: items,
+    isRequired: true
+  },
+  render: args => <form>
+      <RadioGroup {...args} />
+      <button type='submit'>Submit</button>
+    </form>,
+  play: async ({
+    canvas,
+    step,
+    args
+  }) => {
+    await step('should show a validation error message if the user submitted without selecting anything', async () => {
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.keyboard('[Enter]');
+      expect(canvas.getByRole('radiogroup')).toBeInvalid();
+      expect(canvas.getByText(args.errorMessage as string)).toBeInTheDocument();
+    });
+  }
+}`,...(C=(N=d.parameters)==null?void 0:N.docs)==null?void 0:C.source}}};var V,T,G;c.parameters={...c.parameters,docs:{...(V=c.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  tags: ['!dev', '!autodocs'],
+  parameters: {
+    chromatic: {
+      disableSnapshot: true
+    }
+  },
+  args: {
+    label: 'Frukt',
+    description: 'Välj en frukt',
+    children: items,
+    errorMessage: undefined,
+    validate: value => value?.includes('Äpple') ? 'Inga äpplen är tillåtna' : true
+  },
+  render: args => <form>
+      <RadioGroup {...args} />
+      <button type='submit'>Submit</button>
+    </form>,
+  play: async ({
+    canvas,
+    step
+  }) => {
+    await step('it should show the custom error message if the constraints was not satisfied', async () => {
+      await userEvent.tab();
+      await userEvent.keyboard('[Enter]');
+      await userEvent.tab();
+      await userEvent.keyboard('[Enter]');
+      expect(canvas.getByText('Inga äpplen är tillåtna')).toBeInTheDocument();
+    });
+  }
+}`,...(G=(T=c.parameters)==null?void 0:T.docs)==null?void 0:G.source}}};var F,M,H;m.parameters={...m.parameters,docs:{...(F=m.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    ...Normal.args,
+    children: <>
+        <Radio key='radio-apple' value='apple'>
+          Äpple
+        </Radio>
+        <Radio key='radio-banan' value='banan'>
+          Banan
+        </Radio>
+      </>,
+    orientation: 'horizontal'
+  }
+}`,...(H=(M=m.parameters)==null?void 0:M.docs)==null?void 0:H.source}}};const Oe=["Normal","Disabled","OneItemDisabled","Invalid","Required","CustomValidation","Horizontal"];export{c as CustomValidation,i as Disabled,m as Horizontal,p as Invalid,n as Normal,l as OneItemDisabled,d as Required,Oe as __namedExportsOrder,He as default};
