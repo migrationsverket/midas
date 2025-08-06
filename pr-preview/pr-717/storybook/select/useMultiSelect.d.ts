83428b2c8f687a79485c7cf6f08b39e5cbdafb33
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-08-06 06:30:38 +0000

    Remove preview for PR 717 🛬

--- pr-preview/pr-717/storybook/select/useMultiSelect.d.ts
+++
@@ -1,4 +0,0 @@
-import { RefObject } from '../../../../node_modules/react';
-import { MultiSelectDOMProps, MultiSelectProps, MultiSelectState } from './types';
-import { ListBoxOption } from '../list-box';
-export declare function useMultiSelect<T extends ListBoxOption>(props: MultiSelectProps<T>, state: MultiSelectState<T>, ref: RefObject<HTMLElement>): MultiSelectDOMProps<T>;
