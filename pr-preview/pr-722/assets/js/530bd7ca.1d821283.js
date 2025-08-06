ed464939e2e2307e44361eb155ecc90316335d9e
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-08-06 08:49:43 +0000

    Deploy preview for PR 722 🛫

---
+++ pr-preview/pr-722/assets/js/530bd7ca.1d821283.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[3014],{
+
+/***/ 78358:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-722/changelog/page/2","page":2,"postsPerPage":20,"totalPages":2,"totalCount":31,"previousPage":"/pr-preview/pr-722/changelog","blogDescription":"Håll dig uppdaterad med allt som händer i varje release","blogTitle":"Midas changelog"}}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-722/assets/js/f208be22.1d872653.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[6976],{
+
+/***/ 49921:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-722/release-notes/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":15,"previousPage":"/pr-preview/pr-722/release-notes","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
