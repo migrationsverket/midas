aac48ca5741f996a91e594d9fb4ea65d580bde62
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-08-06 08:47:19 +0000

    Deploying to gh-pages from @ migrationsverket/midas@8f319d1878eee15aa7c7a3b5976e0505a232dd12 🚀

---
+++ assets/js/ff0a21c3.4c89a286.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[4347],{
+
+/***/ 36118:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"tags":[{"label":"release-notes","permalink":"/release-notes/tags/release-notes","count":1},{"label":"v11","permalink":"/release-notes/tags/v-11","count":1},{"label":"breaking-change","permalink":"/release-notes/tags/breaking-change","count":1}]}');
+
+/***/ })
+
+}]);
