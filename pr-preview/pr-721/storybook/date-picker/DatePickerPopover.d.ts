dbcc4d1454e812b14d40c4226dd60f208b4a611b
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-08-06 08:44:19 +0000

    Remove preview for PR 721 🛬

--- pr-preview/pr-721/storybook/date-picker/DatePickerPopover.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-interface DatePickerPopoverProps {
-    children?: React.ReactNode;
-}
-export declare const DatePickerPopover: React.ForwardRefExoticComponent<DatePickerPopoverProps & React.RefAttributes<HTMLDivElement>>;
-export {};
