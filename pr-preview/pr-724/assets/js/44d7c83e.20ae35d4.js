19b2e05e0c300d462c8103325810ee4234ec270e
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-08-07 10:04:30 +0000

    Deploy preview for PR 724 🛫

---
+++ pr-preview/pr-724/assets/js/44d7c83e.20ae35d4.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[8905],{
+
+/***/ 45114:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-724/changelog/page/2","page":2,"postsPerPage":20,"totalPages":2,"totalCount":31,"previousPage":"/pr-preview/pr-724/changelog","blogDescription":"Håll dig uppdaterad med allt som händer i varje release","blogTitle":"Midas changelog"}}');
+
+/***/ })
+
+}]);
