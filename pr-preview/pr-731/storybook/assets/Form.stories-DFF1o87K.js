9d6bb1846af128dbdf4024e3104bb77e6b7cefbc
derpbravely <derpbravely@users.noreply.github.com>
2025-08-11 12:23:20 +0000

    Deploy preview for PR 731 🛫

---
+++ pr-preview/pr-731/storybook/assets/Form.stories-DFF1o87K.js
@@ -0,0 +1,89 @@
+import{j as n}from"./jsx-runtime-BYYWji4R.js";import{$ as j}from"./Form-D8xYOjab.js";import{G}from"./Grid-BjSJfDsS.js";import{G as e}from"./GridItem-BVyXLt9X.js";import{T as r}from"./TextField-eEzuqW3V.js";import{S as I}from"./Select-CLhVLOsk.js";import{B as a}from"./Button-CnS-ljRm.js";import{R as g,a as b}from"./Radio-k-aNvoXh.js";import{C as h}from"./CheckboxGroup-DRcybiJZ.js";import{C as k}from"./Checkbox-D41D68yA.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./utils-RHx87K0F.js";import"./SSRProvider-BhYbDCf7.js";import"./clsx-B-dksMZM.js";import"./useFormValidation-BM2DSNys.js";import"./useFocusRing-Fkrzm1DF.js";import"./_class_private_field_init-BOtJyCjG.js";import"./index-DQEgcCId.js";import"./TextFieldBase-CkPVP9U6.js";import"./TextField-CRU4k17S.js";import"./FieldError-CQ3SuNSh.js";import"./Text-CZ6zcozL.js";import"./Text-BgvnLaBz.js";import"./Input-Cwo2ePyd.js";import"./Hidden-BC9qER92.js";import"./Button-HNQZo37q.js";import"./useLabels-FEgier_d.js";import"./context-zNjZoWML.js";import"./useButton-BLF2LAsl.js";import"./useTextField-Q3Tcne7I.js";import"./useControlledState-H1tiOFpU.js";import"./useField-NuRgCgVr.js";import"./TextField.module-C33Y04O9.js";import"./Label-D98nMll0.js";import"./Dialog-F_hA-Xwt.js";import"./RSPContexts-HQcWPJyv.js";import"./OverlayArrow-DmYXKOE9.js";import"./Collection-CFZkBOo3.js";import"./index-NDK0uC61.js";import"./Separator-kamZUolC.js";import"./SelectionManager-nWppjZFm.js";import"./useEvent-D_ha2BWP.js";import"./scrollIntoView-Dsrcj58z.js";import"./FocusScope-D8MYeKfJ.js";import"./useDescription-BqzP9r9Z.js";import"./ListKeyboardDelegate-DlWRCE7o.js";import"./PressResponder-CHbj7vBO.js";import"./useLocalizedStringFormatter-DiUIqqhg.js";import"./LocalizedStringFormatter-BB0Nk989.js";import"./getScrollParent-CvodOSaS.js";import"./VisuallyHidden-Cz8nU2Hj.js";import"./Dialog-4xhjNuAh.js";import"./useLocalizedStringFormatter-9M_dEwOo.js";import"./x-Dz2R9y6b.js";import"./createLucideIcon-uZw3gxGR.js";import"./Heading-CV1Pcj_U.js";import"./info-CSjK64PU.js";import"./Tag-CTTltAhl.js";import"./ListBox-CxpvDsGt.js";import"./DragAndDrop-P3C1Zhz8.js";import"./inertValue-C_mH9YkE.js";import"./useListState-CM3QJImq.js";import"./useHighlightSelectionDescription-BrF90ArE.js";import"./useUpdateEffect-BzF61qSB.js";import"./useHasTabbableChild-Cg4wFD4g.js";import"./check-yXF3Bjrk.js";import"./ListBoxSection-B2XotoC7.js";import"./useObserveElement-BP4zVUS4.js";import"./chevron-down-CPcpcOqy.js";import"./Button.module-Bmb634uG.js";import"./Group-BpZQK8Ex.js";import"./useToggleState-DcHMzLVj.js";const wn={component:j,title:"Examples/Form",tags:["autodocs"],argTypes:{}},m={args:{},render:()=>{const t=["Banan","Apple","Mango"];return n.jsxs(G,{children:[n.jsx(e,{size:12,children:n.jsx(r,{label:"Ange ditt fullständiga namn",description:"Glöm inte dina eventuella mellannamn!"})}),n.jsx(e,{size:12,children:n.jsx(r,{label:"Personnummmer",description:"Anges på formen ÅÅMMDD-XXXX"})}),n.jsx(e,{size:12,children:n.jsx(I,{label:"Vilken är din favoritfrukt",placeholder:"Välj en frukt",selectionMode:"single",options:t.map(o=>({id:o,name:o}))})}),n.jsx(e,{children:n.jsx(a,{children:"Skicka"})})]})}},s={args:{},render:()=>{const t=["A","B","C"],o=["Anledning A","Anledning B","Anledning C"];return n.jsx("div",{children:n.jsxs(G,{children:[n.jsx(e,{size:{xs:12,sm:6},children:n.jsx(g,{label:"Anledning",description:"Ange anledning till att du söker i databasen",children:o.map(i=>n.jsx(b,{value:i,children:i},i))})}),n.jsx(e,{size:{xs:12,sm:6},children:n.jsx(h,{label:"Databas",description:"Välj databas att söka i",children:t.map(i=>n.jsx(k,{value:i,children:i},i))})}),n.jsx(e,{size:{xs:12,sm:6},children:n.jsx(r,{label:"Namn",description:""})}),n.jsx(e,{size:{xs:12,sm:6},children:n.jsx(r,{label:"Personnummer"})}),n.jsx(e,{size:{xs:12,sm:6},children:n.jsx(I,{label:"Kön",description:"Kan lämnas tomt",placeholder:"Välj kön",selectionMode:"single",options:t.map(i=>({name:i,id:i}))})}),n.jsx(e,{size:{xs:12,sm:6},children:n.jsx(r,{label:"Ärendekod"})}),n.jsx(e,{size:"auto",children:n.jsx(a,{children:"Sök"})}),n.jsx(e,{size:"auto",children:n.jsx(a,{variant:"secondary",children:"Rensa"})})]})})}};var d,l,p;m.parameters={...m.parameters,docs:{...(d=m.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {},
+  render: () => {
+    const ITEMS = ['Banan', 'Apple', 'Mango'];
+    return <Grid>
+        <GridItem size={12}>
+          <TextField label='Ange ditt fullständiga namn' description='Glöm inte dina eventuella mellannamn!' />
+        </GridItem>
+        <GridItem size={12}>
+          <TextField label='Personnummmer' description='Anges på formen ÅÅMMDD-XXXX' />
+        </GridItem>
+        <GridItem size={12}>
+          <Select label='Vilken är din favoritfrukt' placeholder='Välj en frukt' selectionMode='single' options={ITEMS.map(i => ({
+          id: i,
+          name: i
+        }))} />
+        </GridItem>
+
+        <GridItem>
+          <Button>Skicka</Button>
+        </GridItem>
+      </Grid>;
+  }
+}`,...(p=(l=m.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var c,x,u;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {},
+  render: () => {
+    const ITEMS = ['A', 'B', 'C'];
+    const RADIOITEMS = ['Anledning A', 'Anledning B', 'Anledning C'];
+    return <div>
+        <Grid>
+          <GridItem size={{
+          xs: 12,
+          sm: 6
+        }}>
+            <RadioGroup label='Anledning' description='Ange anledning till att du söker i databasen'>
+              {RADIOITEMS.map((item: string) => <Radio value={item} key={item}>
+                  {item}
+                </Radio>)}
+            </RadioGroup>
+          </GridItem>
+          <GridItem size={{
+          xs: 12,
+          sm: 6
+        }}>
+            <CheckboxGroup label='Databas' description='Välj databas att söka i'>
+              {ITEMS.map((item: string) => <Checkbox value={item} key={item}>
+                  {item}
+                </Checkbox>)}
+            </CheckboxGroup>
+          </GridItem>
+          <GridItem size={{
+          xs: 12,
+          sm: 6
+        }}>
+            <TextField label='Namn' description='' />
+          </GridItem>
+          <GridItem size={{
+          xs: 12,
+          sm: 6
+        }}>
+            <TextField label='Personnummer' />
+          </GridItem>
+
+          <GridItem size={{
+          xs: 12,
+          sm: 6
+        }}>
+            <Select label='Kön' description='Kan lämnas tomt' placeholder='Välj kön' selectionMode='single' options={ITEMS.map(i => ({
+            name: i,
+            id: i
+          }))} />
+          </GridItem>
+          <GridItem size={{
+          xs: 12,
+          sm: 6
+        }}>
+            <TextField label='Ärendekod' />
+          </GridItem>
+
+          <GridItem size='auto'>
+            <Button>Sök</Button>
+          </GridItem>
+          <GridItem size='auto'>
+            <Button variant='secondary'>Rensa</Button>
+          </GridItem>
+        </Grid>
+      </div>;
+  }
+}`,...(u=(x=s.parameters)==null?void 0:x.docs)==null?void 0:u.source}}};const Nn=["SimpleForm","TwoColumnForm"];export{m as SimpleForm,s as TwoColumnForm,Nn as __namedExportsOrder,wn as default};
