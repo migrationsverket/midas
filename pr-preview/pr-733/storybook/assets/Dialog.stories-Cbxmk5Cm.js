7aebdf00f4ef4f413147b2933a55ded94f3fd407
derpbravely <derpbravely@users.noreply.github.com>
2025-08-12 12:10:22 +0000

    Deploy preview for PR 733 🛫

---
+++ pr-preview/pr-733/storybook/assets/Dialog.stories-Cbxmk5Cm.js
@@ -0,0 +1,121 @@
+import{j as i}from"./jsx-runtime-BYYWji4R.js";import{M as m}from"./Dialog-w08lby5h.js";import{o as f}from"./storybook-DLHntVpb.js";import{$ as n}from"./Dialog-BTPkDfgN.js";import{B as s}from"./Button-46Wjn1qU.js";import{S as v}from"./Select-Dk_JOTrH.js";import{T as e}from"./Text-BgvnLaBz.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./OverlayArrow-DJSGdA-H.js";import"./utils-RHx87K0F.js";import"./SSRProvider-BhYbDCf7.js";import"./index-DQEgcCId.js";import"./useFocusRing-zhkOjX4v.js";import"./_class_private_field_init-BOtJyCjG.js";import"./Button-BnGcr9X4.js";import"./Hidden-BC9qER92.js";import"./useLabels-FEgier_d.js";import"./context-zNjZoWML.js";import"./useButton-DQcVpLGQ.js";import"./useControlledState-H1tiOFpU.js";import"./useLocalizedStringFormatter-9M_dEwOo.js";import"./LocalizedStringFormatter-BB0Nk989.js";import"./x-Dz2R9y6b.js";import"./createLucideIcon-uZw3gxGR.js";import"./Heading-CV1Pcj_U.js";import"./RSPContexts-HQcWPJyv.js";import"./CalendarDate-Lfx2W0H2.js";import"./Collection-h-VYV8ts.js";import"./index-NDK0uC61.js";import"./Separator-BmLx0v9B.js";import"./SelectionManager-CFyGcN4y.js";import"./useEvent-D_ha2BWP.js";import"./scrollIntoView-Dsrcj58z.js";import"./FocusScope-p3_VE_8x.js";import"./useDescription-BqzP9r9Z.js";import"./ListKeyboardDelegate-Dxk4Tqtx.js";import"./Text-CZ6zcozL.js";import"./PressResponder-DWi2SZR7.js";import"./useLocalizedStringFormatter-DiUIqqhg.js";import"./getScrollParent-CvodOSaS.js";import"./VisuallyHidden-BekSNXsj.js";import"./Button.module-Bmb634uG.js";import"./Tag-DQT6HBax.js";import"./ListBox-DqnZQnhD.js";import"./DragAndDrop-P3C1Zhz8.js";import"./inertValue-C_mH9YkE.js";import"./useListState-v0RAzSIj.js";import"./useField-GoO-rf0S.js";import"./useHighlightSelectionDescription-Bt50waqb.js";import"./useUpdateEffect-BzF61qSB.js";import"./useHasTabbableChild-Dw2OyaZg.js";import"./TextField-C4YKJjV4.js";import"./FieldError-BjFojtrn.js";import"./Form-Lr15ufU6.js";import"./useFormValidation-DmuZGU-W.js";import"./Input-BMo7u0M7.js";import"./useTextField-0IVN8eZA.js";import"./Checkbox-DTYof62l.js";import"./useToggleState-DcHMzLVj.js";import"./check-yXF3Bjrk.js";import"./ListBoxSection-BgwkEpe5.js";import"./useObserveElement-BP4zVUS4.js";import"./chevron-down-CPcpcOqy.js";import"./Label-ofhypfUm.js";import"./info-CSjK64PU.js";const yi={component:m,subcomponents:{DialogTrigger:n},title:"Components/Modal",tags:["autodocs"],parameters:{layout:"centered"},render:a=>i.jsxs(n,{children:[i.jsx(s,{children:"Öppna"}),i.jsxs(m,{title:"Enter your name",...a,children:[i.jsx(v,{autoFocus:!0,placeholder:"Select...",defaultSelectedKeys:["kiwi"],label:"Select fruits",selectionMode:"multiple",options:f,isClearable:!0,isSelectableAll:!1}),i.jsx(s,{slot:"close",children:"Submit"})]})]})},o={args:{isDismissable:!0}},t={},r={render:a=>i.jsxs(n,{children:[i.jsx(s,{children:"Öppna"}),i.jsxs(m,{title:"Read all the text",...a,children:[i.jsx(e,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe atque necessitatibus pariatur aliquam vel incidunt blanditiis rem maxime. Modi enim dolorem optio id error reprehenderit nisi non iste? Natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, dolores eligendi rerum distinctio dignissimos repellat magni est veniam, ratione, totam quo eius aperiam dolorum quod minima corporis quibusdam! Tempore, nam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, laborum praesentium deserunt incidunt minima doloremque eligendi odio iure officia sunt, delectus rem quam soluta dolores modi, illo expedita molestiae eaque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis provident, dolorem perspiciatis nesciunt dicta explicabo sequi doloremque neque fugit? Ratione adipisci dolor saepe nam fugit provident asperiores voluptas! Molestiae, cumque."}),i.jsx(e,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe atque necessitatibus pariatur aliquam vel incidunt blanditiis rem maxime. Modi enim dolorem optio id error reprehenderit nisi non iste? Natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, dolores eligendi rerum distinctio dignissimos repellat magni est veniam, ratione, totam quo eius aperiam dolorum quod minima corporis quibusdam! Tempore, nam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, laborum praesentium deserunt incidunt minima doloremque eligendi odio iure officia sunt, delectus rem quam soluta dolores modi, illo expedita molestiae eaque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis provident, dolorem perspiciatis nesciunt dicta explicabo sequi doloremque neque fugit? Ratione adipisci dolor saepe nam fugit provident asperiores voluptas! Molestiae, cumque."}),i.jsx(e,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe atque necessitatibus pariatur aliquam vel incidunt blanditiis rem maxime. Modi enim dolorem optio id error reprehenderit nisi non iste? Natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, dolores eligendi rerum distinctio dignissimos repellat magni est veniam, ratione, totam quo eius aperiam dolorum quod minima corporis quibusdam! Tempore, nam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, laborum praesentium deserunt incidunt minima doloremque eligendi odio iure officia sunt, delectus rem quam soluta dolores modi, illo expedita molestiae eaque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis provident, dolorem perspiciatis nesciunt dicta explicabo sequi doloremque neque fugit? Ratione adipisci dolor saepe nam fugit provident asperiores voluptas! Molestiae, cumque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe atque necessitatibus pariatur aliquam vel incidunt blanditiis rem maxime. Modi enim dolorem optio id error reprehenderit nisi non iste? Natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, dolores eligendi rerum distinctio dignissimos repellat magni est veniam, ratione, totam quo eius aperiam dolorum quod minima corporis quibusdam! Tempore, nam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, laborum praesentium deserunt incidunt minima doloremque eligendi odio iure officia sunt, delectus rem quam soluta dolores modi, illo expedita molestiae eaque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis provident, dolorem perspiciatis nesciunt dicta explicabo sequi doloremque neque fugit? Ratione adipisci dolor saepe nam fugit provident asperiores voluptas! Molestiae, cumque."}),i.jsx(e,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe atque necessitatibus pariatur aliquam vel incidunt blanditiis rem maxime. Modi enim dolorem optio id error reprehenderit nisi non iste? Natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, dolores eligendi rerum distinctio dignissimos repellat magni est veniam, ratione, totam quo eius aperiam dolorum quod minima corporis quibusdam! Tempore, nam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, laborum praesentium deserunt incidunt minima doloremque eligendi odio iure officia sunt, delectus rem quam soluta dolores modi, illo expedita molestiae eaque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis provident, dolorem perspiciatis nesciunt dicta explicabo sequi doloremque neque fugit? Ratione adipisci dolor saepe nam fugit provident asperiores voluptas! Molestiae, cumque."}),i.jsx(e,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe atque necessitatibus pariatur aliquam vel incidunt blanditiis rem maxime. Modi enim dolorem optio id error reprehenderit nisi non iste? Natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, dolores eligendi rerum distinctio dignissimos repellat magni est veniam, ratione, totam quo eius aperiam dolorum quod minima corporis quibusdam! Tempore, nam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, laborum praesentium deserunt incidunt minima doloremque eligendi odio iure officia sunt, delectus rem quam soluta dolores modi, illo expedita molestiae eaque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis provident, dolorem perspiciatis nesciunt dicta explicabo sequi doloremque neque fugit? Ratione adipisci dolor saepe nam fugit provident asperiores voluptas! Molestiae, cumque."}),i.jsx(e,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe atque necessitatibus pariatur aliquam vel incidunt blanditiis rem maxime. Modi enim dolorem optio id error reprehenderit nisi non iste? Natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, dolores eligendi rerum distinctio dignissimos repellat magni est veniam, ratione, totam quo eius aperiam dolorum quod minima corporis quibusdam! Tempore, nam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, laborum praesentium deserunt incidunt minima doloremque eligendi odio iure officia sunt, delectus rem quam soluta dolores modi, illo expedita molestiae eaque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis provident, dolorem perspiciatis nesciunt dicta explicabo sequi doloremque neque fugit? Ratione adipisci dolor saepe nam fugit provident asperiores voluptas! Molestiae, cumque."}),i.jsx(s,{slot:"close",children:"Submit"})]})]})};var u,d,l;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    isDismissable: true
+  }
+}`,...(l=(d=o.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var p,c,q;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(q=(c=t.parameters)==null?void 0:c.docs)==null?void 0:q.source}}};var g,b,x;r.parameters={...r.parameters,docs:{...(g=r.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: args => <DialogTrigger>
+      <Button>Öppna</Button>
+      <Modal title='Read all the text' {...args}>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe
+          atque necessitatibus pariatur aliquam vel incidunt blanditiis rem
+          maxime. Modi enim dolorem optio id error reprehenderit nisi non iste?
+          Natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Itaque, dolores eligendi rerum distinctio dignissimos repellat magni
+          est veniam, ratione, totam quo eius aperiam dolorum quod minima
+          corporis quibusdam! Tempore, nam. Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Tempore, laborum praesentium deserunt
+          incidunt minima doloremque eligendi odio iure officia sunt, delectus
+          rem quam soluta dolores modi, illo expedita molestiae eaque! Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Facilis provident,
+          dolorem perspiciatis nesciunt dicta explicabo sequi doloremque neque
+          fugit? Ratione adipisci dolor saepe nam fugit provident asperiores
+          voluptas! Molestiae, cumque.
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe
+          atque necessitatibus pariatur aliquam vel incidunt blanditiis rem
+          maxime. Modi enim dolorem optio id error reprehenderit nisi non iste?
+          Natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Itaque, dolores eligendi rerum distinctio dignissimos repellat magni
+          est veniam, ratione, totam quo eius aperiam dolorum quod minima
+          corporis quibusdam! Tempore, nam. Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Tempore, laborum praesentium deserunt
+          incidunt minima doloremque eligendi odio iure officia sunt, delectus
+          rem quam soluta dolores modi, illo expedita molestiae eaque! Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Facilis provident,
+          dolorem perspiciatis nesciunt dicta explicabo sequi doloremque neque
+          fugit? Ratione adipisci dolor saepe nam fugit provident asperiores
+          voluptas! Molestiae, cumque.
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe
+          atque necessitatibus pariatur aliquam vel incidunt blanditiis rem
+          maxime. Modi enim dolorem optio id error reprehenderit nisi non iste?
+          Natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Itaque, dolores eligendi rerum distinctio dignissimos repellat magni
+          est veniam, ratione, totam quo eius aperiam dolorum quod minima
+          corporis quibusdam! Tempore, nam. Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Tempore, laborum praesentium deserunt
+          incidunt minima doloremque eligendi odio iure officia sunt, delectus
+          rem quam soluta dolores modi, illo expedita molestiae eaque! Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Facilis provident,
+          dolorem perspiciatis nesciunt dicta explicabo sequi doloremque neque
+          fugit? Ratione adipisci dolor saepe nam fugit provident asperiores
+          voluptas! Molestiae, cumque. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Dolore saepe atque necessitatibus pariatur aliquam
+          vel incidunt blanditiis rem maxime. Modi enim dolorem optio id error
+          reprehenderit nisi non iste? Natus! Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Itaque, dolores eligendi rerum
+          distinctio dignissimos repellat magni est veniam, ratione, totam quo
+          eius aperiam dolorum quod minima corporis quibusdam! Tempore, nam.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore,
+          laborum praesentium deserunt incidunt minima doloremque eligendi odio
+          iure officia sunt, delectus rem quam soluta dolores modi, illo
+          expedita molestiae eaque! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Facilis provident, dolorem perspiciatis nesciunt
+          dicta explicabo sequi doloremque neque fugit? Ratione adipisci dolor
+          saepe nam fugit provident asperiores voluptas! Molestiae, cumque.
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe
+          atque necessitatibus pariatur aliquam vel incidunt blanditiis rem
+          maxime. Modi enim dolorem optio id error reprehenderit nisi non iste?
+          Natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Itaque, dolores eligendi rerum distinctio dignissimos repellat magni
+          est veniam, ratione, totam quo eius aperiam dolorum quod minima
+          corporis quibusdam! Tempore, nam. Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Tempore, laborum praesentium deserunt
+          incidunt minima doloremque eligendi odio iure officia sunt, delectus
+          rem quam soluta dolores modi, illo expedita molestiae eaque! Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Facilis provident,
+          dolorem perspiciatis nesciunt dicta explicabo sequi doloremque neque
+          fugit? Ratione adipisci dolor saepe nam fugit provident asperiores
+          voluptas! Molestiae, cumque.
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe
+          atque necessitatibus pariatur aliquam vel incidunt blanditiis rem
+          maxime. Modi enim dolorem optio id error reprehenderit nisi non iste?
+          Natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Itaque, dolores eligendi rerum distinctio dignissimos repellat magni
+          est veniam, ratione, totam quo eius aperiam dolorum quod minima
+          corporis quibusdam! Tempore, nam. Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Tempore, laborum praesentium deserunt
+          incidunt minima doloremque eligendi odio iure officia sunt, delectus
+          rem quam soluta dolores modi, illo expedita molestiae eaque! Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Facilis provident,
+          dolorem perspiciatis nesciunt dicta explicabo sequi doloremque neque
+          fugit? Ratione adipisci dolor saepe nam fugit provident asperiores
+          voluptas! Molestiae, cumque.
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore saepe
+          atque necessitatibus pariatur aliquam vel incidunt blanditiis rem
+          maxime. Modi enim dolorem optio id error reprehenderit nisi non iste?
+          Natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Itaque, dolores eligendi rerum distinctio dignissimos repellat magni
+          est veniam, ratione, totam quo eius aperiam dolorum quod minima
+          corporis quibusdam! Tempore, nam. Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Tempore, laborum praesentium deserunt
+          incidunt minima doloremque eligendi odio iure officia sunt, delectus
+          rem quam soluta dolores modi, illo expedita molestiae eaque! Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Facilis provident,
+          dolorem perspiciatis nesciunt dicta explicabo sequi doloremque neque
+          fugit? Ratione adipisci dolor saepe nam fugit provident asperiores
+          voluptas! Molestiae, cumque.
+        </Text>
+        <Button slot='close'>Submit</Button>
+      </Modal>
+    </DialogTrigger>
+}`,...(x=(b=r.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};const Ei=["Default","NotDismissable","Scrollable"];export{o as Default,t as NotDismissable,r as Scrollable,Ei as __namedExportsOrder,yi as default};
---
+++ pr-preview/pr-733/storybook/assets/Label.stories-7826tQ0Z.js
@@ -0,0 +1,17 @@
+import{L as e}from"./Label-ofhypfUm.js";import{e as a}from"./index-Q7OVwvWC.js";import"./jsx-runtime-BYYWji4R.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./Button-BnGcr9X4.js";import"./utils-RHx87K0F.js";import"./SSRProvider-BhYbDCf7.js";import"./Hidden-BC9qER92.js";import"./useFocusRing-zhkOjX4v.js";import"./_class_private_field_init-BOtJyCjG.js";import"./index-DQEgcCId.js";import"./useLabels-FEgier_d.js";import"./context-zNjZoWML.js";import"./useButton-DQcVpLGQ.js";import"./Dialog-BTPkDfgN.js";import"./RSPContexts-HQcWPJyv.js";import"./OverlayArrow-DJSGdA-H.js";import"./useControlledState-H1tiOFpU.js";import"./Collection-h-VYV8ts.js";import"./index-NDK0uC61.js";import"./Separator-BmLx0v9B.js";import"./SelectionManager-CFyGcN4y.js";import"./useEvent-D_ha2BWP.js";import"./scrollIntoView-Dsrcj58z.js";import"./FocusScope-p3_VE_8x.js";import"./useDescription-BqzP9r9Z.js";import"./ListKeyboardDelegate-Dxk4Tqtx.js";import"./Text-CZ6zcozL.js";import"./PressResponder-DWi2SZR7.js";import"./useLocalizedStringFormatter-DiUIqqhg.js";import"./LocalizedStringFormatter-BB0Nk989.js";import"./getScrollParent-CvodOSaS.js";import"./VisuallyHidden-BekSNXsj.js";import"./Dialog-w08lby5h.js";import"./useLocalizedStringFormatter-9M_dEwOo.js";import"./Button-46Wjn1qU.js";import"./Button.module-Bmb634uG.js";import"./x-Dz2R9y6b.js";import"./createLucideIcon-uZw3gxGR.js";import"./Heading-CV1Pcj_U.js";import"./info-CSjK64PU.js";const Y={component:e,title:"Components/Typography/Label",tags:["autodocs"]},t={args:{children:"I am a label"},play:async({canvas:i,args:{children:m}})=>{a(i.getByText(m)).toHaveStyle({"font-size":"16px","line-height":"20px","font-weight":500})}};var r,o,p;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: 'I am a label'
+  },
+  play: async ({
+    canvas,
+    args: {
+      children
+    }
+  }) => {
+    expect(canvas.getByText(children as string)).toHaveStyle({
+      'font-size': '16px',
+      'line-height': '20px',
+      'font-weight': 500
+    });
+  }
+}`,...(p=(o=t.parameters)==null?void 0:o.docs)==null?void 0:p.source}}};const Z=["_Label"];export{t as _Label,Z as __namedExportsOrder,Y as default};
