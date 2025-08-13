4b08744489d4acfa01d392c3dfd8c09c217f702f
derpbravely <derpbravely@users.noreply.github.com>
2025-08-13 08:40:04 +0000

    Deploy preview for PR 736 🛫

---
+++ pr-preview/pr-736/storybook/assets/ButtonGroup.stories-DSIYlQh8.js
@@ -0,0 +1,11 @@
+import{j as o}from"./jsx-runtime-BYYWji4R.js";import{B as a}from"./Button-CHyBMGdU.js";import{c as m}from"./clsx-B-dksMZM.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button.module-Bmb634uG.js";import"./Button-BnGcr9X4.js";import"./utils-RHx87K0F.js";import"./SSRProvider-BhYbDCf7.js";import"./Hidden-BC9qER92.js";import"./useFocusRing-zhkOjX4v.js";import"./_class_private_field_init-BOtJyCjG.js";import"./index-DQEgcCId.js";import"./useLabels-FEgier_d.js";import"./context-zNjZoWML.js";import"./useButton-DQcVpLGQ.js";const l="_buttonGroup_fh6r8_4",d={buttonGroup:l},e=({children:r,className:i,"aria-label":u,...c})=>o.jsx("div",{role:"group","aria-label":u,className:m(d.buttonGroup,i),...c,children:r});try{e.displayName="ButtonGroup",e.__docgenInfo={description:"Group several buttons together.",displayName:"ButtonGroup",props:{}}}catch{}const V={component:e,title:"Components/Button/ButtonGroup",tags:["autodocs"],parameters:{layout:"centered"},args:{},argTypes:{}},t={args:{"aria-label":"Vill du acceptera ändringen?"},render:({...r})=>o.jsxs(e,{...r,children:[o.jsx(a,{children:"Fortsätt"}),o.jsx(a,{variant:"secondary",children:"Avbryt"})]})};var n,s,p;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    'aria-label': 'Vill du acceptera ändringen?'
+  },
+  render: ({
+    ...args
+  }) => <ButtonGroup {...args}>
+      <Button>Fortsätt</Button>
+      <Button variant='secondary'>Avbryt</Button>
+    </ButtonGroup>
+}`,...(p=(s=t.parameters)==null?void 0:s.docs)==null?void 0:p.source}}};const C=["Primary"];export{t as Primary,C as __namedExportsOrder,V as default};
