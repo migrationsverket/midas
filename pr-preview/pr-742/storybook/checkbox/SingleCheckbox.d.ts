2b91d138a8d9fa45f3f0707c3c8b05a3e792037f
derpbravely <derpbravely@users.noreply.github.com>
2025-08-14 13:03:14 +0000

    Remove preview for PR 742 🛬

--- pr-preview/pr-742/storybook/checkbox/SingleCheckbox.d.ts
+++
@@ -1,3 +0,0 @@
-import { CheckboxComponentProps } from './types';
-import * as React from 'react';
-export declare const SingleCheckbox: React.ForwardRefExoticComponent<CheckboxComponentProps & React.RefAttributes<HTMLLabelElement>>;
