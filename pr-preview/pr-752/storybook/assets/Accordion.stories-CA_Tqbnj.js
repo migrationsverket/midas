cc2a9fa8af0beb84c1d45e5581a09182310f22b2
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-08-21 19:18:53 +0000

    Deploy preview for PR 752 🛫

---
+++ pr-preview/pr-752/storybook/assets/Accordion.stories-CA_Tqbnj.js
@@ -0,0 +1,139 @@
+import{j as t}from"./jsx-runtime-BYYWji4R.js";import{e as h,u as b}from"./index-Q7OVwvWC.js";import{c as G}from"./clsx-B-dksMZM.js";import{a as U,$ as Z,A as s}from"./AccordionItem-DE3E4HLm.js";import{R as J}from"./index-ClcD9ViR.js";import{l as Q,h as v}from"./test-DcKGE-up.js";import{c as X}from"./createLucideIcon-uZw3gxGR.js";import{B as Y}from"./Button-CPevayqD.js";import"./icon-map-yTTVA-eu.js";import"./info-CSjK64PU.js";import"./check-yXF3Bjrk.js";import"./Button-v_l_ujRb.js";import"./utils-RHx87K0F.js";import"./SSRProvider-BhYbDCf7.js";import"./Hidden-BC9qER92.js";import"./useFocusRing-zhkOjX4v.js";import"./_class_private_field_init-BOtJyCjG.js";import"./index-DQEgcCId.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./useLabels-FEgier_d.js";import"./context-zNjZoWML.js";import"./useButton-DQcVpLGQ.js";import"./useControlledState-H1tiOFpU.js";import"./useEvent-D_ha2BWP.js";import"./chevron-down-CPcpcOqy.js";import"./Heading-CV1Pcj_U.js";import"./RSPContexts-HQcWPJyv.js";import"./Button.module-Bmb634uG.js";const ee="_root_pas7q_4",te="_contained_pas7q_8",g={root:ee,contained:te},W="accordion",r=({variant:e,children:n,className:a,isContained:$,...F})=>{const f=$||e==="contained";return t.jsx(U.Provider,{value:{isContained:f},children:t.jsx(Z,{"data-testid":W,className:G(g.root,f?g.contained:g.uncontained,a),...F,children:n})})};try{r.displayName="Accordion",r.__docgenInfo={description:"Accordions help reduce visual clutter on a page by organizing content into collapsible sections.",displayName:"Accordion",props:{variant:{defaultValue:null,description:"Display either the larger contained variant or a smaller uncontained variant\n@deprecated since 11.0.0. Please use `isContained` instead",name:"variant",required:!1,type:{name:"enum",value:[{value:'"uncontained"'},{value:'"contained"'}]}},children:{defaultValue:null,description:"The children of the component. A function may be provided to alter the children based on component state.",name:"children",required:!1,type:{name:"ReactNode"}},isDisabled:{defaultValue:null,description:"Whether all items are disabled.",name:"isDisabled",required:!1,type:{name:"boolean"}},isContained:{defaultValue:null,description:"",name:"isContained",required:!1,type:{name:"boolean"}},allowsMultipleExpanded:{defaultValue:null,description:"Whether multiple items can be expanded at the same time.",name:"allowsMultipleExpanded",required:!1,type:{name:"boolean"}},expandedKeys:{defaultValue:null,description:"The currently expanded keys in the group (controlled).",name:"expandedKeys",required:!1,type:{name:"Iterable<Key>"}},defaultExpandedKeys:{defaultValue:null,description:"The initial expanded keys in the group (uncontrolled).",name:"defaultExpandedKeys",required:!1,type:{name:"Iterable<Key>"}},onExpandedChange:{defaultValue:null,description:"Handler that is called when items are expanded or collapsed.",name:"onExpandedChange",required:!1,type:{name:"(keys: Set<Key>) => any"}},className:{defaultValue:null,description:"The CSS [className](https://developer.mozilla.org/en-US/docs/Web/API/Element/className) for the element. A function may be provided to compute the class based on component state.",name:"className",required:!1,type:{name:"string | ((values: DisclosureGroupRenderProps & { defaultClassName: string; }) => string)"}},style:{defaultValue:null,description:"The inline [style](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style) for the element. A function may be provided to compute the style based on component state.",name:"style",required:!1,type:{name:"CSSProperties | ((values: DisclosureGroupRenderProps & { defaultStyle: CSSProperties; }) => CSSProperties)"}},id:{defaultValue:null,description:"The element's unique identifier. See [MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/id).",name:"id",required:!1,type:{name:"string"}}}}}catch{}/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const ne=[["path",{d:"M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z",key:"1rqfz7"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}]],ae=X("file",ne),y=["Ett","Två","Tre","Fyra"],ke={component:r,subcomponents:{AccordionItem:s},title:"Components/Accordion",tags:["autodocs"],args:{}},i={args:{className:"test",children:y.map(e=>t.jsxs(s,{id:e,title:"En öppningsbar panel "+e.toLocaleLowerCase(),children:["Innehåll i öppningsbar panel ",e,". Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore nesciunt at aliquam! Blanditiis quasi consequuntur doloremque harum commodi odit velit pariatur voluptate aliquid, inventore praesentium tempore dignissimos officia sint libero!"]},e))},play:async({canvas:e,step:n})=>{await n("It should preserve its classNames when being passed new ones",async()=>{const a=e.getByTestId(W);await h(a).toHaveClass(g.root,"test")})}},o={args:{...i.args,isContained:!0}},l={parameters:{chromatic:{disableSnapshot:!0}},args:{...i.args,allowsMultipleExpanded:!0}},c={args:{...i.args,defaultExpandedKeys:["Två"]}},d={args:{...o.args,children:y.map((e,n)=>t.jsxs(s,{id:e,title:t.jsxs("div",{style:{display:"flex",alignItems:"center",gap:"1rem"},children:[t.jsx(ae,{}),t.jsxs("b",{children:["En öppningsbar panel ' + ",e.toLocaleLowerCase()]}),t.jsxs("p",{style:{margin:0},children:["2025-03-0",n]})]}),children:["Innehåll i öppningsbarpanel ",e," Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus perspiciatis officia, voluptate ratione quam nemo quod aut maiores animi nostrum, in labore adipisci ullam suscipit esse vel odit tenetur dicta. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit dolorem tempora laboriosam asperiores eum dignissimos accusantium voluptate eligendi beatae vel quis rerum error dolore cum incidunt pariatur accusamus, illum consequuntur?"]},e))}},p={args:{},tags:["!dev","!autodocs"],parameters:{chromatic:{disableSnapshot:!0}},render:()=>t.jsxs(r,{children:[t.jsxs(s,{title:"AccordionItem with dynamic content",children:["Knowledge is knowing a tomato is a fruit; wisdom is not putting it in a fruit salad.",t.jsx(ie,{})]}),t.jsx(s,{title:"Another AccordionItem",children:"More text about another subject..."})]}),play:async({canvas:e})=>{await b.click(e.getByRole("button",{name:"AccordionItem with dynamic content"})),await b.click(e.getByTestId("btn-0")),await h(e.getByTestId("hidden-content")).toBeVisible()}},m={tags:["!dev","!autodocs"],parameters:{chromatic:{disableSnapshot:!0}},render:()=>t.jsx(r,{children:t.jsx(s,{title:"Test",children:t.jsx(Y,{isDisabled:!0,children:"Test"})})}),play:async({canvas:e,step:n,globals:{scheme:a}})=>{await n("it should not have the disabled style even if it contains disabled children",async()=>{await h(e.getByRole("heading",{level:2,name:"Test"})).toHaveStyle({color:Q(v("#171717"),v("#f2f2f2"),a)})})}},ie=()=>{const[e,n]=J.useState(!1);return t.jsxs("div",{children:[t.jsx("button",{onClick:()=>n(a=>!a),"data-testid":"btn-0",children:e?"hide":"show"}),e?t.jsx("div",{style:{background:"black",color:"white",height:"auto",display:"flex",alignItems:"center",justifyContent:"center",padding:"2rem"},"data-testid":"hidden-content",children:"Pineapples were once so rare and expensive in Europe that people used them as a status symbol—even renting them for parties to show off wealth, without ever eating them!"}):null]})},u={args:{variant:"contained",children:y.map((e,n)=>t.jsxs(s,{id:e,title:"En öppningsbar panel "+e.toLocaleLowerCase(),type:n===2?"success":n===3?"warning":void 0,children:["Innehåll i öppningsbar panel ",e," Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus perspiciatis officia, voluptate ratione quam nemo quod aut maiores animi nostrum, in labore adipisci ullam suscipit esse vel odit tenetur dicta. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit dolorem tempora laboriosam asperiores eum dignissimos accusantium voluptate eligendi beatae vel quis rerum error dolore cum incidunt pariatur accusamus, illum consequuntur?"]},e))}};var x,I,w;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    className: 'test',
+    children: ITEMS.map(item => <AccordionItem id={item} key={item} title={'En öppningsbar panel ' + item.toLocaleLowerCase()}>
+        Innehåll i öppningsbar panel {item}. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Dolore nesciunt at aliquam! Blanditiis
+        quasi consequuntur doloremque harum commodi odit velit pariatur
+        voluptate aliquid, inventore praesentium tempore dignissimos officia
+        sint libero!
+      </AccordionItem>)
+  },
+  play: async ({
+    canvas,
+    step
+  }) => {
+    await step('It should preserve its classNames when being passed new ones', async () => {
+      const accordion = canvas.getByTestId(ACCORDION_TEST_ID);
+      await expect(accordion).toHaveClass(styles.root, 'test');
+    });
+  }
+}`,...(w=(I=i.parameters)==null?void 0:I.docs)==null?void 0:w.source}}};var A,q,S;o.parameters={...o.parameters,docs:{...(A=o.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    isContained: true
+  }
+}`,...(S=(q=o.parameters)==null?void 0:q.docs)==null?void 0:S.source}}};var E,C,T;l.parameters={...l.parameters,docs:{...(E=l.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  parameters: {
+    chromatic: {
+      disableSnapshot: true
+    }
+  },
+  args: {
+    ...Default.args,
+    allowsMultipleExpanded: true
+  }
+}`,...(T=(C=l.parameters)==null?void 0:C.docs)==null?void 0:T.source}}};var D,L,j;c.parameters={...c.parameters,docs:{...(D=c.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    defaultExpandedKeys: ['Två']
+  }
+}`,...(j=(L=c.parameters)==null?void 0:L.docs)==null?void 0:j.source}}};var _,k,B;d.parameters={...d.parameters,docs:{...(_=d.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    ...Contained.args,
+    children: ITEMS.map((item, i) => <AccordionItem id={item} key={item} title={<div style={{
+      display: 'flex',
+      alignItems: 'center',
+      gap: '1rem'
+    }}>
+            <File />
+            <b>En öppningsbar panel ' + {item.toLocaleLowerCase()}</b>
+            <p style={{
+        margin: 0
+      }}>2025-03-0{i}</p>
+          </div>}>
+        Innehåll i öppningsbarpanel {item} Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Repellendus perspiciatis officia,
+        voluptate ratione quam nemo quod aut maiores animi nostrum, in labore
+        adipisci ullam suscipit esse vel odit tenetur dicta. Lorem ipsum dolor,
+        sit amet consectetur adipisicing elit. Impedit dolorem tempora
+        laboriosam asperiores eum dignissimos accusantium voluptate eligendi
+        beatae vel quis rerum error dolore cum incidunt pariatur accusamus,
+        illum consequuntur?
+      </AccordionItem>)
+  }
+}`,...(B=(k=d.parameters)==null?void 0:k.docs)==null?void 0:B.source}}};var R,M,N;p.parameters={...p.parameters,docs:{...(R=p.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {},
+  tags: ['!dev', '!autodocs'],
+  parameters: {
+    chromatic: {
+      disableSnapshot: true
+    }
+  },
+  render: () => <Accordion>
+      <AccordionItem title='AccordionItem with dynamic content'>
+        Knowledge is knowing a tomato is a fruit; wisdom is not putting it in a
+        fruit salad.
+        <ExpandableStuff />
+      </AccordionItem>
+      <AccordionItem title='Another AccordionItem'>
+        More text about another subject...
+      </AccordionItem>
+    </Accordion>,
+  play: async ({
+    canvas
+  }) => {
+    await userEvent.click(canvas.getByRole('button', {
+      name: 'AccordionItem with dynamic content'
+    }));
+    await userEvent.click(canvas.getByTestId('btn-0'));
+    await expect(canvas.getByTestId('hidden-content')).toBeVisible();
+  }
+}`,...(N=(M=p.parameters)==null?void 0:M.docs)==null?void 0:N.source}}};var V,K,H;m.parameters={...m.parameters,docs:{...(V=m.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  tags: ['!dev', '!autodocs'],
+  parameters: {
+    chromatic: {
+      disableSnapshot: true
+    }
+  },
+  render: () => <Accordion>
+      <AccordionItem title='Test'>
+        <Button isDisabled>Test</Button>
+      </AccordionItem>
+    </Accordion>,
+  play: async ({
+    canvas,
+    step,
+    globals: {
+      scheme
+    }
+  }) => {
+    await step('it should not have the disabled style even if it contains disabled children', async () => {
+      await expect(canvas.getByRole('heading', {
+        level: 2,
+        name: 'Test'
+      })).toHaveStyle({
+        color: lightDark(hexToRgb('#171717'), hexToRgb('#f2f2f2'), scheme)
+      });
+    });
+  }
+}`,...(H=(K=m.parameters)==null?void 0:K.docs)==null?void 0:H.source}}};var P,z,O;u.parameters={...u.parameters,docs:{...(P=u.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    variant: 'contained',
+    children: ITEMS.map((item, i) => <AccordionItem id={item} key={item} title={'En öppningsbar panel ' + item.toLocaleLowerCase()} type={i === 2 ? 'success' : i === 3 ? 'warning' : undefined}>
+        Innehåll i öppningsbar panel {item} Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Repellendus perspiciatis officia,
+        voluptate ratione quam nemo quod aut maiores animi nostrum, in labore
+        adipisci ullam suscipit esse vel odit tenetur dicta. Lorem ipsum dolor,
+        sit amet consectetur adipisicing elit. Impedit dolorem tempora
+        laboriosam asperiores eum dignissimos accusantium voluptate eligendi
+        beatae vel quis rerum error dolore cum incidunt pariatur accusamus,
+        illum consequuntur?
+      </AccordionItem>)
+  }
+}`,...(O=(z=u.parameters)==null?void 0:z.docs)==null?void 0:O.source}}};const Be=["Default","Contained","AllowsMultipleExpanded","DefaultExpandedKeys","CustomTriggerElements","DynamicContent","DS1060","AccordionHasBackground"];export{u as AccordionHasBackground,l as AllowsMultipleExpanded,o as Contained,d as CustomTriggerElements,m as DS1060,i as Default,c as DefaultExpandedKeys,p as DynamicContent,Be as __namedExportsOrder,ke as default};
