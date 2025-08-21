ae80d573a367f62e9edfb1b5a13de877a0b56005
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-08-21 19:27:43 +0000

    Deploy preview for PR 752 🛫

---
+++ pr-preview/pr-752/storybook/assets/RangeCalendar.stories-C4ttFa8J.js
@@ -0,0 +1,70 @@
+import{R as A}from"./RangeCalendar-D6-VNnpG.js";import{u as t,e as i}from"./index-Q7OVwvWC.js";import{m as e}from"./storybook-DLHntVpb.js";import"./jsx-runtime-BYYWji4R.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./CalendarHeader-DLsIlU05.js";import"./Button-v_l_ujRb.js";import"./utils-RHx87K0F.js";import"./SSRProvider-BhYbDCf7.js";import"./clsx-B-dksMZM.js";import"./Hidden-BC9qER92.js";import"./useFocusRing-zhkOjX4v.js";import"./_class_private_field_init-BOtJyCjG.js";import"./index-DQEgcCId.js";import"./useLabels-FEgier_d.js";import"./context-zNjZoWML.js";import"./useButton-DQcVpLGQ.js";import"./RSPContexts-HQcWPJyv.js";import"./Text-CZ6zcozL.js";import"./useDateFormatter-DzWwJ2YB.js";import"./CalendarDate-Lfx2W0H2.js";import"./useControlledState-H1tiOFpU.js";import"./useLocalizedStringFormatter-DiUIqqhg.js";import"./LocalizedStringFormatter-BB0Nk989.js";import"./useUpdateEffect-BzF61qSB.js";import"./useEvent-D_ha2BWP.js";import"./getScrollParent-CvodOSaS.js";import"./scrollIntoView-Dsrcj58z.js";import"./useDescription-BqzP9r9Z.js";import"./VisuallyHidden-BekSNXsj.js";import"./Button-BuQMu1l2.js";import"./Button.module-Bmb634uG.js";import"./createLucideIcon-uZw3gxGR.js";import"./Heading-CV1Pcj_U.js";import"./Text-BgvnLaBz.js";const it={component:A,title:"Components/Calendar/RangeCalendar",tags:["autodocs"]},a={},r={args:{defaultValue:{start:e.subtract({days:2}),end:e.add({days:2})}}},o={args:{isDisabled:!0},parameters:{a11y:{element:"body",config:{rules:[{id:"color-contrast",enabled:!1}]},options:{rules:{"color-contrast":{enabled:!1}}}}}},s={tags:["!dev","!autodocs"],parameters:{chromatic:{disableSnapshot:!0}},play:async({canvas:n,step:S})=>{await S("it should be possible to select today and two days ahead with the keyboard",async()=>{const h=e.day.toString(),k=e.add({days:1}).day.toString(),R=e.add({days:2}).day.toString();await t.tab(),await t.tab(),await t.tab(),await t.keyboard("[Space]"),await t.keyboard("[ArrowRight]"),await i(n.getByRole("gridcell",{name:h})).toHaveAttribute("aria-selected","true"),await i(n.getByRole("gridcell",{name:k})).toHaveAttribute("aria-selected","true"),await i(n.getByRole("gridcell",{name:R})).toHaveAttribute("aria-selected","true")})}};var d,c,m;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:"{}",...(m=(c=a.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var p,l,u;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    defaultValue: {
+      start: mockedNow.subtract({
+        days: 2
+      }),
+      end: mockedNow.add({
+        days: 2
+      })
+    }
+  }
+}`,...(u=(l=r.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var y,b,g;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  },
+  parameters: {
+    a11y: {
+      element: 'body',
+      config: {
+        rules: [{
+          // Dont check for color contrast on disabled elements
+          id: 'color-contrast',
+          enabled: false
+        }]
+      },
+      options: {
+        rules: {
+          'color-contrast': {
+            enabled: false
+          }
+        }
+      } satisfies RunOptions
+    }
+  }
+}`,...(g=(b=o.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var w,f,v;s.parameters={...s.parameters,docs:{...(w=s.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  tags: ['!dev', '!autodocs'],
+  parameters: {
+    chromatic: {
+      disableSnapshot: true
+    }
+  },
+  play: async ({
+    canvas,
+    step
+  }) => {
+    await step('it should be possible to select today and two days ahead with the keyboard', async () => {
+      const todaysDate = mockedNow.day.toString();
+      const tomorrow = mockedNow.add({
+        days: 1
+      }).day.toString();
+      const dayAfterTomorrow = mockedNow.add({
+        days: 2
+      }).day.toString();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.tab();
+      await userEvent.keyboard('[Space]');
+      await userEvent.keyboard('[ArrowRight]');
+      await expect(canvas.getByRole('gridcell', {
+        name: todaysDate
+      })).toHaveAttribute('aria-selected', 'true');
+      await expect(canvas.getByRole('gridcell', {
+        name: tomorrow
+      })).toHaveAttribute('aria-selected', 'true');
+      await expect(canvas.getByRole('gridcell', {
+        name: dayAfterTomorrow
+      })).toHaveAttribute('aria-selected', 'true');
+    });
+  }
+}`,...(v=(f=s.parameters)==null?void 0:f.docs)==null?void 0:v.source}}};const dt=["Primary","SelectedDates","Disabled","KeyboardTest"];export{o as Disabled,s as KeyboardTest,a as Primary,r as SelectedDates,dt as __namedExportsOrder,it as default};
