432a2aab16d3d31fcf998b7fcbd10fef35089d66
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-08-25 11:59:57 +0000

    Deploying to gh-pages from @ migrationsverket/midas@f50ee50a5b061e63f11be48657a6440823b24208 🚀

--- dev-docs/storybook/theme/tokens.d.ts
+++ dev-docs/storybook/theme/tokens.d.ts
@@ -45,6 +45,7 @@ export declare const baseColors: {
     signalGreen30: string;
     signalGreen100: string;
     signalYellow10: string;
+    signalYellow20: string;
     signalYellow100: string;
     signalRed10: string;
     signalRed20: string;
@@ -69,16 +70,16 @@ export declare const baseColors: {
 };
 export declare const typography: {
     fontFamily: string;
-    lineHeight10: number;
-    lineHeight20: number;
-    lineHeight30: number;
-    lineHeight40: number;
-    lineHeight50: number;
-    lineHeight60: number;
-    lineHeight70: number;
-    lineHeight80: number;
-    lineHeight90: number;
-    lineHeight100: number;
+    lineHeight10: string;
+    lineHeight20: string;
+    lineHeight30: string;
+    lineHeight40: string;
+    lineHeight50: string;
+    lineHeight60: string;
+    lineHeight70: string;
+    lineHeight80: string;
+    lineHeight90: string;
+    lineHeight100: string;
     fontSize10: string;
     fontSize20: string;
     fontSize30: string;
@@ -115,6 +116,7 @@ export declare const spacing: {
 };
 export declare const states: {
     focus: string;
+    focusInset: string;
 };
 export declare const transitions: {
     slow: string;
@@ -215,6 +217,8 @@ export declare const semantic: {
     buttonBackgroundDangerActive: string;
     buttonBackgroundDisabled: string;
     buttonBorderSecondary: string;
+    buttonBackgroundIconHover: string;
+    buttonBackgroundIconActive: string;
     badgeBackground: string;
     calendarDateBackgroundHover: string;
     calendarDateBackgroundSelected: string;
@@ -232,4 +236,25 @@ export declare const zIndex: {
     modal: number;
     toast: number;
     skipToContent: number;
+};
+export declare const sizing: {
+    '00': string;
+    '05': string;
+    '10': string;
+    '15': string;
+    '20': string;
+    '30': string;
+    '40': string;
+    '50': string;
+    '60': string;
+    '70': string;
+    '75': string;
+    '80': string;
+    '90': string;
+    '100': string;
+    '110': string;
+    '120': string;
+    '130': string;
+    '140': string;
+    '150': string;
 };
