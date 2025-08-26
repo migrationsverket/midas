631d8dad4a9bf1733e773278ee79d1b1246883b4
derpbravely <derpbravely@users.noreply.github.com>
2025-08-26 12:46:06 +0000

    Remove preview for PR 760 🛬

--- pr-preview/pr-760/storybook/checkbox/CheckboxGroupItem.d.ts
+++
@@ -1,3 +0,0 @@
-import { CheckboxGroupItemProps } from './types';
-import * as React from 'react';
-export declare const CheckBoxGroupItem: React.ForwardRefExoticComponent<CheckboxGroupItemProps & React.RefAttributes<HTMLLabelElement>>;
