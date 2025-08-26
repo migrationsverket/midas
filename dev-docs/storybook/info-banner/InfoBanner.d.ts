cb2d64631ab36a82ffd57edb670144d02a50b967
derpbravely <197733689+derpbravely@users.noreply.github.com>
2025-08-26 12:48:48 +0000

    Deploying to gh-pages from @ migrationsverket/midas@3fe21636ea7c27f6b4041e39eaf614858a151c76 🚀

--- dev-docs/storybook/info-banner/InfoBanner.d.ts
+++ dev-docs/storybook/info-banner/InfoBanner.d.ts
@@ -5,16 +5,28 @@ export interface InfoBannerProps extends React.DetailedHTMLProps<React.HTMLAttri
      * Determines the visual style and semantic meaning of the InfoBanner (e.g., success, info, warning, important).
      **/
     type: FeedbackStatus;
-    /** Specify the title */
+    /** The title of the banner. */
     title?: string;
-    /** Specify the message. Element or string */
+    /** The message to be displayed in the banner. Can be a string or a React node. */
     message?: string | React.ReactNode;
-    /** Additional elements displayed inside the banner */
+    /** Additional elements to be displayed inside the banner. */
     children?: React.ReactNode;
     /**
-     *  Specify if the InfoBanner should have a dismiss button in the top right corner
+     * If true, a dismiss button will be displayed in the top right corner.
      */
     isDismissable?: boolean;
+    /**
+     * The initial visibility of the banner when it is uncontrolled.
+     */
+    defaultOpen?: boolean;
+    /**
+     * Controls the visibility of the banner when it is controlled.
+     */
+    isOpen?: boolean;
+    /**
+     * Callback fired when the visibility of the banner changes.
+     */
+    onOpenChange?: (isOpen: boolean) => void;
 }
 /**
  * Displays a static message as an inline banner
