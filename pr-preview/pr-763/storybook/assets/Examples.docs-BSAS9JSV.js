16e04be555d1c6369f142978bc7088841b6dac4e
derpbravely <derpbravely@users.noreply.github.com>
2025-08-27 08:34:48 +0000

    Remove preview for PR 763 🛬

--- pr-preview/pr-763/storybook/assets/Examples.docs-BSAS9JSV.js
+++
@@ -1,3 +0,0 @@
-import{j as t}from"./jsx-runtime-BYYWji4R.js";import{useMDXComponents as r}from"./index-DUy19JZU.js";import{M as m}from"./index-KLfbbrFA.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-CC1RhaTv.js";import"./index-DQEgcCId.js";import"./index-CBft-ReK.js";import"./index-DrFu-skq.js";function o(e){const n={h1:"h1",p:"p",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{title:"Examples/Intro"}),`
-`,t.jsx(n.h1,{id:"exempel-från-teamet",children:"Exempel från teamet"}),`
-`,t.jsx(n.p,{children:"Här samlar vi lite olika exempel på mönster som ofta återkommer...."})]})}function u(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{u as default};
