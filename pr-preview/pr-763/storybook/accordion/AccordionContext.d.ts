031c388a5a7610df13292b8de4ad4aff550bb5eb
derpbravely <derpbravely@users.noreply.github.com>
2025-08-27 07:36:01 +0000

    Deploy preview for PR 763 🛫

---
+++ pr-preview/pr-763/storybook/accordion/AccordionContext.d.ts
@@ -0,0 +1,4 @@
+export interface AccordionContextValue {
+    isContained: boolean;
+}
+export declare const AccordionContext: import('../../../../node_modules/react').Context<AccordionContextValue | undefined>;
---
+++ pr-preview/pr-763/storybook/checkbox/CheckboxGroupItem.d.ts
@@ -0,0 +1,3 @@
+import { CheckboxGroupItemProps } from './types';
+import * as React from 'react';
+export declare const CheckBoxGroupItem: React.ForwardRefExoticComponent<CheckboxGroupItemProps & React.RefAttributes<HTMLLabelElement>>;
