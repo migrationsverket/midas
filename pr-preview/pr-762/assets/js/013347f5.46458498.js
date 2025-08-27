ba9c7ea6a36356452423014ed42b6a9f3e9f152b
derpbravely <derpbravely@users.noreply.github.com>
2025-08-27 07:14:57 +0000

    Deploy preview for PR 762 🛫

---
+++ pr-preview/pr-762/assets/js/013347f5.46458498.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[2776],{
+
+/***/ 4496:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"tag":{"label":"breaking-change","permalink":"/pr-preview/pr-762/release-notes/tags/breaking-change","allTagsPath":"/pr-preview/pr-762/release-notes/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/pr-preview/pr-762/release-notes/tags/breaking-change","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
---
+++ pr-preview/pr-762/storybook/list-box/ListBoxItem.d.ts
@@ -0,0 +1,3 @@
+import { ListBoxItemProps } from 'react-aria-components';
+import { ListBoxItemElement } from './types';
+export declare const ListBoxItem: <T extends ListBoxItemElement>({ children, className, ...rest }: ListBoxItemProps<T>) => import("react/jsx-runtime").JSX.Element;
