a5d4058b2ab28c4c055068163928f9ab65c67f79
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-08-27 09:17:17 +0000

    Deploying to gh-pages from @ migrationsverket/midas@608d1b35f692c58f4f76a72e1c8e53caa2f8c0fe 🚀

---
+++ dev-docs/storybook/assets/Spinner.stories-CZ7_21k4.js
@@ -0,0 +1,29 @@
+import{j as a}from"./jsx-runtime-BYYWji4R.js";import{c as l}from"./clsx-B-dksMZM.js";import{c as S}from"./createLucideIcon-uZw3gxGR.js";import{$ as v}from"./VisuallyHidden-BekSNXsj.js";import{u as h}from"./useLocalizedStringFormatter-9M_dEwOo.js";import{e as w}from"./index-Q7OVwvWC.js";import{B as C}from"./Button-DdEN9KJL.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./utils-RHx87K0F.js";import"./SSRProvider-BhYbDCf7.js";import"./useFocusRing-zhkOjX4v.js";import"./_class_private_field_init-BOtJyCjG.js";import"./index-DQEgcCId.js";import"./LocalizedStringFormatter-BB0Nk989.js";import"./context-zNjZoWML.js";import"./Button.module-DKVuWS4g.js";import"./Button-v_l_ujRb.js";import"./Hidden-BC9qER92.js";import"./useLabels-FEgier_d.js";import"./useButton-DQcVpLGQ.js";/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],B=S("loader-circle",k),j="_container_3nw95_4",L="_spinner_3nw95_8",O="_dark_3nw95_18",n={container:j,spinner:L,dark:O},P={loadingPleaseWait:"Loading, please wait..."},W={loadingPleaseWait:"Laddar, vänligen vänta..."},b={en:P,sv:W},o=({small:e,isOnColor:i=!1,className:c})=>{const N=h(b);return a.jsxs("div",{className:l(n.container,c),role:"status",children:[a.jsx(B,{className:l(n.spinner,i&&n.dark),size:e?16:32,strokeWidth:e?2:3,absoluteStrokeWidth:!0}),a.jsx(v,{children:N.format("loadingPleaseWait")})]})};try{o.displayName="Spinner",o.__docgenInfo={description:"",displayName:"Spinner",props:{small:{defaultValue:{value:"false"},description:"Displays a smaller spinner when set to true",name:"small",required:!1,type:{name:"boolean"}},isOnColor:{defaultValue:{value:"false"},description:"For use on coloured background",name:"isOnColor",required:!1,type:{name:"boolean"}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}}}}}catch{}const ee={component:o,title:"Components/Spinner",tags:["autodocs"],parameters:{layout:"centered"},args:{small:!1,isOnColor:!1,className:"test-class"}},s={play:async({canvas:e,step:i,args:c})=>{await i("it should accept custom classNames",async()=>{await w(e.getByRole("status")).toHaveClass(n.container,c.className)})}},r={args:{small:!0}},t={args:{small:!0,isOnColor:!0},render:e=>a.jsxs(C,{children:[a.jsx(o,{...e}),"Laddar"]})};var m,p,d;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  play: async ({
+    canvas,
+    step,
+    args
+  }) => {
+    await step('it should accept custom classNames', async () => {
+      await expect(canvas.getByRole('status')).toHaveClass(styles.container, args.className as string);
+    });
+  }
+}`,...(d=(p=s.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var u,g,f;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    small: true
+  }
+}`,...(f=(g=r.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};var y,_,x;t.parameters={...t.parameters,docs:{...(y=t.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    small: true,
+    isOnColor: true
+  },
+  render: args => <Button>
+      <Spinner {...args} />
+      Laddar
+    </Button>
+}`,...(x=(_=t.parameters)==null?void 0:_.docs)==null?void 0:x.source}}};const ae=["Primary","Small","InButton"];export{t as InButton,s as Primary,r as Small,ae as __namedExportsOrder,ee as default};
