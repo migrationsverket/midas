3246fab96d2cd57803f1606a780d106143cb16b0
derpbravely <derpbravely@users.noreply.github.com>
2025-08-29 09:11:59 +0000

    Remove preview for PR 769 🛬

--- pr-preview/pr-769/storybook/assets/index-BZJU4NaV.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-tGYExsMk.js";import{r as o}from"./index-DCig4GnH.js";var t=o();const m=r(t);export{m as P,t as r};
--- pr-preview/pr-769/storybook/file-upload/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { FileTrigger, DropZone } from './FileUpload';
