bd641ff5c265ef4e46f18d3402e9405040b7c82a
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-09-03 19:27:47 +0200

    docs: add visual color representation of tokens

---
+++ apps/docs/src/components/tokens/ColorSpectrum.tsx
@@ -0,0 +1,49 @@
+import {tokens} from '@midas-ds/components/theme'
+export const ColorSpectrum = () => {
+  const filteredColors = Object.entries(tokens.color).filter(
+    ([colorName]) => !["black", "white", "blackHover", "whiteHover", "purple", "red"].includes(colorName)
+  );
+
+  const signalColors = filteredColors.filter(([colorName]) => colorName.startsWith('signal') && colorName !== 'signalRed');
+  const nonSignalColors = filteredColors.filter(([colorName]) => !colorName.startsWith('signal') || colorName === 'signalRed');
+
+  return (
+    <div style={{ display: 'flex', flexFlow: 'row wrap', marginBottom: '2rem' }}>
+      {nonSignalColors.map(([colorName, colorShades]) => (
+        <div key={colorName} style={{ flexBasis: '100%' }}>
+          <div style={{ display: 'flex', flexFlow: 'row wrap' }}>
+            {Object.entries(colorShades).map(([shadeName, shadeValue]) => (
+              <div
+                key={shadeName}
+                style={{
+                  backgroundColor: shadeValue.$value,
+                  width: 60,
+                  height: 60,
+                }}
+              />
+            ))}
+          </div>
+        </div>
+      ))}
+      <div style={{ flexBasis: '100%' }}>
+        <div style={{ display: 'flex', flexFlow: 'row wrap' }}>
+          {signalColors.map(([colorName, colorShades]) => (
+            <>
+              {Object.entries(colorShades).map(([shadeName, shadeValue]) => (
+                <div
+                  key={shadeName}
+                  style={{
+                    backgroundColor: shadeValue.$value,
+                    width: 60,
+                    height: 60,
+                  }}
+                />
+              ))}
+            </>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
---
+++ apps/docs/src/components/tokens/Layers.tsx
@@ -0,0 +1,39 @@
+import {Grid, Text, GridItem } from '@midas-ds/components'
+import { tokens, flat } from '@midas-ds/components/theme'
+
+export const Layers = () => {
+  return (
+    <Grid
+      className='card-guidelines'
+    >
+      <GridItem
+        size={6}
+        style={{ colorScheme: 'light', border: '1px solid var(--ifm-color-emphasis-200)' }}
+      >
+        <div style={{ backgroundColor: tokens.background['@'].$value, padding: '1rem' }}>
+          <Text>background</Text>
+          <div style={{ backgroundColor: flat.layer01Base, padding: '1rem', margin: '1rem 0 0 0' }}>
+            <Text>layer01</Text>
+            <div style={{ backgroundColor: tokens.layer['02'].base.$value, padding: '1rem', margin: '1rem 0 0 0' }}>
+              <Text>layer02</Text>
+            </div>
+          </div>
+        </div>
+      </GridItem>
+      <GridItem
+        size={6}
+        style={{ colorScheme: 'dark' }}
+      >
+        <div style={{ backgroundColor: tokens.background['@'].$value, padding: '1rem' }}>
+          <Text>background</Text>
+          <div style={{ backgroundColor: tokens.layer['01'].base.$value, padding: '1rem', margin: '1rem 0 0 0' }}>
+            <Text>layer01</Text>
+            <div style={{ backgroundColor: tokens.layer['02'].base.$value, padding: '1rem', margin: '1rem 0 0 0' }}>
+              <Text>layer02</Text>
+            </div>
+          </div>
+        </div>
+      </GridItem>
+    </Grid>
+  )
+}
