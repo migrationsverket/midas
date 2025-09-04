ded9bfa8c792ed0e5989b6920443dd210db3b5d8
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-09-04 10:50:01 +0200

    docs: update tokens page with headers and remove tokens from list that are only refs

--- packages/components/src/theme/index.ts
+++ packages/components/src/theme/index.ts
@@ -3,7 +3,8 @@ import tokens from  './generated-object'
 import * as gen2 from './generated-tokens'
 export { tokens }
 
-export const flat = gen2
+export const flat = gen2 // TODO: remove after refactor
+export const variables = gen2
 
 export * from './tokens'
-export const theme = { ...tokens_legacy, ...flat }
+export const theme = { ...tokens_legacy, ...flat, ...variables }
