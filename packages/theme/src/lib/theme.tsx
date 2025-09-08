81e8ffb98ff4f48dd0a7003d49b38cfbd8d52ab7
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-09-08 11:05:49 +0200

    chore: add boilerplate for new theme package

---
+++ packages/theme/src/lib/theme.tsx
@@ -0,0 +1,11 @@
+import styles from './theme.module.css'
+
+export function Theme() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Theme!</h1>
+    </div>
+  )
+}
+
+export default Theme
