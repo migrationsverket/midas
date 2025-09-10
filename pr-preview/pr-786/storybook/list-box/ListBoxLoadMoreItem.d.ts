4230734ed0b443d0ffa3234e5d4da59e793f1fd4
derpbravely <derpbravely@users.noreply.github.com>
2025-09-10 05:50:43 +0000

    Remove preview for PR 786 🛬

--- pr-preview/pr-786/storybook/list-box/ListBoxLoadMoreItem.d.ts
+++
@@ -1,4 +0,0 @@
-import { ListBoxLoadMoreItemProps } from 'react-aria-components';
-import * as React from 'react';
-export type { ListBoxLoadMoreItemProps };
-export declare const ListBoxLoadMoreItem: React.FC<ListBoxLoadMoreItemProps>;
