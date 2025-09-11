ca07684b880f01c76a2824c27f8b678a13da3b1c
derpbravely <197733689+derpbravely@users.noreply.github.com>
2025-09-11 11:49:19 +0200

    chore(theme): streamline build config

--- packages/theme/vite.config.ts
+++ packages/theme/vite.config.ts
@@ -1,68 +1,32 @@
-/// <reference types='vitest' />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
-import * as path from 'path'
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin'
-import { extname, relative, resolve } from 'node:path'
-import { globSync } from 'glob'
-import { fileURLToPath } from 'node:url'
-const src = resolve(__dirname, 'src')
-const index = resolve(src, 'index.ts')
+import dts from 'vite-plugin-dts'
+import type { UserConfig } from 'vite'
 
-export default defineConfig(() => ({
+export default {
   root: __dirname,
   cacheDir: '../../node_modules/.vite/packages/theme',
   plugins: [
-    react(),
     nxViteTsPaths(),
-    nxCopyAssetsPlugin(['*.md']),
     dts({
       entryRoot: 'src',
-      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+      tsconfigPath: 'tsconfig.lib.json',
       pathsToAliases: false,
     }),
   ],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
     outDir: '../../dist/packages/theme',
     cssCodeSplit: true,
     emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
     lib: {
-      // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/lib/index.ts',
-      name: 'theme',
-      fileName: 'index',
+      entry: [
+        'src/lib/index.ts',
+        'src/lib/style-dictionary-dist/variables.css',
+      ],
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
-      formats: ['es' as const],
-    },
-    rollupOptions: {
-      // External packages that should not be bundled into your library.
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
-      input: Object.fromEntries([
-        ['index', index],
-        [
-          'variables.css',
-          resolve(src, 'lib/style-dictionary-dist/variables.css'),
-        ],
-      ]),
-      output: [
-        {
-          assetFileNames: 'assets/[name][extname]',
-          entryFileNames: '[name].js',
-        },
-      ],
+      formats: ['es'],
     },
   },
-}))
+} satisfies UserConfig
