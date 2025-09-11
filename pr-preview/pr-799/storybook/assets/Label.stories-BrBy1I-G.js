5a8bfdf110dd5c3cc0e423eb04e44feecd474232
derpbravely <derpbravely@users.noreply.github.com>
2025-09-11 12:38:50 +0000

    Deploy preview for PR 799 🛫

---
+++ pr-preview/pr-799/storybook/assets/Label.stories-BrBy1I-G.js
@@ -0,0 +1,17 @@
+import{L as e}from"./Label-YEmhP7ci.js";import"./iframe-BdgLdgYE.js";import"./preload-helper-Dp1pzeXC.js";import"./clsx-B-dksMZM.js";import"./Button-WyhW3_uk.js";import"./utils-BvcxwFyW.js";import"./Hidden-CBixdTTx.js";import"./useFocusRing-CTNShohj.js";import"./index-bdnO63b_.js";import"./index-BK0Ncq4b.js";import"./useLabels-CvTKfdWu.js";import"./useButton-D5My0K8O.js";import"./Dialog-DgWq9cLy.js";import"./RSPContexts-Dc_gSHep.js";import"./OverlayArrow-BtUkbWER.js";import"./useResizeObserver-YhNeHsC-.js";import"./useControlledState-CnQUdn_q.js";import"./Collection-BL_MIQ9r.js";import"./index-CkIM88CH.js";import"./ListKeyboardDelegate-CPp65dVC.js";import"./SelectionManager-Ny1Sem7f.js";import"./useEvent-CC6p_nb1.js";import"./scrollIntoView-Dsrcj58z.js";import"./FocusScope-7ZFiyo7C.js";import"./useDescription-BLhSGx0Q.js";import"./Separator-C2Cu4Kwo.js";import"./Text-l-A5Z5Hz.js";import"./PressResponder-C9ap61WA.js";import"./useLocalizedStringFormatter-ILYc6jSb.js";import"./LocalizedStringFormatter-BB0Nk989.js";import"./getScrollParent-CvodOSaS.js";import"./VisuallyHidden-CbJqQ_pY.js";import"./Dialog-B7Y2zPU9.js";import"./useLocalizedStringFormatter-e74m6xoj.js";import"./Button-DoG7b90P.js";import"./Button.module-DRhvPh0f.js";import"./x-BRdayo4W.js";import"./createLucideIcon-C7t1_r_C.js";import"./Heading-Dpx6Lkvg.js";import"./info-DLkcqjGU.js";const{expect:a}=__STORYBOOK_MODULE_TEST__,Q={component:e,title:"Components/Typography/Label",tags:["autodocs"]},t={args:{children:"I am a label"},play:async({canvas:i,args:{children:m}})=>{a(i.getByText(m)).toHaveStyle({"font-size":"16px","line-height":"20px","font-weight":500})}};var o,r,p;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'I am a label'
+  },
+  play: async ({
+    canvas,
+    args: {
+      children
+    }
+  }) => {
+    expect(canvas.getByText(children as string)).toHaveStyle({
+      'font-size': '16px',
+      'line-height': '20px',
+      'font-weight': 500
+    });
+  }
+}`,...(p=(r=t.parameters)==null?void 0:r.docs)==null?void 0:p.source}}};const V=["_Label"];export{t as _Label,V as __namedExportsOrder,Q as default};
