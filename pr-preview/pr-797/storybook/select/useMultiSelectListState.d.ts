9e0574098c079c4fba9938dbc5cf69e9238c1ac1
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-09-11 09:06:01 +0000

    Remove preview for PR 797 🛬

--- pr-preview/pr-797/storybook/select/useMultiSelectListState.d.ts
+++
@@ -1,3 +0,0 @@
-import { MultiSelectListProps, MultiSelectListState } from './types';
-import { ListBoxOption } from '../list-box';
-export declare function useMultiSelectListState<T extends ListBoxOption>(props: MultiSelectListProps<T>): MultiSelectListState<T>;
