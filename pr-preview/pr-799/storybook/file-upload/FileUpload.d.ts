016fcbd06d81b13f9cf63747ae874ee6b64b740a
derpbravely <derpbravely@users.noreply.github.com>
2025-09-12 07:48:36 +0000

    Remove preview for PR 799 🛬

--- pr-preview/pr-799/storybook/file-upload/FileUpload.d.ts
+++
@@ -1,4 +0,0 @@
-import { DropZoneProps, FileTriggerProps } from 'react-aria-components';
-import * as React from 'react';
-export declare const DropZone: React.FC<DropZoneProps>;
-export declare const FileTrigger: React.FC<FileTriggerProps>;
