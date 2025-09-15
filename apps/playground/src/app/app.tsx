787e74f234d7b84caf5bfb593ff310e85f33a16b
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-09-15 21:03:53 +0200

    chore: fix errors in playground app, disable for future builds
    Remove target build from playground app since it's likely to block future builds and is completely unrelated to the functionality of the component lib and other packages.
    `nx affected -t build` now works as expected.

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -1,25 +1,6 @@
-import { Tab, TabList, Tabs, TabPanel, Text } from '@midas-ds/components'
-
 export default function App() {
   return (
     <>
-      <Tabs tabs={['Bananas', 'Apples']}>
-        <Text>Old information about Bananas</Text>
-        <Text>Old information about Apples</Text>
-      </Tabs>
-
-      <Tabs>
-        <TabList>
-          <Tab id='bananas'>Bananas</Tab>
-          <Tab id='apples'>Apples</Tab>
-        </TabList>
-        <TabPanel id='bananas'>
-          <Text>New declarative style information about Bananas</Text>
-        </TabPanel>
-        <TabPanel id='apples'>
-          <Text>New declarative style information about Apples</Text>
-        </TabPanel>
-      </Tabs>
     </>
   )
 }
--- packages/theme/src/lib/index.ts
+++ packages/theme/src/lib/index.ts
@@ -1,6 +1,6 @@
 import tokenDictionary from './style-dictionary-dist/token-dictionary'
 import * as variables from './style-dictionary-dist/variables'
-import './global.css'
+
 
 export * from './style-dictionary-dist/token-dictionary'
 export { tokenDictionary, variables }
