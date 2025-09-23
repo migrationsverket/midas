a02021fa109c1ad2107c85c56bbb697e11e2e922
derpbravely <derpbravely@users.noreply.github.com>
2025-09-23 05:52:24 +0000

    Remove preview for PR 812 🛬

--- pr-preview/pr-812/storybook/assets/Components.docs-DHr5hYYn.js
+++
@@ -1,3 +0,0 @@
-import{j as n}from"./iframe-C3NgjWPG.js";import{useMDXComponents as r}from"./index-DJNyYRXN.js";import{M as s}from"./blocks-DfIP1NJY.js";import"./preload-helper-Dp1pzeXC.js";import"./index-BGktG_m9.js";import"./index-ChLPNIiH.js";function o(e){const t={h1:"h1",p:"p",...r(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Components/Intro"}),`
-`,n.jsx(t.h1,{id:"komponenter",children:"Komponenter"}),`
-`,n.jsx(t.p,{children:"Här listas exempel på hur du kan använda vardera komponent. Props, kodexempel se hur de beter sig responsivt m.m."})]})}function x(e={}){const{wrapper:t}={...r(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{x as default};
