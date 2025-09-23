e8d089bc875f9193ef2fb5f6f05ace34222b1dd0
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-09-23 13:39:22 +0200

    fix(table-styles): update vite config with readme and tspaths

--- packages/table-styles/vite.config.ts
+++ packages/table-styles/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import * as path from 'path'
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin'
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
 
 export default defineConfig(() => ({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/packages/table-styles',
+  plugins: [
+    nxViteTsPaths(),
+    nxCopyAssetsPlugin(['*.md']),
+  ],
   build: {
     outDir: '../../dist/packages/table-styles',
     emptyOutDir: true,
