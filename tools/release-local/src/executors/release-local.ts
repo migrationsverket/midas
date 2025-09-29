8d23f37588297fd74a2104f6aa6c1beee1694143
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-09-29 08:26:02 +0200

    chore(release-local): cleanup release-local executor

--- tools/release-local/src/executors/release-local.ts
+++ tools/release-local/src/executors/release-local.ts
@@ -1,31 +1,20 @@
 import { logger, PromiseExecutor } from '@nx/devkit'
 import { ReleaseLocalExecutorSchema } from './schema'
-import { startLocalRegistry } from '@nx/js/plugins/jest/local-registry';
-import {releaseVersion, releasePublish} from 'nx/release'
+import { releaseVersion, releasePublish } from 'nx/release'
+import { readFileSync, promises as fs } from 'fs'
+import path = require('path')
+import { execSync } from 'node:child_process'
 
-const runExecutor: PromiseExecutor<
-  ReleaseLocalExecutorSchema
-> = async (options, context) => {
-
-  const registry = options.registry ?? 'http://localhost:4873/';
-
-  const localRegistryTarget = `${context.projectName}:local-registry`// '@midas-ds/source:local-registry';
-  // storage folder for the local registry
-  const storage = './tmp/local-registry/storage';
-  logger.info(`🔄 Starting local Verdaccio registry on ${registry}`);
-
-  // 1. Start Verdaccio programmatically
-/*  global.stopVerdaccio = await startLocalRegistry({
-    localRegistryTarget,
-    storage,
-    verbose: true,
-    clearStorage: true,
-  });*/
+const runExecutor: PromiseExecutor<ReleaseLocalExecutorSchema> = async (
+  options,
+  context,
+) => {
+  const registry = options.registry ?? 'http://localhost:4873/'
+  // Verdaccio is running in the background with a cleared storage from task `nx run local-registry-clear`
 
   try {
-    // 2. Run release (build + bump dummy versions)
-    logger.info('📦 Running Nx Release API…');
-    const {projectsVersionData} = await releaseVersion({
+    logger.info('📦 Running Nx Release API…')
+    const { projectsVersionData } = await releaseVersion({
       specifier: 'prerelease',
       preid: 'local',
       dryRun: false,
@@ -34,35 +23,70 @@ const runExecutor: PromiseExecutor<
       stageChanges: false,
       firstRelease: true,
       gitPush: false,
+      verbose: false,
       versionActionsOptionsOverrides: {
         skipLockFileUpdate: true,
-      }
-    });
+      },
+    })
 
-    // 3. Publish to local registry
-    logger.info('🚀 Publishing packages into local Verdaccio…');
+    logger.info('🚀 Publishing packages into local Verdaccio…')
     const publishStatus = await releasePublish({
       registry,
       verbose: true,
       dryRun: false,
-      firstRelease: true
-    });
+      firstRelease: true,
+    })
 
-    logger.info(publishStatus) // check for exit code 1 and throw
+    const erroredProjects = Object.keys(publishStatus)
+      .filter(key => publishStatus[key].code === 1)
+      .join(', ')
+    if (erroredProjects.length > 0) {
+      throw new Error(
+        `Project ${erroredProjects} did not publish successfully.`,
+      )
+    } else {
+      logger.info('🐔 Published successfully.')
+    }
 
-    // 4. Optionally run tests TODO: depending on tests, clear storage or not?
+    // Collect package versions
+    const packageVersions = Object.entries(projectsVersionData).map(
+      ([projectName, { newVersion, currentVersion }]) => {
+        const project = context.projectGraph.nodes[projectName]
+        const packageJson = JSON.parse(
+          readFileSync(
+            path.resolve(context.root, project.data.root, 'package.json'),
+          ).toString(),
+        )
+        return {
+          projectName,
+          newVersion,
+          currentVersion,
+          packageJsonName: packageJson.name,
+          packageJsonNameAndVersion: `${packageJson.name}@${newVersion}`,
+        }
+      },
+    )
 
-    logger.info('✅ Release to local Verdaccio completed');
-    return { success: true };
+    // We don't do this now
+    if (options.runTests) {
+      logger.info(`🚀 Installing published packages..`)
+
+      execSync(
+        `npm install ${packageVersions.map(p => p.packageJsonName).join(' ')} --registry=${registry}`,
+      )
+
+      logger.info(`👑 Running tests`)
+      execSync(`nx test playground`)
+    }
+
+    logger.info('✅ Release to local Verdaccio completed')
+    return { success: true }
   } catch (e: any) {
-    logger.error(`❌ Error: ${e.message}`);
-    return { success: false };
+    logger.error(`❌ Error: ${e.message}`)
+    return { success: false }
   } finally {
-    // 5. Stop Verdaccio cleanly
-    logger.info('🛑 Stopping Verdaccio…');
-    // Stops anyway?
+    logger.info('Finished.')
   }
-
 }
 
 export default runExecutor
