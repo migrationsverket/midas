97442d214dfd891a0c0848dde3b95d5979d492e5
derpbravely <197733689+derpbravely@users.noreply.github.com>
2025-10-02 13:12:19 +0200

    docs: update typography example

--- apps/storybook/src/examples/Typography.stories.tsx
+++ apps/storybook/src/examples/Typography.stories.tsx
@@ -10,13 +10,23 @@ export default meta
 export const Example = {
   render: () => (
     <>
-      <Heading level={1}>Om Migrationsverket</Heading>
+      <Heading
+        enableMargins
+        level={1}
+      >
+        Om Migrationsverket
+      </Heading>
       <Text elementType='p'>
         Migrationsverket ansvarar för att pröva ansökningar från personer som
         vill bosätta sig i Sverige, komma på besök, söka skydd eller bli svenska
         medborgare. Vi är en stor myndighet med medarbetare över hela landet.
       </Text>
-      <Heading level={2}>Det här gör Migrationsverket</Heading>
+      <Heading
+        enableMargins
+        level={2}
+      >
+        Det här gör Migrationsverket
+      </Heading>
       <Text elementType='p'>
         I Migrationsverkets uppdrag ingår, utöver att pröva ansökningar för
         personer som söker sig till Sverige, bland annat att erbjuda boende till
@@ -30,7 +40,12 @@ export const Example = {
         genom lagar och förordningar och ett regleringsbrev som vi får från
         regeringen varje år.
       </Text>
-      <Heading level={3}>Huvudsakliga områden</Heading>
+      <Heading
+        enableMargins
+        level={3}
+      >
+        Huvudsakliga områden
+      </Heading>
       <Text>Arbeta</Text>
     </>
   ),
