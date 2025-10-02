474ff859b495b6cc724f19ac1667a2bc83937c80
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-10-02 11:04:53 +0200

    docs: add storybook info to designer.mdx

---
+++ apps/docs/src/components/ThemedImage.tsx
@@ -0,0 +1,14 @@
+import useBaseUrl from '@docusaurus/useBaseUrl'
+import styles from '@site/src/css/index.module.css'
+
+export function ThemedImage({imageSrc, imageSrcDark}) {
+  const imgSrc = useBaseUrl(imageSrc)
+  const imgSrcDark = useBaseUrl(imageSrcDark)
+
+  return (
+    <>
+      <img className={`${styles.lightImage}`} src={imgSrc} alt="" />
+      <img className={`${styles.darkImage}`} src={imgSrcDark} alt="" />
+    </>
+  )
+}
