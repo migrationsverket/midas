502b9cc81162675994ff06c48ce5d2af26e8b255
derpbravely <197733689+derpbravely@users.noreply.github.com>
2025-10-10 06:07:57 +0000

    Deploying to gh-pages from @ migrationsverket/midas@ca809fdd2525775a72ac345d447aaaad63c1ef20 🚀

---
+++ unreleased/storybook/assets/Label.stories-Bhpmr0-7.js
@@ -0,0 +1,17 @@
+import{L as e}from"./Label-B0c3ID-4.js";import"./iframe-LmtyvVxW.js";import"./preload-helper-Dp1pzeXC.js";import"./clsx-Ciqy0D92.js";import"./clsx-B-dksMZM.js";import"./Button-DWgJTq8v.js";import"./utils-BM2MYLln.js";import"./Hidden-D0nkbJWz.js";import"./useFocusRing-mCS4avcZ.js";import"./index-lUogjaLo.js";import"./index-DziwUmlz.js";import"./useLabels-O7W2F-cZ.js";import"./useButton-pg_MM66x.js";import"./Dialog-Bjmngnno.js";import"./RSPContexts-avIJnElu.js";import"./OverlayArrow-DLKZfN1c.js";import"./useResizeObserver-BOOZTtEv.js";import"./useControlledState-24ogAR4t.js";import"./Collection-BYb0xKHw.js";import"./index-DaJpiOgH.js";import"./context-CoRegpw-.js";import"./SelectionManager-DUK-hnKO.js";import"./useEvent-BK_Pud7O.js";import"./scrollIntoView-Dsrcj58z.js";import"./FocusScope-Dj8n5FoC.js";import"./useDescription-iGPMGeje.js";import"./Separator-CbqL5i0i.js";import"./Text-Dc6Jy17-.js";import"./PressResponder-qncAm94B.js";import"./useLocalizedStringFormatter-CAKM-uXA.js";import"./LocalizedStringFormatter-BB0Nk989.js";import"./getScrollParent-CvodOSaS.js";import"./VisuallyHidden-DobBJpU_.js";import"./useLocalizedStringFormatter-DeJ6PmFl.js";import"./Button-TLzp0gOG.js";import"./Button.module-CcWMkJAG.js";import"./x-37Zys_Lj.js";import"./createLucideIcon-CYD91O0M.js";import"./Heading-PFVb7z1A.js";import"./info-Cbj7OohB.js";import"./Popover-nTNyd-Di.js";const{expect:a}=__STORYBOOK_MODULE_TEST__,V={component:e,title:"Components/Typography/Label",tags:["autodocs"]},t={args:{children:"I am a label"},play:async({canvas:i,args:{children:m}})=>{a(i.getByText(m)).toHaveStyle({"font-size":"16px","line-height":"20px","font-weight":500})}};var o,r,p;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'I am a label'
+  },
+  play: async ({
+    canvas,
+    args: {
+      children
+    }
+  }) => {
+    expect(canvas.getByText(children as string)).toHaveStyle({
+      'font-size': '16px',
+      'line-height': '20px',
+      'font-weight': 500
+    });
+  }
+}`,...(p=(r=t.parameters)==null?void 0:r.docs)==null?void 0:p.source}}};const W=["_Label"];export{t as _Label,W as __namedExportsOrder,V as default};
---
+++ unreleased/storybook/assets/index-lUogjaLo.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-LmtyvVxW.js";import{r as o}from"./index-DziwUmlz.js";var t=o();const m=r(t);export{m as $,t as r};
