f03ef5446fc89f9ce880f5790a3e8300f291ccb2
derpbravely <197733689+derpbravely@users.noreply.github.com>
2025-10-16 13:22:30 +0200

    chore: update playground imports

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -1,83 +1,87 @@
-import { RacItem, RacSelect, Button } from '@midas-ds/components'
-import { Form} from 'react-aria-components'
+import { ListBoxItem, RacSelect, Button } from '@midas-ds/components'
+import { Form } from 'react-aria-components'
 import '@midas-ds/components/default.css'
 
 export default function App() {
   return (
-    <>
-      <div style={{ display: 'flex', gap: '1rem', flexDirection: 'column' }}>
+    <div style={{ display: 'flex', gap: '1rem', flexDirection: 'column' }}>
+      <RacSelect
+        label='Label'
+        description='Description'
+        onChange={x => {
+          console.log('change', x)
+        }}
+      >
+        <ListBoxItem>
+          Very long string that goes on for a long time Very long string that
+          goes on for a long timeVery long string that goes on for a long
+          time{' '}
+        </ListBoxItem>
+        <ListBoxItem>Goodbye</ListBoxItem>
+      </RacSelect>
+      <RacSelect
+        isInvalid
+        selectionMode='multiple'
+        errorMessage='Error message'
+        label='Label'
+        description='Description'
+        onChange={x => {
+          console.log('change', x)
+        }}
+      >
+        <ListBoxItem>Hello</ListBoxItem>
+        <ListBoxItem>Banan</ListBoxItem>
+        <ListBoxItem>Glass</ListBoxItem>
+        <ListBoxItem>Äpple</ListBoxItem>
+        <ListBoxItem>
+          Gemini GEMINI GEMINI GEMINI GEMINI Gemini GEMINI GEMINI GEMINI GEMINI
+          GEMINI GEMINI GEMINI GEMINI Gemini GEMINI GEMINI GEMINI GEMINI
+        </ListBoxItem>
+        <ListBoxItem>Gråsugga</ListBoxItem>
+        <ListBoxItem>Valhaj</ListBoxItem>
+        <ListBoxItem>Kladdkaka</ListBoxItem>
+        <ListBoxItem>Dry Martini</ListBoxItem>
+      </RacSelect>
+      <RacSelect
+        isDisabled
+        errorMessage='Error message'
+        label='Label'
+        description='Description'
+        onChange={x => {
+          console.log('change', x)
+        }}
+      >
+        <ListBoxItem>Hello</ListBoxItem>
+        <ListBoxItem>Goodbye</ListBoxItem>
+      </RacSelect>
+      <RacSelect
+        showTags
+        selectionMode='multiple'
+        errorMessage='Error message'
+        label='Show me the tags'
+        description='Description'
+        onChange={x => {
+          console.log('change', x)
+        }}
+      >
+        <ListBoxItem>Hello</ListBoxItem>
+        <ListBoxItem>Goodbye</ListBoxItem>
+        <ListBoxItem>Hello</ListBoxItem>
+        <ListBoxItem>Goodbye</ListBoxItem>
+        <ListBoxItem>Hello</ListBoxItem>
+        <ListBoxItem>Goodbye</ListBoxItem>
+      </RacSelect>
+      <Form>
         <RacSelect
-          label={'Label'}
-          description={'Description'}
-          onChange={x => {
-            console.log('change', x)
-          }}
+          isRequired
+          label='Required*'
+          description='Description'
         >
-          <RacItem>Very long string that goes on for a long time Very long string that goes on for a long timeVery long string that goes on for a long time </RacItem>
-          <RacItem>Goodbye</RacItem>
+          <ListBoxItem>Hello</ListBoxItem>
+          <ListBoxItem>Goodbye</ListBoxItem>
         </RacSelect>
-        <RacSelect
-          isInvalid
-          selectionMode={'multiple'}
-          errorMessage={'Error message'}
-          label={'Label'}
-          description={'Description'}
-          onChange={x => {
-            console.log('change', x)
-          }}
-        >
-          <RacItem>Hello</RacItem>
-          <RacItem>Banan</RacItem>
-          <RacItem>Glass</RacItem>
-          <RacItem>Äpple</RacItem>
-          <RacItem>Gemini GEMINI GEMINI GEMINI GEMINI Gemini GEMINI GEMINI GEMINI GEMINI GEMINI GEMINI GEMINI GEMINI Gemini GEMINI GEMINI GEMINI GEMINI</RacItem>
-          <RacItem>Gråsugga</RacItem>
-          <RacItem>Valhaj</RacItem>
-          <RacItem>Kladdkaka</RacItem>
-          <RacItem>Dry Martini</RacItem>
-        </RacSelect>
-        <RacSelect
-          isDisabled
-          errorMessage={'Error message'}
-          label={'Label'}
-          description={'Description'}
-          onChange={x => {
-            console.log('change', x)
-          }}
-        >
-          <RacItem>Hello</RacItem>
-          <RacItem>Goodbye</RacItem>
-        </RacSelect>
-        <RacSelect
-          showTags
-          selectionMode={'multiple'}
-          errorMessage={'Error message'}
-          label={'Show me the tags'}
-          description={'Description'}
-          onChange={x => {
-            console.log('change', x)
-          }}
-        >
-          <RacItem>Hello</RacItem>
-          <RacItem>Goodbye</RacItem>
-          <RacItem>Hello</RacItem>
-          <RacItem>Goodbye</RacItem>
-          <RacItem>Hello</RacItem>
-          <RacItem>Goodbye</RacItem>
-        </RacSelect>
-        <Form>
-          <RacSelect
-            isRequired
-            label={'Required*'}
-            description={'Description'}
-          >
-            <RacItem>Hello</RacItem>
-            <RacItem>Goodbye</RacItem>
-          </RacSelect>
-          <Button type={'submit'}>submit</Button>
-        </Form>
-
-      </div>
-    </>
+        <Button type='submit'>submit</Button>
+      </Form>
+    </div>
   )
 }
