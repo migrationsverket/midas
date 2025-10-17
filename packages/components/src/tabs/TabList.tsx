96c1bf78e83eaeb351f495f3ab0b641b074d123e
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-10-17 10:30:58 +0200

    fix(tabs): fix displaying horizontal/vertical tabs

--- packages/components/src/tabs/TabList.tsx
+++ packages/components/src/tabs/TabList.tsx
@@ -11,7 +11,7 @@ export const TabList = <T extends object>({
 }: TabListProps<T>) => {
   return (
     <AriaTabList
-      className={clsx(styles.list, className)}
+      className={clsx(styles.tabList, className)}
       {...props}
     />
   )
