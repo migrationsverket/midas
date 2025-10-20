1eb7a745cb795382e06115dda9c26f24e99afcb2
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-10-20 21:06:38 +0200

    feat(datepicker-styles): align more styles with midas-datepicker

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -1,6 +1,76 @@
-export default function App() {
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import '@midas-ds/datepicker-styles/lib/react-datepicker.css'
+import {CalendarDays} from 'lucide-react'
+
+const RangeMonthPicker = () => {
+  const [startDate, setStartDate] = useState<Date | null>(
+    new Date("2014/02/08"),
+  );
+  const [endDate, setEndDate] = useState<Date | null>(new Date("2014/04/08"));
+
   return (
     <>
+      <DatePicker
+        selected={startDate}
+        onChange={(date: Date | null) => setStartDate(date)}
+        selectsStart
+        startDate={startDate}
+        endDate={endDate}
+        dateFormat="MM/yyyy"
+      />
+      <DatePicker
+        selected={endDate}
+        onChange={(date: Date | null) => setEndDate(date)}
+        selectsEnd
+        startDate={startDate}
+        endDate={endDate}
+        dateFormat="MM/yyyy"
+
+      />
     </>
-  )
+  );
+};
+
+export default function App() {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const renderMonthContent = (month, shortMonth, longMonth, day) => {
+    const fullYear = new Date(day).getFullYear();
+    const tooltipText = `Tooltip for month: ${longMonth} ${fullYear}`;
+
+    return <span title={tooltipText}>{shortMonth}</span>;
+  };
+  return (
+    <>
+      <div>
+        <h2>React Datepicker Example Month Selector</h2>
+        <DatePicker
+          showPopperArrow={false}
+          selected={selectedDate}
+          showIcon
+          preventOpenOnFocus
+          toggleCalendarOnIconClick
+          icon={<CalendarDays height={20}/>}
+          renderMonthContent={renderMonthContent}
+          showMonthYearPicker
+          onChange={(date: Date | null) => setSelectedDate(date)}
+          dateFormat="MM-yyyy"
+          placeholderText="Select a date"
+        />
+        <h2>React Datepicker Example standard</h2>
+        <DatePicker
+          showIcon
+          preventOpenOnFocus
+          toggleCalendarOnIconClick
+          icon={<CalendarDays height={20}/>}
+          showPopperArrow={false}
+          dateFormat="MM-yyyy"
+          placeholderText="Select a date"
+        />
+        <h2>Variants..</h2>
+        <RangeMonthPicker />
+        {selectedDate && <p>Selected Date: {selectedDate.toDateString()}</p>}
+      </div>
+    </>
+  );
 }
