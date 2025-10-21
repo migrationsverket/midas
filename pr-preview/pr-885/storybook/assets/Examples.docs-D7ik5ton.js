66f1de950c2ff99c0f5eef16c0056ec2bae6c227
derpbravely <derpbravely@users.noreply.github.com>
2025-10-21 13:36:39 +0000

    Remove preview for PR 885 🛬

--- pr-preview/pr-885/storybook/assets/Examples.docs-D7ik5ton.js
+++
@@ -1,3 +0,0 @@
-import{j as e}from"./iframe-DAsMKx6b.js";import{useMDXComponents as r}from"./index-CsV3-LQ_.js";import{M as m}from"./blocks-2c6skzC3.js";import"./preload-helper-Dp1pzeXC.js";import"./index-xov5c4TQ.js";import"./index-CJtBfqc2.js";function o(t){const n={h1:"h1",p:"p",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(m,{title:"Examples/Intro"}),`
-`,e.jsx(n.h1,{id:"exempel-från-teamet",children:"Exempel från teamet"}),`
-`,e.jsx(n.p,{children:"Här samlar vi lite olika exempel på mönster som ofta återkommer...."})]})}function c(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{c as default};
