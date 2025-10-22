8714587103dc99755a16c84921ad8a082e7ec5a4
derpbravely <derpbravely@users.noreply.github.com>
2025-10-22 06:24:40 +0000

    Remove preview for PR 887 🛬

--- pr-preview/pr-887/storybook/assets/Examples.docs-50CHX3ob.js
+++
@@ -1,3 +0,0 @@
-import{j as e}from"./iframe-D0MnHx7_.js";import{useMDXComponents as r}from"./index-DglV3kJE.js";import{M as m}from"./blocks-BXvMJQcU.js";import"./preload-helper-Dp1pzeXC.js";import"./index-CQZ3zNG1.js";import"./index-1Vm2fim1.js";function o(t){const n={h1:"h1",p:"p",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(m,{title:"Examples/Intro"}),`
-`,e.jsx(n.h1,{id:"exempel-från-teamet",children:"Exempel från teamet"}),`
-`,e.jsx(n.p,{children:"Här samlar vi lite olika exempel på mönster som ofta återkommer...."})]})}function c(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{c as default};
--- pr-preview/pr-887/storybook/assets/inertValue-JdXB6oFO.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./iframe-D0MnHx7_.js";function s(r){const t=e.version.split(".");return parseInt(t[0],10)>=19?r:r?"true":void 0}export{s as $};
