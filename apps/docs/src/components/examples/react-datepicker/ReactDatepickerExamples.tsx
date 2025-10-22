6a4508e21f2d74a8df025bc4836ea90ba7f7c3da
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-10-22 09:22:15 +0200

    docs: add month-selector example for react datepicker

--- apps/docs/src/components/examples/react-datepicker/ReactDatepickerExamples.tsx
+++ apps/docs/src/components/examples/react-datepicker/ReactDatepickerExamples.tsx
@@ -1,8 +1,11 @@
 import { CalendarDays } from 'lucide-react'
 import DatePicker from 'react-datepicker'
 import { useState } from 'react'
+import { registerLocale, setDefaultLocale } from 'react-datepicker'
+import { sv } from 'date-fns/locale/sv'
+registerLocale('sv', sv)
+setDefaultLocale('sv')
 import '@midas-ds/datepicker-styles/lib/react-datepicker.css'
-
 
 export const DefaultReactDatepickerExample = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
@@ -16,6 +19,25 @@ export const DefaultReactDatepickerExample = () => {
       icon={<CalendarDays height={20} />}
       showPopperArrow={false}
       dateFormat='dd-MM-yyyy'
+      placeholderText='Select a date'
+    />
+  )
+}
+
+export const MonthSelectExample = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  return (
+    <DatePicker
+      showPopperArrow={false}
+      selected={selectedDate}
+      showIcon
+      preventOpenOnFocus
+      toggleCalendarOnIconClick
+      icon={<CalendarDays height={20} />}
+      /*renderMonthContent={renderMonthContent}*/
+      showMonthYearPicker
+      onChange={(date: Date | null) => setSelectedDate(date)}
+      dateFormat='MM-yyyy'
       placeholderText='Select a date'
     />
   )
