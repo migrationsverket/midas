8313c2ee9d78233aacec7a1d6306045bf0163dce
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2025-10-29 18:27:14 +0100

    refactor(textfield): move data attributes from template to css

--- packages/components/src/textfield/TextArea.tsx
+++ packages/components/src/textfield/TextArea.tsx
@@ -18,7 +18,6 @@ export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
     <TextFieldBase {...rest}>
       <AriaTextArea
         className={clsx(styles.textArea, className)}
-        data-readonly={rest.isReadOnly || undefined}
         cols={cols}
         ref={ref}
         rows={rows}
