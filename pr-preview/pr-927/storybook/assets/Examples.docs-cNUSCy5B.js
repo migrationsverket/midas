6c27600c3e973556c95586a759cbaf1c90dd66bc
derpbravely <derpbravely@users.noreply.github.com>
2025-11-14 08:50:48 +0000

    Remove preview for PR 927 🛬

--- pr-preview/pr-927/storybook/assets/Examples.docs-cNUSCy5B.js
+++
@@ -1,3 +0,0 @@
-import{j as e}from"./iframe-BlNSK8M8.js";import{useMDXComponents as r}from"./index-DYS_Czf_.js";import{M as m}from"./blocks-BMyUPZ3Y.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BHe3mj7v.js";import"./index-B8FlXCLo.js";function o(t){const n={h1:"h1",p:"p",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(m,{title:"Examples/Intro"}),`
-`,e.jsx(n.h1,{id:"exempel-från-teamet",children:"Exempel från teamet"}),`
-`,e.jsx(n.p,{children:"Här samlar vi lite olika exempel på mönster som ofta återkommer...."})]})}function c(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{c as default};
--- pr-preview/pr-927/storybook/assets/index-BHe3mj7v.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-BlNSK8M8.js";import{r as o}from"./index-B8FlXCLo.js";var t=o();const m=r(t);export{m as $,t as r};
