10196e45a2d776ed7e36108d03f5a65794ff8409
derpbravely <197733689+derpbravely@users.noreply.github.com>
2025-11-25 08:43:33 +0100

    docs: move IconCopy to a separate file

---
+++ apps/docs/src/components/IconCopy.tsx
@@ -0,0 +1,53 @@
+import { Focusable } from 'react-aria-components'
+import {
+  toastQueue,
+  TooltipTrigger,
+  Tooltip,
+  Button,
+} from '@midas-ds/components'
+
+export interface IconCopyProps {
+  name: string
+}
+
+export function IconCopy({ name }: IconCopyProps) {
+  const handleClick = async () => {
+    try {
+      await navigator.clipboard.writeText(name)
+      toastQueue.add(
+        {
+          type: 'success',
+          message: `Kopierade "${name}"`,
+        },
+        { timeout: 5000 },
+      )
+    } catch (error) {
+      toastQueue.add(
+        {
+          type: 'warning',
+          message: `Misslyckades att kopiera "${name}", ${error}`,
+        },
+        { timeout: 5000 },
+      )
+    }
+  }
+
+  return (
+    <TooltipTrigger>
+      <Focusable>
+        <Button
+          onClick={handleClick}
+          style={{
+            padding: 0,
+            background: 'none',
+            border: 'none',
+            fontWeight: 400,
+          }}
+        >
+          {name}
+        </Button>
+      </Focusable>
+      <Tooltip>Klicka för att kopiera</Tooltip>
+    </TooltipTrigger>
+  )
+}
