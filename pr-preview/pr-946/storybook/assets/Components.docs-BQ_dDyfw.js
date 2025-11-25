7e55c950385b36e1e799d2c15a129863aa3ad9df
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2025-11-25 18:55:40 +0000

    Remove preview for PR 946 🛬

--- pr-preview/pr-946/storybook/assets/Components.docs-BQ_dDyfw.js
+++
@@ -1,3 +0,0 @@
-import{j as n}from"./iframe-Dme4aFdT.js";import{useMDXComponents as r}from"./index-BJ3fXnwh.js";import{M as s}from"./blocks-D6qowQAh.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C7JOf_RQ.js";import"./index-C6e9A8g1.js";function o(e){const t={h1:"h1",p:"p",...r(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Components/Intro"}),`
-`,n.jsx(t.h1,{id:"komponenter",children:"Komponenter"}),`
-`,n.jsx(t.p,{children:"Här listas exempel på hur du kan använda vardera komponent. Props, kodexempel se hur de beter sig responsivt m.m."})]})}function x(e={}){const{wrapper:t}={...r(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{x as default};
