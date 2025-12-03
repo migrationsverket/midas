3952346a601f6af193a130206a3e4aacbdf2660a
derpbravely <derpbravely@users.noreply.github.com>
2025-12-03 13:08:21 +0000

    Remove preview for PR 962 🛬

--- pr-preview/pr-962/storybook/assets/Components.docs-CkcYPTBw.js
+++
@@ -1,3 +0,0 @@
-import{j as n}from"./iframe-wyIkUD1D.js";import{useMDXComponents as r}from"./index-DvOsCjZh.js";import{M as m}from"./blocks-Co4_-JuL.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C8_xscMq.js";import"./index-BVT2M0jp.js";import"./index-DWYf6tA-.js";function o(e){const t={h1:"h1",p:"p",...r(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{title:"Components/Intro"}),`
-`,n.jsx(t.h1,{id:"komponenter",children:"Komponenter"}),`
-`,n.jsx(t.p,{children:"Här listas exempel på hur du kan använda vardera komponent. Props, kodexempel se hur de beter sig responsivt m.m."})]})}function u(e={}){const{wrapper:t}={...r(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{u as default};
--- pr-preview/pr-962/storybook/assets/index-C8_xscMq.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-wyIkUD1D.js";import{r as o}from"./index-BVT2M0jp.js";var t=o();const m=r(t);export{m as R,t as r};
