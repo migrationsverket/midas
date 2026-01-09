af291846080028b7556e2ed700634fc116405155
derpbravely <derpbravely@users.noreply.github.com>
2026-01-09 07:03:46 +0000

    Remove preview for PR 996 🛬

--- pr-preview/pr-996/storybook/assets/Badge.stories-B3OhMQTq.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./iframe-BRBGZIqz.js";import{B as t,a as i,b as m}from"./BadgeContainer-CWgqYdoq.js";import{B as p}from"./Button-CNcByJgx.js";import"./preload-helper-PPVm8Dsz.js";import"./createLucideIcon-9r_u-ur5.js";import"./clsx-Ciqy0D92.js";import"./clsx-B-dksMZM.js";import"./Button.module-Co5e5YHp.js";import"./Button-Di6ycfiR.js";import"./utils-OvVzRtGp.js";import"./Hidden-Bjo-j34-.js";import"./useFocusRing-TRszBoqc.js";import"./index-BZiahUN9.js";import"./index-D1MdiIlN.js";import"./useLabels-BD_4aIe0.js";import"./useButton-DDvnlXFn.js";import"./useLocalizedStringFormatter-DdmTdBPq.js";import"./LocalizedStringFormatter-wnoc2RuQ.js";import"./VisuallyHidden-V6qs-Wg7.js";const W={component:t,title:"Components/Badge",tags:["autodocs"],parameters:{layout:"centered"},args:{},render:s=>r.jsx(p,{variant:"tertiary","aria-label":"Notiser",children:r.jsxs(i,{children:[r.jsx(m,{}),r.jsx(t,{...s,"data-testid":"badge"})]})})},e={},a={args:{children:"1"}},o={args:{children:"123"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: '1'
-  }
-}`,...a.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: '123'
-  }
-}`,...o.parameters?.docs?.source}}};const v=["NoLabel","WithLabel","LongLabel"];export{o as LongLabel,e as NoLabel,a as WithLabel,v as __namedExportsOrder,W as default};
--- pr-preview/pr-996/storybook/assets/Examples.docs-BdVdWHIn.js
+++
@@ -1,3 +0,0 @@
-import{j as t}from"./iframe-BRBGZIqz.js";import{useMDXComponents as r}from"./index-C794jhkS.js";import{M as m}from"./blocks-D1cEfrwE.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BZiahUN9.js";import"./index-D1MdiIlN.js";import"./index-C07XiCjk.js";function o(e){const n={h1:"h1",p:"p",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{title:"Examples/Intro"}),`
-`,t.jsx(n.h1,{id:"exempel-från-teamet",children:"Exempel från teamet"}),`
-`,t.jsx(n.p,{children:"Här samlar vi lite olika exempel på mönster som ofta återkommer...."})]})}function f(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{f as default};
