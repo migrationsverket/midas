764c1f2608b48937d6d5163219fa784484218710
Philip Hjalmrud Ekströmer <philip.hjalmrud.ekstromer@migrationsverket.se>
2026-01-14 14:03:16 +0100

    chore: experiment with aria-disabled

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -1,5 +1,53 @@
-import { TableBasicPage } from '../pages/TableBasicPage';
+import { useState } from 'react'
+import { Tooltip, TooltipTrigger, Button, Checkbox } from '@midas-ds/components'
 
 export default function App() {
-  return <TableBasicPage />;
+  const [isDisabled, setIsDisabled] = useState(true)
+
+  const handleButtonPress = () => {
+    alert('Knappen är aktiv och du klickade på den!')
+  }
+
+  return (
+    <div
+      style={{
+        padding: '2rem',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+      }}
+    >
+      <Checkbox
+        isSelected={!isDisabled}
+        onChange={selected => setIsDisabled(!selected)}
+      >
+        Enable button
+      </Checkbox>
+
+      {isDisabled ? (
+        <TooltipTrigger>
+          <Button
+            variant='secondary'
+            aria-label='Spara'
+            aria-disabled={isDisabled}
+            onPress={isDisabled ? undefined : handleButtonPress}
+          >
+            Hejsan
+          </Button>
+          <Tooltip>
+            Du kan inte klicka på knappen när den inte är aktiverad
+          </Tooltip>
+        </TooltipTrigger>
+      ) : (
+        <Button
+          variant='secondary'
+          aria-label='Spara'
+          aria-disabled={isDisabled}
+          onPress={isDisabled ? undefined : handleButtonPress}
+        >
+          Hejsan
+        </Button>
+      )}
+    </div>
+  )
 }
