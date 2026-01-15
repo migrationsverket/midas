dd4f2a2d4b3e07d1281f44c42b141dc65c153f36
phieksmig <phieksmig@users.noreply.github.com>
2026-01-15 09:27:42 +0000

    Remove preview for PR 1008 🛬

--- pr-preview/pr-1008/storybook/assets/Examples.docs-DCdTi-24.js
+++
@@ -1,3 +0,0 @@
-import{j as t}from"./iframe-Cjp05Xvk.js";import{useMDXComponents as r}from"./index-CTM976G-.js";import{M as m}from"./blocks-yDi5ELvF.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DQoebJgT.js";import"./index-BqZRieL_.js";import"./index-CRzahONy.js";function o(e){const n={h1:"h1",p:"p",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{title:"Examples/Intro"}),`
-`,t.jsx(n.h1,{id:"exempel-från-teamet",children:"Exempel från teamet"}),`
-`,t.jsx(n.p,{children:"Här samlar vi lite olika exempel på mönster som ofta återkommer...."})]})}function f(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{f as default};
--- pr-preview/pr-1008/storybook/assets/index-DQoebJgT.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-Cjp05Xvk.js";import{r as o}from"./index-BqZRieL_.js";var t=o();const m=r(t);export{m as R,t as r};
