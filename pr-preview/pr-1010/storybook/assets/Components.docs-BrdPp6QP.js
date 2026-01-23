47e5e0b8c04f3913e6d54b460d2f38a85169fd57
derpbravely <derpbravely@users.noreply.github.com>
2026-01-23 07:22:03 +0000

    Remove preview for PR 1010 🛬

--- pr-preview/pr-1010/storybook/assets/Components.docs-BrdPp6QP.js
+++
@@ -1,3 +0,0 @@
-import{j as n}from"./iframe-B63rKYqU.js";import{useMDXComponents as r}from"./index-CDGfYz8X.js";import{M as m}from"./blocks-DwmeEt6T.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BYawnw9P.js";import"./index-uItY6MEj.js";import"./index-B8cjTzbQ.js";function o(e){const t={h1:"h1",p:"p",...r(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{title:"Components/Intro"}),`
-`,n.jsx(t.h1,{id:"komponenter",children:"Komponenter"}),`
-`,n.jsx(t.p,{children:"Här listas exempel på hur du kan använda vardera komponent. Props, kodexempel se hur de beter sig responsivt m.m."})]})}function u(e={}){const{wrapper:t}={...r(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{u as default};
--- pr-preview/pr-1010/storybook/assets/chevron-left-Dye2RTR9.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./createLucideIcon-Cv9mqkPS.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],t=e("chevron-left",o);export{t as C};
