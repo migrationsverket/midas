e7a6ce90508648616c35d2e2c1906fe8b44391b2
Philip Hjalmrud Ekströmer <philip.hjalmrud.ekstromer@migrationsverket.se>
2026-01-28 12:06:35 +0100

    chore: add error message list example

--- apps/docs/src/components/examples/feedback-pattern/FeedbackExamples.tsx
+++ apps/docs/src/components/examples/feedback-pattern/FeedbackExamples.tsx
@@ -9,7 +9,10 @@ import {
   Grid,
   GridItem,
   InfoBanner,
+  TextField,
+  Link,
 } from '@midas-ds/components'
+import { Form } from 'react-aria-components'
 
 export const ButtonInfobannerExample = () => {
   const [value, setValue] = useState<Key | Key[] | null>(null)
@@ -54,7 +57,7 @@ export const ButtonInfobannerExample = () => {
             <ListBoxItem id='mango'>Mango</ListBoxItem>
           </Select>
         </GridItem>
-        <GridItem>
+        <GridItem size={4}>
           <Button
             variant='secondary'
             aria-label='Lägg till i fruktkorgen'
@@ -96,7 +99,7 @@ export const InactiveButtonExample = () => {
             <ListBoxItem id='mango'>Mango</ListBoxItem>
           </Select>
         </GridItem>
-        <GridItem>
+        <GridItem size={4}>
           <TooltipTrigger>
             <Button
               variant='secondary'
@@ -115,6 +118,55 @@ export const InactiveButtonExample = () => {
           </TooltipTrigger>
         </GridItem>
       </Grid>
+    </div>
+  )
+}
+
+export const ErrorMessageListExample = () => {
+  const [value, setValue] = useState<string>('')
+
+  const [submitted, setSubmitted] = useState(false)
+  const hasError = submitted && value.trim() === ''
+
+  return (
+    <div className='card'>
+      <Form
+        onSubmit={e => {
+          e.preventDefault()
+          setSubmitted(true)
+        }}
+      >
+        <Grid>
+          {hasError && (
+            <GridItem size={12}>
+              <InfoBanner type='warning'>
+                Justera dessa fält:
+                <li>
+                  <Link href='#example-field'>Du måste ange ett förnamn</Link>
+                </li>
+              </InfoBanner>
+            </GridItem>
+          )}
+          <GridItem size={12}>
+            <TextField
+              id='example-field'
+              label='Ange ditt förnamn'
+              isInvalid={hasError}
+              errorMessage='Du måste ange ett förnamn'
+              value={value}
+              onChange={setValue}
+            />
+          </GridItem>
+          <GridItem size={2}>
+            <Button
+              variant='primary'
+              type='submit'
+            >
+              Skicka
+            </Button>
+          </GridItem>
+        </Grid>
+      </Form>
     </div>
   )
 }
