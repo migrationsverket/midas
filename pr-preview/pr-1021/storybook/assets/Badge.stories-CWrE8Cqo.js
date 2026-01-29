488f9a11557a00c39eb2115d6fa717db74026f49
derpbravely <derpbravely@users.noreply.github.com>
2026-01-29 12:25:31 +0000

    Remove preview for PR 1021 🛬

--- pr-preview/pr-1021/storybook/assets/Badge.stories-CWrE8Cqo.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./iframe-CRizNrQm.js";import{B as t,a as i,b as m}from"./BadgeContainer-BJDQPSUg.js";import{B as p}from"./Button-BfSqLmBa.js";import"./preload-helper-PPVm8Dsz.js";import"./createLucideIcon-B6uSx5I0.js";import"./clsx-Ciqy0D92.js";import"./clsx-B-dksMZM.js";import"./Button.module-Co5e5YHp.js";import"./Button-B_2FCxCF.js";import"./utils-K8G9J7j3.js";import"./Hidden-BVxPU0E9.js";import"./useFocusRing-DbEG3syD.js";import"./index-BLxVqi3w.js";import"./index-U_5gcQv1.js";import"./useLabels-DaIxsHkM.js";import"./useButton-C-avZcWZ.js";import"./useLocalizedStringFormatter-BUb6pTdq.js";import"./LocalizedStringFormatter-wnoc2RuQ.js";import"./VisuallyHidden-CSY-auv0.js";const W={component:t,title:"Components/Badge",tags:["autodocs"],parameters:{layout:"centered"},args:{},render:s=>r.jsx(p,{variant:"tertiary","aria-label":"Notiser",children:r.jsxs(i,{children:[r.jsx(m,{}),r.jsx(t,{...s,"data-testid":"badge"})]})})},e={},a={args:{children:"1"}},o={args:{children:"123"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: '1'
-  }
-}`,...a.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: '123'
-  }
-}`,...o.parameters?.docs?.source}}};const v=["NoLabel","WithLabel","LongLabel"];export{o as LongLabel,e as NoLabel,a as WithLabel,v as __namedExportsOrder,W as default};
