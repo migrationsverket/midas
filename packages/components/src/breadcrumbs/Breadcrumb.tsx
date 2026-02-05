9319d4c7ac0f881daaf8fc1ccae38933b208b6bb
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2026-02-05 21:15:05 +0100

    refactor(components,breadcrumbs): use isCurrent prop to render separator

--- packages/components/src/breadcrumbs/Breadcrumb.tsx
+++ packages/components/src/breadcrumbs/Breadcrumb.tsx
@@ -1,14 +1,37 @@
+import { ReactNode } from 'react'
 import {
   Breadcrumb as AriaBreadcrumb,
   type BreadcrumbProps as AriaBreadcrumbProps,
+  type BreadcrumbRenderProps,
 } from 'react-aria-components'
 import clsx from '../utils/clsx'
+import styles from './Breadcrumbs.module.css'
 
 export type BreadcrumbProps = AriaBreadcrumbProps
 
-export const Breadcrumb = ({ className, ...rest }: BreadcrumbProps) => (
+type RenderProps = BreadcrumbRenderProps & { defaultChildren: ReactNode }
+
+export const Breadcrumb = ({
+  className,
+  children,
+  ...rest
+}: BreadcrumbProps) => (
   <AriaBreadcrumb
     className={clsx(className)}
     {...rest}
-  />
+  >
+    {(renderProps: RenderProps) => {
+      const showSeparator = !renderProps.isCurrent
+      return (
+        <>
+          {typeof children === 'function' ? children(renderProps) : children}
+          {showSeparator ? (
+            <span aria-hidden="true" className={styles.separator}>
+              /
+            </span>
+          ) : null}
+        </>
+      )
+    }}
+  </AriaBreadcrumb>
 )
