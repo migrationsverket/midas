758bea44eee8cf895e8bcc78cc5814c97267d0fa
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2026-02-05 09:16:04 +0100

    chore(components,breadcrumbs): remove unnecessary exports

--- packages/components/src/breadcrumbs/index.ts
+++ packages/components/src/breadcrumbs/index.ts
@@ -1,4 +1,2 @@
 export * from './Breadcrumbs'
 export * from './Breadcrumb'
-export type { BreadcrumbItem, BreadcrumbsProps } from './Breadcrumbs'
-export type { BreadcrumbProps } from './Breadcrumb'
