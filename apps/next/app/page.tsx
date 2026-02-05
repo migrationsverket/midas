ce75bb293904fc1ba199896ee7a6bc67dd48874f
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2026-02-05 12:08:10 +0100

    chore: add version export, add e2e version confirmation

--- apps/next/app/page.tsx
+++ apps/next/app/page.tsx
@@ -1,5 +1,10 @@
-import { Heading } from '@midas-ds/components'
+import { Heading, VERSION } from '@midas-ds/components'
 
 export default function Home() {
-  return <Heading level={1}>midas components e2e tests</Heading>
+  return (
+    <>
+      <Heading level={1}>midas components e2e tests</Heading>
+      <span data-midas-version={VERSION} />
+    </>
+  )
 }
