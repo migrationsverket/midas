39e495e2aead434e9849321f613c78bcd5eaf655
derpbravely <197733689+derpbravely@users.noreply.github.com>
2026-02-05 09:38:30 +0100

    test(next): add wildcard import paths for all midas packages

--- apps/next/next.config.ts
+++ apps/next/next.config.ts
@@ -1,34 +1,31 @@
 import type { NextConfig } from 'next'
 import { PHASE_PRODUCTION_BUILD, type PHASE_TYPE } from 'next/constants'
-import { TurbopackOptions } from 'next/dist/server/config-shared'
-import { join } from 'node:path'
+import { readdirSync } from 'node:fs'
+import { resolve } from 'node:path'
 
-const resolveAlias = (phase: PHASE_TYPE): TurbopackOptions['resolveAlias'] => {
-  if (process.env.E2E_REGISTRY) {
-    return {
-      '@midas-ds/components': 'apps/next/node_modules/@midas-ds/components',
-    }
-  }
+const workspaceRoot = resolve(__dirname, '../..')
 
-  if (phase === PHASE_PRODUCTION_BUILD) {
-    return {
-      '@midas-ds/components': 'dist/packages/components',
-    }
-  }
+const packages = readdirSync(resolve(workspaceRoot, 'packages'))
 
-  // use aliases defined in tsconfig.json
-  return {}
-}
+const importPath = process.env.E2E_REGISTRY
+  ? 'apps/next/node_modules/@midas-ds'
+  : 'dist/packages'
 
-export default function (
+const addImportPath = (path: string) => (moduleName: string) => [
+  moduleName,
+  `${path}/${moduleName}`,
+]
+
+export default (
   phase: PHASE_TYPE,
   { defaultConfig }: { defaultConfig: NextConfig },
-): NextConfig {
-  return {
-    ...defaultConfig,
-    turbopack: {
-      root: join(__dirname, '../..'),
-      resolveAlias: resolveAlias(phase),
-    },
-  }
-}
+): NextConfig => ({
+  ...defaultConfig,
+  turbopack: {
+    root: workspaceRoot,
+    resolveAlias:
+      process.env.E2E_REGISTRY || phase === PHASE_PRODUCTION_BUILD
+        ? Object.fromEntries(packages.map(addImportPath(importPath)))
+        : {},
+  },
+})
