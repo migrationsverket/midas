c53c5c43ace58df36801d1624b90b22ab87088c1
derpbravely <197733689+derpbravely@users.noreply.github.com>
2026-02-09 15:20:07 +0100

    feat(layout): add sidebar drawer

--- packages/layout/src/layout/Layout.stories.tsx
+++ packages/layout/src/layout/Layout.stories.tsx
@@ -5,7 +5,9 @@ import { Panel } from '../panel'
 import { Navbar } from '../navbar'
 import { Navigation } from '../navigation'
 import { NavigationLink } from '../navigation-link'
-import { House } from 'lucide-react'
+import { House, Menu } from 'lucide-react'
+import { Button } from '@midas-ds/components'
+import { useId, useState } from 'react'
 
 type Story = StoryObj<typeof Layout>
 
@@ -27,68 +29,89 @@ export default {
 } satisfies Meta<typeof Layout>
 
 export const Primary: Story = {
-  render: ({ children, ...rest }) => (
-    <Layout {...rest}>
-      <Header>Header</Header>
-      <Panel aria-label='left panel'>
-        <Navigation>
+  render: ({ children, ...rest }) => {
+    const drawerId = useId()
+
+    const [isNavigationOpen, setIsNavigationOpen] = useState(false)
+
+    const toggleIsNavigationOpen = () => setIsNavigationOpen(x => !x)
+
+    return (
+      <Layout {...rest}>
+        <Header>
+          <Button
+            aria-controls={drawerId}
+            aria-expanded={isNavigationOpen}
+            aria-haspopup='dialog'
+            icon={Menu}
+            onPress={toggleIsNavigationOpen}
+            variant='icon'
+          />
+        </Header>
+        <Panel aria-label='left panel'>
+          <Navigation
+            id={drawerId}
+            isOpen={isNavigationOpen}
+            onOpenChange={setIsNavigationOpen}
+          >
+            <ul>
+              <li>
+                <NavigationLink
+                  href='/'
+                  isActive
+                >
+                  <House />
+                  Hem
+                </NavigationLink>
+              </li>
+              <li>
+                <NavigationLink href='/categories'>Kategorier</NavigationLink>
+                <ul aria-label='Sidor'>
+                  <li>
+                    <NavigationLink href='/categories/products'>
+                      Produkter
+                    </NavigationLink>
+                  </li>
+                  <li>
+                    <NavigationLink href='/categories/services'>
+                      Tjänster
+                    </NavigationLink>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </Navigation>
+        </Panel>
+        <main style={{ height: '5rem', padding: '1rem' }}>{children}</main>
+        <Panel
+          aria-label='right panel'
+          style={{ gridArea: 'panelRight' }}
+        >
+          Panel
+        </Panel>
+        <Navbar>
           <ul>
             <li>
               <NavigationLink
                 href='/'
                 isActive
+                variant='navbar'
               >
                 <House />
                 Hem
               </NavigationLink>
             </li>
             <li>
-              <NavigationLink href='/categories'>Kategorier</NavigationLink>
-              <ul aria-label='Sidor'>
-                <li>
-                  <NavigationLink href='/categories/products'>
-                    Produkter
-                  </NavigationLink>
-                </li>
-                <li>
-                  <NavigationLink href='/categories/services'>
-                    Tjänster
-                  </NavigationLink>
-                </li>
-              </ul>
+              <NavigationLink
+                href='/categories'
+                variant='navbar'
+              >
+                Kategorier
+              </NavigationLink>
             </li>
           </ul>
-        </Navigation>
-      </Panel>
-      <main style={{ height: '5rem', padding: '1rem' }}>{children}</main>
-      <Panel
-        aria-label='right panel'
-        style={{ gridArea: 'panelRight' }}
-      >
-        Panel
-      </Panel>
-      <Navbar>
-        <ul>
-          <li>
-            <NavigationLink
-              href='/'
-              isActive
-              variant='navbar'
-            >
-              <House />
-              Hem
-            </NavigationLink>
-          </li>
-          <li>
-            <NavigationLink
-              href='/categories'
-              variant='navbar'
-            >
-              Kategorier
-            </NavigationLink>
-          </li>
-        </ul>
-      </Navbar>
-    </Layout>
-  ),
+        </Navbar>
+      </Layout>
+    )
+  },
 }
--- packages/layout/src/navigation/Navigation.tsx
+++ packages/layout/src/navigation/Navigation.tsx
@@ -1,23 +1,44 @@
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react'
 import { clsx } from '@midas-ds/components'
 import styles from './Navigation.module.css'
+import { Drawer } from './Drawer'
+import { ModalOverlayProps } from 'react-aria-components'
 
-export interface NavigationProps extends DetailedHTMLProps<
-  HTMLAttributes<HTMLElement>,
-  HTMLElement
-> {
+export interface NavigationProps
+  extends
+    DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>,
+    Pick<ModalOverlayProps, 'isOpen' | 'onOpenChange'> {
   children?: ReactNode
+  id: string
 }
 
 export const Navigation = ({
   children,
   className,
+  id,
+  isOpen,
+  onOpenChange,
   ...rest
 }: NavigationProps) => (
-  <nav
-    className={clsx(className, styles.navigation)}
-    {...rest}
-  >
-    {children}
-  </nav>
+  <>
+    <nav
+      className={clsx(className, styles.navigation)}
+      {...rest}
+    >
+      {children}
+    </nav>
+    <Drawer
+      id={id}
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+    >
+      <nav
+        id={id}
+        className={clsx(className, styles.navigation)}
+        {...rest}
+      >
+        {children}
+      </nav>
+    </Drawer>
+  </>
 )
