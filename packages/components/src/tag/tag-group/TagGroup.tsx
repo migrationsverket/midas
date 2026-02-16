4de21acc0361c04c6ee2eb42375ee8f0504fabfd
derpbravely <197733689+derpbravely@users.noreply.github.com>
2026-02-16 15:13:04 +0100

    feat(components, tag): add color prop
    - Refactor Tag, TagGroup, TagList components for a modular API
    in line with the React aria documentation.
    
    - Add section on dynamic children.

---
+++ packages/components/src/tag/tag-group/TagGroup.tsx
@@ -0,0 +1,43 @@
+import { Children, forwardRef, isValidElement } from 'react'
+import {
+  TagGroup as AriaTagGroup,
+  TagGroupContext,
+  useContextProps,
+  type TagGroupProps,
+} from 'react-aria-components'
+import clsx from '../../utils/clsx'
+import { TagList } from '../tag-list'
+
+export type { TagGroupProps }
+
+export const TagGroup = forwardRef<HTMLDivElement, TagGroupProps>(
+  (props, ref) => {
+    const [{ className, children, ...rest }, mergedRef] = useContextProps(
+      props,
+      ref,
+      TagGroupContext,
+    )
+
+    const providedTagList = Children.toArray(children)
+      .filter(isValidElement)
+      .find(child => child.type === TagList)
+
+    if (!providedTagList && process.env.NODE_ENV === 'development') {
+      console.warn(
+        'Using a TagGroup without providing a TagList component is deprecated since v17.0.0, see: https://designsystem.migrationsverket.se/components/tag/',
+      )
+    }
+
+    return (
+      <AriaTagGroup
+        className={clsx(className)}
+        ref={mergedRef}
+        {...rest}
+      >
+        {providedTagList ? children : <TagList>{children}</TagList>}
+      </AriaTagGroup>
+    )
+  },
+)
+
+TagGroup.displayName = 'TagGroup'
