9a6c1693116734ed561aceb71115100a98f1d230
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2026-02-25 13:15:33 +0100

    feat(components,tabs): add size prop with large/medium variants

--- packages/components/src/tabs/Tabs.tsx
+++ packages/components/src/tabs/Tabs.tsx
@@ -8,18 +8,22 @@ import {
 import clsx from '../utils/clsx'
 import { TabsContext } from './TabsContext'
 import styles from './Tabs.module.css'
+import { type Size } from '../common/types'
 
 export interface TabsProps extends AriaTabsProps {
   variant?: 'uncontained' | 'contained'
+  /** @default 'large' */
+  size?: Size
 }
 
 export const Tabs: React.FC<TabsProps> = ({
   className,
   variant = 'uncontained',
+  size = 'large',
   ...rest
 }) => {
   return (
-    <TabsContext.Provider value={{ variant }}>
+    <TabsContext.Provider value={{ variant, size }}>
       <AriaTabs
         className={clsx(styles.tabs, className)}
         {...rest}
