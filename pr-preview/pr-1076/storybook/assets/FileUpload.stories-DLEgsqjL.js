4222e788737c1f42102ab23a8d446366b9604896
derpbravely <derpbravely@users.noreply.github.com>
2026-02-25 15:49:49 +0000

    Remove preview for PR 1076 🛬

--- pr-preview/pr-1076/storybook/assets/FileUpload.stories-DLEgsqjL.js
+++
@@ -1,5 +0,0 @@
-import{D as r}from"./FileUpload-BVvgaLo4.js";import"./iframe-pduEcsSH.js";import"./preload-helper-PPVm8Dsz.js";import"./utils-BDr4Ozea.js";import"./clsx-B-dksMZM.js";import"./intlStrings-ZMTSFH_M.js";import"./Text-rxoULlv-.js";import"./useFocusRing-DIwubJgO.js";import"./index-CLoMZda8.js";import"./index-BcjOI7az.js";import"./useLabels-vBTL4yFU.js";import"./useDescription-DMdZFyiC.js";import"./useLocalizedStringFormatter-DvjpYU0E.js";import"./LocalizedStringFormatter-wnoc2RuQ.js";import"./useButton-CrWzEdIJ.js";import"./VisuallyHidden-g_-Wgf1Q.js";import"./Input-DNt3FWb6.js";import"./Hidden-CcVqblB0.js";import"./PressResponder-GpeM64gV.js";import"./clsx-Ciqy0D92.js";const C={component:r,title:"Components/DropZone",tags:["autodocs"],args:{}},o={args:{children:"Drop something here"}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: 'Drop something here'
-  }
-}`,...o.parameters?.docs?.source}}};const E=["DropIt"];export{o as DropIt,E as __namedExportsOrder,C as default};
--- pr-preview/pr-1076/storybook/assets/index-CLoMZda8.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-pduEcsSH.js";import{r as o}from"./index-BcjOI7az.js";var t=o();const m=r(t);export{m as R,t as r};
