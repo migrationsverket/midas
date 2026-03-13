b505f4f98cc91c2bffc8aa15242c4e09e944343f
derpbravely <197733689+derpbravely@users.noreply.github.com>
2026-03-13 11:42:34 +0100

    fix(layout, panel): add animations to dismiss-panel

--- packages/layout/src/panel/dismiss-panel/DismissPanel.tsx
+++ packages/layout/src/panel/dismiss-panel/DismissPanel.tsx
@@ -3,6 +3,14 @@
 import { Button, useLocalizedStringFormatter, clsx } from '@midas-ds/components'
 import { X } from 'lucide-react'
 import { useControlledState } from '@react-stately/utils'
+import { type PressEvent } from 'react-aria-components'
+import { forwardRef, useRef } from 'react'
+import {
+  filterDOMProps,
+  useEnterAnimation,
+  useExitAnimation,
+  useObjectRef,
+} from '@react-aria/utils'
 import { PanelBody, type PanelBodyProps } from '../panel-body'
 import { PanelHeader } from '../panel-header'
 import { PanelTitle } from '../panel-title'
@@ -15,33 +23,52 @@ export interface DismissTriggerProps {
   onOpenChange?: (isOpen: boolean) => void
 }
 
-export const DismissPanel = ({
-  children,
-  className,
-  isOpen: isOpenProp,
-  defaultOpen = false,
-  onOpenChange,
-  title,
-  ...rest
-}: PanelBodyProps & DismissTriggerProps) => {
-  const strings = useLocalizedStringFormatter(messages)
-
+export const DismissPanel = (props: PanelBodyProps & DismissTriggerProps) => {
   const [isOpen, setIsOpen] = useControlledState(
-    isOpenProp,
-    defaultOpen,
-    onOpenChange,
+    props.isOpen,
+    props.defaultOpen || false,
+    props.onOpenChange,
   )
+
+  const ref = useRef<HTMLElement>(null)
+
+  const isExiting = useExitAnimation(ref, isOpen)
 
   const handlePress = () => setIsOpen(previouslyOpen => !previouslyOpen)
 
-  if (!isOpen) {
+  if (!isOpen && !isExiting) {
     return null
   }
 
   return (
+    <DismissPanelInner
+      isExiting={isExiting}
+      onPress={handlePress}
+      ref={ref}
+      {...props}
+    />
+  )
+}
+
+const DismissPanelInner = forwardRef<
+  HTMLElement,
+  PanelBodyProps &
+    DismissTriggerProps & {
+      isExiting: boolean
+      onPress: (e: PressEvent) => void
+    }
+>(({ className, title, onPress, children, isExiting, ...rest }, ref) => {
+  const strings = useLocalizedStringFormatter(messages)
+  const objectRef = useObjectRef(ref)
+  const isEntering = useEnterAnimation(objectRef)
+
+  return (
     <PanelBody
       className={clsx(className, styles.dismissPanel)}
-      {...rest}
+      ref={objectRef}
+      data-entering={isEntering || undefined}
+      data-exiting={isExiting || undefined}
+      {...filterDOMProps(rest)}
     >
       <PanelHeader>
         <div>
@@ -55,7 +82,7 @@ export const DismissPanel = ({
         <Button
           variant='icon'
           aria-label={strings.format('closePanel')}
-          onPress={handlePress}
+          onPress={onPress}
         >
           <X size={20} />
         </Button>
@@ -63,4 +90,4 @@ export const DismissPanel = ({
       {children}
     </PanelBody>
   )
-}
+})
