40bc639a11c609331aba39b9a2b7b4ae49274d7c
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2026-03-17 10:17:18 +0000

    Remove preview for PR 1108 🛬

--- pr-preview/pr-1108/storybook/assets/Examples.docs-mSDnKoWa.js
+++
@@ -1,3 +0,0 @@
-import{j as t}from"./iframe-DJvMsPK5.js";import{useMDXComponents as r}from"./index-ClrjzIO4.js";import{M as m}from"./blocks-CFFDUtS1.js";import"./preload-helper-PPVm8Dsz.js";import"./index-Ipi0mz-M.js";import"./index-B3xZpXqe.js";import"./index-B9Ss_QBk.js";function o(e){const n={h1:"h1",p:"p",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{title:"Examples/Intro"}),`
-`,t.jsx(n.h1,{id:"exempel-från-teamet",children:"Exempel från teamet"}),`
-`,t.jsx(n.p,{children:"Här samlar vi lite olika exempel på mönster som ofta återkommer...."})]})}function f(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{f as default};
