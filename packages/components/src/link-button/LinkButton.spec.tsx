c200e16ebee7bc693e08f865549dfc1d2cbe1725
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2026-03-26 10:28:00 +0100

    fix(components,link-button): move redundant :hover rules under @media (hover: hover)

--- packages/components/src/link-button/LinkButton.spec.tsx
+++ packages/components/src/link-button/LinkButton.spec.tsx
@@ -3,7 +3,7 @@ import { composeStories } from '@storybook/react-vite'
 import * as stories from './LinkButton.stories'
 import { render } from '../../test-utils'
 
-const { Disabled } = composeStories(stories)
+const { Primary, Secondary, Tertiary, Danger, Disabled } = composeStories(stories)
 
 describe('given a disabled LinkButton', async () => {
   it('should have cursor not allowed when disabled', async () => {
@@ -13,5 +13,37 @@ describe('given a disabled LinkButton', async () => {
     await link.hover()
 
     await expect.element(link).toHaveStyle({ cursor: 'not-allowed' })
+  })
+})
+
+describe('given a primary LinkButton', async () => {
+  it('should set data-hovered when hovered', async () => {
+    const { getByRole } = await render(<Primary />)
+    await getByRole('link').hover()
+    await expect.element(getByRole('link')).toHaveAttribute('data-hovered')
+  })
+})
+
+describe('given a secondary LinkButton', async () => {
+  it('should set data-hovered when hovered', async () => {
+    const { getByRole } = await render(<Secondary />)
+    await getByRole('link').hover()
+    await expect.element(getByRole('link')).toHaveAttribute('data-hovered')
+  })
+})
+
+describe('given a tertiary LinkButton', async () => {
+  it('should set data-hovered when hovered', async () => {
+    const { getByRole } = await render(<Tertiary />)
+    await getByRole('link').hover()
+    await expect.element(getByRole('link')).toHaveAttribute('data-hovered')
+  })
+})
+
+describe('given a danger LinkButton', async () => {
+  it('should set data-hovered when hovered', async () => {
+    const { getByRole } = await render(<Danger />)
+    await getByRole('link').hover()
+    await expect.element(getByRole('link')).toHaveAttribute('data-hovered')
   })
 })
