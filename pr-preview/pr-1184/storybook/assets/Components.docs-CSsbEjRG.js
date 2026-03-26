8953e56c031525bc74c9a1c096b47a44744ccbe9
derpbravely <derpbravely@users.noreply.github.com>
2026-03-26 09:28:27 +0000

    Remove preview for PR 1184 🛬

--- pr-preview/pr-1184/storybook/assets/Components.docs-CSsbEjRG.js
+++
@@ -1,3 +0,0 @@
-import{j as n}from"./iframe-76Zbcpfm.js";import{useMDXComponents as r}from"./index-Dqv3sGMt.js";import{M as m}from"./blocks-BWUhOPAM.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CYYDks4G.js";import"./index-Bikl_74K.js";import"./index-BvVm8IC3.js";function o(e){const t={h1:"h1",p:"p",...r(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{title:"Components/Intro"}),`
-`,n.jsx(t.h1,{id:"komponenter",children:"Komponenter"}),`
-`,n.jsx(t.p,{children:"Här listas exempel på hur du kan använda vardera komponent. Props, kodexempel se hur de beter sig responsivt m.m."})]})}function u(e={}){const{wrapper:t}={...r(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{u as default};
--- pr-preview/pr-1184/storybook/assets/formatter-EIJCOSYU-Dcw7fZnN.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./blocks-BWUhOPAM.js";import{h as t}from"./iframe-76Zbcpfm.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CYYDks4G.js";import"./index-Bikl_74K.js";import"./index-BvVm8IC3.js";var e=t(o(),1),c=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{c as formatter};
