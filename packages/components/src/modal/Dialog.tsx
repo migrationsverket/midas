f94eaafbb563b3c21c99fc71cdddb8d59a3f8490
derpbravely <197733689+derpbravely@users.noreply.github.com>
2026-03-27 08:16:22 +0100

    refactor(components, modal): widen children type definition
    - accept childrenOrFunction
    - forward ref prop
    - update hideCloseButton comment

--- packages/components/src/modal/Dialog.tsx
+++ packages/components/src/modal/Dialog.tsx
@@ -4,6 +4,7 @@ import {
   ModalOverlay,
   Dialog as AriaDialog,
   ModalOverlayProps,
+  composeRenderProps,
 } from 'react-aria-components'
 import * as React from 'react'
 import { Button } from '../button'
@@ -14,69 +15,67 @@ import { useLocalizedStringFormatter } from '../utils/intl'
 import messages from './intl/translations.json'
 import clsx from '../utils/clsx'
 
-export interface ModalProps
-  extends ModalOverlayProps,
-    React.RefAttributes<HTMLDivElement> {
+export interface ModalProps extends ModalOverlayProps {
   /**
    * An optional title for the dialog. If omitted, please provide an aria-label for accessibility.
    */
   title?: React.ReactNode
-  children: React.ReactNode
   /**
-   * Hide close button in modal header. Use with caution!
+   * Hide close button in modal header. Use with caution, may trap mobile users.
    */
   hideCloseButton?: boolean
 }
 
 export { DialogTrigger }
 
-export const Modal: React.FC<ModalProps> = ({
-  children,
-  title,
-  hideCloseButton = false,
-  className,
-  ...props
-}) => {
-  const strings = useLocalizedStringFormatter(messages)
+export const Modal = React.forwardRef<HTMLDivElement, ModalProps>(
+  ({ children, title, hideCloseButton = false, className, ...props }, ref) => {
+    const strings = useLocalizedStringFormatter(messages)
 
-  return (
-    <ModalOverlay
-      {...props}
-      className={clsx(styles.overlay, className)}
-    >
-      <AriaModal className={styles.modal}>
-        <AriaDialog>
-          <div className={styles.modalHeader}>
-            <div className={styles.modalTitle}>
-              {title && (
-                <Heading
-                  level={3}
-                  elementType='h2'
-                  slot='title'
-                >
-                  {title}
-                </Heading>
-              )}
-            </div>
-            {!hideCloseButton && (
-              <Button
-                slot='close'
-                variant='tertiary'
-                icon={X}
-                iconPlacement='right'
+    return (
+      <ModalOverlay
+        {...props}
+        ref={ref}
+        className={clsx(styles.overlay, className)}
+      >
+        {composeRenderProps(children, children => (
+          <AriaModal className={styles.modal}>
+            <AriaDialog>
+              <div className={styles.modalHeader}>
+                <div className={styles.modalTitle}>
+                  {title && (
+                    <Heading
+                      level={3}
+                      elementType='h2'
+                      slot='title'
+                    >
+                      {title}
+                    </Heading>
+                  )}
+                </div>
+                {!hideCloseButton && (
+                  <Button
+                    slot='close'
+                    variant='tertiary'
+                    icon={X}
+                    iconPlacement='right'
+                  >
+                    {strings.format('close')}
+                  </Button>
+                )}
+              </div>
+              <div
+                className={styles.modalBody}
+                tabIndex={-1}
               >
-                {strings.format('close')}
-              </Button>
-            )}
-          </div>
-          <div
-            className={styles.modalBody}
-            tabIndex={-1}
-          >
-            {children}
-          </div>
-        </AriaDialog>
-      </AriaModal>
-    </ModalOverlay>
-  )
-}
+                {children}
+              </div>
+            </AriaDialog>
+          </AriaModal>
+        ))}
+      </ModalOverlay>
+    )
+  },
+)
+
+Modal.displayName = 'Modal'
