0c4cb101313b9c92ec75b164902e84d28da3248a
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2026-04-01 14:02:34 +0200

    fix(components,time-field): make TimeFieldProps generic, remove React.FC and data-testid

--- apps/docs/src/components/examples/time-field/TimeFieldExamples.tsx
+++ apps/docs/src/components/examples/time-field/TimeFieldExamples.tsx
@@ -3,13 +3,13 @@ import { TimeField } from '@midas-ds/components'
 import { Time } from '@internationalized/date'
 
 export const TimeFieldControlled = () => {
-  const [time, setTime] = React.useState<Time>(new Time(9, 0))
+  const [time, setTime] = React.useState<Time | null>(new Time(9, 0))
   return (
     <div className='card'>
       <TimeField
         label='Välj en tid'
         value={time}
-        onChange={value => value && setTime(value as Time)}
+        onChange={setTime}
       />
       <pre>Vald tid: {time?.toString()}</pre>
     </div>
