ef56d62a86916cac282a912b477d3d39ecaf4970
derpbravely <197733689+derpbravely@users.noreply.github.com>
2026-04-09 14:14:36 +0200

    test(layout): add skip to content test

--- packages/layout/src/layout/Layout.spec.tsx
+++ packages/layout/src/layout/Layout.spec.tsx
@@ -3,21 +3,22 @@ import { composeStories } from '@storybook/react-vite'
 import { render } from 'vitest-browser-react'
 import styles from './Layout.module.css'
 import * as stories from './Layout.stories'
+import { userEvent } from 'vitest/browser'
 
 const { Primary } = composeStories(stories)
 
 describe('given a primary Layout', async () => {
-  it('should render', async () => {
-    const { getByText } = await render(<Primary />)
-
-    await expect.element(getByText('Content')).toBeVisible()
-  })
-
   it('should accept a custom className', async () => {
     const { container } = await render(<Primary className='custom-class' />)
-
     await expect
       .element(container.querySelector(`.${styles.layout}`) as HTMLElement)
       .toHaveClass(styles.layout, 'custom-class')
+  })
+
+  it('should be possible to skip to content (DS1375)', async () => {
+    const { getByRole } = await render(<Primary />)
+    await userEvent.tab()
+    await userEvent.keyboard('[Enter]')
+    await expect.element(getByRole('main')).toHaveFocus()
   })
 })
--- packages/layout/src/layout/Layout.tsx
+++ packages/layout/src/layout/Layout.tsx
@@ -1,17 +1,21 @@
 import clsx from 'clsx'
 import { DetailedHTMLProps, HTMLAttributes } from 'react'
 import styles from './Layout.module.css'
+import { SkipToContent } from './skip-to-content'
 
 export type LayoutProps = DetailedHTMLProps<
   HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 >
 
-export const Layout = ({ className, ...rest }: LayoutProps) => {
+export const Layout = ({ children, className, ...rest }: LayoutProps) => {
   return (
     <div
       className={clsx(className, styles.layout)}
       {...rest}
-    />
+    >
+      <SkipToContent />
+      {children}
+    </div>
   )
 }
