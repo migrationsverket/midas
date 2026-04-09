a88c5d7ab1e269208708e475d58066a3b345a3df
derpbravely <197733689+derpbravely@users.noreply.github.com>
2026-04-09 11:37:57 +0200

    test(layout): create multiple viewport setups

--- packages/layout/src/header/mobile-menu/MobileMenu.spec.tsx
+++ packages/layout/src/header/mobile-menu/MobileMenu.spec.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, beforeEach } from 'vitest'
 import { render } from 'vitest-browser-react'
 import { userEvent } from 'vitest/browser'
 import { composeStories } from '@storybook/react-vite'
@@ -6,24 +6,33 @@ import * as stories from './MobileMenu.stories'
 
 const { Primary, Controlled } = composeStories(stories)
 
-describe('given an uncontrolled MobileMenu', async () => {
-  it('should be possible to open and close', async () => {
-    const { getByRole } = await render(<Primary />)
-    await getByRole('button', { name: 'Open menu' }).click()
-    const dismissButton = getByRole('button', { name: 'Dismiss' })
-    await expect.element(dismissButton).toBeVisible()
-    await userEvent.keyboard('[Escape]')
-    await expect.element(dismissButton).not.toBeInTheDocument()
+describe('MobileMenu', () => {
+  beforeEach(({ skip, task }) => {
+    skip(
+      task.file.projectName === 'desktop',
+      'MobileMenu is only relevant for mobile',
+    )
   })
-})
 
-describe('given a controlled MobileMenu', async () => {
-  it('should be possible to open and close', async () => {
-    const { getByRole } = await render(<Controlled />)
-    await getByRole('button', { name: 'Open menu' }).click()
-    const dismissButton = getByRole('button', { name: 'Dismiss' })
-    await expect.element(dismissButton).toBeVisible()
-    await userEvent.keyboard('[Escape]')
-    await expect.element(dismissButton).not.toBeInTheDocument()
+  describe('given an uncontrolled MobileMenu', async () => {
+    it('should be possible to open and close', async () => {
+      const { getByRole } = await render(<Primary />)
+      await getByRole('button', { name: 'Open menu' }).click()
+      const dismissButton = getByRole('button', { name: 'Dismiss' })
+      await expect.element(dismissButton).toBeVisible()
+      await userEvent.keyboard('[Escape]')
+      await expect.element(dismissButton).not.toBeInTheDocument()
+    })
+  })
+
+  describe('given a controlled MobileMenu', async () => {
+    it('should be possible to open and close', async () => {
+      const { getByRole } = await render(<Controlled />)
+      await getByRole('button', { name: 'Open menu' }).click()
+      const dismissButton = getByRole('button', { name: 'Dismiss' })
+      await expect.element(dismissButton).toBeVisible()
+      await userEvent.keyboard('[Escape]')
+      await expect.element(dismissButton).not.toBeInTheDocument()
+    })
   })
 })
