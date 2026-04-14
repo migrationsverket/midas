ace8a88cd15388c03050012273c91e1a380b8d31
derpbravely <derpbravely@users.noreply.github.com>
2026-04-14 09:03:54 +0000

    Remove preview for PR 1216 🛬

--- pr-preview/pr-1216/storybook/assets/Badge.stories-IpFQC0tZ.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./iframe-BgN3T3xs.js";import{B as t,a as i}from"./BadgeContainer-CCVttuA9.js";import{B as m}from"./bell-D6rpbzEY.js";import{B as p}from"./Button-zGEEwAU5.js";import"./preload-helper-PPVm8Dsz.js";import"./clsx-Ciqy0D92.js";import"./clsx-B-dksMZM.js";import"./createLucideIcon-BePQROhA.js";import"./Button-BtxXkeGG.js";import"./utils-Cy9VbIrx.js";import"./Hidden-BUpoh45I.js";import"./useFocusRing-s6LcsVmw.js";import"./index-C8VIyOAM.js";import"./index-DDvZmCCm.js";import"./useLabel-C7Safkp7.js";import"./useLabels-CtvG29M_.js";import"./useButton-CYBdLk3p.js";import"./Button.module-BB7N-cLd.js";import"./useLocalizedStringFormatter-B8VZY1gF.js";import"./LocalizedStringFormatter-wnoc2RuQ.js";import"./VisuallyHidden-DwUK2ATG.js";const O={component:t,title:"Components/Badge",tags:["autodocs"],parameters:{layout:"centered"},args:{},render:s=>r.jsx(p,{variant:"tertiary","aria-label":"Notiser",children:r.jsxs(i,{children:[r.jsx(m,{}),r.jsx(t,{...s,"data-testid":"badge"})]})})},e={},o={args:{children:"1"}},a={args:{children:"123"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: '1'
-  }
-}`,...o.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: '123'
-  }
-}`,...a.parameters?.docs?.source}}};const R=["NoLabel","WithLabel","LongLabel"];export{a as LongLabel,e as NoLabel,o as WithLabel,R as __namedExportsOrder,O as default};
--- pr-preview/pr-1216/storybook/assets/formatter-EIJCOSYU-Bg_NUWyp.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./blocks-nOY6Sv3T.js";import{n as t}from"./iframe-BgN3T3xs.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C8VIyOAM.js";import"./index-DDvZmCCm.js";import"./index-DFoRH5SL.js";var e=t(o(),1),n=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{n as formatter};
