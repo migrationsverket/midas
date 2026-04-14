faea24614621cde1e9dc654f3b34327796832225
derpbravely <derpbravely@users.noreply.github.com>
2026-04-14 09:08:45 +0000

    Remove preview for PR 1218 🛬

--- pr-preview/pr-1218/storybook/assets/Components.docs-ESYchYCK.js
+++
@@ -1,3 +0,0 @@
-import{j as n}from"./iframe-CNCYpet-.js";import{useMDXComponents as r}from"./index-CMq5txQf.js";import{M as m}from"./blocks-DqMWXe3b.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BUDe2ZxC.js";import"./index-DWOJOWmI.js";import"./index-wiJ-pny3.js";function o(e){const t={h1:"h1",p:"p",...r(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{title:"Components/Intro"}),`
-`,n.jsx(t.h1,{id:"komponenter",children:"Komponenter"}),`
-`,n.jsx(t.p,{children:"Här listas exempel på hur du kan använda vardera komponent. Props, kodexempel se hur de beter sig responsivt m.m."})]})}function u(e={}){const{wrapper:t}={...r(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{u as default};
