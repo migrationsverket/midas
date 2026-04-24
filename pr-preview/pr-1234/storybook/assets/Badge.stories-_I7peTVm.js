8b9b9563416c007330d589d80b6f194f6445c90f
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2026-04-24 11:19:12 +0000

    Remove preview for PR 1234 🛬

--- pr-preview/pr-1234/storybook/assets/Badge.stories-_I7peTVm.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./iframe-Cpv3N_Z7.js";import{B as t,a as i}from"./BadgeContainer-Bc0Y_L5L.js";import{B as m}from"./bell-tdRuSS42.js";import{B as p}from"./Button-DMMScCvt.js";import"./preload-helper-PPVm8Dsz.js";import"./clsx-Ciqy0D92.js";import"./clsx-B-dksMZM.js";import"./createLucideIcon-CPN07aPY.js";import"./Button-yYifVGdg.js";import"./utils-Ds8IarQh.js";import"./Hidden-CRjjTzwz.js";import"./useFocusRing-ypjCw1vR.js";import"./index-wkqyPuv6.js";import"./index-DfwSWRjw.js";import"./useLabel-DY4t24dq.js";import"./useLabels-Cb5TS_nA.js";import"./useButton-D1nJ6Ar2.js";import"./Button.module-BB7N-cLd.js";import"./useLocalizedStringFormatter-DBqNCzPo.js";import"./LocalizedStringFormatter-wnoc2RuQ.js";import"./VisuallyHidden-UFh7Pb-i.js";const O={component:t,title:"Components/Badge",tags:["autodocs"],parameters:{layout:"centered"},args:{},render:s=>r.jsx(p,{variant:"tertiary","aria-label":"Notiser",children:r.jsxs(i,{children:[r.jsx(m,{}),r.jsx(t,{...s,"data-testid":"badge"})]})})},e={},o={args:{children:"1"}},a={args:{children:"123"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: '1'
-  }
-}`,...o.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: '123'
-  }
-}`,...a.parameters?.docs?.source}}};const R=["NoLabel","WithLabel","LongLabel"];export{a as LongLabel,e as NoLabel,o as WithLabel,R as __namedExportsOrder,O as default};
--- pr-preview/pr-1234/storybook/assets/index-wkqyPuv6.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-Cpv3N_Z7.js";import{r as o}from"./index-DfwSWRjw.js";var t=o();const m=r(t);export{m as R,t as r};
