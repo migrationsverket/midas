88866c886e5c5ea88f0e7d14ae9430cbbb8562fd
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2026-06-29 11:57:11 +0000

    Deploying to gh-pages from @ migrationsverket/midas@82bdb31fc5720dfe22fac2915eebc7b5153ef3d8 🚀

--- storybook/assets/formatter-EIJCOSYU-Dbo7-NuL.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./blocks-Cpv99-gK.js";import{n as t}from"./iframe-DP3vvJW_.js";import"./preload-helper-PPVm8Dsz.js";import"./index-nwZ9FQBx.js";import"./index-B0BlSEXc.js";import"./index-BC044ghQ.js";var e=t(o(),1),n=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{n as formatter};
--- storybook/assets/index-nwZ9FQBx.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-DP3vvJW_.js";import{r as o}from"./index-B0BlSEXc.js";var t=o();const m=r(t);export{m as R,t as r};
