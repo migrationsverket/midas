a5b1d9757fcce7289d960092b1c572599bf311e0
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2026-06-29 12:27:25 +0000

    Remove preview for PR 1272 🛬

--- pr-preview/pr-1272/storybook/assets/checkbox-Dn1ukUfb.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t}from"./Checkbox-Db-nL4ee.js";import{n}from"./CheckboxGroup-AVaN5IlW.js";var r=e((()=>{t(),n()}));export{r as t};
--- pr-preview/pr-1272/storybook/assets/table-G2JXQ5Ib.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t}from"./Select-Cag1X9Sv.js";import{s as n}from"./Table-BW2BsaS2.js";var r=e((()=>{t()})),i=e((()=>{n()}));export{r as n,i as t};
