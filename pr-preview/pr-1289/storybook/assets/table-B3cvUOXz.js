f26583163e2af183fbac7b2fa4905eaea0315b9d
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2026-06-30 10:14:18 +0000

    Remove preview for PR 1289 🛬

--- pr-preview/pr-1289/storybook/assets/table-B3cvUOXz.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t}from"./Select-B3J29kY0.js";import{s as n}from"./Table-BgfX3MJ9.js";var r=e((()=>{t()})),i=e((()=>{n()}));export{r as n,i as t};
