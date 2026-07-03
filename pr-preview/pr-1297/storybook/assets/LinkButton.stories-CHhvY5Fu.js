189a2b01386362c1e2b9e6d13d277e7c999d6cc6
phieksmig <phieksmig@users.noreply.github.com>
2026-07-03 09:28:54 +0000

    Deploy preview for PR 1297 🛫

---
+++ pr-preview/pr-1297/storybook/assets/LinkButton.stories-CHhvY5Fu.js
@@ -0,0 +1,47 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t,t as n}from"./LinkButton-DfnZd2c6.js";var r,i,a,o,s,c,l,u,d,f;e((()=>{t(),r={component:n,title:`Components/LinkButton`,tags:[`autodocs`],parameters:{layout:`centered`},argTypes:{children:{name:`label`},variant:{options:[`primary`,`secondary`,`tertiary`,`danger`],control:{type:`radio`},defaultValue:`primary`},isDisabled:{options:[!0,!1],control:{type:`radio`}},iconPlacement:{options:[`right`,`left`],control:{type:`radio`},defaultValue:`left`}},args:{size:`large`}},i={args:{children:`Till E-tjänst`,href:`#`,"data-testid":`link-button`,className:`test-class`}},a={args:{...i.args,variant:`secondary`}},o={args:{...i.args,variant:`tertiary`}},s={args:{...i.args,children:`Föregående`,iconPlacement:`left`}},c={args:{...i.args,isDisabled:!0}},l={args:{children:`Till E-tjänst`,"data-testid":`link-button`,onPress:()=>alert(`navigation fn`)}},u={args:{"data-testid":`link-button`,children:`Radera`,variant:`danger`}},d={args:{children:`Öppnas i ny flik`,href:`https://example.com`,target:`_blank`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Till E-tjänst',
+    href: '#',
+    'data-testid': 'link-button',
+    className: 'test-class'
+  }
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    variant: 'secondary'
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    variant: 'tertiary'
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    children: 'Föregående',
+    iconPlacement: 'left'
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    isDisabled: true
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Till E-tjänst',
+    'data-testid': 'link-button',
+    onPress: () => alert('navigation fn')
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    'data-testid': 'link-button',
+    children: 'Radera',
+    variant: 'danger'
+  }
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Öppnas i ny flik',
+    href: 'https://example.com',
+    target: '_blank'
+  }
+}`,...d.parameters?.docs?.source}}},f=[`Primary`,`Secondary`,`Tertiary`,`Previous`,`Disabled`,`AppLink`,`Danger`,`NewTab`]}))();export{l as AppLink,u as Danger,c as Disabled,d as NewTab,s as Previous,i as Primary,a as Secondary,o as Tertiary,f as __namedExportsOrder,r as default};
