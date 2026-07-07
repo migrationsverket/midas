e5b61b5e18f90d0be05eb0111d51a4074f965416
phieksmig <phieksmig@users.noreply.github.com>
2026-07-07 09:42:35 +0000

    Remove preview for PR 1302 🛬

--- pr-preview/pr-1302/storybook/assets/Badge.stories-DjsOYy9B.js
+++
@@ -1,9 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{t}from"./iframe-CadyijZg.js";import{t as n}from"./Button-BY8nmPCI.js";import{t as r,vt as i}from"./lucide-react-COhgruO7.js";import{t as a}from"./button-CB30shvN.js";import{i as o,n as s,r as c,t as l}from"./BadgeContainer-CUauzSHM.js";var u,d,f,p,m,h;e((()=>{r(),o(),a(),s(),u=t(),d={component:c,title:`Components/Badge`,tags:[`autodocs`],parameters:{layout:`centered`},args:{},render:e=>(0,u.jsx)(n,{variant:`tertiary`,"aria-label":`Notiser`,children:(0,u.jsxs)(l,{children:[(0,u.jsx)(i,{}),(0,u.jsx)(c,{...e,"data-testid":`badge`})]})})},f={},p={args:{children:`1`}},m={args:{children:`123`}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: '1'
-  }
-}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: '123'
-  }
-}`,...m.parameters?.docs?.source}}},h=[`NoLabel`,`WithLabel`,`LongLabel`]}))();export{m as LongLabel,f as NoLabel,p as WithLabel,h as __namedExportsOrder,d as default};
