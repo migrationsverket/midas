a2f2fd94adf7cd902376d521bb109757b678d25c
pixelrickdreamer <pixelrickdreamer@users.noreply.github.com>
2026-08-28 11:39:05 +0000

    Remove preview for PR 1330 🛬

--- pr-preview/pr-1330/storybook/assets/button-CrgH9D3c.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t}from"./Button-FX04xKXP.js";import{n}from"./ButtonGroup-BdkgYHpR.js";var r=e((()=>{t(),n()}));export{r as t};
--- pr-preview/pr-1330/storybook/assets/table-C65Gpx7m.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{s as t}from"./Table-D9saaf4N.js";var n=e((()=>{t()}));export{n as t};
