21706f03638660d4f828b680c989ff91023ba5c0
derpbravely <197733689+derpbravely@users.noreply.github.com>
2026-09-14 15:01:40 +0200

    chore(components,radio): remove unused css class

--- packages/components/src/radio/RadioField.tsx
+++ packages/components/src/radio/RadioField.tsx
@@ -2,14 +2,9 @@ import {
   RadioFieldProps as AriaRadioFieldProps,
   RadioField as AriaRadioField,
 } from 'react-aria-components'
-import clsx from '../utils/clsx'
-import styles from './Radio.module.css'
 
 export type RadioFieldProps = AriaRadioFieldProps
 
-export const RadioField = ({ className, ...rest }: RadioFieldProps) => (
-  <AriaRadioField
-    className={clsx(styles.radioField, className)}
-    {...rest}
-  />
+export const RadioField = (props: RadioFieldProps) => (
+  <AriaRadioField {...props} />
 )
