1c87e70587ba13bc1bd85ad860f8f5d146e631c8
derpbravely <197733689+derpbravely@users.noreply.github.com>
2026-09-15 14:20:52 +0200

    chore(test-utils): configure @nx/dependency-checks

--- tools/test-utils/eslint.config.mjs
+++ tools/test-utils/eslint.config.mjs
@@ -4,30 +4,25 @@ import jsoncEslintParser from 'jsonc-eslint-parser'
 export default [
   ...baseConfig,
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    // Override or add rules here
-    rules: {},
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    // Override or add rules here
-    rules: {},
-  },
-  {
-    files: ['**/*.js', '**/*.jsx'],
-    // Override or add rules here
-    rules: {},
-  },
-  {
     files: ['**/*.json'],
     rules: {
       '@nx/dependency-checks': [
         'error',
         {
           ignoredFiles: [
-            '{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}',
-            '{projectRoot}/vite.config.{js,ts,mjs,mts}',
+            '{projectRoot}/vite.config.ts',
+            '{projectRoot}/rollup.config.cjs',
           ],
+          ignoredDependencies: [
+            // bundled dependencies
+            '@faker-js/faker',
+            '@internationalized/date',
+            'react-aria-components',
+          ],
+          includeTransitiveDependencies: false,
+          checkMissingDependencies: true,
+          checkObsoleteDependencies: true,
+          checkVersionMismatches: true,
         },
       ],
     },
