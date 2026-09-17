81d5f99f2943a37246046a835314887a3c6da797
derpbravely <derpbravely@users.noreply.github.com>
2026-09-17 12:53:01 +0000

    Deploy preview for PR 1367 🛫

---
+++ pr-preview/pr-1367/assets/js/cde82716.9397d15c.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunk_midas_ds_source"] = self["webpackChunk_midas_ds_source"] || []).push([[7829],{
+
+/***/ 28619
+(module) {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-1367/release-notes","page":1,"postsPerPage":10,"totalPages":3,"totalCount":27,"nextPage":"/pr-preview/pr-1367/release-notes/page/2","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ }
+
+}]);
