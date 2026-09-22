ab65b73e1d7c0479fd0d81bd504f33f311285661
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2026-09-22 18:26:31 +0200

    fix: avoid nx eslint crash and version-resolution failure for new logo package

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -24,7 +24,12 @@ export * from './layout'
 export * from './link'
 export { RouterProvider } from './link'
 export * from './link-button'
-export * from '@midas-ds/logo'
+export {
+  Logo,
+  LogoContext,
+  type LogoProps,
+  type LogoContextValue,
+} from '@midas-ds/logo'
 export * from './modal'
 export * from './popover'
 export * from './progress-bar'
