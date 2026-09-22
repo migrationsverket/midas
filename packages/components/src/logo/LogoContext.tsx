499ba5cecc0a39fdfcd3412b1913fea221473ccf
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2026-09-22 18:04:49 +0200

    feat(components): re-export Logo from @midas-ds/logo

--- packages/components/src/logo/LogoContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-import { type LogoProps } from './Logo'
-
-export type LogoContextValue = Pick<LogoProps, 'size'>
-
-export const LogoContext = createContext<LogoProps>({
-  size: undefined,
-})
