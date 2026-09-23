fabb5b7d79ac1b5cdf3b1fc3f8e4d33bf3c7be73
pixelrickdreamer <137166900+pixelrickdreamer@users.noreply.github.com>
2026-09-23 07:07:04 +0000

    Deploying to gh-pages from @ migrationsverket/midas@a24f3f6611d6415db13356c2edef9c735f75ef9a 🚀

---
+++ storybook/assets/checkbox-BWmSPViT.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t}from"./Checkbox-FIWSrTxy.js";import{n}from"./CheckboxGroup-BRlsKU5Y.js";var r=e((()=>{t(),n()}));export{r as t};
--- storybook/assets/checkbox-CxqBM8OR.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-CT_b8DTk.js";import{n as t}from"./Checkbox-LGauVOQX.js";import{n}from"./CheckboxGroup-DBdR6LkA.js";var r=e((()=>{t(),n()}));export{r as t};
